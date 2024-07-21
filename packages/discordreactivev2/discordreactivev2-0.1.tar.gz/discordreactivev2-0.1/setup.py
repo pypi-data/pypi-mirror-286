f168ef286d231f21dacac7f0a67392827a0fe9da
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:02 +0000

    Add package discordreactivev2/0.1/discordreactivev2-0.1.tar.gz

---
+++ packages/discordreactivev2/discordreactivev2-0.1.tar.gz/discordreactivev2-0.1/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="discordreactivev2",
+    packages=setuptools.find_packages(),
+    version="0.1",
+    license="MIT",
+    description="Kunders Package",
+    author="Kunder",
+    url="https://github.com/0xe2d0/evil-pip",
+    download_url="https://github.com/0xe2d0/evil-pip/tarball/master",
+    keywords=[""],
+    install_requires=[""],
+    classifiers=[],
+)
