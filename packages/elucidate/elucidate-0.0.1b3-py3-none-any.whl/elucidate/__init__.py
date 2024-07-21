c2bfb164d20b0685129f84a6b17cbc7e59186a11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:05 +0000

    Add package elucidate/0.0.1b3/elucidate-0.0.1b3-py3-none-any.whl

---
+++ packages/elucidate/elucidate-0.0.1b3-py3-none-any.whl/elucidate/__init__.py
@@ -0,0 +1,11 @@
+## Copyright 2024 Kakusui LLC (https://kakusui.org) (https://github.com/Kakusui) (https://github.com/Kakusui/Elucidate)
+## Use of this source code is governed by an GNU Lesser General Public License v2.1
+## license that can be found in the LICENSE file.
+
+from .version import VERSION as __version__  # noqa
+
+__author__ = "Kaden Bilyeu (Bikatr7) <Bikatr7@proton.me>"
+
+from .elucidate import Elucidate
+
+__all__ = ["Elucidate"]
