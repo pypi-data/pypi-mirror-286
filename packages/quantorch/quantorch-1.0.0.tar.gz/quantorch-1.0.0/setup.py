9ba96a04b6e16fbbf6d5c819b80a42cae191740b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:12 +0000

    Add package quantorch/1.0.0/quantorch-1.0.0.tar.gz

---
+++ packages/quantorch/quantorch-1.0.0.tar.gz/quantorch-1.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="quantorch",
+    version="1.0.0",
+    description="High-Performance PyTorch Library for Derivatives Pricing",
+
+    author="Jialue Chen",
+    author_email="jialuechen@outlook.com",
+    url="https://github.com/jialuechen/quantorch",
+    packages=find_packages(),
+    install_requires=[
+        "torch>=1.7.1",
+        "numpy>=1.19.2",
+        "scipy>=1.5.2"
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
