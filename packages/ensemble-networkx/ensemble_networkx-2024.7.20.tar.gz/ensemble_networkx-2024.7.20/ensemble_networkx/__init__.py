bf67502b03aa3a3d5232cf2c98537faad264a57c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:02:56 +0000

    Add package ensemble-networkx/2024.7.20/ensemble_networkx-2024.7.20.tar.gz

---
+++ packages/ensemble-networkx/ensemble_networkx-2024.7.20.tar.gz/ensemble_networkx-2024.7.20/ensemble_networkx/__init__.py
@@ -0,0 +1,53 @@
+#!/bin/usr/env python
+#
+# =======
+# Version
+# =======
+__version__= "2024.7.20"
+__author__ = "Josh L. Espinoza"
+__email__ = "jespinoz@jcvi.org, jol.espinoz@gmail.com"
+__url__ = "https://github.com/jolespin/ensemble_networkx"
+__license__ = "BSD-3"
+__developmental__ = True
+
+# =======
+# Direct Exports
+# =======
+__functions__ = [
+    "pairwise_biweight_midcorrelation",
+    "umap_fuzzy_simplical_set_graph",
+    "pairwise_mcc",
+] + [
+    "get_symmetric_category",
+    "redundant_to_condensed",
+    "condensed_to_redundant",
+    "convert_network",
+] + [
+    "connectivity",
+    "grouped_node_connectivity_from_numpy", 
+    "grouped_node_connectivity_from_pandas_dataframe", 
+    "group_connectivity_from_numpy", 
+    "group_connectivity_from_pandas_dataframe",
+    "density",
+    "centralization",
+    "heterogeneity",
+    "entropy",
+    "evenness",
+    "topological_overlap_measure",
+    "community_detection",
+    "edge_cluster_cooccurrence",
+]
+__classes__ = [
+    'EnsembleAssociationNetwork', 
+    'SampleSpecificPerturbationNetwork', 
+    'DifferentialEnsembleAssociationNetwork', 
+    'CategoricalEngineeredFeature',
+    'Symmetric',
+    "ClusteredNetwork",
+    "AggregateNetwork",
+    # "Network",
+    ]
+
+__all__ = sorted(__functions__ + __classes__)
+
+from .ensemble_networkx import *
