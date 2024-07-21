c6ee41e499a14052a081a15934b2b94c551d2c9c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:03:02 +0000

    Add package CaseLawSscraper/0.1/CaseLawSscraper-0.1.tar.gz

---
+++ packages/CaseLawSscraper/CaseLawSscraper-0.1.tar.gz/CaseLawSscraper-0.1/tests/test_module3.py
@@ -0,0 +1,9 @@
+import unittest
+from CaseLawSscraper.module3 import hello_world
+
+class TestModule3(unittest.TestCase):
+    def test_hello_world(self):
+        self.assertIsNone(hello_world())
+
+if __name__ == '__main__':
+    unittest.main()
