c2f464cea446a6e971a128a3a3c18b84ba9b1aa8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 15:03:07 +0000

    Add package uneq/0.1.4/uneq-0.1.4-py3-none-any.whl

---
+++ packages/uneq/uneq-0.1.4-py3-none-any.whl/uneq/uneq.py
@@ -0,0 +1,73 @@
+import re
+import shlex
+from pathlib import Path
+
+from ffmpeg_progress_yield import FfmpegProgress
+from rich.progress import Progress
+
+from . import console
+
+
+def parse_eq_file(file_path):
+    filters = []
+    preamp_gain = 0.0
+    with open(file_path, "r") as f:
+        for line in f:
+            if line.strip() and not line.strip().startswith("#"):
+                preamp_match = re.match(r"Preamp: ([\-\d\.]+) dB", line)
+                if preamp_match:
+                    preamp_gain = float(preamp_match.group(1))
+                    continue
+
+                filter_match = re.match(
+                    r"Filter (\d+): ON (\w+) Fc (\d+) Hz Gain ([\-\d\.]+) dB(?: Q ([\d\.]+))?",
+                    line,
+                )
+                if filter_match:
+                    _filter_num = int(filter_match.group(1))
+                    filter_type = filter_match.group(2)
+                    frequency = int(filter_match.group(3))
+                    gain = float(filter_match.group(4))
+                    q_value = float(filter_match.group(5)) if filter_match.group(5) else None
+
+                    gain = -gain
+
+                    if filter_type == "PK":
+                        filter_str = f"equalizer=f={frequency}:width_type=q:width={q_value}:gain={gain}"
+                    elif filter_type == "LSC":
+                        filter_str = f"lowshelf=f={frequency}:width_type=q:width={q_value}:gain={gain}"
+                    elif filter_type == "HSC":
+                        filter_str = f"highshelf=f={frequency}:width_type=q:width={q_value}:gain={gain}"
+
+                    filters.append(filter_str)
+
+    preamp_gain = -preamp_gain
+
+    return f"volume={preamp_gain}dB," + ",".join(filters)
+
+
+def generate_ffmpeg_command(input_path: Path, eq_file_path: Path):
+    filters_command = parse_eq_file(eq_file_path)
+
+    output_path = input_path.parent / f"uneq_{input_path.name}"
+
+    return (
+        f'ffmpeg -y -i "{input_path}" -map 0 -c:v copy -filter:a:0 "{filters_command}" "{output_path}"',
+        output_path,
+    )
+
+
+def uneq(input_path: Path, eq_file_path: Path):
+    console.print(f"Running uneq for file [dim]{input_path}[/dim] with EQ loaded from [dim]{eq_file_path}[/dim]")
+
+    ffmpeg_command, output_path = generate_ffmpeg_command(input_path, eq_file_path)
+
+    with Progress(console=console) as progress:
+        task = progress.add_task("[red]Encoding...", total=100)
+
+        ff = FfmpegProgress(shlex.split(ffmpeg_command))
+
+        for ff_progress in ff.run_command_with_progress():
+            progress.update(task, completed=ff_progress)
+
+    console.print(f"Done, wrote output file to [dim]{output_path}[/dim]")
