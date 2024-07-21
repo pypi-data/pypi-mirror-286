5c64c8cb9891429a8799f8147819074f665c9e60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 15:03:03 +0000

    Add package seqlogic/0.12.0/seqlogic-0.12.0.tar.gz

---
+++ packages/seqlogic/seqlogic-0.12.0.tar.gz/seqlogic-0.12.0/tests/test_union.py
@@ -0,0 +1,31 @@
+"""Test seqlogic.bits.Union class."""
+
+# For error testing
+# pylint: disable=unused-variable
+
+import pytest
+
+from seqlogic import Union, Vector
+
+
+def test_empty():
+    with pytest.raises(ValueError):
+
+        class EmptyUnion(Union):
+            pass
+
+
+class Simple(Union):
+    a: Vector[2]
+    b: Vector[3]
+    c: Vector[4]
+
+
+def test_simple():
+    u = Simple("3b000")
+
+    assert str(u.a) == "2b00"
+    assert str(u.b) == "3b000"
+    assert str(u.c) == "4bX000"
+
+    assert u.size == 4
