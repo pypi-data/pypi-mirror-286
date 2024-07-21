03c742bf4dfa857daa09be27c70b52e785c79037
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:03:31 +0000

    Add package adparser/0.0.2/adparser-0.0.2.tar.gz

---
+++ packages/adparser/adparser-0.0.2.tar.gz/adparser-0.0.2/src/adparser/Visitors/__init__.py
@@ -0,0 +1,6 @@
+# from this package we can access all implemented visitors
+
+from adparser.Visitors.Selectors import TextLineSelector, LinkSelector, \
+    ParagraphSelector, HeadingSelector, ListSelector,\
+    SourceSelector, TableSelector, AudioSelector, VideoSelector,\
+    ImageSelector
