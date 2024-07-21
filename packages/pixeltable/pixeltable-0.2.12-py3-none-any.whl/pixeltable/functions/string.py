e8ed6c1907659713f87d344a20658c050a0d52b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:02:58 +0000

    Add package pixeltable/0.2.12/pixeltable-0.2.12-py3-none-any.whl

---
+++ packages/pixeltable/pixeltable-0.2.12-py3-none-any.whl/pixeltable/functions/string.py
@@ -0,0 +1,21 @@
+from typing import Any
+
+import pixeltable.func as func
+from pixeltable.type_system import StringType
+from pixeltable.utils.code import local_public_names
+
+
+@func.udf(return_type=StringType(), param_types=[StringType()])
+def str_format(format_str: str, *args: Any, **kwargs: Any) -> str:
+    """Return a formatted version of format_str, using substitutions from args and kwargs:
+    - {<int>} will be replaced by the corresponding element in args
+    - {<key>} will be replaced by the corresponding value in kwargs
+    """
+    return format_str.format(*args, **kwargs)
+
+
+__all__ = local_public_names(__name__)
+
+
+def __dir__():
+    return __all__
