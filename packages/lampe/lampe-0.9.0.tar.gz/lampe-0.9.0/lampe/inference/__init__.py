573a3f2b1f67ded0f09f70744ff2157ddcf9c032
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:03:08 +0000

    Add package lampe/0.9.0/lampe-0.9.0.tar.gz

---
+++ packages/lampe/lampe-0.9.0.tar.gz/lampe-0.9.0/lampe/inference/__init__.py
@@ -0,0 +1,10 @@
+r"""Inference components such as estimators, training losses and MCMC samplers."""
+
+from .amnre import *
+from .bnre import *
+from .cnre import *
+from .dcp import *
+from .fmpe import *
+from .mcmc import *
+from .npe import *
+from .nre import *
