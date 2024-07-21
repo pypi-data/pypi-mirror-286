07ff03c39ef5564356d46afccc5a6cd88d228adf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 15:03:03 +0000

    Add package dbt-pumpkin/0.2.0/dbt_pumpkin-0.2.0.tar.gz

---
+++ packages/dbt-pumpkin/dbt_pumpkin-0.2.0.tar.gz/dbt_pumpkin-0.2.0/dbt_pumpkin/data.py
@@ -0,0 +1,117 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from enum import Enum
+from typing import TYPE_CHECKING
+
+from dbt_pumpkin.exception import PropertyNotAllowedError, PropertyRequiredError
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+@dataclass
+class YamlFormat:
+    indent: int
+    offset: int
+
+
+class ResourceType(Enum):
+    SEED = "seed"
+    SOURCE = "source"
+    MODEL = "model"
+    SNAPSHOT = "snapshot"
+
+    @property
+    def plural_name(self) -> str:
+        # all resource types conform to this rule
+        return self.value + "s"
+
+    @classmethod
+    def values(cls) -> set[str]:
+        return cls._value2member_map_.keys()
+
+    def __str__(self):
+        return self.value
+
+
+@dataclass(frozen=True)
+class TableColumn:
+    name: str
+    dtype: str
+    data_type: str
+    is_numeric: bool
+    is_string: bool
+
+
+@dataclass(frozen=True)
+class Table:
+    resource_id: ResourceID
+    columns: list[TableColumn]
+
+    def __post_init__(self):
+        if not self.columns:
+            raise PropertyRequiredError("columns", self.resource_id)  # noqa: EM101
+
+    def __hash__(self):
+        return hash(self.resource_id)
+
+
+@dataclass(frozen=True)
+class ResourceConfig:
+    yaml_path_template: str | None
+    numeric_precision_and_scale: bool
+    string_length: bool
+
+
+@dataclass(frozen=True)
+class ResourceID:
+    unique_id: str
+
+    @property
+    def name(self) -> str:
+        return self.unique_id.split(".")[-1]
+
+    def __str__(self):
+        return self.unique_id
+
+
+@dataclass(frozen=True)
+class ResourceColumn:
+    name: str
+    quote: bool
+    data_type: str | None
+    description: str | None
+
+
+@dataclass(frozen=True)
+class Resource:
+    unique_id: ResourceID
+    name: str
+    source_name: str | None
+    database: str
+    schema: str
+    identifier: str
+    type: ResourceType
+    path: Path | None
+    yaml_path: Path | None
+    columns: list[ResourceColumn]
+    config: ResourceConfig | None
+
+    def __post_init__(self):
+        # Validate invariants
+        if self.type == ResourceType.SOURCE:
+            if not self.source_name:
+                raise PropertyRequiredError("source_name", self.unique_id)  # noqa: EM101
+            if self.path:
+                raise PropertyNotAllowedError("path", self.unique_id)  # noqa: EM101
+            if not self.yaml_path:
+                raise PropertyRequiredError("yaml_path", self.unique_id)  # noqa: EM101
+        else:
+            if self.source_name is not None:
+                raise PropertyNotAllowedError("source_name", self.unique_id)  # noqa: EM101
+            if not self.path:
+                raise PropertyRequiredError("path", self.unique_id)  # noqa: EM101
+
+    def __hash__(self):
+        return hash(self.unique_id)
---
+++ packages/dbt-pumpkin/dbt_pumpkin-0.2.0.tar.gz/dbt_pumpkin-0.2.0/dbt_pumpkin/storage.py
@@ -0,0 +1,58 @@
+from __future__ import annotations
+
+import logging
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+
+from ruamel.yaml import YAML
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+    from dbt_pumpkin.data import YamlFormat
+
+logger = logging.getLogger(__name__)
+
+
+class Storage:
+    @abstractmethod
+    def load_yaml(self, files: set[Path]) -> dict[Path, any]:
+        raise NotImplementedError
+
+    @abstractmethod
+    def save_yaml(self, files: dict[Path, any]):
+        raise NotImplementedError
+
+
+class DiskStorage(Storage):
+    def __init__(self, root_dir: Path, yaml_format: YamlFormat | None):
+        self._root_dir = root_dir
+
+        self._yaml = YAML(typ="rt")
+        self._yaml.preserve_quotes = True
+        if yaml_format:
+            self._yaml.map_indent = yaml_format.indent
+            self._yaml.sequence_indent = yaml_format.indent + yaml_format.offset
+            self._yaml.sequence_dash_offset = yaml_format.offset
+
+    def load_yaml(self, files: set[Path]) -> dict[Path, any]:
+        result: dict[Path, any] = {}
+
+        for file in files:
+            resolved_file = self._root_dir / file
+            if not resolved_file.exists():
+                logger.debug("File doesn't exist, skipping: %s", resolved_file)
+                continue
+
+            logger.debug("Loading file: %s", resolved_file)
+            result[file] = self._yaml.load(resolved_file)
+
+        return result
+
+    def save_yaml(self, files: dict[Path, any]):
+        for file, content in files.items():
+            resolved_file = self._root_dir / file
+            resolved_file.parent.mkdir(exist_ok=True)
+
+            logger.debug("Saving file: %s", resolved_file)
+            self._yaml.dump(content, resolved_file)
