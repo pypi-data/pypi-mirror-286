212a8d94fe7529ff6cf22fa804801905eaaa4f40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:08 +0000

    Add package psnawp-api/1.3.3/psnawp_api-1.3.3.tar.gz

---
+++ packages/psnawp-api/psnawp_api-1.3.3.tar.gz/psnawp_api-1.3.3/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+setup(
+    name="psnawp_api",
+    version="1.3.3",
+    author="isFakeAccount",
+    author_email='trevorphillips@gmx.us',
+    url="https://pypi.org/project/PSNAWP/",
+    homepage="https://github.com/isFakeAccount/psnawp",
+    repository="https://github.com/isFakeAccount/psnawp",
+    documentation="https://psnawp.readthedocs.io/en/latest/",
+    keywords=["PSN", "PlayStation"],
+    description="Python API Wrapper for PlayStation Network API",
+    long_description="""Use `PSNAWP <https://pypi.python.org/pypi/PSNAWP>`_ instead.""",
+    license="MIT",
+    install_requires=['PSNAWP==1.3.3'],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Software Development :: Libraries :: Python Modules"
+    ],
+)
