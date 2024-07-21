4c8fae84a5e4f99cc5def93f0e6a08ec9aa30bc5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:02:59 +0000

    Add package ruff-pyspark-filter-linter/0.1.0/ruff_pyspark_filter_linter-0.1.0-py3-none-any.whl

---
+++ packages/ruff-pyspark-filter-linter/ruff_pyspark_filter_linter-0.1.0-py3-none-any.whl/ruff_pyspark_filter_linter/log_utils.py
@@ -0,0 +1,10 @@
+import logging
+import sys
+
+
+def get_console_handler():
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.INFO)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    return console_handler
