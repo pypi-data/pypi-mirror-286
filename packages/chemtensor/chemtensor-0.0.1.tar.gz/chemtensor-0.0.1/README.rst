fc069aa9acd4e55222513418aaa29c2aec9c89e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:03:08 +0000

    Add package chemtensor/0.0.1/chemtensor-0.0.1.tar.gz

---
+++ packages/chemtensor/chemtensor-0.0.1.tar.gz/chemtensor-0.0.1/README.rst
@@ -0,0 +1,6 @@
+ChemTensor
+==========
+
+Tensor network algorithms for chemical systems.
+
+This package is currently a placeholder and will provide a Python interface to `chemtensor <https://github.com/qc-tum/chemtensor>`_.
---
+++ packages/chemtensor/chemtensor-0.0.1.tar.gz/chemtensor-0.0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name="chemtensor",
+    version="0.0.1",
+    author="Christian B. Mendl",
+    author_email="christian.b.mendl@gmail.com",
+    packages=["chemtensor"],
+    url="https://github.com/qc-tum/chemtensor",
+    install_requires=[
+        "numpy",
+    ],
+)
