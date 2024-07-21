13ec2bd072abad78c0337a1cb29abadfa2827d44
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:03:31 +0000

    Add package gptk/0.3.18/gptk-0.3.18.tar.gz

---
+++ packages/gptk/gptk-0.3.18.tar.gz/gptk-0.3.18/src/gptk/__init__.py
@@ -0,0 +1,33 @@
+'''J.T. Hartzfeld's Personal Library
+    This is a collection of generally useful snippets and bits that make
+    their way into most of my projects.
+'''
+import os
+from pathlib import Path
+from auto_all import start_all, end_all, public
+
+
+start_all()
+
+
+try:
+    from .encoding import *
+except ImportError:
+    from gptk.encoding import *
+
+try:
+    from .singletons import *
+except ImportError:
+    from gptk.singletons import *
+
+try:
+    from .enums import *
+except ImportError:
+    from gptk.enums import *
+
+try:
+    from .autoprops import *
+except ImportError:
+    from gptk.autoprops import *
+    
+end_all()
