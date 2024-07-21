5a81602a5f7fecf1d6861d74fab6dee28b2eafd6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:02:48 +0000

    Add package my-swiki/10.1.2/my_swiki-10.1.2.tar.gz

---
+++ packages/my-swiki/my_swiki-10.1.2.tar.gz/my_swiki-10.1.2/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='my_swiki',
+    version='10.1.2',
+    description='A program for searching and reading Wikipedia articles in multiple languages',
+    py_modules=['my_swiki'],
+    author='k0ng999',
+    author_email='baydar_14@mail.ru',
+    long_description=open('README.md', encoding='utf-8').read(),
+    long_description_content_type='text/markdown',
+    license='MIT',
+)
