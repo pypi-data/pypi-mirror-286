722863f557036c65e62320fc09c38aecb25d76b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 15:02:59 +0000

    Add package synphage/0.2.5/synphage-0.2.5.tar.gz

---
+++ packages/synphage/synphage-0.2.5.tar.gz/synphage-0.2.5/synphage/resources/__init__.py
@@ -0,0 +1,20 @@
+from .local_resource import InputOutputConfig, LocalFilesystemIOManager
+from .ncbi_resource import ncbi_resource
+
+import os
+from dagster import PipesSubprocessClient
+
+
+init_local_io_manager = LocalFilesystemIOManager(
+    input_dir=str(os.getenv("DATA_DIR")), output_dir=str(os.getenv("OUTPUT_DIR"))
+)
+
+
+RESOURCES_LOCAL = {
+    "local_resource": InputOutputConfig(
+        input_dir=str(os.getenv("DATA_DIR")), output_dir=str(os.getenv("OUTPUT_DIR"))
+    ),
+    "ncbi_connection": ncbi_resource,
+    "io_manager": init_local_io_manager.get_io_manager(),
+    "pipes_subprocess_client": PipesSubprocessClient(),
+}
