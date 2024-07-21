b03457056416a6f571f84c1f43425d318478aee9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:15 +0000

    Add package nigeria-geodata/0.1.1/nigeria_geodata-0.1.1-py3-none-any.whl

---
+++ packages/nigeria-geodata/nigeria_geodata-0.1.1-py3-none-any.whl/nigeria_geodata/config.py
@@ -0,0 +1,80 @@
+"""
+Configuration settings module.
+Authors:
+Date:
+"""
+
+from typing import Dict
+from nigeria_geodata.datasources import DataSource
+from nigeria_geodata.models.common import DataSourceInfo
+
+
+class Config:
+    """
+    Configuration class for managing API keys and data source settings.
+    """
+
+    DATA_SOURCES = {
+        DataSource.GRID3: {
+            "SERVICE_URL": "https://services3.arcgis.com/BU6Aadhn6tbBEdyk/ArcGIS/rest/services?f=json",
+            "SERVICE_INFO_URL": "https://services3.arcgis.com/BU6Aadhn6tbBEdyk/ArcGIS/rest/info/?f=json",
+        },
+    }
+
+    @staticmethod
+    def get_service_url(source: DataSourceInfo) -> str:
+        """
+        Get the service URL for a given data source.
+
+        Args:
+            source (str): The name of the data source (e.g., 'GRID3').
+
+        Returns:
+            str: The service URL for the specified data source.
+        """
+
+        return Config.DATA_SOURCES.get(source, {}).get("SERVICE_URL")
+
+    @staticmethod
+    def get_service_info_url(source: DataSourceInfo) -> str:
+        """
+        Get the service URL for a given data source.
+
+        Args:
+            source (str): The name of the data source (e.g., 'GRID3').
+
+        Returns:
+            str: The service URL for the specified data source.
+        """
+
+        return Config.DATA_SOURCES.get(source, {}).get("SERVICE_INFO_URL")
+
+    @staticmethod
+    def get_health_check_url(source: DataSourceInfo) -> str:
+        """
+        Get the service URL for a given data source.
+
+        Args:
+            source (str): The name of the data source (e.g., 'GRID3').
+
+        Returns:
+            str: The service URL for the specified data source.
+        """
+
+        return Config.DATA_SOURCES.get(source, {}).get("HEALTH_CHECK_URL")
+
+    @staticmethod
+    def get_data_source_info(source: DataSourceInfo) -> Dict[str, DataSourceInfo]:
+        """
+        Get detailed information about a given data source.
+
+        Args:
+            source (str): The name of the data source (e.g., 'GRID3').
+
+        Returns:
+            DataSourceInfo: The detailed information for the specified data source.
+        """
+        sources = {ds.name: ds for ds in DataSource.list_sources()}
+        return sources.get(source)
+
+    #
