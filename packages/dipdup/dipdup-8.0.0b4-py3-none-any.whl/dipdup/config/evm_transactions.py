32581a3ace88a42629a83dcf5d02cc53bb14edaf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 03:02:58 +0000

    Add package dipdup/8.0.0b4/dipdup-8.0.0b4-py3-none-any.whl

---
+++ packages/dipdup/dipdup-8.0.0b4-py3-none-any.whl/dipdup/config/evm_transactions.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
+from typing import Literal
+
+from pydantic import ConfigDict
+from pydantic.dataclasses import dataclass
+
+from dipdup.config import Alias
+from dipdup.config import HandlerConfig
+from dipdup.config._mixin import CodegenMixin
+from dipdup.config.evm import EvmContractConfig
+from dipdup.config.evm import EvmDatasourceConfigU
+from dipdup.config.evm import EvmIndexConfig
+from dipdup.models.evm_node import EvmNodeHeadSubscription
+from dipdup.subscriptions import Subscription
+from dipdup.utils import pascal_to_snake
+from dipdup.utils import snake_to_pascal
+
+if TYPE_CHECKING:
+    from collections.abc import Iterator
+
+
+@dataclass(config=ConfigDict(extra='forbid'), kw_only=True)
+class EvmTransactionsHandlerConfig(HandlerConfig, CodegenMixin):
+    """Subsquid transaction handler
+
+    :param callback: Callback name
+    :param from_: Transaction sender
+    :param to: Transaction receiver
+    :param method: Method name
+    """
+
+    # FIXME: Can't use `from_` field alias in dataclasses
+    from_: Alias[EvmContractConfig] | None = None
+    to: Alias[EvmContractConfig] | None = None
+    method: str | None = None
+
+    def iter_imports(self, package: str) -> Iterator[tuple[str, str]]:
+        yield 'dipdup.context', 'HandlerContext'
+        yield package, 'models as models'
+
+        if self.typed_contract and self.method:
+            yield 'dipdup.models.evm', 'EvmTransaction'
+            transaction_module = pascal_to_snake(self.method)
+            transaction_cls = snake_to_pascal(self.method) + 'Input'
+            module_name = self.typed_contract.module_name
+            yield f'{package}.types.{module_name}.evm_transactions.{transaction_module}', transaction_cls
+        else:
+            yield 'dipdup.models.evm', 'EvmTransactionData'
+
+    def iter_arguments(self) -> Iterator[tuple[str, str]]:
+        yield 'ctx', 'HandlerContext'
+
+        if self.typed_contract and self.method:
+            transaction_cls = snake_to_pascal(self.method) + 'Input'
+            yield 'transaction', f'EvmTransaction[{transaction_cls}]'
+        else:
+            yield 'transaction', 'EvmTransactionData'
+
+    @property
+    def typed_contract(self) -> EvmContractConfig | None:
+        if self.method and self.to:
+            return self.to
+        return None
+
+
+@dataclass(config=ConfigDict(extra='forbid'), kw_only=True)
+class EvmTransactionsIndexConfig(EvmIndexConfig):
+    """Index that uses Subsquid Network as a datasource for transactions
+
+    :param kind: always 'evm.transactions'
+    :param datasources: `evm` datasources to use
+    :param handlers: Transaction handlers
+    :param first_level: Level to start indexing from
+    :param last_level: Level to stop indexing at
+    """
+
+    kind: Literal['evm.transactions']
+
+    datasources: tuple[Alias[EvmDatasourceConfigU], ...]
+    handlers: tuple[EvmTransactionsHandlerConfig, ...]
+
+    first_level: int = 0
+    last_level: int = 0
+
+    def get_subscriptions(self) -> set[Subscription]:
+        return {EvmNodeHeadSubscription(transactions=True)}
---
+++ packages/dipdup/dipdup-8.0.0b4-py3-none-any.whl/dipdup/indexes/evm.py
@@ -0,0 +1,125 @@
+import random
+from abc import ABC
+from abc import abstractmethod
+from typing import TYPE_CHECKING
+from typing import Any
+from typing import Generic
+from typing import TypeVar
+
+from dipdup.config.evm import EvmContractConfig
+from dipdup.datasources.evm_node import NODE_LAST_MILE
+from dipdup.datasources.evm_node import EvmNodeDatasource
+from dipdup.datasources.evm_subsquid import EvmSubsquidDatasource
+from dipdup.exceptions import ConfigurationError
+from dipdup.index import Index
+from dipdup.index import IndexQueueItemT
+from dipdup.package import DipDupPackage
+from dipdup.prometheus import Metrics
+
+if TYPE_CHECKING:
+    from dipdup.context import DipDupContext
+
+EVM_SUBSQUID_READAHEAD_LIMIT = 2500
+
+IndexConfigT = TypeVar('IndexConfigT', bound=Any)
+DatasourceT = TypeVar('DatasourceT', bound=Any)
+
+
+_sighashes: dict[str, str] = {}
+
+
+def get_sighash(package: DipDupPackage, method: str, to: EvmContractConfig | None = None) -> str:
+    """Method in config is either a full signature or a method name. We need to convert it to a sighash first."""
+
+    key = method + (to.module_name if to else '')
+    if key in _sighashes:
+        return _sighashes[key]
+
+    if {'(', ')'} <= set(method) and not to:
+        from web3 import Web3
+
+        _sighashes[key] = Web3.keccak(text=method).hex()[:10]
+    elif to:
+        _sighashes[key] = package.get_converted_evm_abi(to.module_name)['methods'][method]['sighash']
+    else:
+        raise ConfigurationError('`to` field is missing; `method` is expected to be a full signature')
+    return _sighashes[key]
+
+
+class EvmIndex(
+    Generic[IndexConfigT, IndexQueueItemT, DatasourceT],
+    Index[IndexConfigT, IndexQueueItemT, DatasourceT],
+    ABC,
+):
+    subsquid_datasources: tuple[Any, ...]
+    node_datasources: tuple[Any, ...]
+
+    def __init__(
+        self,
+        ctx: 'DipDupContext',
+        config: IndexConfigT,
+        datasources: tuple[DatasourceT, ...],
+    ) -> None:
+        super().__init__(ctx, config, datasources)
+        self.subsquid_datasources = tuple(d for d in datasources if isinstance(d, EvmSubsquidDatasource))
+        self.node_datasources = tuple(d for d in datasources if isinstance(d, EvmNodeDatasource))
+        self._subsquid_started: bool = False
+
+    @abstractmethod
+    async def _synchronize_subsquid(self, sync_level: int) -> None: ...
+
+    @abstractmethod
+    async def _synchronize_node(self, sync_level: int) -> None: ...
+
+    async def _get_node_sync_level(
+        self,
+        subsquid_level: int,
+        index_level: int,
+        node: EvmNodeDatasource | None = None,
+    ) -> int | None:
+        if not self.node_datasources:
+            return None
+        node = node or random.choice(self.node_datasources)
+
+        node_sync_level = await node.get_head_level()
+        subsquid_lag = abs(node_sync_level - subsquid_level)
+        subsquid_available = subsquid_level - index_level
+        self._logger.info('Subsquid is %s levels behind; %s available', subsquid_lag, subsquid_available)
+        if subsquid_available < NODE_LAST_MILE:
+            return node_sync_level
+        return None
+
+    async def _synchronize(self, sync_level: int) -> None:
+        """Fetch event logs via Fetcher and pass to message callback"""
+        index_level = await self._enter_sync_state(sync_level)
+        if index_level is None:
+            return
+
+        levels_left = sync_level - index_level
+        if levels_left <= 0:
+            return
+
+        if self.subsquid_datasources:
+            subsquid_sync_level = await self.subsquid_datasources[0].get_head_level()
+            Metrics.set_sqd_processor_chain_height(subsquid_sync_level)
+        else:
+            subsquid_sync_level = 0
+
+        node_sync_level = await self._get_node_sync_level(subsquid_sync_level, index_level)
+
+        # NOTE: Fetch last blocks from node if there are not enough realtime messages in queue
+        if node_sync_level:
+            sync_level = min(sync_level, node_sync_level)
+            self._logger.debug('Using node datasource; sync level: %s', sync_level)
+            await self._synchronize_node(sync_level)
+        else:
+            sync_level = min(sync_level, subsquid_sync_level)
+            await self._synchronize_subsquid(sync_level)
+
+        if not self.node_datasources and not self._subsquid_started:
+            self._subsquid_started = True
+            self._logger.info('No `evm.node` datasources available; polling Subsquid')
+            for datasource in self.subsquid_datasources:
+                await datasource.start()
+
+        await self._exit_sync_state(sync_level)
