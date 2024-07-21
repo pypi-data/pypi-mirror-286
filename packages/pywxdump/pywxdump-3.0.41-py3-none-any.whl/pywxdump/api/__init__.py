d4967b16e3d870c5d45b8205520cc9618a2d58f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-21 14:03:10 +0000

    Add package pywxdump/3.0.41/pywxdump-3.0.41-py3-none-any.whl

---
+++ packages/pywxdump/pywxdump-3.0.41-py3-none-any.whl/pywxdump/api/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-#
+# -------------------------------------------------------------------------------
+# Name:         __init__.py
+# Description:  
+# Author:       xaoyaoo
+# Date:         2023/12/14
+# -------------------------------------------------------------------------------
+from .api import api
+from .utils import read_session, save_session
+
+if __name__ == '__main__':
+    pass
