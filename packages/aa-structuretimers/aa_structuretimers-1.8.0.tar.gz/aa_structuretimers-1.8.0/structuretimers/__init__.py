e0b12caa4728564ae42613282c63e2c645fdd613
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:23 +0000

    Add package aa-structuretimers/1.8.0/aa_structuretimers-1.8.0.tar.gz

---
+++ packages/aa-structuretimers/aa_structuretimers-1.8.0.tar.gz/aa_structuretimers-1.8.0/structuretimers/__init__.py
@@ -0,0 +1,8 @@
+"""An app for keeping track of Eve Online structure timers
+with Alliance Auth and Discord.
+"""
+
+default_app_config = "structuretimers.apps.StructureTimersConfig"
+
+__version__ = "1.8.0"
+__title__ = "Structure Timers"
