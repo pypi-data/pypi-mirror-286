b822acbf1a6e7e558f75a44396b3abdb61c450fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:31 +0000

    Add package prosper-bot/0.7.2/prosper_bot-0.7.2.tar.gz

---
+++ packages/prosper-bot/prosper_bot-0.7.2.tar.gz/prosper_bot-0.7.2/prosper_bot/cli/__init__.py
@@ -0,0 +1,28 @@
+from prosper_shared.omni_config import Config, ConfigKey, input_schema
+
+DRY_RUN_CONFIG = "prosper-bot.cli.dry-run"
+VERBOSE_CONFIG = "prosper-bot.cli.verbose"
+APP_NAME = "prosper-bot"
+
+
+@input_schema
+def _schema():
+    return {
+        APP_NAME: {
+            "cli": {
+                ConfigKey(
+                    "verbose", "Prints additional debug messages.", default=False
+                ): bool,
+                ConfigKey(
+                    "dry-run",
+                    "Run the loop but don't actually place any orders.",
+                    default=False,
+                ): bool,
+            }
+        }
+    }
+
+
+def build_config() -> Config:
+    """Compiles all the config sources into a single config."""
+    return Config.autoconfig(APP_NAME, validate=True)
