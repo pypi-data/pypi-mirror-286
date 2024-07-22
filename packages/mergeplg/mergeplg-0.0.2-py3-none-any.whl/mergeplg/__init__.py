791d52bd7bc7c36f104525fc670fbc37592e119e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:04:12 +0000

    Add package mergeplg/0.0.2/mergeplg-0.0.2-py3-none-any.whl

---
+++ packages/mergeplg/mergeplg-0.0.2-py3-none-any.whl/mergeplg/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2024 Christian Chwala. All rights reserved.
+
+mergeplg: A great package.
+"""
+
+from __future__ import annotations
+
+__version__ = "0.0.2"
+
+__all__ = ["__version__"]
