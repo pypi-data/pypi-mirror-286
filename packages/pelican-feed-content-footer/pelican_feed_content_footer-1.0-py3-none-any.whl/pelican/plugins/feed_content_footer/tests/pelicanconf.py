b81ca223f236ddfde4b2d45d516720802e6f2c0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:32 +0000

    Add package pelican-feed-content-footer/1.0/pelican_feed_content_footer-1.0-py3-none-any.whl

---
+++ packages/pelican-feed-content-footer/pelican_feed_content_footer-1.0-py3-none-any.whl/pelican/plugins/feed_content_footer/tests/pelicanconf.py
@@ -0,0 +1,20 @@
+PATH = 'content'
+SITEURL = 'https://example.com'
+
+# Disable HTML output
+ARTICLE_SAVE_AS = ''
+AUTHOR_SAVE_AS = ''
+CATEGORY_SAVE_AS = ''
+TAG_SAVE_AS = ''
+ARCHIVES_SAVE_AS = ''
+AUTHORS_SAVE_AS = ''
+CATEGORIES_SAVE_AS = ''
+TAGS_SAVE_AS = ''
+INDEX_SAVE_AS = ''
+
+# Use 'simple' theme without CSS and images
+THEME = 'simple'
+
+# Shut up warnings
+TIMEZONE = 'UTC'
+STATIC_PATHS = []
