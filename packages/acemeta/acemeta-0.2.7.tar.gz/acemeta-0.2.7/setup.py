27e54e5c8e3ab08b0de020daa25f1846508ba6c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:45 +0000

    Add package acemeta/0.2.7/acemeta-0.2.7.tar.gz

---
+++ packages/acemeta/acemeta-0.2.7.tar.gz/acemeta-0.2.7/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+# Die README-Datei lesen und ihren Inhalt in long_description speichern
+with open("README.md", 'r', encoding='utf-8') as f:
+    description = f.read()
+
+setup(
+    name='acemeta',  # Name des Pakets
+    version='0.2.7',  # Version des Pakets
+    author='Annhilati',  # Autor
+    #author_email='your.email@example.com',  # E-Mail des Autors
+    description='Library for typical workflows',  # Kurze Beschreibung
+    long_description=description,  # Lange Beschreibung aus README.md
+    long_description_content_type='text/markdown',  # Inhaltstyp der langen Beschreibung
+    #url='https://github.com/yourusername/your-repo',  # URL des Projekts
+    packages=find_packages(),  # Automatische Paketerkennung
+    install_requires=[
+        "requests"
+    ]
+)
