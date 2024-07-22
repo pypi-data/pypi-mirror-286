7fc2c8926a308944d379b3561f4da4005f1ee0ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:44 +0000

    Add package hello-gemini/0.2.0/hello_gemini-0.2.0.tar.gz

---
+++ packages/hello-gemini/hello_gemini-0.2.0.tar.gz/hello_gemini-0.2.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    long_description=f.read()
+
+setup(
+    name='hello_gemini',
+    version='0.2.0',
+    packages=find_packages(),
+    install_requires=[],
+    entry_points={
+        'console_scripts': [
+            'hello_gemini = hello_gemini:hello_gemini',
+        ]
+    },
+    long_description=long_description,
+    long_description_content_type="text/markdown"
+)
