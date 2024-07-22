15e86e2ca31343c7b4320817101ed324200361ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:33 +0000

    Add package pary/0.0.2/pary-0.0.2-py3-none-any.whl

---
+++ packages/pary/pary-0.0.2-py3-none-any.whl/pary/__init__.py
@@ -0,0 +1,26 @@
+"""
+Pary
+=====
+
+A productivity package for actuaries and data analysts.
+
+
+Documentation
+----------------------------
+Documentation is available in [the Azure DevOps project](https://dev.azure.com/mclavier/Pary).
+
+
+  >>> import pary as pr
+
+
+Available subpackages
+---------------------
+data
+    Basic functions to import csv data
+"""
+
+
+
+# import modules that should be available when the package is imported
+
+import pary.data
