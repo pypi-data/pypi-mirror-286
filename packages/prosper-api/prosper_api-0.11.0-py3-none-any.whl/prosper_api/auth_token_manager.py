ba4dbcad10fb215169edf3ab4b165f9253b8f94a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:04:32 +0000

    Add package prosper-api/0.11.0/prosper_api-0.11.0-py3-none-any.whl

---
+++ packages/prosper-api/prosper_api-0.11.0-py3-none-any.whl/prosper_api/auth_token_manager.py
@@ -0,0 +1,170 @@
+import json
+import logging
+from datetime import datetime, timedelta
+from os import makedirs
+from os.path import dirname, isfile, join
+
+import requests
+from platformdirs import user_cache_dir
+from prosper_shared.omni_config import Config, ConfigKey, SchemaType, config_schema
+from schema import Optional, Regex
+
+logger = logging.getLogger(__name__)
+
+_AUTH_URL = "https://api.prosper.com/v1/security/oauth/token"
+_ACCESS_TOKEN_KEY = "access_token"
+_REFRESH_TOKEN_KEY = "refresh_token"
+_EXPIRES_IN_KEY = "expires_in"
+_EXPIRES_AT_KEY = "expires_at"
+
+
+_CLIENT_ID_CONFIG_PATH = "prosper-api.credentials.client-id"
+_CLIENT_SECRET_CONFIG_PATH = "prosper-api.credentials.client-secret"
+_USERNAME_CONFIG_PATH = "prosper-api.credentials.username"
+_PASSWORD_CONFIG_PATH = "prosper-api.credentials.password"
+_TOKEN_CACHE_CONFIG_PATH = "prosper-api.auth.token-cache"
+_DEFAULT_TOKEN_CACHE_PATH = join(user_cache_dir("prosper-api"), "token-cache")
+
+
+@config_schema
+def _schema() -> SchemaType:
+    return {
+        "prosper-api": {
+            "credentials": {
+                ConfigKey("client-id", "The client-id from Prosper."): Regex(
+                    r"^[a-f0-9]{32}$"
+                ),
+                Optional(
+                    ConfigKey(
+                        "client-secret",
+                        "The client-secret from Prosper; can be stored and accessed securely using the keyring library.",
+                    )
+                ): Regex(r"^[a-f0-9]{32}$"),
+                ConfigKey("username", "Your Prosper username"): str,
+                Optional(
+                    ConfigKey(
+                        "password",
+                        "Your Prosper password; can be stored and accessed securely using the keyring library.",
+                    )
+                ): str,
+            },
+            "auth": {
+                ConfigKey(
+                    "token-cache",
+                    "The filesystem location where the auth token will be cached.",
+                    default=_DEFAULT_TOKEN_CACHE_PATH,
+                ): str
+            },
+        }
+    }
+
+
+class AuthTokenManager:
+    """Token manager for Prosper API tokens.
+
+    Uses credentials configured in the config file or in the OS key storage. The
+    credentials are cached and reused until they expire. After expiration, the manager
+    attempts to refresh the auth token using the refresh token; if that fails, the
+    full auth is re-executed.
+
+    See Also:
+        https://developers.prosper.com/docs/authenticating-with-oauth-2-0/password-flow/
+    """
+
+    def __init__(self, config: Config):
+        """Creates and AuthTokenManager instance.
+
+        Args:
+            config (Config): A prosper-api config
+        """
+        self.token_cache_path = config.get_as_str(_TOKEN_CACHE_CONFIG_PATH)
+        self.client_id = config.get_as_str(_CLIENT_ID_CONFIG_PATH)
+        self.client_secret = config.get_as_str(_CLIENT_SECRET_CONFIG_PATH)
+        self.username = config.get_as_str(_USERNAME_CONFIG_PATH)
+        self.password = config.get_as_str(_PASSWORD_CONFIG_PATH)
+
+        if self.client_secret or self.password:
+            logger.warning(
+                "Providing secrets via config files or the command line is not secure. Please switch to the "
+                "[more secure](https://github.com/grahamtt/prosper-api#more-secure) storage method."
+            )
+
+        self.token = None
+        if isfile(self.token_cache_path):
+            with open(self.token_cache_path) as token_cache_file:
+                self.token = json.load(token_cache_file)
+
+    def _initial_auth(self):
+        payload = {
+            "grant_type": "password",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret
+            if self.client_secret
+            else self._fetch_secret(self.client_id),
+            "username": self.username,
+            "password": self.password
+            if self.password
+            else self._fetch_secret(self.username),
+        }
+        headers = {"accept": "application/json"}
+        response = requests.request("POST", _AUTH_URL, data=payload, headers=headers)
+        response.raise_for_status()
+        self.token = response.json()
+        self._cache_token()
+
+    def _refresh_auth(self):
+        payload = {
+            "grant_type": "refresh_token",
+            "client_id": self.client_id,
+            "client_secret": self.client_secret
+            if self.client_secret
+            else self._fetch_secret(self.client_id),
+            "refresh_token": self.token[_REFRESH_TOKEN_KEY],
+        }
+        headers = {"accept": "application/json"}
+        response = requests.request("POST", _AUTH_URL, data=payload, headers=headers)
+        response.raise_for_status()
+        self.token = response.json()
+        self._cache_token()
+
+    def _cache_token(self):
+        self.token[_EXPIRES_AT_KEY] = (
+            datetime.now() + timedelta(seconds=self.token[_EXPIRES_IN_KEY] - 10)
+        ).timestamp()
+        logging.debug(f"Set expires at to {self.token[_EXPIRES_AT_KEY]}")
+        makedirs(dirname(self.token_cache_path), exist_ok=True)
+        with open(self.token_cache_path, "w") as token_cache_file:
+            json.dump(self.token, token_cache_file)
+
+    def _fetch_secret(self, id):
+        import keyring  # noqa: autoimport
+
+        return keyring.get_password("prosper-api", id)
+
+    def get_token(self):
+        """Get the auth token, generating it or refreshing it if necessary.
+
+        Returns
+            str: A valid authorization token for Prosper APIs.
+        """
+        try:
+            if self.token is None:
+                logger.info(
+                    "No cached auth token found; performing initial authentication"
+                )
+                self._initial_auth()
+            elif self.token[_EXPIRES_AT_KEY] <= datetime.now().timestamp():
+                logger.info("Cached auth token is expired; attempting to refresh it")
+                try:
+                    self._refresh_auth()
+                except Exception as ex:
+                    logger.info(
+                        "Failed to refresh auth token; performing full authentication"
+                    )
+                    logging.debug("Refresh auth token failure", exc_info=ex)
+                    self._initial_auth()
+        except Exception as ex:
+            logger.error("Failed to authenticate", exc_info=ex)
+            return None
+
+        return self.token[_ACCESS_TOKEN_KEY]
---
+++ packages/prosper-api/prosper_api-0.11.0-py3-none-any.whl/prosper_api/models/enums.py
@@ -0,0 +1,374 @@
+from enum import Enum
+
+
+class _DefaultEnum(Enum):
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}.{self.name}"
+
+
+class SortOrder(_DefaultEnum):
+    """Sort orders."""
+
+    ASCENDING = "asc"
+    DESCENDING = "desc"
+
+
+class SearchListingsSortBy(_DefaultEnum):
+    """Listing sort bys."""
+
+    AMOUNT_FUNDED = "amount_funded"
+    AMOUNT_REMAINING = "amount_remaining"
+    BORROWER_RATE = "borrower_rate"
+    FICO_SCORE = "fico_score"
+    INVESTMENT_TYPEID = "investment_typeid"
+    LENDER_YIELD = "lender_yield"
+    LISTING_AMOUNT = "listing_amount"
+    LISTING_CATEGORY_ID = "listing_category_id"
+    LISTING_CREATION_DATE = "listing_creation_date"
+    LISTING_END_DATE = "listing_end_date"
+    LISTING_NUMBER = "listing_number"
+    LISTING_START_DATE = "listing_start_date"
+    LISTING_STATUS = "listing_status"
+    LISTING_TERM = "listing_term"
+    PARTIAL_FUNDING_INDICATOR = "partial_funding_indicator"
+    PERCENT_FUNDED = "percent_funded"
+    PROSPER_RATING = "prosper_rating"
+    PROSPER_SCORE = "prosper_score"
+    VERIFICATION_STAGE = "verification_stage"
+
+
+class ListNotesSortBy(_DefaultEnum):
+    """Listing sort bys."""
+
+    LOAN_NOTE_ID = "loan_note_id"
+    NOTE_OWNERSHIP_AMOUNT = "note_ownership_amount"
+    AMOUNT_BORROWED = "amount_borrowed"
+    BORROWER_RATE = "borrower_rate"
+    PROSPER_RATING = "prosper_rating"
+    TERM = "term"
+    AGE_IN_MONTHS = "age_in_months"
+    ORIGINATION_DATE = "origination_date"
+    NOTE_STATUS = "note_status"
+
+
+class ListOrdersSortBy(_DefaultEnum):
+    """Listing sort bys."""
+
+    LISTING_TITLE = "listing_title"
+    PROSPER_RATING = "prosper_rating"
+    LISTING_AMOUNT = "listing_amount"
+    LOAN_TERM_MONTHS = "loan_term_months"
+    ESTIMATED_END_DATE = "estimated_end_date"
+
+
+class ListLoansSortBy(_DefaultEnum):
+    """Listing sort bys."""
+
+    LOAN_NUMBER = "loan_number"
+    BORROWER_RATE = "borrower_rate"
+    PROSPER_RATING = "prosper_rating"
+    ORIGINATION_DATE = "origination_date"
+
+
+class ProsperRating(_DefaultEnum):
+    """Prosper ratings."""
+
+    NA = "N/A"
+    HR = "HR"
+    E = "E"
+    D = "D"
+    C = "C"
+    B = "B"
+    A = "A"
+    AA = "AA"
+
+
+class IncomeRange(_DefaultEnum):
+    """Income ranges."""
+
+    NOT_DISPLAYED = 0
+    ZERO_DOLLARS = 1
+    BETWEEN_1_AND_24999_DOLLARS = 2
+    BETWEEN_25000_AND_49999_DOLLARS = 3
+    BETWEEN_50000_AND_74999_DOLLARS = 4
+    BETWEEN_75000_AND_99999_DOLLARS = 5
+    MORE_THAN_100000_DOLLARS = 6
+    NOT_EMPLOYED = 7
+
+
+class ListingCategory(_DefaultEnum):
+    """Listing categories."""
+
+    DEBT_CONSOLIDATION = 1
+    HOME_IMPROVEMENT = 2
+    BUSINESS = 3
+    PERSONAL_LOAN = 4
+    STUDENT_USE = 5
+    AUTO_MOTORCYCLE_RV_BOAT = 6
+    OTHER = 7
+    BABY_AND_ADOPTION = 8
+    BOAT = 9
+    COSMETIC_PROCEDURES = 10
+    ENGAGEMENT_RING_FINANCING = 11
+    GREEN_LOANS = 12
+    HOUSEHOLD_EXPENSES = 13
+    LARGE_PURCHASE = 14
+    MEDICAL_DENTAL = 15
+    MOTORCYCLE = 16
+    RV = 17
+    TAXES = 18
+    VACATION = 19
+    WEDDING_LOANS = 20
+    SPECIAL_OCCASION = 21
+
+
+class ListingStatus(_DefaultEnum):
+    """Listing statuses."""
+
+    ACTIVE = 2
+    WITHDRAWN = 3
+    EXPIRED = 4
+    COMPLETED = 5
+    CANCELLED = 6
+    PENDING_REVIEW = 7
+
+
+class InvestmentType(_DefaultEnum):
+    """Investment type of the listing."""
+
+    WHOLE = 0
+    FRACTIONAL = 1
+
+
+class InvestmentProduct(_DefaultEnum):
+    """Investment product of the listing."""
+
+    STANDARD = 0
+    SERIES_1 = 1
+
+
+class Occupation(_DefaultEnum):
+    """Occupations."""
+
+    ACCOUNTANT_CPA = "Accountant/CPA"
+    ANALYST = "Analyst"
+    ARCHITECT = "Architect"
+    ATTORNEY = "Attorney"
+    BIOLOGIST = "Biologist"
+    BUS_DRIVER = "Bus Driver"
+    CAR_DEALER = "Car Dealer"
+    CHEMIST = "Chemist"
+    CIVIL_SERVICE = "Civil Service"
+    CLERGY = "Clergy"
+    CLERICAL = "Clerical"
+    COMPUTER_PROGRAMMER = "Computer Programmer"
+    CONSTRUCTION = "Construction"
+    DENTIST = "Dentist"
+    DOCTOR = "Doctor"
+    ENGINEER_CHEMICAL = "Engineer - Chemical"
+    ENGINEER_ELECTRICAL = "Engineer - Electrical"
+    ENGINEER_MECHANICAL = "Engineer - Mechanical"
+    EXECUTIVE = "Executive"
+    FIREMAN = "Fireman"
+    FLIGHT_ATTENDANT = "Flight Attendant"
+    FOOD_SERVICE = "Food Service"
+    FOOD_SERVICE_MANAGEMENT = "Food Service Management"
+    HOMEMAKER = "Homemaker"
+    JUDGE = "Judge"
+    LABORER = "Laborer"
+    LANDSCAPING = "Landscaping"
+    MEDICAL_TECHNICIAN = "Medical Technician"
+    MILITARY_ENLISTED = "Military Enlisted"
+    MILITARY_OFFICER = "Military Officer"
+    NURSE_PLN = "Nurse (LPN)"
+    NURSE_RN = "Nurse (RN)"
+    NURSES_AIDE = "Nurse's Aide"
+    PHARMACIST = "Pharmacist"
+    PILOT = "Pilot - Private/Commercial"
+    POLICE_CORRECTION_OFFICER = "Police Officer/Correction Officer"
+    POSTAL_SERVICE = "Postal Service"
+    PRINCIPAL = "Principal"
+    PROFESSIONAL = "Professional"
+    PROFESSOR = "Professor"
+    PSYCHOLOGIST = "Psychologist"
+    REALTOR = "Realtor"
+    RELIGIOUS = "Religious"
+    RETAIL_MANAGEMENT = "Retail Management"
+    SALES_COMMISSION = "Sales - Commission"
+    SALES_RETAIL = "Sales - Retail"
+    SCIENTIST = "Scientist"
+    ADMINISTRATIVE_ASSISTANT = "Administrative Assistant"
+    SELF_EMPLOYED = "Self Employed"
+    SKILLED_LABOR = "Skilled Labor"
+    SOCIAL_WORKER = "Social Worker"
+    STUDENT_COLLEGE_FRESHMAN = "Student - College Freshman"
+    STUDENT_COLLEGE_SOPHOMORE = "Student - College Sophomore"
+    STUDENT_COLLEGE_JUNIOR = "Student - College Junior"
+    STUDENT_COLLEGE_SENIOR = "Student - College Senior"
+    STUDENT_COLLEGE_GRADUATE_STUDENT = "Student - College Graduate Student"
+    STUDENT_COMMUNITY_COLLEGE = "Student - Community College"
+    STUDENT_TECHNICAL_SCHOOL = "Student - Technical School"
+    TEACHER = "Teacher"
+    TEACHERS_AIDE = "Teacher's Aide"
+    TRADESMAN_CARPENTER = "Tradesman - Carpenter"
+    TRADESMAN_ELECTRICIAN = "Tradesman - Electrician"
+    TRADESMAN_MECHANIC = "Tradesman - Mechanic"
+    TRADESMAN_PLUMBER = "Tradesman - Plumber"
+    TRUCK_DRIVER = "Truck Driver"
+    WAITER_WAITRESS = "Waiter/Waitress"
+    OTHER = "Other"
+    INVESTOR = "Investor"
+
+
+class EmploymentStatus(_DefaultEnum):
+    """Employment statuses."""
+
+    EMPLOYED = "Employed"
+    SELF_EMPLOYED = "Self-employed"
+    RETIRED = "Retired"
+    NOT_EMPLOYED = "Not employed"
+    OTHER = "Other"
+
+
+class FICOScore(_DefaultEnum):
+    """FICO scores."""
+
+    LESS_THAN_600 = "<600"
+    BETWEEN_600_AND_619 = "600-619"
+    BETWEEN_620_AND_639 = "620-639"
+    BETWEEN_640_AND_659 = "640-659"
+    BETWEEN_660_AND_679 = "660-679"
+    BETWEEN_680_AND_699 = "680-699"
+    BETWEEN_700_AND_719 = "700-719"
+    BETWEEN_720_AND_739 = "720-739"
+    BETWEEN_740_AND_759 = "740-759"
+    BETWEEN_760_AND_779 = "760-779"
+    BETWEEN_780_AND_799 = "780-799"
+    BETWEEN_800_AND_819 = "800-819"
+    BETWEEN_820_AND_850 = "820-850"
+
+
+class BorrowerState(_DefaultEnum):
+    """Borrower state abbreviations."""
+
+    ALASKA = "AK"
+    ALABAMA = "AL"
+    ARKANSAS = "AR"
+    ARIZONA = "AZ"
+    CALIFORNIA = "CA"
+    COLORADO = "CO"
+    CONNECTICUT = "CT"
+    DISTRICT_OF_COLUMBIA = "DC"
+    DELAWARE = "DE"
+    FLORIDA = "FL"
+    GEORGIA = "GA"
+    HAWAII = "HI"
+    IOWA = "IA"
+    IDAHO = "ID"
+    ILLINOIS = "IL"
+    INDIANA = "IN"
+    KANSAS = "KS"
+    KENTUCKY = "KY"
+    LOUISIANA = "LA"
+    MASSACHUSETTS = "MA"
+    MARYLAND = "MD"
+    MAINE = "ME"
+    MICHIGAN = "MI"
+    MINNESOTA = "MN"
+    MISSOURI = "MO"
+    MISSISSIPPI = "MS"
+    MONTANA = "MT"
+    NORTH_CAROLINA = "NC"
+    NORTH_DAKOTA = "ND"
+    NEBRASKA = "NE"
+    NEW_HAMPSHIRE = "NH"
+    NEW_JERSEY = "NJ"
+    NEW_MEXICO = "NM"
+    NEVADA = "NV"
+    NEW_YORK = "NY"
+    OHIO = "OH"
+    OKLAHOMA = "OK"
+    OREGON = "OR"
+    PENNSYLVANIA = "PA"
+    RHODE_ISLAND = "RI"
+    SOUTH_CAROLINA = "SC"
+    SOUTH_DAKOTA = "SD"
+    TENNESSEE = "TN"
+    TEXAS = "TX"
+    UTAH = "UT"
+    VIRGINIA = "VA"
+    VERMONT = "VT"
+    WASHINGTON = "WA"
+    WISCONSIN = "WI"
+    WEST_VIRGINIA = "WV"
+    WYOMING = "WY"
+
+
+class LoanStatus(_DefaultEnum):
+    """Status of loan."""
+
+    ORIGINATION_DELAYED = 0
+    CURRENT = 1
+    CHARGED_OFF = 2
+    DEFAULTED = 3
+    COMPLETED = 4
+    FINAL_PAYMENT_IN_PROGRESS = 5
+    CANCELLED = 6
+
+
+class LoanDefaultReason(_DefaultEnum):
+    """Reason for loan default."""
+
+    NOT_IN_DEFAULT = 0
+    DELINQUENCY = 1
+    BANKRUPTCY = 2
+    DECEASED = 3
+    REPURCHASED = 4
+    PAID_IN_FULL = 5
+    SETTLED_IN_FULL = 6
+    SOLD = 7
+
+
+class OrderStatus(_DefaultEnum):
+    """Status of order."""
+
+    IN_PROGRESS = "IN_PROGRESS"
+    COMPLETED = "COMPLETED"
+
+
+class OrderSource(_DefaultEnum):
+    """Source of order."""
+
+    API = "API"
+    AI = "AI"
+    MI = "MI"
+
+
+class BidStatus(_DefaultEnum):
+    """Status of bid."""
+
+    PENDING = "PENDING"
+    INVESTED = "INVESTED"
+    EXPIRED = "EXPIRED"
+
+
+class BidResult(_DefaultEnum):
+    """Result of bid."""
+
+    NONE = "NONE"
+    AMOUNT_BID_TOO_HIGH = "AMOUNT_BID_TOO_HIGH"
+    AMOUNT_BID_TOO_LOW = "AMOUNT_BID_TOO_LOW"
+    BID_FAILED = "BID_FAILED"
+    BID_SUCCEEDED = "BID_SUCCEEDED"
+    CANNOT_BID_ON_FRACTIONAL_LOANS = "CANNOT_BID_ON_FRACTIONAL_LOANS"
+    CANNOT_BID_ON_OWN_LISTING = "CANNOT_BID_ON_OWN_LISTING"
+    INSUFFICIENT_FUNDS = "INSUFFICIENT_FUNDS"
+    INTERNAL_ERROR = "INTERNAL_ERROR"
+    INVESTMENT_ORDER_ALREADY_PROCESSED = "INVESTMENT_ORDER_ALREADY_PROCESSED"
+    LENDER_NOT_ELIGIBLE_TO_BID = "LENDER_NOT_ELIGIBLE_TO_BID"
+    LISTING_NOT_BIDDABLE = "LISTING_NOT_BIDDABLE"
+    SUITABILITY_REQUIREMENTS_NOT_MET = "SUITABILITY_REQUIREMENTS_NOT_MET"
+    PARTIAL_BID_SUCCEEDED = "PARTIAL_BID_SUCCEEDED"
