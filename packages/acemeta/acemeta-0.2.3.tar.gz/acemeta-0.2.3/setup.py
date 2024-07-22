6c7d05c82ee2c8d43e0aa38728acd53e645bef6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:30 +0000

    Add package acemeta/0.2.3/acemeta-0.2.3.tar.gz

---
+++ packages/acemeta/acemeta-0.2.3.tar.gz/acemeta-0.2.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+with open('README.md', encoding='utf-8') as readme:
+    description = readme.read() 
+
+setup(
+    name="acemeta",
+    version="0.2.3",
+    packages=find_packages(),
+    install_requires=["requests"],  # Hier können Abhängigkeiten aufgelistet werden
+    author="Annhilati",
+    #author_email="Ihre Email",
+    #description="Eine kurze Beschreibung Ihres Pakets",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    #url="https://example.com/mypackage",  # URL zu Ihrem Projekt (optional)
+)
