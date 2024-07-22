462a06f3f186ab164449a03bd68db6a17b1e60de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:42 +0000

    Add package dockerfile-git-hash/0.1.1/dockerfile-git-hash-0.1.1.tar.gz

---
+++ packages/dockerfile-git-hash/dockerfile-git-hash-0.1.1.tar.gz/dockerfile-git-hash-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dockerfile-git-hash",
+    version="0.1.1",
+    packages=find_packages(),
+    install_requires=[
+        # Add any dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            'dockerfile-git-hash=dockerfile_git_hash.dockerfile_git_hash:main',
+        ],
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A tool to update commit hashes in Dockerfiles",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/Ralf12358/dockerfile-git-hash",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
