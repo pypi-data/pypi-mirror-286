18769716e08c31a211cdcb419fc05cfb682070e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:04:11 +0000

    Add package binary-refinery/0.6.43/binary-refinery-0.6.43.tar.gz

---
+++ packages/binary-refinery/binary-refinery-0.6.43.tar.gz/binary-refinery-0.6.43/refinery/units/crypto/cipher/rsakey.py
@@ -0,0 +1,83 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+import base64
+import enum
+import itertools
+import json
+import textwrap
+
+from refinery.units import Arg, Unit
+from refinery.lib.json import flattened
+from refinery.units.crypto.cipher.rsa import normalize_rsa_key
+
+from Cryptodome.Util.asn1 import DerSequence
+from Cryptodome.Util import number
+
+
+class RSAFormat(str, enum.Enum):
+    PEM = 'PEM'
+    DER = 'DER'
+    XKMS = 'XKMS'
+    TEXT = 'TEXT'
+    JSON = 'JSON'
+
+
+class rsakey(Unit):
+    """
+    Parse RSA keys in various formats; PEM, DER, Microsoft BLOB, and W3C-XKMS (XML) format are supported.
+    The same formats are supported for the input format, but you can also specify a key in the following
+    format, where both modulus and exponent have to be hex-encoded: `[modulus]:[exponent]`
+    """
+    def __init__(
+        self,
+        public: Arg.Switch('-p', help='Force public key output even if the input is private.') = False,
+        output: Arg(help='Select an output format (PEM/DER/XKMS/TEXT/JSON), default is PEM.') = RSAFormat.PEM
+    ):
+        super().__init__(public=public, output=Arg.AsOption(output, RSAFormat))
+
+    def _xkms_wrap(self, number: int):
+        size, r = divmod(number.bit_length(), 8)
+        size += int(bool(r))
+        return base64.b64encode(number.to_bytes(size, 'big'))
+
+    def process(self, data):
+        key = normalize_rsa_key(data, force_public=self.args.public)
+        out = self.args.output
+        if out is RSAFormat.PEM:
+            yield key.export_key('PEM')
+            return
+        if out is RSAFormat.DER:
+            yield key.export_key('DER')
+            return
+        components = {
+            'Modulus' : key.n,
+            'Exponent': key.e,
+        }
+        if key.has_private():
+            decoded = DerSequence()
+            decoded.decode(key.export_key('DER'))
+            it = itertools.islice(decoded, 3, None)
+            for v in ('D', 'P', 'Q', 'DP', 'DQ', 'InverseQ'):
+                try:
+                    components[v] = next(it)
+                except StopIteration:
+                    break
+        if out is RSAFormat.XKMS:
+            for tag in components:
+                components[tag] = base64.b64encode(number.long_to_bytes(components[tag])).decode('ascii')
+            tags = '\n'.join(F'\t<{tag}>{value}</{tag}>' for tag, value in components.items())
+            yield F'<RSAKeyPair>\n{tags}\n</RSAKeyPair>'.encode(self.codec)
+            return
+        components['BitSize'] = key.n.bit_length()
+        for tag, value in components.items():
+            if value.bit_length() > 32:
+                components[tag] = F'{value:X}'
+        if out is RSAFormat.JSON:
+            yield json.dumps(components, indent=4).encode(self.codec)
+            return
+        if out is RSAFormat.TEXT:
+            table = list(flattened(components))
+            for key, value in table:
+                value = F'0x{value}' if isinstance(value, str) else str(value)
+                value = '\n'.join(F'{L}' for L in textwrap.wrap(value, 80))
+                yield F'-- {key + " ":-<77}\n{value!s}'.encode(self.codec)
---
+++ packages/binary-refinery/binary-refinery-0.6.43.tar.gz/binary-refinery-0.6.43/refinery/units/formats/pcap.py
@@ -0,0 +1,121 @@
+#!/usr/bin/env python3
+# -*- coding: utf - 8 -* -
+from typing import NamedTuple, Union, TYPE_CHECKING
+
+from refinery.units import Arg, Unit
+from refinery.lib.vfs import VirtualFileSystem, VirtualFile
+from refinery.lib.structures import MemoryFile
+from refinery.lib.tools import NoLogging
+
+import logging
+
+if TYPE_CHECKING:
+    from ipaddress import IPv4Address, IPv6Address
+    TIPAddr = Union[IPv4Address, IPv6Address]
+
+
+class Conversation(NamedTuple):
+    src_addr: str
+    dst_addr: str
+    src_port: int
+    dst_port: int
+
+    @classmethod
+    def FromID(cls, stream_id):
+        src, sp = stream_id.src
+        dst, dp = stream_id.dst
+        return cls(str(src), str(dst), sp, dp)
+
+    @property
+    def src(self):
+        return F'{self.src_addr}:{self.src_port}'
+
+    @property
+    def dst(self):
+        return F'{self.dst_addr}:{self.dst_port}'
+
+    def __hash__(self):
+        return hash(frozenset((self.src, self.dst)))
+
+    def swapped(self):
+        cls = self.__class__
+        return cls(self.dst_addr, self.src_addr, self.dst_port, self.src_port)
+
+    def __eq__(self, other):
+        return hash(self) == hash(other)
+
+    def __str__(self):
+        return F'[{self.src}] --> [{self.dst}]'
+
+    def src_to_dst(self):
+        return {'src': self.src, 'dst': self.dst}
+
+    def dst_to_src(self):
+        return {'src': self.dst, 'dst': self.src}
+
+
+class pcap(Unit):
+    """
+    Performs TCP stream reassembly from packet capture (PCAP) files. By default, the unit emits the parts of
+    each TCP conversation, attaching several pieces of metadata to each such output: Included are the source
+    and destination socket address as well as the variable `stream` which identifies the conversation which
+    it was part of. The chunks are returned in the order that the bytes were exchanged between source and
+    destination. When the `--merge` parameter is specified, the unit instead collects all bytes going forward
+    and backwards, respectively, and emitting these as two chunks, for each TCP conversation that took place.
+    """
+
+    def __init__(self, merge: Arg.Switch('-m', help='Merge both parts of each TCP conversation into one chunk.') = False):
+        super().__init__(merge=merge)
+
+    @Unit.Requires('pypcapkit[scapy]<0.16.0', 'all')
+    def _pcapkit():
+        import pcapkit
+        return pcapkit
+
+    @Unit.Requires('dictdumper==0.8.4', 'all')
+    def _pcapkit_python37_bugfix():
+        """
+        TODO: This artificial requirement pins a dependency of pypcapkit that otherwise breaks for
+        Python 3.7; see issue [#51](https://github.com/binref/refinery/issues/51). Once that issue
+        is resolved, this should be removed.
+        """
+
+    def process(self, data):
+        with NoLogging():
+            pcapkit = self._pcapkit
+            logging.getLogger('pcapkit').disabled = True
+
+        merge = self.args.merge
+
+        with VirtualFileSystem() as fs:
+            vf = VirtualFile(fs, data, 'pcap')
+            extraction = pcapkit.extract(
+                fin=vf.path, engine='scapy', store=False, nofile=True, extension=False, tcp=True, strict=True)
+            tcp: list = list(extraction.reassembly.tcp)
+
+        tcp.sort(key=lambda p: min(p.index, default=0))
+
+        count, convo = 0, None
+        src_buffer = MemoryFile()
+        dst_buffer = MemoryFile()
+        for stream in tcp:
+            this_convo = Conversation.FromID(stream.id)
+            if this_convo != convo:
+                if count and merge:
+                    if src_buffer.tell():
+                        yield self.labelled(src_buffer.getvalue(), **convo.src_to_dst())
+                        src_buffer.truncate(0)
+                    if dst_buffer.tell():
+                        yield self.labelled(dst_buffer.getvalue(), **convo.dst_to_src())
+                        dst_buffer.truncate(0)
+                count = count + 1
+                convo = this_convo
+            for packet in stream.packets:
+                if not merge:
+                    yield self.labelled(packet.data, **this_convo.src_to_dst(), stream=count)
+                elif this_convo.src == convo.src:
+                    src_buffer.write(packet.data)
+                elif this_convo.dst == convo.src:
+                    dst_buffer.write(packet.data)
+                else:
+                    raise RuntimeError(F'direction of packet {convo!s} in conversation {count} is unknown')
