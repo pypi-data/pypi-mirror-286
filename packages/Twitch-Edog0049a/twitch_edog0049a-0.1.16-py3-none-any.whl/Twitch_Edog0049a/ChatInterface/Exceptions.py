cdac8829e55543880895458b54c8e0bb46911c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:14 +0000

    Add package Twitch-Edog0049a/0.1.16/twitch_edog0049a-0.1.16-py3-none-any.whl

---
+++ packages/Twitch-Edog0049a/twitch_edog0049a-0.1.16-py3-none-any.whl/Twitch_Edog0049a/ChatInterface/Exceptions.py
@@ -0,0 +1,25 @@
+"""
+    _summary_
+
+_extended_summary_
+"""
+
+class InvalidMessageError(Exception):
+    """
+    InvalidMessageError _summary_
+
+    _extended_summary_
+
+    :param Exception: _description_
+    :type Exception: _type_
+    """
+class InvalidLoginError(Exception):
+    """
+    InvalidLoginError _summary_
+
+    _extended_summary_
+
+    :param Exception: _description_
+    :type Exception: _type_
+    """
+  
