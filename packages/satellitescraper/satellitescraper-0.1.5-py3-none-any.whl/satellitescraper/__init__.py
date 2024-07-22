36429318ab9f5cec347f8f8faa9e250fb801fb0a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:04 +0000

    Add package satellitescraper/0.1.5/satellitescraper-0.1.5-py3-none-any.whl

---
+++ packages/satellitescraper/satellitescraper-0.1.5-py3-none-any.whl/satellitescraper/__init__.py
@@ -0,0 +1,8 @@
+__name__ = "satellitescraper"
+__version__ = "0.1.5"
+__author__ = "Mostafa Mabrok| mostafa.m.mabrok@gmail.com"
+__release_date__ = '19-July-2024'
+from .satellitescraper import map_api
+from .file_size import get_folder_size
+from .sanity_checker import sanity_check
+from .tiles_sticher import update_stitcher_db, get_bbox_lat_lon, stitch_whole_tile
