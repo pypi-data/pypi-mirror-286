70fb5a786ee344f24ff18d41218386887c890cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:04:15 +0000

    Add package edsl/0.1.29/edsl-0.1.29-py3-none-any.whl

---
+++ packages/edsl/edsl-0.1.29-py3-none-any.whl/edsl/data_transfer_models.py
@@ -0,0 +1,34 @@
+"""This module contains the data transfer models for the application."""
+
+from collections import UserDict
+
+
+class AgentResponseDict(UserDict):
+    """A dictionary to store the response of the agent to a question."""
+
+    def __init__(
+        self,
+        *,
+        question_name,
+        answer,
+        prompts,
+        usage=None,
+        comment=None,
+        cached_response=None,
+        raw_model_response=None,
+        simple_model_raw_response=None,
+    ):
+        """Initialize the AgentResponseDict object."""
+        usage = usage or {"prompt_tokens": 0, "completion_tokens": 0}
+        super().__init__(
+            {
+                "answer": answer,
+                "comment": comment,
+                "question_name": question_name,
+                "prompts": prompts,
+                "usage": usage,
+                "cached_response": cached_response,
+                "raw_model_response": raw_model_response,
+                "simple_model_raw_response": simple_model_raw_response,
+            }
+        )
