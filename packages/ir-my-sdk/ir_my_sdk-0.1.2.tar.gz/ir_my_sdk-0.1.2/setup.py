d9785faa547eb1e0445b1e2aa3c21d2a5df5174e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:04:41 +0000

    Add package ir-my-sdk/0.1.2/ir_my_sdk-0.1.2.tar.gz

---
+++ packages/ir-my-sdk/ir_my_sdk-0.1.2.tar.gz/ir_my_sdk-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='ir_my_sdk',
+    version='0.1.2',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[],
+    author='Irvan Ariyanto',
+    author_email='irvan.ariyanto104@gmail.com',
+    description='A simple SDK',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/irvanariyanto/my_sdk',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    pythion_requires='>=3.6',
+)
