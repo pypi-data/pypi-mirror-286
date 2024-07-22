5093d2556805c2eeff5d90183af4b51c231873f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:04:19 +0000

    Add package genal-python/0.7/genal_python-0.7-py3-none-any.whl

---
+++ packages/genal-python/genal_python-0.7-py3-none-any.whl/genal/constants.py
@@ -0,0 +1,27 @@
+STANDARD_COLUMNS = ["CHR", "POS", "SNP", "EA", "NEA", "BETA", "SE", "P"]
+REF_PANELS = ["eur", "sas", "eas", "amr", "afr"]
+REF_PANEL_COLUMNS = ["CHR", "SNP", "POS", "A1", "A2"]
+CHECKS_DICT = {
+    "CHR": False,
+    "POS": False,
+    "P": False,
+    "EA": False,
+    "NEA": False,
+    "BETA": False,
+    "SNP": False,
+    "NA_removal": False,
+}
+MR_METHODS_NAMES = {
+    "IVW": "Inverse-Variance Weighted",
+    "IVW-RE": "Inverse Variance Weighted (Random Effects)",
+    "IVW-FE": "Inverse Variance Weighted (Fixed Effects)",
+    "UWR": "Unweighted Regression",
+    "WM": "Weighted Median",
+    "WM-pen": "Penalised Weighted Median",
+    "Simple-median": "Simple Median",
+    "Sign": "Sign concordance test",
+    "Egger": ("MR Egger", "Egger Intercept"),
+    "Egger-boot": ("MR Egger bootstrap", "Egger Intercept bootstrap"),
+    "Simple-mode": "Simple mode",
+    "Weighted-mode": "Weighted mode",
+}
