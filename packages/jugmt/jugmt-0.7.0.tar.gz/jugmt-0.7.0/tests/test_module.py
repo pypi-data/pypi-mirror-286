d599b72fc5cac7fdf70f77c8cb46b2d597ed6796
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:42 +0000

    Add package jugmt/0.7.0/jugmt-0.7.0.tar.gz

---
+++ packages/jugmt/jugmt-0.7.0.tar.gz/jugmt-0.7.0/tests/test_module.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+from pathlib import Path
+
+from jugmt.document import Document
+
+
+def test_module(tmp_path):
+
+    paths = list(Path("example").resolve().glob("*.docx"))
+    assert len(paths) > 0, "No documents/*.docx available for testing"
+
+    for path in paths:
+        document, errors = Document().from_docx(path)
+
+        document_json_str = document.to_json()
+        assert document_json_str, "Failed producing a string of JSON"
+
+        document_html_str = document.to_html()
+        assert document_html_str, "Failed producing a string of HTML"
+
+        assert document.check(), "Unexpected failure"
+
+        assert len(document.figures) == 2, "Unexpected amouni of figures in document"
