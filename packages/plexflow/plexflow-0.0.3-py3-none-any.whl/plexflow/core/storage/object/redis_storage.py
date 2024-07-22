91206be04ff265dfa2dbfc2ac01384dd018c0b27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:04:53 +0000

    Add package plexflow/0.0.3/plexflow-0.0.3-py3-none-any.whl

---
+++ packages/plexflow/plexflow-0.0.3-py3-none-any.whl/plexflow/core/storage/object/redis_storage.py
@@ -0,0 +1,120 @@
+import json
+import pickle
+import redis
+
+class RedisObjectStore:
+    """A class used to represent a Redis Object Store.
+
+    Attributes:
+        client (Redis): A Redis client instance.
+        default_ttl (int): The default TTL (in seconds) for stored objects.
+
+    """
+
+    def __init__(self, host='localhost', port=6379, db=0, default_ttl=3600):
+        """Constructs all the necessary attributes for the RedisObjectStore object.
+
+        Args:
+            host (str, optional): The host of the Redis server. Defaults to 'localhost'.
+            port (int, optional): The port of the Redis server. Defaults to 6379.
+            db (int, optional): The database index of the Redis server. Defaults to 0.
+            default_ttl (int, optional): The default TTL (in seconds) for stored objects. Defaults to 3600.
+
+        """
+
+        self.client = redis.Redis(host=host, port=port, db=db)
+        self.default_ttl = default_ttl
+
+    def _serialize(self, obj, use_json=False):
+        """Serializes an object.
+
+        Args:
+            obj (object): The object to serialize.
+            use_json (bool, optional): Whether to use JSON for serialization. If False, pickle is used.
+
+        Returns:
+            str: The serialized object.
+
+        Raises:
+            Exception: If an error occurs during serialization.
+
+        """
+
+        try:
+            return json.dumps(obj) if use_json else pickle.dumps(obj)
+        except (TypeError, pickle.PicklingError) as e:
+            raise Exception("Failed to serialize object") from e
+
+    def _store(self, key, serialized_object, ttl=None):
+        """Stores a serialized object in the Redis store.
+
+        Args:
+            key (str): The key under which the object is stored.
+            serialized_object (str): The serialized object to store.
+            ttl (int, optional): The TTL (in seconds) for the stored object. If None, the object is stored permanently.
+
+        Raises:
+            Exception: If an error occurs during storing the object.
+
+        """
+
+        try:
+            self.client.set(key, serialized_object, ex=ttl)
+        except redis.RedisError as e:
+            raise Exception("Failed to store object in Redis") from e
+
+    def store(self, key, obj, use_json=False):
+        """Stores a serialized version of an object in the Redis store permanently.
+
+        Args:
+            key (str): The key under which the object is stored.
+            obj (object): The object to store.
+            use_json (bool, optional): Whether to use JSON for serialization. If False, pickle is used.
+
+        """
+
+        serialized_object = self._serialize(obj, use_json)
+        self._store(key, serialized_object)
+
+    def store_temporarily(self, key, obj, ttl=None, use_json=False):
+        """Stores a serialized version of an object in the Redis store temporarily.
+
+        Args:
+            key (str): The key under which the object is stored.
+            obj (object): The object to store.
+            ttl (int, optional): The TTL (in seconds) for the stored object. If None, the default TTL is used.
+            use_json (bool, optional): Whether to use JSON for serialization. If False, pickle is used.
+
+        """
+
+        serialized_object = self._serialize(obj, use_json)
+        ttl = self.default_ttl if ttl is None else ttl
+        self._store(key, serialized_object, ttl)
+
+    def retrieve(self, key, use_json=False):
+        """Retrieves an object from the Redis store and deserializes it.
+
+        Args:
+            key (str): The key of the object to retrieve.
+            use_json (bool, optional): Whether to use JSON for deserialization. If False, pickle is used.
+
+        Returns:
+            object: The retrieved and deserialized object. If the object does not exist, returns None.
+
+        Raises:
+            Exception: If an error occurs during retrieving or deserializing the object.
+
+        """
+
+        try:
+            serialized_object = self.client.get(key)
+        except redis.RedisError as e:
+            raise Exception("Failed to retrieve object from Redis") from e
+
+        if serialized_object is None:
+            return None
+
+        try:
+            return json.loads(serialized_object) if use_json else pickle.loads(serialized_object)
+        except (json.JSONDecodeError, pickle.UnpicklingError) as e:
+            raise Exception("Failed to deserialize object") from e
