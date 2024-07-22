a548bb17e6b06c026f2af033f57c99ece9860cd4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:36 +0000

    Add package pkgsize/1.0.0/pkgsize-1.0.0.tar.gz

---
+++ packages/pkgsize/pkgsize-1.0.0.tar.gz/pkgsize-1.0.0/setup.py
@@ -0,0 +1,41 @@
+from setuptools import setup, find_packages
+import os
+
+
+def package_files(directory):
+    paths = []
+    for (path, directories, filenames) in os.walk(directory):
+        for filename in filenames:
+            paths.append(os.path.join('..', path, filename))
+    return paths
+
+
+extra_files = package_files('pkgsize/scripts')
+
+setup(
+    name='pkgsize',
+    version='1.0.0',
+    author='Navid M',
+    author_email='navidnm_@outlook.com',
+    description='CLI for finding size of Python libraries on PyPi, and comparing size with other libraries.',
+    long_description=open('../../README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://gitlab.com/navid-m/pkgsize',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': extra_files},
+    entry_points={
+        'console_scripts': [
+            'pkgsize=pkgsize.pkgsize:main',
+        ],
+    },
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+    ],
+    python_requires='>=3.6',
+)
