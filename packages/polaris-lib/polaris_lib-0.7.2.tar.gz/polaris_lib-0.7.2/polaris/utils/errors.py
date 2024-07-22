cb75ff9f0f79369ad21505e843617178106fbc23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:03 +0000

    Add package polaris-lib/0.7.2/polaris_lib-0.7.2.tar.gz

---
+++ packages/polaris-lib/polaris_lib-0.7.2.tar.gz/polaris_lib-0.7.2/polaris/utils/errors.py
@@ -0,0 +1,39 @@
+class InvalidDatasetError(ValueError):
+    pass
+
+
+class InvalidBenchmarkError(ValueError):
+    pass
+
+
+class InvalidResultError(ValueError):
+    pass
+
+
+class PolarisChecksumError(ValueError):
+    pass
+
+
+class PolarisHubError(Exception):
+    pass
+
+
+class PolarisUnauthorizedError(PolarisHubError):
+    DEFAULT_ERROR_MSG = (
+        "You are not logged in to Polaris or your login has expired. "
+        "You can use the Polaris CLI to easily authenticate yourself again, see `polaris login --help`."
+    )
+
+    def __init__(self, message: str = DEFAULT_ERROR_MSG):
+        super().__init__(message)
+
+
+class TestAccessError(Exception):
+    # Prevent pytest to collect this as a test
+    __test__ = False
+
+    pass
+
+
+class InvalidZarrChecksum(Exception):
+    pass
