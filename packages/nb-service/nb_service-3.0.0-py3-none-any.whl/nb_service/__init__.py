18f8cebc3f40848dc813a255ae22fa488effd905
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:32 +0000

    Add package nb-service/3.0.0/nb_service-3.0.0-py3-none-any.whl

---
+++ packages/nb-service/nb_service-3.0.0-py3-none-any.whl/nb_service/__init__.py
@@ -0,0 +1,22 @@
+from netbox.plugins import PluginConfig
+from .version import __version__
+
+
+class NbserviceConfig(PluginConfig):
+    name = 'nb_service'
+    base_url = 'nb_service'
+    verbose_name = 'Service Management'
+    description = 'ITSM Service Management'
+    version = __version__
+    author = 'Renato Almeida de Oliveira'
+    author_email = 'renato.almeida.oliveira@gmail.com'
+    min_version = "4.0.0"
+    required_settings = []
+    default_settings = {}
+
+    def ready(self):
+        from . import signals
+        super().ready()
+
+
+config = NbserviceConfig # noqa
