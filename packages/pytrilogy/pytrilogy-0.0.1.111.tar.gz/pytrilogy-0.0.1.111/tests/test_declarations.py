cf2d19336814c5561f043720a799104c23a4a0e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:35 +0000

    Add package pytrilogy/0.0.1.111/pytrilogy-0.0.1.111.tar.gz

---
+++ packages/pytrilogy/pytrilogy-0.0.1.111.tar.gz/pytrilogy-0.0.1.111/tests/test_declarations.py
@@ -0,0 +1,16 @@
+from trilogy.parser import parse
+
+
+def test_declarations():
+    declarations = """key namespace.user_id int metadata(description="the description");
+    metric namespace.count <- count(namespace.user_id);
+    metric namespace.distinct_count <- count_distinct(namespace.user_id); # the distinct count of user ids
+    """
+    env, _ = parse(declarations)
+
+    assert env.concepts["namespace.user_id"].namespace == "namespace"
+    assert env.concepts["namespace.count"].namespace == "namespace"
+    assert (
+        env.concepts["namespace.distinct_count"].metadata.description
+        == "the distinct count of user ids"
+    )
