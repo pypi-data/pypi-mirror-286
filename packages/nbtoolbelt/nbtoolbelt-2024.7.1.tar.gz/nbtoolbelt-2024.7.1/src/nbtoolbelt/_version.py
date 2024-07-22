341ccaedb891898fe675dc15717e9810d28dc880
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:38 +0000

    Add package nbtoolbelt/2024.7.1/nbtoolbelt-2024.7.1.tar.gz

---
+++ packages/nbtoolbelt/nbtoolbelt-2024.7.1.tar.gz/nbtoolbelt-2024.7.1/src/nbtoolbelt/_version.py
@@ -0,0 +1,17 @@
+"""
+Version, easily accessible in other files
+
+Copyright (c) 2017 - Eindhoven University of Technology, The Netherlands
+
+This software is made available under the terms of the MIT License.
+"""
+
+# TODO: should each tool have its own version?
+
+import importlib.metadata
+
+#: Package name
+package_name = 'nbtoolbelt'
+
+#: Version number, as string
+__version__ = importlib.metadata.version(package_name)
