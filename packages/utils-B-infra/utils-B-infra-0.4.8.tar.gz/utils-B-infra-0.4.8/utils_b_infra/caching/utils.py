1fb916dc662d2f30ba92fb9bd3ffd8e94f59c4d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 03:03:44 +0000

    Add package utils-B-infra/0.4.8/utils-B-infra-0.4.8.tar.gz

---
+++ packages/utils-B-infra/utils-B-infra-0.4.8.tar.gz/utils-B-infra-0.4.8/utils_b_infra/caching/utils.py
@@ -0,0 +1,23 @@
+import asyncio
+import hashlib
+from typing import Any, Callable, Dict, Tuple
+
+
+async def key_builder(
+        func: Callable[..., Any],
+        app_space: str,
+        namespace: str,
+        args: Tuple[Any, ...],
+        kwargs: Dict[str, Any]
+) -> str:
+    kwargs_copy = kwargs.copy()
+    sorted_kwargs = tuple(sorted(kwargs_copy.items()))
+
+    key_str = f"{func.__name__}:{args}:{sorted_kwargs}"
+    key_hash = hashlib.md5(key_str.encode()).hexdigest()
+    key = f"{app_space}:{namespace}:{key_hash}"
+    return key
+
+
+def run_coro_in_background(coro):
+    asyncio.create_task(coro)
