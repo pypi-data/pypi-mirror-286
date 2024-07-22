ac6a53172e642a5181803a51d3cf58e1c60bed26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-22 04:03:44 +0000

    Add package Man-os2/0.1.1/man_os2-0.1.1.tar.gz

---
+++ packages/Man-os2/man_os2-0.1.1.tar.gz/man_os2-0.1.1/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+# Read the contents of README.md for the long description
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="Man_os2",  # Replace with your own library name
+    version="0.1.1",
+    author="Ahmd ZOUITANE",
+    author_email="zouitane.ahmed02@gmail.com",
+    description="#### Role:\nThe `Man_os2` library provides a set of utility functions for file and directory management tasks such as creating, removing, renaming, and listing files and directories. It also includes functions for file operations like reading and writing, as well as retrieving system and environmental information.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/ZOUITANE-Ahmed/Man_os2.git",  # Replace with the URL of your GitHub repo
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.12",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "colorama>=0.4.4",
+    ],
+)
