30a7638429b4fc32cdfe4cc6b56abb66cf6b8d11
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:02 +0000

    Add package feriados-municipais/0.1.2/feriados-municipais-0.1.2.tar.gz

---
+++ packages/feriados-municipais/feriados-municipais-0.1.2.tar.gz/feriados-municipais-0.1.2/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='feriados-municipais',
+    version='0.1.2',
+    license='MIT License',
+    author='Luiz Fernando Teles Rodrigues',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='euluizfernando2001@gmail.com',
+    keywords='feriados br municipais brasil',
+    description=u'Versão beta da biblioteca de feriados municipais.',
+    packages=find_packages(),
+    install_requires=['datetime'],)
