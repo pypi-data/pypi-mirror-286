f4c85dcd2502154e7a48901de6a5de3a15b98602
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:24 +0000

    Add package garminmaps/0.0.6/garminmaps-0.0.6.tar.gz

---
+++ packages/garminmaps/garminmaps-0.0.6.tar.gz/garminmaps-0.0.6/garminmaps/activities.py
@@ -0,0 +1,16 @@
+from garminmaps.mapping import update_map
+
+
+class Activity:
+    def __init__(self, payload, garmin_client):
+        self.id = payload["activityId"]
+        self.type = payload["activityType"]["typeKey"]
+        self.distance = payload["distance"]
+        self.duration = payload["duration"]
+        self.gpx = garmin_client.download_activity(
+            self.id, garmin_client.ActivityDownloadFormat.GPX
+        )
+
+    # Plots the activity on the given map
+    def plot(self, map, line_color) -> None:
+        update_map(self, map, line_color)
