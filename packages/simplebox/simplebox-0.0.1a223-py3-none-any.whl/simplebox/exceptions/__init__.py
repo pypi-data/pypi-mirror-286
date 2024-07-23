72814093f6e59baeac87a09f75fee800989553ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:33 +0000

    Add package simplebox/0.0.1a223/simplebox-0.0.1a223-py3-none-any.whl

---
+++ packages/simplebox/simplebox-0.0.1a223-py3-none-any.whl/simplebox/exceptions/__init__.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from ._exceptions import (BasicException, CallException, TimeoutExpiredException, HttpException, RestInternalException,
+                          ArgumentException, TypeException, NonePointerException, NotFountException, EmptyException,
+                          InstanceException, ValidatorException, CommandException, LengthException, raise_exception)
---
+++ packages/simplebox/simplebox-0.0.1a223-py3-none-any.whl/simplebox/json.py
@@ -0,0 +1,131 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+from collections.abc import Callable
+from typing import Any, IO, Union, Optional
+
+import orjson
+
+from .config.json import JsonConfig
+
+"""
+This module just wraps the orjson module and set params by JsonConfig(global config). 
+For all usages, please refer to the orjson module.
+https://github.com/ijl/orjson
+"""
+
+
+def _build_option(option, orjson_option):
+    if option is None:
+        return orjson_option
+    return option | orjson_option
+
+
+def _inner_dumps(obj, *, default: Optional[Callable[[Any], Any]] = None, newline: bool = None, indent2: bool = None,
+                 naive_utc: bool = None, non_str_keys: bool = None, omit_microseconds: bool = None,
+                 passthrough_dataclass: bool = None, passthrough_datetime: bool = None,
+                 passthrough_subclass: bool = None, serialize_dataclass: bool = None,
+                 serialize_numpy: bool = None, serialize_uuid: bool = None, sort_keys: bool = None,
+                 strict_integer: bool = None, utc_z: bool = None) -> bytes:
+    """
+    Serialize ``obj`` to a JSON formatted ``str``.
+    :params ensure_ascii: If ``ensure_ascii`` is false, then the strings written to ``fp`` can
+    contain non-ASCII characters if they appear in strings contained in
+    ``obj``. Otherwise, all such characters are escaped in JSON strings.
+    """
+    option = None
+    if newline is True or JsonConfig.newline:
+        option = _build_option(option, orjson.OPT_APPEND_NEWLINE)
+    if indent2 is True or JsonConfig.indent2:
+        option = _build_option(option, orjson.OPT_INDENT_2)
+    if naive_utc is True or JsonConfig.naive_utc:
+        option = _build_option(option, orjson.OPT_NAIVE_UTC)
+    if non_str_keys is True or JsonConfig.non_str_keys:
+        option = _build_option(option, orjson.OPT_NON_STR_KEYS)
+    if omit_microseconds is True or JsonConfig.omit_microseconds:
+        option = _build_option(option, orjson.OPT_OMIT_MICROSECONDS)
+    if passthrough_dataclass is True or JsonConfig.passthrough_dataclass:
+        option = _build_option(option, orjson.OPT_PASSTHROUGH_DATACLASS)
+    if passthrough_datetime is True or JsonConfig.passthrough_datetime:
+        option = _build_option(option, orjson.OPT_PASSTHROUGH_DATETIME)
+    if passthrough_subclass is True or JsonConfig.passthrough_subclass:
+        option = _build_option(option, orjson.OPT_PASSTHROUGH_SUBCLASS)
+    if serialize_dataclass is True or JsonConfig.serialize_dataclass:
+        option = _build_option(option, orjson.OPT_SERIALIZE_DATACLASS)
+    if serialize_numpy is True or JsonConfig.serialize_numpy:
+        option = _build_option(option, orjson.OPT_SERIALIZE_NUMPY)
+    if serialize_uuid is True or JsonConfig.serialize_uuid:
+        option = _build_option(option, orjson.OPT_SERIALIZE_UUID)
+    if sort_keys is True or JsonConfig.sort_keys:
+        option = _build_option(option, orjson.OPT_SORT_KEYS)
+    if strict_integer is True or JsonConfig.strict_integer:
+        option = _build_option(option, orjson.OPT_STRICT_INTEGER)
+    if utc_z is True or JsonConfig.utc_z:
+        option = _build_option(option, orjson.OPT_UTC_Z)
+
+    return orjson.dumps(obj, default=default or JsonConfig.default, option=option)
+
+
+def _inner_loads(s: Union[bytes, bytearray, memoryview, str]) -> Any:
+    """
+    Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance
+    containing a JSON document) to a Python object.
+    """
+    return orjson.loads(s)
+
+
+def dump(obj, fp: IO[bytes or str], encoding="UTF-8", *, default=None, newline: bool = None, indent2: bool = None,
+         naive_utc: bool = None,
+         omit_microseconds: bool = None, passthrough_dataclass: bool = None, passthrough_datetime: bool = None,
+         passthrough_subclass: bool = None, serialize_dataclass: bool = None, serialize_numpy: bool = None,
+         serialize_uuid: bool = None, sort_keys: bool = None, strict_integer: bool = None, utc_z: bool = None) -> None:
+    """Serialize ``obj`` as a JSON formatted stream to ``fp`` (a
+    ``.write()``-supporting file-like object).
+    :params fp: open("file", "w"), not bytes.
+
+    """
+    data = _inner_dumps(obj, default=default, newline=newline, indent2=indent2, naive_utc=naive_utc,
+                        omit_microseconds=omit_microseconds,
+                        passthrough_dataclass=passthrough_dataclass, passthrough_datetime=passthrough_datetime,
+                        passthrough_subclass=passthrough_subclass, serialize_dataclass=serialize_dataclass,
+                        serialize_numpy=serialize_numpy, serialize_uuid=serialize_uuid, sort_keys=sort_keys,
+                        strict_integer=strict_integer, utc_z=utc_z)
+    # noinspection PyBroadException
+    try:
+        fp.write(data)
+    except BaseException:
+        fp.write(data.decode(encoding=encoding))
+
+
+def dumps(obj, *, default=None, encoding="UTF-8", newline: bool = None, indent2: bool = None, naive_utc: bool = None,
+          omit_microseconds: bool = None, passthrough_dataclass: bool = None, passthrough_datetime: bool = None,
+          passthrough_subclass: bool = None, serialize_dataclass: bool = None, serialize_numpy: bool = None,
+          serialize_uuid: bool = None, sort_keys: bool = None, strict_integer: bool = None, utc_z: bool = None) -> str:
+    """
+    Serialize ``obj`` to a JSON formatted ``str``.
+    :params ensure_ascii: If ``ensure_ascii`` is false, then the strings written to ``fp`` can
+    contain non-ASCII characters if they appear in strings contained in
+    ``obj``. Otherwise, all such characters are escaped in JSON strings.
+    """
+    return _inner_dumps(obj, default=default, newline=newline, indent2=indent2, naive_utc=naive_utc,
+                        omit_microseconds=omit_microseconds,
+                        passthrough_dataclass=passthrough_dataclass, passthrough_datetime=passthrough_datetime,
+                        passthrough_subclass=passthrough_subclass, serialize_dataclass=serialize_dataclass,
+                        serialize_numpy=serialize_numpy, serialize_uuid=serialize_uuid, sort_keys=sort_keys,
+                        strict_integer=strict_integer, utc_z=utc_z).decode(encoding=encoding)
+
+
+def load(fp: IO[str or bytes]) -> Any or dict or list:
+    """
+    Deserialize ``fp`` (a ``.read()``-supporting file-like object containing
+    a JSON document) to a Python object.
+    """
+
+    return _inner_loads(fp.read())
+
+
+def loads(s: Union[bytes, bytearray, memoryview, str]) -> Any:
+    """
+    Deserialize ``s`` (a ``str``, ``bytes`` or ``bytearray`` instance
+    containing a JSON document) to a Python object.
+    """
+    return _inner_loads(s)
