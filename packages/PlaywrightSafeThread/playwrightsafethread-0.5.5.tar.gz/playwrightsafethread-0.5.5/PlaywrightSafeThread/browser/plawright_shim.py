d923714fda9aab10b08432a119c384c67d52c88c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:00 +0000

    Add package PlaywrightSafeThread/0.5.5/playwrightsafethread-0.5.5.tar.gz

---
+++ packages/PlaywrightSafeThread/playwrightsafethread-0.5.5.tar.gz/playwrightsafethread-0.5.5/PlaywrightSafeThread/browser/plawright_shim.py
@@ -0,0 +1,25 @@
+# Taken from playwright.__main__ so it is possible to run the command line
+# tool directly from a python execution
+import sys
+import subprocess
+
+from playwright._impl._driver import compute_driver_executable, get_driver_env
+
+
+def run_playwright(*args: str) -> int:
+    env = get_driver_env()
+
+    # NOTE: we do this to deal with pyinstaller
+    if getattr(sys, "frozen", False):
+        env.setdefault("PLAYWRIGHT_BROWSERS_PATH", "0")
+    k = {}
+    if "win" in sys.platform:
+        k["creationflags"] = subprocess.CREATE_NO_WINDOW
+
+    driver_executable = compute_driver_executable()
+    completed_process = subprocess.run([str(driver_executable), *args], env=env, capture_output=True,
+                                       **k,
+                                       )
+
+
+    return completed_process.returncode
