c93a8c03bbac7f67c2c672d18bbae1ffe5ff493e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:04:03 +0000

    Add package myfunctions-yasinozturk3580/0.0.13/myfunctions_yasinozturk3580-0.0.13-py3-none-any.whl

---
+++ packages/myfunctions-yasinozturk3580/myfunctions_yasinozturk3580-0.0.13-py3-none-any.whl/myfunctions_yasinozturk3580/math.py
@@ -0,0 +1,18 @@
+"""
+    This is how you use  this function
+"""
+
+def addition(NUMBER1,NUMBER2):
+    """
+    This is how you use  this function
+    e.g addition(2,3)
+    """
+    print(NUMBER1 + NUMBER2)
+
+
+    def multiplication(NUMBER1,NUMBER2):
+    """
+    This is how you use  this function
+    e.g multiplication(2,5)
+    """
+    print(NUMBER1 + NUMBER2)
