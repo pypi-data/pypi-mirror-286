2124dab8fe90d8b3ed7cb16f3eca61efda85d95d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:24 +0000

    Add package fftvis/0.0.7/fftvis-0.0.7-py3-none-any.whl

---
+++ packages/fftvis/fftvis-0.0.7-py3-none-any.whl/fftvis/tests/test_compare_matvis.py
@@ -0,0 +1,161 @@
+import matvis
+import pytest
+import numpy as np
+from fftvis import simulate
+from pyuvsim.analyticbeam import AnalyticBeam
+
+
+def test_simulate():
+    """ """
+    # Simulation parameters
+    ntimes = 10
+    nfreqs = 5
+    nants = 3
+    nsrcs = 20
+
+    # Set random set
+    rng = np.random.default_rng(42)
+
+    # Define frequency and time range
+    freqs = np.linspace(100e6, 200e6, nfreqs)
+    lsts = np.linspace(0, np.pi, ntimes)
+
+    # Set up the antenna positions
+    antpos = {k: np.array([k * 10, 0, 0]) for k in range(nants)}
+
+    # Define a Gaussian beam
+    beam = AnalyticBeam("gaussian", diameter=14.0)
+
+    # Set sky model
+    ra = np.linspace(0.0, 2.0 * np.pi, nsrcs)
+    dec = np.linspace(-0.5 * np.pi, 0.5 * np.pi, nsrcs)
+    sky_model = rng.uniform(0, 1, size=(nsrcs, 1)) * (freqs[None] / 150e6) ** -2.5
+
+    # Use matvis as a reference
+    mvis = matvis.simulate_vis(
+        antpos, sky_model, ra, dec, freqs, lsts, beams=[beam], precision=2
+    )
+
+    # Use fftvis to simulate visibilities
+    fvis = simulate.simulate_vis(
+        antpos, sky_model, ra, dec, freqs, lsts, beam, precision=2, eps=1e-10
+    )
+
+    # Should have shape (nfreqs, ntimes, nants, nants)
+    assert fvis.shape == (nfreqs, ntimes, nants, nants)
+
+    # Check that the results are the same
+    assert np.allclose(mvis, fvis, atol=1e-5)
+
+    # Test polarized visibilities
+    # Use matvis as a reference
+    mvis = matvis.simulate_vis(
+        antpos,
+        sky_model,
+        ra,
+        dec,
+        freqs,
+        lsts,
+        beams=[beam],
+        precision=2,
+        polarized=True,
+    )
+
+    # Use fftvis to simulate visibilities
+    fvis = simulate.simulate_vis(
+        antpos,
+        sky_model,
+        ra,
+        dec,
+        freqs,
+        lsts,
+        beam,
+        precision=2,
+        eps=1e-10,
+        polarized=True,
+    )
+
+    # Should have shape (nfreqs, ntimes, nfeeds, nfeeds, nants, nants)
+    assert fvis.shape == (nfreqs, ntimes, 2, 2, nants, nants)
+
+    # Check that the polarized results are the same
+    assert np.allclose(mvis, fvis, atol=1e-5)
+
+    # Simulate with specified baselines
+    sim_baselines = [(0, 1), (0, 2), (1, 2)]
+    fvis = simulate.simulate_vis(
+        antpos,
+        sky_model,
+        ra,
+        dec,
+        freqs,
+        lsts,
+        beam,
+        baselines=sim_baselines,
+        precision=2,
+        eps=1e-10,
+        polarized=True,
+    )
+
+    # Should have shape (nfreqs, ntimes, 2, 2, len(sim_baselines))
+    assert fvis.shape == (nfreqs, ntimes, 2, 2, len(sim_baselines))
+
+    # Check that the polarized results are the same
+    for bi, bl in enumerate(sim_baselines):
+        assert np.allclose(fvis[:, :, :, :, bi], mvis[:, :, :, :, bl[0], bl[1]])
+
+    # Test with precision 1
+    # Use matvis as a reference
+    mvis = matvis.simulate_vis(
+        antpos, sky_model, ra, dec, freqs, lsts, beams=[beam], precision=1
+    )
+
+    # Use fftvis to simulate visibilities
+    fvis = simulate.simulate_vis(
+        antpos, sky_model, ra, dec, freqs, lsts, beam, precision=1, eps=6e-8
+    )
+
+    # Should have shape (nfreqs, ntimes, nants, nants)
+    assert fvis.shape == (nfreqs, ntimes, nants, nants)
+
+    # Check that the results are the same
+    assert np.allclose(mvis, fvis, atol=1e-4)
+
+
+def test_simulate_non_coplanar():
+    # Simulation parameters
+    ntimes = 10
+    nfreqs = 5
+    nants = 3
+    nsrcs = 20
+
+    # Set random set
+    rng = np.random.default_rng(42)
+
+    # Define frequency and time range
+    freqs = np.linspace(100e6, 200e6, nfreqs)
+    lsts = np.linspace(0, np.pi, ntimes)
+
+    # Set up the antenna positions
+    antpos = {k: np.array([k * 10, 0, k]) for k in range(nants)}
+
+    # Define a Gaussian beam
+    beam = AnalyticBeam("gaussian", diameter=14.0)
+
+    # Set sky model
+    ra = np.linspace(0.0, 2.0 * np.pi, nsrcs)
+    dec = np.linspace(-0.5 * np.pi, 0.5 * np.pi, nsrcs)
+    sky_model = rng.uniform(0, 1, size=(nsrcs, 1)) * (freqs[None] / 150e6) ** -2.5
+
+    # Use matvis as a reference
+    mvis = matvis.simulate_vis(
+        antpos, sky_model, ra, dec, freqs, lsts, beams=[beam], precision=2
+    )
+
+    # Use fftvis to simulate visibilities
+    fvis = simulate.simulate_vis(
+        antpos, sky_model, ra, dec, freqs, lsts, beam, precision=2, eps=1e-10
+    )
+
+    # Check that the results are the same
+    assert np.allclose(mvis, fvis, atol=1e-5)
---
+++ packages/fftvis/fftvis-0.0.7-py3-none-any.whl/fftvis/utils.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+IDEALIZED_BL_TOL = 1e-8  # bl_error_tol for redcal.get_reds when using antenna positions calculated from reds
+speed_of_light = 299792458.0  # m/s
+
+
+def get_pos_reds(antpos, decimals=3, include_autos=True):
+    """
+    Figure out and return list of lists of redundant baseline pairs. This function is a modified version of the
+    get_pos_reds function in redcal. It is used to calculate the redundant baseline groups from antenna positions
+    rather than from a list of baselines. This is useful for simulating visibilities with fftvis.
+
+    Parameters:
+    ----------
+        antpos: dict
+            dictionary of antenna positions in the form {ant_index: np.array([x,y,z])}.
+        decimals: int, optional
+            Number of decimal places to round to when determining redundant baselines. default is 3.
+        include_autos: bool, optional
+            if True, include autos in the list of pos_reds. default is False
+    Returns:
+    -------
+        reds: list of lists of redundant tuples of antenna indices (no polarizations),
+        sorted by index with the first index of the first baseline the lowest in the group.
+    """
+    # Create a dictionary of redundant baseline groups
+    uv_to_red_key = {}
+    reds = {}
+
+    # Compute baseline lengths and round to specified precision
+    baselines = np.round(
+        [
+            antpos[aj] - antpos[ai]
+            for ai in antpos
+            for aj in antpos
+            if ai < aj or include_autos and ai == aj
+        ],
+        decimals,
+    )
+
+    ci = 0
+    for ai in antpos:
+        for aj in antpos:
+            if ai < aj or include_autos and ai == aj:
+                u, v, _ = baselines[ci]
+
+                if (u, v) not in uv_to_red_key and (-u, -v) not in uv_to_red_key:
+                    reds[(ai, aj)] = [(ai, aj)]
+                    uv_to_red_key[(u, v)] = (ai, aj)
+                elif (-u, -v) in uv_to_red_key:
+                    reds[uv_to_red_key[(-u, -v)]].append((aj, ai))
+                elif (u, v) in uv_to_red_key:
+                    reds[uv_to_red_key[(u, v)]].append((ai, aj))
+
+                ci += 1
+
+    reds_list = []
+    for k in reds:
+        red = reds[k]
+        ant1, ant2 = red[0]
+        _, bly, _ = antpos[ant2] - antpos[ant1]
+        if bly < 0:
+            reds_list.append([(bl[1], bl[0]) for bl in red])
+        else:
+            reds_list.append(red)
+
+    return reds_list
