23dab6b2cf753889a63b8a96c3f8dc302a16604b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:13 +0000

    Add package kalabash/2.2.4/kalabash-2.2.4.tar.gz

---
+++ packages/kalabash/kalabash-2.2.4.tar.gz/kalabash-2.2.4/README.rst
@@ -0,0 +1,12 @@
+Kalabash
+========
+
+Kalabash Platform.
+
+Features
+--------
+
+* Easy to use
+* Modular design
+  * Admin panel
+  * User management
---
+++ packages/kalabash/kalabash-2.2.4.tar.gz/kalabash-2.2.4/frontend/src/views/Dashboard.vue
@@ -0,0 +1,15 @@
+<template>
+<div>
+  <v-toolbar flat>
+    <v-toolbar-title><translate>Welcome to Kalabash</translate></v-toolbar-title>
+  </v-toolbar>
+  <v-alert
+    type="info"
+    border="top"
+    colored-border
+    elevation="2"
+    class="mt-10"
+    >
+  </v-alert>
+</div>
+</template>
