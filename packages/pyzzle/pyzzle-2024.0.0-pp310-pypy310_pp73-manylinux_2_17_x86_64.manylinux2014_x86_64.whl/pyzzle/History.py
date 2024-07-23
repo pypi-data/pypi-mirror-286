4d0db1a33d42589b4d3ec8282f5da953b9480cbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:04:07 +0000

    Add package pyzzle/2024.0.0/pyzzle-2024.0.0-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/pyzzle/pyzzle-2024.0.0-pp310-pypy310_pp73-manylinux_2_17_x86_64.manylinux2014_x86_64.whl/pyzzle/History.py
@@ -0,0 +1,53 @@
+from enum import Enum
+
+
+class HistoryCode(Enum):
+    """
+    Enumeration of the history.
+
+    The result number corresponds to the judgment result
+    1. Add
+    2. Drop
+    3. Drop(in Kick process)
+    4. Move
+    """
+    ADD = 1
+    DROP = 2
+    DROP_KICK = 3
+    MOVE = 4
+
+
+class HistoryItem(list):
+    def __init__(self, code, ori, i, j, word):
+        super().__init__([code, ori, i, j, word])
+    @property
+    def code(self):
+        return self[0]
+    @property
+    def ori(self):
+        return self[1]
+    @property
+    def i(self):
+        return self[2]
+    @property
+    def j(self):
+        return self[3]
+    @property
+    def word(self):
+        return self[4]
+
+
+class HistoryItemMove(HistoryItem):
+    def __init__(self, code, direction, n):
+        if code != HistoryCode.MOVE:
+            raise ValueError("'code' must be HistoryCode.Move")
+        super().__init__(code, direction, n, None, None)
+    @property
+    def code(self):
+        return self[0]
+    @property
+    def direction(self):
+        return self[1]
+    @property
+    def n(self):
+        return self[2]
