b5c92c50038df83a4eaa9c4b6bb122099e9d6700
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:03:32 +0000

    Add package danila-lib/2.2.6/danila-lib-2.2.6.tar.gz

---
+++ packages/danila-lib/danila-lib-2.2.6.tar.gz/danila-lib-2.2.6/danila/danila_rama.py
@@ -0,0 +1,10 @@
+from danila.danila_rama_classify import Danila_rama_classify
+
+
+class Danila_rama:
+    def __init__(self, yolov5_dir, danila_rama_params):
+        self.danila_rama_params = danila_rama_params
+        self.danila_rama_classify = Danila_rama_classify(yolov5_dir, self.danila_rama_params.danila_rama_classify_params)
+
+    def rama_classify(self,img,detail):
+        return self.danila_rama_classify.rama_classify(img, detail)
