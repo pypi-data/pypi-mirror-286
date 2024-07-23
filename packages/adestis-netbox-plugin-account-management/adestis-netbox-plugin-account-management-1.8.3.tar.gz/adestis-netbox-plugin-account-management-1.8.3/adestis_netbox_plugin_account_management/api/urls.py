3720b3ae427fb6afc6d192aa6d12ae33e5fb5c46
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:04:38 +0000

    Add package adestis-netbox-plugin-account-management/1.8.3/adestis-netbox-plugin-account-management-1.8.3.tar.gz

---
+++ packages/adestis-netbox-plugin-account-management/adestis-netbox-plugin-account-management-1.8.3.tar.gz/adestis-netbox-plugin-account-management-1.8.3/adestis_netbox_plugin_account_management/api/urls.py
@@ -0,0 +1,10 @@
+from netbox.api.routers import NetBoxRouter
+from . import views
+
+app_name = 'adestis_netbox_plugin_account_management'
+
+router = NetBoxRouter()
+router.register('systems', views.SystemListViewSet)
+router.register('login-credentials', views.LoginCredentialsViewSet)
+
+urlpatterns = router.urls
