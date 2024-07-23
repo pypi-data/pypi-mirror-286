7077edeaaf5ba600fe11958323e806dae0350793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:28 +0000

    Add package uneval/0.0.1/uneval-0.0.1-py3-none-any.whl

---
+++ packages/uneval/uneval-0.0.1-py3-none-any.whl/uneval/__init__.py
@@ -0,0 +1,3 @@
+__all__ = "Expression", "quote", "to_ast", "to_expression", "to_code", "if_", "for_", "lambda_"
+
+from .expression import quote, to_ast, to_expression, Expression, to_code, if_, for_, lambda_
