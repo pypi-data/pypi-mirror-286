fcb3705451dbe1ceba3894957344d108c4c7fda0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 16:05:09 +0000

    Add package middleware-apm/1.0.0/middleware_apm-1.0.0-py3-none-any.whl

---
+++ packages/middleware-apm/middleware_apm-1.0.0-py3-none-any.whl/middleware/config.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import configparser
+
+
+class Config:
+    def __init__(self):
+        if len(sys.argv) > 1 and sys.argv[1] == "help":
+            return
+
+        config_file = os.environ.get("MIDDLEWARE_CONFIG_FILE", os.path.join(os.getcwd(), 'middleware.ini'))
+        if not os.path.exists(config_file):
+            exit_with_error(f"File not found: {config_file}")
+
+        self.config = configparser.ConfigParser()
+        try:
+            self.config.read(config_file)
+        except configparser.Error as e:
+            exit_with_error(f"Error reading configuration file: {e}")
+
+        pid = os.getpid()
+        self.project_name = self.get_config("middleware.common", "project_name", None)
+        self.service_name = self.get_config("middleware.common", "service_name", f"Service-{pid}")
+        self.access_token = self.get_config("middleware.common", "access_token", "")
+        self.collect_traces = self.get_config_boolean("middleware.common", "collect_traces", True)
+        self.collect_metrics = self.get_config_boolean("middleware.common", "collect_metrics", False)
+        self.collect_logs = self.get_config_boolean("middleware.common", "collect_logs", False)
+        self.collect_profiling = self.get_config_boolean("middleware.common", "collect_profiling", False)
+        self.otel_propagators = self.get_config("middleware.common", "otel_propagators", "b3")
+
+        project_name_attr = f"project.name={self.project_name}," if self.project_name else ""
+        source_service_url = self.get_config("middleware.common", "mw_agent_service", "localhost")
+
+        mw_agent_service = os.environ.get("MW_AGENT_SERVICE", None)
+        if mw_agent_service is not None and mw_agent_service != "":
+            source_service_url = mw_agent_service
+
+        self.exporter_otlp_endpoint = f"http://{source_service_url}:9319"
+        self.resource_attributes = f"{project_name_attr}mw.app.lang=python,runtime.metrics.python=true"
+        
+        # Allowing users to override full OTLP endpoint 
+        # Priority OTEL_EXPORTER_OTLP_ENDPOINT > MW_TARGET
+        mw_target = os.environ.get("MW_TARGET", None)
+        if mw_target is not None and mw_target != "":
+            self.exporter_otlp_endpoint = mw_target
+            
+        exporter_otlp_endpoint = os.environ.get("OTEL_EXPORTER_OTLP_ENDPOINT", None)
+        if exporter_otlp_endpoint is not None and exporter_otlp_endpoint != "":
+            self.exporter_otlp_endpoint = exporter_otlp_endpoint
+        
+        # Allowing users to pass Middleware API Key via ENV variable
+        mw_api_key = os.environ.get("MW_API_KEY", None)
+        if mw_api_key is not None and mw_api_key != "":
+            self.access_token = mw_api_key
+        
+        # Passing Middleware API Key as a resource attribute, to validate ingestion requests in serverless setup
+        if self.access_token is not None and self.access_token != "":
+            self.resource_attributes = f"{self.resource_attributes},mw.account_key={self.access_token}"
+
+    def get_config(self, section, key, default):
+        return self.config.get(section, key, fallback=default)
+
+    def get_config_boolean(self, section, key, default):
+        return self.config.getboolean(section, key, fallback=default)
+
+
+def exit_with_error(message):
+    print(message)
+    sys.exit(1)
+
+
+config = Config()
