4c3c992a4f77ae68436983ac30c13cebf50b1bcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:11 +0000

    Add package impy-array/2.4.3/impy_array-2.4.3-py3-none-any.whl

---
+++ packages/impy-array/impy_array-2.4.3-py3-none-any.whl/impy/arrays/__init__.py
@@ -0,0 +1,17 @@
+from .imgarray import ImgArray
+from .labeledarray import LabeledArray
+from .label import Label
+from .phasearray import PhaseArray
+from .specials import PropArray
+from .lazy import LazyImgArray
+from .big import BigImgArray
+
+__all__ = [
+    "ImgArray",
+    "LabeledArray",
+    "Label",
+    "PhaseArray",
+    "PropArray",
+    "LazyImgArray",
+    "BigImgArray",
+]
