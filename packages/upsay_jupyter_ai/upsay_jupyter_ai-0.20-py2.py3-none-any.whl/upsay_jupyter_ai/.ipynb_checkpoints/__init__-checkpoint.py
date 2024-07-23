b1d5a154cf6691d5e9bd1fc5eb46b707eac9ed77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:24 +0000

    Add package upsay_jupyter_ai/0.20/upsay_jupyter_ai-0.20-py2.py3-none-any.whl

---
+++ packages/upsay_jupyter_ai/upsay_jupyter_ai-0.20-py2.py3-none-any.whl/upsay_jupyter_ai/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+"""Testing the deployment of UPSayAI, a Jupyter-integrated Generative AI toolkit for Université Paris-Saclay students."""
+
+__version__ = "0.20"
+
+from .config import UPSayAI
+
+__all__ = ["UPSayAI"]
