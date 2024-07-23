63ae4f76f9a42c0bce20d86eb467d60c0dd6755b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:04:10 +0000

    Add package mw-python-sdk/0.0.6/mw_python_sdk-0.0.6-py3-none-any.whl

---
+++ packages/mw-python-sdk/mw_python_sdk-0.0.6-py3-none-any.whl/mw_python_sdk/common.py
@@ -0,0 +1,10 @@
+
+"""
+This module contains common variables and functions for the ModelWhale SDK for Python.
+
+Attributes:
+    BASE_URL (str): The base URL for the ModelWhale API.
+"""
+
+#Base URL for the ModelWhale api.
+BASE_URL = "https://www.heywhale.com"
