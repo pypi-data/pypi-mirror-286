c0bda36dcfd52697735e60911ae5de97a2fffa48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 11:03:47 +0000

    Add package blar-graph/1.0.6/blar_graph-1.0.6-py3-none-any.whl

---
+++ packages/blar-graph/blar_graph-1.0.6-py3-none-any.whl/blar_graph/run.py
@@ -0,0 +1,17 @@
+import traceback
+import uuid
+
+from blar_graph.db_managers import Neo4jManager
+from blar_graph.graph_construction.core.graph_builder import GraphConstructor
+
+repoId = str(uuid.uuid4())
+graph_manager = Neo4jManager(repoId, "test1")
+
+try:
+    graph_constructor = GraphConstructor(graph_manager)
+    graph_constructor.build_graph("src")
+    graph_manager.close()
+except Exception as e:
+    print(e)
+    print(traceback.format_exc())
+    graph_manager.close()
