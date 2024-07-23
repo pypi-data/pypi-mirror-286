e2292b489f3711e124c5d6be40d46c850b90fd73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:10 +0000

    Add package hairydogm/0.4.1/hairydogm-0.4.1.tar.gz

---
+++ packages/hairydogm/hairydogm-0.4.1.tar.gz/hairydogm-0.4.1/src/hairydogm/i18n/__init__.py
@@ -0,0 +1,15 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2017-2023 Alex Root Junior <https://github.com/JrooTJunior>
+# Copyright (c) 2023-present Hitalo M. <https://github.com/HitaloM>
+
+from hairydogm.i18n.context import get_i18n, gettext, lazy_gettext, lazy_ngettext, ngettext
+from hairydogm.i18n.core import I18n
+
+__all__ = (
+    "I18n",
+    "get_i18n",
+    "gettext",
+    "lazy_gettext",
+    "lazy_ngettext",
+    "ngettext",
+)
