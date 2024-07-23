3940f87e4e3b868c89c8f84e02ea3a5917252b00
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:03:49 +0000

    Add package FJ-STT/0.1/FJ_STT-0.1.tar.gz

---
+++ packages/FJ-STT/FJ_STT-0.1.tar.gz/FJ_STT-0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='FJ_STT',
+    version='0.1',
+    author='Fardeen Shaikh',
+    author_email='fs5544100@gamil.com',
+    description='This is speech to text package created by Fardeen Shaikh',
+)
+# packages = find_packages(),
+install_requirements = [
+    'selenium',
+    'webdriver_manager'
+]
