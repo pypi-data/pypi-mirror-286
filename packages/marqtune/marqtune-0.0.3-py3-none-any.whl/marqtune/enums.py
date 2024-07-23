1544d04242d4709e0b77a412d83173c6cf0f5974
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:11 +0000

    Add package marqtune/0.0.3/marqtune-0.0.3-py3-none-any.whl

---
+++ packages/marqtune/marqtune-0.0.3-py3-none-any.whl/marqtune/enums.py
@@ -0,0 +1,35 @@
+from enum import Enum
+
+
+class ModelType(str, Enum):
+    """
+    Enum for model type.
+    """
+    OPEN_CLIP = "open_clip"
+    MARQTUNED = "marqtuned"
+
+
+class DatasetType(str, Enum):
+    """
+    Enum for dataset type.
+    """
+    TRAINING = "training"
+    EVALUATION = "evaluation"
+
+
+class InstanceType(str, Enum):
+    """
+    Enum for instance type.
+    """
+    BASIC = "marqtune.basic"
+    PERFORMANCE = "marqtune.performance"
+
+
+class DataSchemaTypes(str, Enum):
+    """
+    Enum for data schema types.
+    """
+    TEXT = "text"
+    IMAGE_POINTER = "image_pointer"
+    SCORE = "score"
+    DOC_ID = "doc_id"
