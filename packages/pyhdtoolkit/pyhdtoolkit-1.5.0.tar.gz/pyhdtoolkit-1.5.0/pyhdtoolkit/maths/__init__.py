1819e32973471598688a52a48142a815178a7973
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:53 +0000

    Add package pyhdtoolkit/1.5.0/pyhdtoolkit-1.5.0.tar.gz

---
+++ packages/pyhdtoolkit/pyhdtoolkit-1.5.0.tar.gz/pyhdtoolkit-1.5.0/pyhdtoolkit/maths/__init__.py
@@ -0,0 +1,3 @@
+from . import stats_fitting, utils
+
+__all__ = [stats_fitting, utils]
---
+++ packages/pyhdtoolkit/pyhdtoolkit-1.5.0.tar.gz/pyhdtoolkit-1.5.0/pyhdtoolkit/utils/contexts.py
@@ -0,0 +1,42 @@
+"""
+.. _utils-contexts:
+
+Context Utilities
+-----------------
+
+Provides useful contexts to use functions in.
+"""
+import time
+
+from contextlib import contextmanager
+from typing import Callable, Iterator
+
+
+@contextmanager
+def timeit(function: Callable) -> Iterator[None]:
+    """
+    .. versionadded:: 0.4.0
+
+    Returns the time elapsed when executing code in the context via *function*.
+    Original code from is from :user:`Jaime Coello de Portugal <jaimecp89>`.
+
+    Args:
+        function (Callable): any callable taking one argument. This was conceived
+            with a `lambda` in mind.
+
+    Returns:
+        The elapsed time as an argument for the provided function.
+
+    Example:
+        .. code-block:: python
+
+            with timeit(lambda spanned: logger.debug(f"Did some stuff in {spanned} seconds")):
+                some_stuff()
+                some_other_stuff()
+    """
+    start_time = time.time()
+    try:
+        yield
+    finally:
+        time_used = time.time() - start_time
+        function(time_used)
