e5d12d0444ae0c44ac4d64117e28c2b73484d9ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:06:27 +0000

    Add package reolink-aio/0.9.5/reolink_aio-0.9.5-py3-none-any.whl

---
+++ packages/reolink-aio/reolink_aio-0.9.5-py3-none-any.whl/reolink_aio/enums.py
@@ -0,0 +1,98 @@
+""" Enums for reolink features """
+
+from enum import Enum
+
+
+class SubType(str, Enum):
+    """Subscription type"""
+
+    push = "push"
+    long_poll = "long_poll"
+    all = "all"
+
+    def __repr__(self) -> str:
+        return self.value
+
+    def __str__(self) -> str:
+        return self.value
+
+
+class VodRequestType(Enum):
+    """VOD url request types"""
+
+    RTMP = "RTMP"
+    PLAYBACK = "playback"
+    FLV = "FLV"
+
+
+class SpotlightModeEnum(Enum):
+    """Options for the spotlight mode"""
+
+    off = 0
+    auto = 1
+    onatnight = 2
+    schedule = 3
+    adaptive = 5
+    autoadaptive = 4
+
+
+class StatusLedEnum(Enum):
+    """Options for the status led mode"""
+
+    stayoff = "KeepOff"
+    auto = "Off"
+    alwaysonatnight = "On"
+    alwayson = "Always"
+
+
+class DayNightEnum(Enum):
+    """Options for the DayNight setting"""
+
+    auto = "Auto"
+    color = "Color"
+    blackwhite = "Black&White"
+
+
+class HDREnum(Enum):
+    """Options for the HDR setting"""
+
+    off = 0
+    on = 2
+    auto = 1
+
+
+class PtzEnum(Enum):
+    """Options for PTZ control"""
+
+    stop = "Stop"
+    left = "Left"
+    right = "Right"
+    up = "Up"
+    down = "Down"
+    zoomin = "ZoomInc"
+    zoomout = "ZoomDec"
+
+
+class GuardEnum(Enum):
+    """Options for PTZ Guard"""
+
+    set = "setPos"
+    goto = "toPos"
+
+
+class TrackMethodEnum(Enum):
+    """Options for AI Track Method"""
+
+    # off = 0
+    # pantilt = 1
+    digital = 2
+    digitalfirst = 3
+    pantiltfirst = 4
+
+
+class BatteryEnum(Enum):
+    """Battery status"""
+
+    discharging = 0
+    charging = 1
+    chargecomplete = 2
