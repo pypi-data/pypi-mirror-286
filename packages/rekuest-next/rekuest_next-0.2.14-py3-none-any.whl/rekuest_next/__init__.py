c908bd389ec3fdef7f5eb674054ce654bc6447c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:39 +0000

    Add package rekuest-next/0.2.14/rekuest_next-0.2.14-py3-none-any.whl

---
+++ packages/rekuest-next/rekuest_next-0.2.14-py3-none-any.whl/rekuest_next/__init__.py
@@ -0,0 +1,22 @@
+__version__ = "0.1.1"
+
+
+
+
+from .utils import acall, afind, areserve, reserved, call, aiterate, iterate
+
+
+
+__all__ = [
+    "acall",
+    "afind",
+    "areserve",
+    "reserved",
+    "call",
+    "find",
+    "reserve",
+    "structur_reg",
+    "imported",
+    "iterate",
+    "aiterate",
+]
