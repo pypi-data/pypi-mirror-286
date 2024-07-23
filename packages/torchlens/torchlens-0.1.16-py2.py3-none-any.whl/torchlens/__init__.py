ee5c1a18076087f5d7afb4ffc90851f93661945e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:04:28 +0000

    Add package torchlens/0.1.16/torchlens-0.1.16-py2.py3-none-any.whl

---
+++ packages/torchlens/torchlens-0.1.16-py2.py3-none-any.whl/torchlens/__init__.py
@@ -0,0 +1,10 @@
+""" Top level package: make the user-facing functions top-level, rest accessed as submodules.
+"""
+from torchlens.user_funcs import (
+    log_forward_pass,
+    show_model_graph,
+    validate_saved_activations,
+    validate_batch_of_models_and_inputs,
+)
+
+from torchlens.model_history import ModelHistory, TensorLogEntry
