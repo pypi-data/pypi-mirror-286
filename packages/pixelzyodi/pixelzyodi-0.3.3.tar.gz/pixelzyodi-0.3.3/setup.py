a55a90969110fcfc6494e057c8a797cc705a6c2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:33 +0000

    Add package pixelzyodi/0.3.3/pixelzyodi-0.3.3.tar.gz

---
+++ packages/pixelzyodi/pixelzyodi-0.3.3.tar.gz/pixelzyodi-0.3.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="pixelzyodi",
+    version="0.3.3",
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
