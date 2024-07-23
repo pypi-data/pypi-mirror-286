2c728e14aa5d8703fb19c7bcd2c7bbcfa1901870
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:03 +0000

    Add package streamlit-imp-custom-model-history/2.0.0/streamlit-imp-custom-model-history-2.0.0.tar.gz

---
+++ packages/streamlit-imp-custom-model-history/streamlit-imp-custom-model-history-2.0.0.tar.gz/streamlit-imp-custom-model-history-2.0.0/setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-imp-custom-model-history",
+    version="2.0.0",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        "streamlit >= 1.36.0",
+    ],
+    extras_require={}
+)
