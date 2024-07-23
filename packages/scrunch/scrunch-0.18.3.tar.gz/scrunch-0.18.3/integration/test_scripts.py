5cb691d0b1ecc7d1c3be5d0dfb54c24d67a8baeb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:37 +0000

    Add package scrunch/0.18.3/scrunch-0.18.3.tar.gz

---
+++ packages/scrunch/scrunch-0.18.3.tar.gz/scrunch-0.18.3/integration/test_scripts.py
@@ -0,0 +1,101 @@
+# coding: utf-8
+import pytest
+
+from pycrunch.shoji import as_entity
+
+from scrunch.scripts import ScriptExecutionError
+from scrunch.mutable_dataset import get_mutable_dataset
+from fixtures import BaseIntegrationTestCase
+
+
+class TestScripts(BaseIntegrationTestCase):
+    def _create_ds(self):
+        ds = self.site.datasets.create(as_entity({"name": "test_script"})).refresh()
+        variable = ds.variables.create(
+            as_entity(
+                {
+                    "name": "pk",
+                    "alias": "pk",
+                    "type": "numeric",
+                }
+            )
+        )
+        return ds, variable
+
+    def test_execute(self):
+        ds, variable = self._create_ds()
+        scrunch_dataset = get_mutable_dataset(ds.body.id, self.site, editor=True)
+        script = """
+        RENAME pk TO varA;
+
+        CHANGE TITLE IN varA WITH "Variable A";
+        """
+        scrunch_dataset.scripts.execute(script)
+        variable.refresh()
+        assert variable.body["alias"] == "varA"
+        assert variable.body["name"] == "Variable A"
+        ds.delete()
+
+    def test_handle_error(self):
+        ds, variable = self._create_ds()
+        scrunch_dataset = get_mutable_dataset(ds.body.id, self.site)
+        script = """BAD-RENAME pk TO varA;"""  # Bad syntax script
+        with pytest.raises(ScriptExecutionError) as err:
+            scrunch_dataset.scripts.execute(script)
+
+        variable.refresh()
+        assert variable.body["alias"] == "pk"  # Unchanged
+        resolutions = err.value.resolutions[0]
+        assert resolutions["column"] == 1
+        assert resolutions["line"] == 1
+        assert resolutions["command"] == 1
+        assert resolutions["message"] == "Invalid command: BAD"
+
+        # Script big enough to trigger async validation
+        async_script = ["""BAD-RENAME pk TO varA;"""] * 50000
+        async_script = "\n".join(async_script)
+        assert len(async_script) > 2 ** 20  # This is the threshold for async
+        with pytest.raises(ScriptExecutionError) as err:
+            scrunch_dataset.scripts.execute(async_script)
+        assert len(err.value.resolutions) == 50000  # All lines raised error
+        resolutions = err.value.resolutions[0]
+        assert resolutions["column"] == 1
+        assert resolutions["line"] == 1
+        assert resolutions["command"] == 1
+        assert resolutions["message"] == "Invalid command: BAD"
+        ds.delete()
+
+    def test_revert_script(self):
+        ds, variable = self._create_ds()
+        scrunch_dataset = get_mutable_dataset(ds.body.id, self.site)
+        s1 = "RENAME pk TO varA;"
+        s2 = 'CHANGE TITLE IN varA WITH "Variable A";'
+
+        scrunch_dataset.scripts.execute(s1)
+        scrunch_dataset.scripts.execute(s2)
+
+        scrunch_dataset.scripts.revert_to(script_number=0)  # Reverted
+        variable.refresh()
+        assert variable.body["name"] == "pk"
+        ds.delete()
+
+    def test_fetch_all_and_collapse(self):
+        raise self.skipTest("Collapse is 504ing in the server.")
+        ds, variable = self._create_ds()
+        scrunch_dataset = get_mutable_dataset(ds.body.id, self.site)
+        s1 = "RENAME pk TO varA;"
+        s2 = 'CHANGE TITLE IN varA WITH "Variable A";'
+
+        scrunch_dataset.scripts.execute(s1)
+        scrunch_dataset.scripts.execute(s2)
+
+        r = scrunch_dataset.scripts.all()
+        assert len(r) == 2
+        assert r[0].body["body"] == s1
+        assert r[1].body["body"] == s2
+
+        scrunch_dataset.scripts.collapse()
+
+        r = scrunch_dataset.scripts.all()
+        assert len(r) == 1
+        ds.delete()
---
+++ packages/scrunch/scrunch-0.18.3.tar.gz/scrunch-0.18.3/scrunch/scripts.py
@@ -0,0 +1,104 @@
+# coding: utf-8
+
+import json
+import pycrunch
+from pycrunch.shoji import TaskError
+
+
+class ScriptExecutionError(Exception):
+    def __init__(self, client_error, resolutions):
+        self.client_error = client_error
+        self.resolutions = resolutions
+
+    def __repr__(self):
+        return json.dumps(self.resolutions, indent=2)
+
+
+DEFAULT_SUBVARIABLE_SYNTAX = False
+
+
+class DatasetScripts:
+    def __init__(self, dataset_resource):
+        """
+        :param dataset_resource: Pycrunch Entity for the dataset.
+        """
+        self.dataset_resource = dataset_resource
+
+    def get_default_syntax_flag(self, strict_subvariable_syntax):
+        """
+        The Syntax flag tells scripts whether to be strict about
+        evaluating subvariable codes with the array[subvar] square bracket
+        syntax (when True) or to allow referring to subvariables by their
+        global code only  (when False).
+
+        If the user did provide a flag (not None), honor it and send that
+        in the request.
+
+        IFF the user did NOT provide a flag (is None), then see if the
+        session's feature flags know something that's been provided from the
+        server, and if nothing's there use the constant's default (currently
+        False).
+
+        :param strict_subvariable_syntax: User provided flag
+        :return: bool
+        """
+        if strict_subvariable_syntax is not None:
+            return strict_subvariable_syntax
+        flags = self.dataset_resource.session.feature_flags
+        return flags.get("clients_strict_subvariable_syntax", DEFAULT_SUBVARIABLE_SYNTAX)
+
+    def execute(self, script_body, strict_subvariable_syntax=None, dry_run=False):
+        strict_subvariable_syntax = self.get_default_syntax_flag(strict_subvariable_syntax)
+        payload = {
+            "body": script_body,
+            "strict_subvariable_syntax": strict_subvariable_syntax
+        }
+        method = self.dataset_resource.scripts.create
+        if dry_run:
+            payload["dry_run"] = True
+            method = self.dataset_resource.scripts.post
+        try:
+            method({'element': 'shoji:entity', 'body': payload})
+        except pycrunch.ClientError as err:
+            if isinstance(err, TaskError):
+                # For async script validation error
+                resolutions = err.message["resolutions"]
+                raise ScriptExecutionError(err, resolutions)
+            elif err.status_code == 400:
+                # For sync validation
+                resolutions = err.args[2]["resolutions"]
+                raise ScriptExecutionError(err, resolutions)
+            raise err  # 404 or something else
+
+    def dry_run(self, script_body, strict_subvariable_syntax=None):
+        self.execute(script_body, strict_subvariable_syntax, dry_run=True)
+
+    def collapse(self):
+        """
+        When a dataset has too many scripts. Collapse will concatenate
+        all the previously executed scripts into one the first. It will delete
+        all savepoints associated with the collapsed scripts.
+        """
+        self.dataset_resource.scripts.collapse.post({})
+
+    def all(self):
+        scripts_index = self.dataset_resource.scripts.index
+        scripts = []
+        for s_url, s in scripts_index.items():
+            scripts.append(s.entity)
+        scripts = sorted(scripts, key=lambda s: s.body["creation_time"])
+        return scripts
+
+    def revert_to(self, id=None, script_number=None):
+        all_scripts = self.all()
+        if script_number is not None:
+            script = all_scripts[script_number]
+        elif id is not None:
+            # We have to do this because currently the API does not expose the
+            # script ID directly.
+            script = [s for s in all_scripts if "scripts/{}/".format(id) in s.self][0]
+        else:
+            raise ValueError("Must indicate either ID or script number")
+
+        resp = script.revert.post({})  # Asynchronous request
+        pycrunch.shoji.wait_progress(resp, self.dataset_resource.session)
