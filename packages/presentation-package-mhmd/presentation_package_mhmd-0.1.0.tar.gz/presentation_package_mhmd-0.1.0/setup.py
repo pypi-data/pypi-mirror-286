11512e7d125dcc9a0aaa92d170252e9c3c547c7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:24 +0000

    Add package presentation-package-mhmd/0.1.0/presentation_package_mhmd-0.1.0.tar.gz

---
+++ packages/presentation-package-mhmd/presentation_package_mhmd-0.1.0.tar.gz/presentation_package_mhmd-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+name='presentation_package_mhmd',
+version='0.1.0',
+author='Your Name',
+author_email='mahmoudsegni1997@gmail.com',
+description='This a package for demo purposes',
+packages=find_packages(),
+classifiers=[
+'Programming Language :: Python :: 3',
+'License :: OSI Approved :: MIT License',
+'Operating System :: OS Independent',
+],
+python_requires='>=3.6',
+)
