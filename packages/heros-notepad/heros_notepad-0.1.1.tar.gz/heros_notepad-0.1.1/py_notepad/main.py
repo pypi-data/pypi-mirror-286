7f3cabfe4756849d0e22de4c599924df44a38384
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:03:30 +0000

    Add package heros-notepad/0.1.1/heros_notepad-0.1.1.tar.gz

---
+++ packages/heros-notepad/heros_notepad-0.1.1.tar.gz/heros_notepad-0.1.1/py_notepad/main.py
@@ -0,0 +1,16 @@
+# py_notepad/main.py
+
+import sys
+from PyQt5.QtWidgets import QApplication
+from py_notepad.py_notepad_widget import NotepadMainWindow
+
+
+def main():
+    app = QApplication(sys.argv)
+    main_window = NotepadMainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
