e899ed1194f42a5284f06bc379b323fcb7352532
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:34 +0000

    Add package zhaohua/1.0.3/zhaohua-1.0.3.tar.gz

---
+++ packages/zhaohua/zhaohua-1.0.3.tar.gz/zhaohua-1.0.3/README.md
@@ -0,0 +1,25 @@
+# zhaohua
+
+`zhaohua` 是一个自用的Python库。主要为一些自定义的函数，方便个人快速调用。
+
+## 功能
+
+- 快速获取文件夹和子文件夹的全部文件名。
+- 随机生成数字、字母、中文姓名。
+- 快速的Excel表格操作函数。
+- 功能会持续更新。
+
+
+## 安装
+pip install zhaohua
+
+
+## 卸载
+pip uninstall zhaohua
+
+
+## 版权信息
+
+版权所有 © 2024 zhaohua. 保留所有权利。
+
+此项目基于 [MIT License](LICENSE) 许可证发布。有关详细信息，请参阅许可证文件。
---
+++ packages/zhaohua/zhaohua-1.0.3.tar.gz/zhaohua-1.0.3/zhaohua/__init__.py
@@ -0,0 +1,13 @@
+from .dir import get_dir
+from .dir import get_file_path
+from .dir import get_all_file_path
+from .dir import get_save_file_path
+
+from .excel import save_to_excel
+
+from .rand import rand_str_list
+from .rand import rand_numbers_list
+from .rand import rand_float_list
+from .rand import rand_person_info_list
+
+from .for_time import get_time
