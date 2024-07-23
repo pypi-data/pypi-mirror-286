437ab4fdad32d33029af2f7967cca9983f984649
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:04:03 +0000

    Add package YtDuration/0.3/ytduration-0.3.tar.gz

---
+++ packages/YtDuration/ytduration-0.3.tar.gz/ytduration-0.3/setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='YtDuration',
+    version='0.3',
+    description='A Python package to get the duration of a YouTube video without using the YouTube Data API.',
+    author='Ranjit',
+    author_email='ranjitmaity95@gmail.com',
+    maintainers=[
+        'h.h_vats@yahoo.com'],
+    url='https://github.com/RanjitM007/YtDuration',  # Replace with your GitHub repository URL
+    packages=find_packages(),
+    install_requires=[
+        'requests',
+        'beautifulsoup4'
+    ],
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
