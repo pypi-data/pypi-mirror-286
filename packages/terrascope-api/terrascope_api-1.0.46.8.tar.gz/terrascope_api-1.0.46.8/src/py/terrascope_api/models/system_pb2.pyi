84e6b1e7676289cf141a2da23633482593b13b08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:03:51 +0000

    Add package terrascope-api/1.0.46.8/terrascope_api-1.0.46.8.tar.gz

---
+++ packages/terrascope-api/terrascope_api-1.0.46.8.tar.gz/terrascope_api-1.0.46.8/src/py/terrascope_api/models/system_pb2.pyi
@@ -0,0 +1,16 @@
+from google.api import annotations_pb2 as _annotations_pb2
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Optional as _Optional
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class PingRequest(_message.Message):
+    __slots__ = ()
+    def __init__(self) -> None: ...
+
+class PingResponse(_message.Message):
+    __slots__ = ("status_code",)
+    STATUS_CODE_FIELD_NUMBER: _ClassVar[int]
+    status_code: int
+    def __init__(self, status_code: _Optional[int] = ...) -> None: ...
