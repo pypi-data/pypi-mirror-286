640488754f58295c07ac464a474f689ff8775550
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:54 +0000

    Add package pyvelop/2024.7.1/pyvelop-2024.7.1-py3-none-any.whl

---
+++ packages/pyvelop/pyvelop-2024.7.1-py3-none-any.whl/pyvelop/const.py
@@ -0,0 +1,10 @@
+"""Constants for the pyvelop module."""
+
+# region #-- generic details --#
+_PACKAGE_NAME: str = "pyvelop"
+_PACKAGE_AUTHOR: str = "uvjim"
+_PACKAGE_VERSION: str = "2024.7.1"
+# endregion
+
+DEF_REDACT: str = "**REDACTED**"
+DEF_EMPTY_NAME: str = "Network Device"
