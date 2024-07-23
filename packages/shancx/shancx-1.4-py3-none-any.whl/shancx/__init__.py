109e4a84fbf75f918beee4120e61361d0943a4ec
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:41 +0000

    Add package shancx/1.4/shancx-1.4-py3-none-any.whl

---
+++ packages/shancx/shancx-1.4-py3-none-any.whl/shancx/__init__.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+# @Time : 2023/09/27 下午8:52
+# @Author : shanchangxi
+# @File : util_log.py
+import time
+import logging  # 引入logging模块
+from logging import handlers
+# from util.util_dir import *
+
+# 第一步，创建一个logger
+loggers = logging.getLogger()
+loggers.setLevel(logging.INFO)  # Log等级开关
+
+# 第二步，创建一个handler，用于写入日志文件
+log_name =  'project_tim_tor.log'
+logfile = log_name
+
+time_rotating_file_handler = handlers.TimedRotatingFileHandler(filename=logfile, when='D', encoding='utf-8')
+time_rotating_file_handler.setLevel(logging.INFO)  # 输出到file的log等级的开关
+
+# 第三步，定义handler的输出格式
+formatter = logging.Formatter("%(asctime)s - %(filename)s[line:%(lineno)d] - %(levelname)s: %(message)s")
+time_rotating_file_handler.setFormatter(formatter)
+
+# 第四步，将handler添加到logger里面
+loggers.addHandler(time_rotating_file_handler)
+
+# 如果需要同時需要在終端上輸出，定義一個streamHandler
+print_handler = logging.StreamHandler()  # 往屏幕上输出
+print_handler.setFormatter(formatter)  # 设置屏幕上显示的格式
+loggers.addHandler(print_handler)
+
+
+
+
+def tim_tor(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        print(f"{func.__name__} took {end_time - start_time:.4f} seconds")
+        loggers.info(f"{func.__name__} took {end_time - start_time:.4f} seconds")
+        return result
+    return wrapper
