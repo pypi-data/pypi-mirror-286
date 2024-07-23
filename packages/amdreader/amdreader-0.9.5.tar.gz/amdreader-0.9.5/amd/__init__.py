9e58b0e0343bd6a8f11cfb0d4577593e83b8164f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:03:34 +0000

    Add package amdreader/0.9.5/amdreader-0.9.5.tar.gz

---
+++ packages/amdreader/amdreader-0.9.5.tar.gz/amdreader-0.9.5/amd/__init__.py
@@ -0,0 +1,9 @@
+"""A terminal-based Markdown document viewer, written in Textual."""
+
+__author__ = "Textualize, Inc"
+__copyright__ = "Copyright Textualize, Inc"
+__credits__ = ["Dave Pearson", "Eivind Teig"]
+__maintainer__ = "Eivind Teig"
+__email__ = "eivind.teig@gmail.com"
+__version__ = "0.9.5"
+__licence__ = "MIT"
