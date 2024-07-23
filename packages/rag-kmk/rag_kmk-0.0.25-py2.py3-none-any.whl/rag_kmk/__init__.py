5fd0f267a3a000703e08815011fe18584cc324e4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:03:31 +0000

    Add package rag-kmk/0.0.25/rag_kmk-0.0.25-py2.py3-none-any.whl

---
+++ packages/rag-kmk/rag_kmk-0.0.25-py2.py3-none-any.whl/rag_kmk/__init__.py
@@ -0,0 +1,35 @@
+"""Top-level package for rag-kmk."""
+
+__author__ = """Murat Karakaya"""
+__email__ = "kmkarakaya@gmail.com"
+__version__ = "0.0.25"
+
+
+from .utils.config import load_config
+#from .knowledge_base import build_knowledge_base
+#from .vector_db import build_vector_db
+#from .chat_flow import build_rag_llm
+
+# Define the initialize_rag function
+def initialize_rag(custom_config_path=None):
+    """
+        This module initialization ensures that rag-kmk is properly set up upon import.
+        Initialize the RAG system with either the default or a custom config.
+    """
+    if custom_config_path:
+        CONFIG= load_config(custom_config_path)
+        return CONFIG
+    else:
+        CONFIG= load_config()
+        return CONFIG
+
+# Load the configuration when the module is imported
+try:
+    CONFIG = initialize_rag()
+    print(f"RAG-KMK initialized with config")
+except Exception as e:
+    print(f"Error initializing rag-kmk module: {e}")
+
+
+
+__all__ = ['build_knowledge_base', 'build_vector_db', 'build_rag_llm', 'initialize_rag', 'CONFIG']
---
+++ packages/rag-kmk/rag_kmk-0.0.25-py2.py3-none-any.whl/rag_kmk/vector_db/__init__.py
@@ -0,0 +1,5 @@
+from .database import create_chroma_client
+
+
+
+__all__ = ['create_chroma_client']
