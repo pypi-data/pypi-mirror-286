eff80d60629753b2a8bf95e9c1c57f09f3c89146
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:19 +0000

    Add package jobflow/0.1.18/jobflow-0.1.18-py3-none-any.whl

---
+++ packages/jobflow/jobflow-0.1.18-py3-none-any.whl/jobflow/utils/log.py
@@ -0,0 +1,29 @@
+"""Tools for logging."""
+
+import logging
+
+
+def initialize_logger(level: int = logging.INFO) -> logging.Logger:
+    """Initialize the default logger.
+
+    Parameters
+    ----------
+    level
+        The log level.
+
+    Returns
+    -------
+    Logger
+        A logging instance with customized formatter and handlers.
+    """
+    import sys
+
+    log = logging.getLogger("jobflow")
+    log.setLevel(level)
+    log.handlers = []  # reset logging handlers if they already exist
+
+    fmt = logging.Formatter("%(asctime)s %(levelname)s %(message)s")
+    screen_handler = logging.StreamHandler(stream=sys.stdout)
+    screen_handler.setFormatter(fmt)
+    log.addHandler(screen_handler)
+    return log
