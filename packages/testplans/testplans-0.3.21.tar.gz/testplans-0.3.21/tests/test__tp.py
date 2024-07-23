850d94563d48c2a6f3d4b8aec44863487f2a613c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:03:58 +0000

    Add package testplans/0.3.21/testplans-0.3.21.tar.gz

---
+++ packages/testplans/testplans-0.3.21.tar.gz/testplans-0.3.21/tests/test__tp.py
@@ -0,0 +1,55 @@
+import os
+import time
+
+import pytest
+import pathlib
+import shutil
+from typing import *
+
+from testplans import *
+from bus_user import *
+
+
+# =====================================================================================================================
+class Test__Tp:
+    @classmethod
+    def setup_class(cls):
+        pass
+
+        class Victim(TpMultyDutBase):
+            pass
+
+        cls.Victim = Victim
+
+    # @classmethod
+    # def teardown_class(cls):
+    #     pass
+    #
+    # def setup_method(self, method):
+    #     passtest__tc.py
+    #
+    # def teardown_method(self, method):
+    #     pass
+
+    # -----------------------------------------------------------------------------------------------------------------
+    @pytest.mark.skip
+    def test__1(self):
+        victim = self.Victim()
+        assert not victim.DEVICES__BREEDER_CLS
+        assert not victim.DEVICES__BY_INDEX
+        assert not victim.INDEX
+        assert not victim.TCS__INST
+
+        assert not victim.result
+        assert not victim.details
+        assert not victim.exx
+        assert victim.progress == 0
+
+        # TODO: FINISH!
+        # TODO: FINISH!
+        # TODO: FINISH!
+        # TODO: FINISH!
+        # TODO: FINISH!
+
+
+# =====================================================================================================================
