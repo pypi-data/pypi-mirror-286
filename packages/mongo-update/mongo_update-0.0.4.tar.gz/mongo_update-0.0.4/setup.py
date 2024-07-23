fde1e8526648d48b9fa18734338107cd9d5e60f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:09 +0000

    Add package mongo-update/0.0.4/mongo_update-0.0.4.tar.gz

---
+++ packages/mongo-update/mongo_update-0.0.4.tar.gz/mongo_update-0.0.4/setup.py
@@ -0,0 +1,9 @@
+from distutils.core import setup
+# import setuptools
+
+packages = ['mongo_update']
+setup(name='mongo_update',
+      version='0.0.4',
+      author='xigua, ',
+      packages=packages,
+      package_dir={'requests': 'requests'}, )
