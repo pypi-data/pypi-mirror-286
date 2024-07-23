057e042a75c1fbbf1c38ecfef481c13c0172722e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 16:04:39 +0000

    Add package joker-clients/0.4.1/joker-clients-0.4.1.tar.gz

---
+++ packages/joker-clients/joker-clients-0.4.1.tar.gz/joker-clients-0.4.1/joker/clients/__init__.py
@@ -0,0 +1,10 @@
+__version__ = "0.4.1"
+
+from joker.clients.cas import ContentAddressedStorageClient
+from joker.clients.monolog import MonologInterface
+from joker.clients.printable import PrintableClient
+
+CASClient = ContentAddressedStorageClient
+
+if __name__ == "__main__":
+    print(__version__)
