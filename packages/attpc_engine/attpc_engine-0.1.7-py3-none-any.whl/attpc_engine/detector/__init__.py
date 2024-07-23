07c90946f44465c114984b7c073d369ea82e27a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:03:34 +0000

    Add package attpc_engine/0.1.7/attpc_engine-0.1.7-py3-none-any.whl

---
+++ packages/attpc_engine/attpc_engine-0.1.7-py3-none-any.whl/attpc_engine/detector/__init__.py
@@ -0,0 +1,11 @@
+"""This is the parent module for all detector effects"""
+
+from attpc_engine.detector.simulator import run_simulation
+from attpc_engine.detector.parameters import (
+    DetectorParams,
+    ElectronicsParams,
+    PadParams,
+    Config,
+)
+
+from attpc_engine.detector.writer import SpyralWriter, SimulationWriter
