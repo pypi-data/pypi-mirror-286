3e4e1df2454c6d3044390b462bd16798977ea5ab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:52 +0000

    Add package hsapi-client/0.9.4/hsapi_client-0.9.4.tar.gz

---
+++ packages/hsapi-client/hsapi_client-0.9.4.tar.gz/hsapi_client-0.9.4/hsapi_client/routes.py
@@ -0,0 +1,27 @@
+from typing import Optional, List
+from pydantic import BaseModel, Field
+from .model import HSAPICall
+from .schemas import v1Route
+
+
+class v1ListRoutesResponse(BaseModel):
+    routes: Optional[List[Optional[v1Route]]] = Field(
+        alias="routes", default=None)
+
+
+class Route(HSAPICall):
+
+    objectPath = "routes"
+
+    def list(self) -> v1ListRoutesResponse:
+        response = self.call('get')
+        return v1ListRoutesResponse(**response.json())
+
+    def delete(self, routeId: str) -> None:
+        self.call('delete', call_path=routeId)
+
+    def enable(self, routeId: int) -> None:
+        self.call('post', f'{routeId}/enable')
+
+    def disable(self, routeId: int) -> None:
+        self.call('post', f'{routeId}/disable')
