d6b99b59cea4bfafda49370eb742e00ead7a2666
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:15 +0000

    Add package tchdlmodel/0.3.0/tchdlmodel-0.3.0.tar.gz

---
+++ packages/tchdlmodel/tchdlmodel-0.3.0.tar.gz/tchdlmodel-0.3.0/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='tchdlmodel',
+    version='0.3.0',  # Incremented version
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow>=2.0.0',
+    ],
+    author='Subhodeep Moitra, Deblina Banerjee, Dr. Pintu Pal',
+    author_email='subhodeep2000@gmail.com, banerjeedeblina07@gmail.com, dr.ppal.aec@gmail.com',
+    description='A custom model for tabular data using attention and feature transformer blocks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/subhodeepmoitra/CheemsNet-one-layer',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
