546809ad3ed8576c4bfe38a455bb3f07cee53951
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:20 +0000

    Add package FileMa/0.1.2/filema-0.1.2.tar.gz

---
+++ packages/FileMa/filema-0.1.2.tar.gz/filema-0.1.2/FileMan/__init__.py
@@ -0,0 +1,42 @@
+# File_Man/__init__.py
+
+# Import functions from File_Man
+from .FileMan import (
+    touch,
+    cat,
+    nano,
+    rm,
+    mkdir,
+    rmdir,
+    ren,
+    mv,
+    cd,
+    tree,
+    pwd,
+    ls,
+    ls_l,
+    istime,
+    size,
+    get_info,
+    get_path
+)
+
+__all__ = [
+    "touch",
+    "cat",
+    "nano",
+    "rm",
+    "mkdir",
+    "rmdir",
+    "ren",
+    "mv",
+    "cd",
+    "tree",
+    "pwd",
+    "ls",
+    "ls_l",
+    "istime",
+    "size",
+    "get_info",
+    "get_path"
+]
