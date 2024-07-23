f7399a99661c3bf1869d38c15d370edc8fe0941b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 11:04:23 +0000

    Add package authutilsvpetrov/0.1.7/authutilsvpetrov-0.1.7.tar.gz

---
+++ packages/authutilsvpetrov/authutilsvpetrov-0.1.7.tar.gz/authutilsvpetrov-0.1.7/setup.py
@@ -0,0 +1,28 @@
+"""
+Setup file for auth_utils package
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name="authutilsvpetrov",
+    version="0.1.7",
+    author="Vladimir Petrov",
+    author_email="",
+    description="description",
+    long_description="longdescription",
+    long_description_content_type="text/markdown",
+    url="",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        "fastapi",
+        "pydantic",
+        "typing",
+        "userutilsvpetrov"
+    ],
+)
