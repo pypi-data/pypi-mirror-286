25d37dc28ce08458b6071e720919dc0f57a7eb98
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:13 +0000

    Add package nonebot-plugin-genshin-cos/0.3.2/nonebot_plugin_genshin_cos-0.3.2-py3-none-any.whl

---
+++ packages/nonebot-plugin-genshin-cos/nonebot_plugin_genshin_cos-0.3.2-py3-none-any.whl/nonebot_plugin_genshin_cos/config.py
@@ -0,0 +1,44 @@
+from nonebot import get_driver
+from nonebot.log import logger
+
+"""
+Config: 配置类
+    - 更新: 解决掉pydantic v2的用户自定义配置类的问题
+"""
+class Config():
+    def __init__(self) -> None:
+        driver_config = get_driver().config
+        try:
+            self.cos_max = driver_config.cos_max
+        except:
+            logger.warning("未找到用户自定义配置 cos_max，使用默认配置")
+            self.cos_max = 10
+
+        try:
+            self.cos_path = driver_config.cos_path
+        except :
+            logger.warning("未找到用户自定义配置 cos_path，使用默认配置")
+            self.cos_path = ""
+
+        try:
+            self.cos_cd = driver_config.cos_cd
+        except:
+            logger.warning("未找到用户自定义配置 cos_cd，使用默认配置")
+            self.cos_cd = 10
+
+        try:
+            self.cos_forward_msg = driver_config.cos_forward_msg
+        except:
+            logger.warning("未找到用户自定义配置 cos_forward_msg，使用默认配置")
+            self.cos_forward_msg = False
+
+        try:
+            self.cos_delay = driver_config.cos_delay
+        except:
+            logger.warning("未找到用户自定义配置 cos_delay，使用默认配置")
+            self.cos_delay = 0.5
+
+'''
+实例化配置类
+'''
+config = Config()
