7f52061b231a4b8e6695ff45c3b91db873f638e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:21 +0000

    Add package SuckSCrypt/1.3/suckscrypt-1.3.tar.gz

---
+++ packages/SuckSCrypt/suckscrypt-1.3.tar.gz/suckscrypt-1.3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='SuckSCrypt',
+    version='1.3',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'pycryptodome'
+    ],
+    description='A Fastest & Secure Encryption System.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='KyzuneX',
+    author_email='kyzunex@gmail.com',
+    url='https://github.com/IT-Club-SMKN-21-Jakarta/SuckSCrypt',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
