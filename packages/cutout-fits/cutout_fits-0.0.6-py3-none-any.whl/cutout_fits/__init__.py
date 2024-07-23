c4c0b05114db2652e72bc79a9cfd7c447d20be57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:38 +0000

    Add package cutout-fits/0.0.6/cutout_fits-0.0.6-py3-none-any.whl

---
+++ packages/cutout-fits/cutout_fits-0.0.6-py3-none-any.whl/cutout_fits/__init__.py
@@ -0,0 +1,11 @@
+"""
+Copyright (c) 2024 Alec Thomson. All rights reserved.
+
+cutout-fits: A package to produce cutouts of (remote) FITS files.
+"""
+
+from __future__ import annotations
+
+from ._version import version as __version__
+
+__all__ = ["__version__"]
