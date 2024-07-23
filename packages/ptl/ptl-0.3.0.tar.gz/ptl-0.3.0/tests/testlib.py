c7b414e46a10311174490fb655459a7cf54443b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:58 +0000

    Add package ptl/0.3.0/ptl-0.3.0.tar.gz

---
+++ packages/ptl/ptl-0.3.0.tar.gz/ptl-0.3.0/tests/testlib.py
@@ -0,0 +1,35 @@
+import textwrap
+from pathlib import Path
+from typing import Optional, Union
+
+import pytest
+
+
+def dedent(text: str, *, strip: bool = True) -> str:
+    if strip:
+        text = text.lstrip('\n')
+    return textwrap.dedent(text)
+
+
+class InFileTestSuiteBase:
+    input_dir: Path
+
+    @pytest.fixture(autouse=True)
+    def base_setup(self, input_dir: Path) -> None:
+        self.input_dir = input_dir
+
+    dedent = staticmethod(dedent)
+
+    def create_file(
+        self, name_or_path: Union[Path, str], content: Optional[str] = None,
+    ) -> Path:
+        if isinstance(name_or_path, Path):
+            infile = name_or_path
+        else:
+            assert Path(name_or_path).name == name_or_path
+            infile = self.input_dir / name_or_path
+        if content:
+            infile.write_text(self.dedent(content))
+        else:
+            infile.touch()
+        return infile
