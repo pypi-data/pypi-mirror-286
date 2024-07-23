b49a3f959c255e23e8128e24f51a237d9c7130e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:48 +0000

    Add package commonroad-prediction/0.1.1/commonroad_prediction-0.1.1-py3-none-any.whl

---
+++ packages/commonroad-prediction/commonroad_prediction-0.1.1-py3-none-any.whl/crpred/advanced_models/mobil_predictor.py
@@ -0,0 +1,21 @@
+from crpred.advanced_models.agent_predictor import AgentPredictor
+from crpred.advanced_models.mobil_agent import MOBILAgent
+from crpred.utility.config import PredictorParams
+
+
+class MOBILPredictor(AgentPredictor):
+    def __init__(self, config: PredictorParams = PredictorParams()):
+        super(MOBILPredictor, self).__init__(config=config)
+        self.num_steps_prediction = config.planning.time_steps_computation
+
+        MOBILAgent.cls_scenario_current = self.scenario
+        for obs in self.scenario.dynamic_obstacles:
+            self.list_agents.append(MOBILAgent(obs))
+
+    def predict(self):
+        for time_step in range(1, self.num_steps_prediction + 1):
+            for agent in self.list_agents:
+                agent.step_forward(time_step)
+
+            self._assign_agents_to_lanelets(time_step)
+        return self.scenario
---
+++ packages/commonroad-prediction/commonroad_prediction-0.1.1-py3-none-any.whl/crpred/utility/common.py
@@ -0,0 +1,36 @@
+import numpy as np
+from commonroad.scenario.lanelet import Lanelet, LaneletNetwork
+from commonroad.scenario.scenario import Scenario
+
+
+def clear_obstacle_trajectory(sc: Scenario, initial_time_step: int = 0):
+    """
+    Clears obstacle trajectories of scenario.
+
+    :param sc: CommonRoad scenario.
+    :param initial_time_step: Initial time step of obstacles.
+    """
+    # clear existing prediction trajectories and lanelet assignment
+    for obstacle in sc.dynamic_obstacles:
+        # clear dynamic obstacles on lanelets (except for the first step)
+        for time_step in range(initial_time_step + 1, len(obstacle.prediction.trajectory.state_list) + 1):
+            for lanelet in sc.lanelet_network.lanelets:
+                try:
+                    lanelet.dynamic_obstacles_on_lanelet.get(time_step).discard(obstacle.obstacle_id)
+
+                except AttributeError:
+                    lanelet.dynamic_obstacles_on_lanelet[time_step] = set()
+
+        obstacle.prediction = None
+
+
+def get_merged_laneletes_from_position(lanelet_network: LaneletNetwork, position: np.ndarray):
+    lanelet_id_list = lanelet_network.find_lanelet_by_position([position])
+    if not any(lanelet_id_list):
+        raise ValueError(f"Position {position} cannot be assigned to a lanelet.")
+
+    current_lanelet = lanelet_network.find_lanelet_by_id(lanelet_id_list[0][0])
+    merged_lanelets, merged_lanelets_id = Lanelet.all_lanelets_by_merging_successors_from_lanelet(
+        current_lanelet, lanelet_network, 300
+    )
+    return merged_lanelets, merged_lanelets_id
