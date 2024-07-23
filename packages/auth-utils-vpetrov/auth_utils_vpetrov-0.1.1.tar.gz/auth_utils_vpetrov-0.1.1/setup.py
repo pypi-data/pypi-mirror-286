9ffd1d00d0cd9082f9e96263fca1b6aa002d1015
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:20 +0000

    Add package auth-utils-vpetrov/0.1.1/auth_utils_vpetrov-0.1.1.tar.gz

---
+++ packages/auth-utils-vpetrov/auth_utils_vpetrov-0.1.1.tar.gz/auth_utils_vpetrov-0.1.1/setup.py
@@ -0,0 +1,17 @@
+"""
+Setup file for auth_utils package
+"""
+from setuptools import setup, find_packages
+
+setup(
+    name='auth_utils_vpetrov',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'fastapi',
+        'pydantic',
+        'user_utils',
+        'typing',
+        'user_utils_vpetrov'
+    ]
+)
