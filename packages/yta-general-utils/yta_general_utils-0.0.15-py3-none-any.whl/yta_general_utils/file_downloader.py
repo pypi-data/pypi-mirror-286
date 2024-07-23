b8872ae99cdab276c06c6f448f7d000151bfb310
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:09 +0000

    Add package yta-general-utils/0.0.15/yta_general_utils-0.0.15-py3-none-any.whl

---
+++ packages/yta-general-utils/yta_general_utils-0.0.15-py3-none-any.whl/yta_general_utils/file_downloader.py
@@ -0,0 +1,82 @@
+import requests
+
+def download_file(url: str, output_filename: str):
+    """
+    Receives a downloadable url as 'url' and downloads that file in
+    our system as 'output_filename'.
+    """
+    if not url:
+        return None
+    
+    if not output_filename:
+        return None
+    
+    r = requests.get(url)
+
+    with open(output_filename, 'wb') as outfile:
+        outfile.write(r.content)
+
+    return True
+
+def download_audio(url, output_filename):
+    """
+    Receives a downloadable url as 'url' and downloads that audio in
+    our system as 'output_filename'.
+    """
+    # TODO: Check formats and that it is a valid audio (and use Enums pls)
+    if not output_filename.endswith('.mp3') and not output_filename.endswith('.wav') and not output_filename.endswith('.m4a'):
+        output_filename = output_filename + '.mp3'
+
+    return download_file(url, output_filename)
+
+def download_image(url: str, output_filename: str):
+    """
+    Receives a downloadable url as 'url' and downloads that image in
+    our system as 'output_filename'.
+    """
+    if not url:
+        return None
+    
+    if not output_filename:
+        return None
+    
+    # TODO: Check formats and that it is a valid image (and use Enums pls)
+    if not output_filename.endswith('.jpg') and not output_filename.endswith('.png') and not output_filename.endswith('.jpeg') and not output_filename.endswith('.webp') and not output_filename.endswith('.bmp'):
+        output_filename = output_filename + '.jpg'
+
+    return download_file(url, output_filename)
+
+def download_image_2(url: str, output_filename: str):
+    """
+    Downloads an image from the provided 'url' if possible.
+
+    # TODO: I need to check this one and compare with the
+    # other method so maybe preserve only one (?)
+    """
+    if not url:
+        return None
+
+    if not output_filename:
+        return None
+    
+    import shutil
+    
+    res = requests.get(url, stream = True)
+
+    if res.status_code == 200:
+        with open(output_filename,'wb') as f:
+            shutil.copyfileobj(res.raw, f)
+        print('Image sucessfully Downloaded: ', output_filename)
+    else:
+        print('Image Couldn\'t be retrieved')
+
+def download_video(url, output_filename):
+    """
+    Receives a downloadable url as 'url' and downloads that video in
+    our system as 'output_filename'.
+    """
+    # TODO: Check formats and that it is a valid audio (and use Enums pls)
+    if not output_filename.endswith('.mp4') and not output_filename.endswith('.mov'):
+        output_filename = output_filename + '.mp4'
+
+    return download_file(url, output_filename)
