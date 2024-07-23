914fc5b33935e695b9ffa74e298cc9e2bf154854
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:15 +0000

    Add package rabbitizer/1.12.0/rabbitizer-1.12.0-pp310-pypy310_pp73-macosx_11_0_arm64.whl

---
+++ packages/rabbitizer/rabbitizer-1.12.0-pp310-pypy310_pp73-macosx_11_0_arm64.whl/rabbitizer/AccessType.pyi
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+# SPDX-FileCopyrightText: © 2022-2024 Decompollaborate
+# SPDX-License-Identifier: MIT
+
+# Automatically generated. DO NOT MODIFY
+
+from __future__ import annotations
+from .Enum import Enum
+class AccessType:
+    INVALID: Enum
+    BYTE: Enum
+    SHORT: Enum
+    WORD: Enum
+    DOUBLEWORD: Enum
+    FLOAT: Enum
+    DOUBLEFLOAT: Enum
+    MAX: Enum
