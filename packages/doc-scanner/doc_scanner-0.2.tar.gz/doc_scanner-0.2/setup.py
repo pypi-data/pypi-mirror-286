aea064b2a68c55f761c50a578459d83eb55812c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:06:26 +0000

    Add package doc-scanner/0.2/doc_scanner-0.2.tar.gz

---
+++ packages/doc-scanner/doc_scanner-0.2.tar.gz/doc_scanner-0.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="doc-scanner",
+    version="0.2",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    include_package_data=True,
+    description="A script to scan HTML documents for forbidden phrases stored in a CSV.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Marcin Walczak",
+    url="https://github.com/marcinwalczak2",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'beautifulsoup4',
+        'requests',
+        'pandas',
+    ],
+    entry_points={
+        'console_scripts': [
+            'docscanner=docscanner.docscanner:main',
+        ],
+    },
+    package_data={
+        'docscanner': ['data/Style Guide Phrases.csv'],
+    },
+)
