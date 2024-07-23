896c1d92835848ab0d17bf8f8c5b0c283a13d151
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:06:22 +0000

    Add package streamlit-imp-custom-model-history/1.0.1/streamlit_imp_custom_model_history-1.0.1.tar.gz

---
+++ packages/streamlit-imp-custom-model-history/streamlit_imp_custom_model_history-1.0.1.tar.gz/streamlit_imp_custom_model_history-1.0.1/setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+setuptools.setup(
+    name="streamlit-imp-custom-model-history",
+    version="1.0.1",
+    author="",
+    author_email="",
+    description="",
+    long_description="",
+    long_description_content_type="text/plain",
+    url="",
+    packages=setuptools.find_packages(),
+    include_package_data=True,
+    classifiers=[],
+    python_requires=">=3.8",
+    install_requires=[
+        # By definition, a Custom Component depends on Streamlit.
+        # If your component has other Python dependencies, list
+        # them here.
+        "streamlit >= 1.36.0",
+    ],
+    extras_require={}
+)
