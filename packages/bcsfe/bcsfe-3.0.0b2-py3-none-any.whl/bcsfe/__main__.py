68894a3e69ec1c64bb025addbcbfc1e0c474fefe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:03:56 +0000

    Add package bcsfe/3.0.0b2/bcsfe-3.0.0b2-py3-none-any.whl

---
+++ packages/bcsfe/bcsfe-3.0.0b2-py3-none-any.whl/bcsfe/__main__.py
@@ -0,0 +1,18 @@
+from __future__ import annotations
+import traceback
+
+from bcsfe import cli
+
+try:
+    cli.main.Main().main()
+except KeyboardInterrupt:
+    cli.main.Main.leave()
+except Exception as e:
+    tb = traceback.format_exc()
+    cli.color.ColoredText.localize("error", error=e, traceback=tb)
+    try:
+        cli.main.Main.exit_editor()
+    except Exception:
+        pass
+    except KeyboardInterrupt:
+        pass
