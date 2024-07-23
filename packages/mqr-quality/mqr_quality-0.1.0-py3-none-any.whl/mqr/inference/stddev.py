c4808ff0bf4ce49690e5567bba8b80c9fc27b9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:03:20 +0000

    Add package mqr-quality/0.1.0/mqr_quality-0.1.0-py3-none-any.whl

---
+++ packages/mqr-quality/mqr_quality-0.1.0-py3-none-any.whl/mqr/inference/stddev.py
@@ -0,0 +1,17 @@
+def solvepower_1sample():
+  pass
+
+def solvepower_2sample():
+  pass
+
+def confint_1sample():
+  pass
+
+def confint_2sample():
+  pass
+
+def hyptest_1sample():
+  pass
+
+def hyptest_2sample():
+  pass
