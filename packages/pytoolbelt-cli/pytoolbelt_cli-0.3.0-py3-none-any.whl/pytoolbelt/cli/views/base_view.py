0f0d8bc2e425eaab76b1a60f223aa4f317888353
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:04:03 +0000

    Add package pytoolbelt-cli/0.3.0/pytoolbelt_cli-0.3.0-py3-none-any.whl

---
+++ packages/pytoolbelt-cli/pytoolbelt_cli-0.3.0-py3-none-any.whl/pytoolbelt/cli/views/base_view.py
@@ -0,0 +1,18 @@
+from rich.console import Console
+from rich.table import Table
+
+
+class BaseTableView:
+    def __init__(self, title: str, headers: list[dict[str, str]]):
+        self.title = title
+        self.table = Table(title=title)
+
+        for header in headers:
+            self.table.add_column(**header)
+
+    def add_row(self, *row: str):
+        self.table.add_row(*row)
+
+    def print_table(self):
+        console = Console()
+        console.print(self.table)
