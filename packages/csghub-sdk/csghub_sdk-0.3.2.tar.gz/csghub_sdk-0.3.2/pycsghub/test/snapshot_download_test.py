d4af7e375968fe6f322cbef79af986db3c77b0f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:06 +0000

    Add package csghub-sdk/0.3.2/csghub_sdk-0.3.2.tar.gz

---
+++ packages/csghub-sdk/csghub_sdk-0.3.2.tar.gz/csghub_sdk-0.3.2/pycsghub/test/snapshot_download_test.py
@@ -0,0 +1,63 @@
+import unittest
+from pycsghub.snapshot_download import snapshot_download
+from pycsghub.file_download import file_download
+from pycsghub.errors import InvalidParameter
+
+
+class MyTestCase(unittest.TestCase):
+    def test_something(self):
+        self.assertEqual(True, False)  # add assertion here
+
+    def test_snapshot_download(self):
+        token = ("4e5b97a59c1f8a954954971bf1cdbf3ce61a35sd5")
+        endpoint = "https://hub.opencsg.com"
+        repo_id = 'OpenCSG/csg-wukong-1B'
+        cache_dir = '/home/test4'
+        result = snapshot_download(repo_id,
+                                   cache_dir=cache_dir,
+                                   endpoint=endpoint,
+                                   token=token)
+        print(result)
+
+    def test_singlefile_download(self):
+        token = ("f3a7b9c1d6e5f8e2a1b5d4f9e6a2b8d7c3a4e2b1d9f6e7a8d2c5a7b4c1e3f5b8a1d4f"
+                 "9b7d6e2f8a5d3b1e7f9c6a8b2d1e4f7d5b6e9f2a4b3c8e1d7f995hd82hf")
+        endpoint = "https://hub-stg.opencsg.com"
+        repo_id = 'wayne0019/lwfmodel'
+        cache_dir = '/home/test6'
+        result = file_download(repo_id,
+                               file_name="README.md",
+                               cache_dir=cache_dir,
+                               endpoint=endpoint,
+                               token=token)
+        print(result)
+
+    def test_singlefile_download_not_exist(self):
+        token = ("4e5b97a59c1f8a954954971bf1cdbf3ce61a35d5")
+        endpoint = "https://hub.opencsg.com"
+        repo_id = 'OpenCSG/csg-wukong-1B'
+        cache_dir = '/home/test5'
+        try:
+            file_download(repo_id,
+                          file_name="wolegequ.hehe",
+                          cache_dir=cache_dir,
+                          endpoint=endpoint,
+                          token=token)
+        except InvalidParameter as e:
+            self.assertEqual(str(e), "file wolegequ.hehe not in repo wayne0019/lwfmodel")
+
+    def test_snapshot_download(self):
+        token = ("4e5b97a59c1f8a954954971bf1cdbf3ce61a35sd5")
+        endpoint = "https://hub.opencsg.com"
+        repo_id = 'AIWizards/tmmluplus'
+        cache_dir = '~/Downloads/'
+        result = snapshot_download(repo_id,
+                                   repo_type="dataset",
+                                   cache_dir=cache_dir,
+                                   endpoint=endpoint,
+                                   token=token)
+        print(result)
+
+
+if __name__ == '__main__':
+    unittest.main()
