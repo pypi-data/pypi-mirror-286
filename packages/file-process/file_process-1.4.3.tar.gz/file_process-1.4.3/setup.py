49af2b5b0e25ed053d4aaa31487ecda6e2bbc330
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:03:46 +0000

    Add package file-process/1.4.3/file_process-1.4.3.tar.gz

---
+++ packages/file-process/file_process-1.4.3.tar.gz/file_process-1.4.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup
+import pathlib
+
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
+
+setup(name="file_process",
+      version="1.4.3",
+      description="A package that does file validation and file preview.",
+      long_description=README,
+      long_description_content_type="text/markdown",
+      license='MIT',
+      author="superbio.ai",
+      url='https://github.com/Superbio-ai/file-process',
+      install_requires=['pandas==1.2.5', 'anndata==0.8.0', 'numpy>=1.22'],
+      packages=['file_process', 'file_process.h5ad', 'file_process.csv', 'file_process.txt'])
