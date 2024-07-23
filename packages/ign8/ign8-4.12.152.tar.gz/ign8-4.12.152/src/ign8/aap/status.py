2fa1e36d70c274b9dc2f2036a5f1e1440c6937c6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:01 +0000

    Add package ign8/4.12.152/ign8-4.12.152.tar.gz

---
+++ packages/ign8/ign8-4.12.152.tar.gz/ign8-4.12.152/src/ign8/aap/status.py
@@ -0,0 +1,12 @@
+# main file for semaphore
+
+import os
+import sys
+import time
+import json
+import requests
+
+from  ..common import prettyllog
+
+def main():
+    prettyllog("status", "check", "login", "dsv", "0", "Testing", "ERROR")
