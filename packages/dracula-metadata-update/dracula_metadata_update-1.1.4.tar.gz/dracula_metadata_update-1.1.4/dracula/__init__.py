2662a96e747c504999f11ef0b4f3c737879a4010
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:48 +0000

    Add package dracula-metadata-update/1.1.4/dracula_metadata_update-1.1.4.tar.gz

---
+++ packages/dracula-metadata-update/dracula_metadata_update-1.1.4.tar.gz/dracula_metadata_update-1.1.4/dracula/__init__.py
@@ -0,0 +1,15 @@
+import sys
+from .dracula import main
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("Usage: python update_metadata.py <folder_path> [image_path]")
+        sys.exit(1)
+
+    folder_path = sys.argv[1]
+    image_path = sys.argv[2] if len(sys.argv) > 2 else None
+    album_name = "드라큘라 10주년 스튜디오 OST"
+
+    print(folder_path, image_path, album_name)
+
+    main(folder_path, album_name, image_path)
