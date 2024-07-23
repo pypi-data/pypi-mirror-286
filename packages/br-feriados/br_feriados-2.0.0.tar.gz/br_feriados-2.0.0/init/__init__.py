303da811009154182d9cfa4d3077a6ca3e58229b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 03:03:20 +0000

    Add package br-feriados/2.0.0/br_feriados-2.0.0.tar.gz

---
+++ packages/br-feriados/br_feriados-2.0.0.tar.gz/br_feriados-2.0.0/init/__init__.py
@@ -0,0 +1,3 @@
+__version__ = '1.0.1'
+
+from ..br_feriados.br_feriados.feriados_br import *
---
+++ packages/br-feriados/br_feriados-2.0.0.tar.gz/br_feriados-2.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as arq:
+    readme = arq.read()
+
+setup(name='br_feriados',
+    version='2.0.0',
+    license='MIT License',
+    author='Luiz Fernando Teles Rodrigues',
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    author_email='euluizfernando2001@gmail.com',
+    keywords='feriados br municipais brasil',
+    description=u'Versão beta da biblioteca de feriados municipais.',
+    packages=['init'],
+    install_requires=['datetime'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
