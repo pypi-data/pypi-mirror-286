4bb2d5b8d5e1d249cac84a59d02fad2fdc94ad0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:03 +0000

    Add package dclab/0.60.9/dclab-0.60.9-cp311-cp311-win_amd64.whl

---
+++ packages/dclab/dclab-0.60.9-cp311-cp311-win_amd64.whl/dclab/rtdc_dataset/fmt_hdf5/basin.py
@@ -0,0 +1,25 @@
+"""RT-DC hdf5 format"""
+from __future__ import annotations
+
+import pathlib
+
+from .. import feat_basin
+
+from .base import RTDC_HDF5
+
+
+class HDF5Basin(feat_basin.Basin):
+    basin_format = "hdf5"
+    basin_type = "file"
+
+    def _load_dataset(self, location, **kwargs):
+        return RTDC_HDF5(location, **kwargs)
+
+    def is_available(self):
+        with self._av_check_lock:
+            avail = False
+            try:
+                avail = pathlib.Path(self.location).exists()
+            except OSError:
+                pass
+        return avail
