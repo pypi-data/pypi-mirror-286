31e7bbb5276c8b2fc4a5fa6c60281d6c4a6cabea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:03:48 +0000

    Add package danila-lib/2.2.3/danila-lib-2.2.3.tar.gz

---
+++ packages/danila-lib/danila-lib-2.2.3.tar.gz/danila-lib-2.2.3/danila/danila_v11.py
@@ -0,0 +1,60 @@
+from danila.danila_detail import Danila_detail
+from danila.danila_detail_params import Danila_detail_params
+from data.result.Text_cut_recognize_result import Text_cut_recognize_result
+from data.result.Text_recognize_result import Text_recognize_result
+
+
+class Danila_v11:
+    def __init__(self, yolov5_dir, detail_classify_version = 1,
+                 rama_detect_version = 1, rama_classify_version = 1, rama_text_detect_version = 1, rama_text_recognize_version = 1,
+                 vagon_text_detect_version = 1, vagon_text_recognize_version = 1,
+                 balka_detect_version = 1, balka_classify_version = 1, balka_text_detect_version = 1, balka_text_recognize_version = 1):
+
+        self.danila_detail = Danila_detail(Danila_detail_params(1,2))
+
+    def detail_classify(self, img):
+        return self.danila_detail.detail_classify(img)
+
+    def detail_text_detect(self, img):
+        return img
+
+    def detail_text_recognize(self, img):
+        return self.danila_detail.detail_classify(img)
+
+    def rama_classify(self, img, detail = None):
+        """rama_classify(Img : openCv frame): String - returns class of rama using CNN network"""
+        """rama_classify uses Rama_classify_class method - classify(Img)"""
+        detail_prod = Text_cut_recognize_result('rama', 1)
+        det = Text_recognize_result(detail_prod)
+        return det
+
+    # returns openCV cut rama with drawn text areas
+    def rama_text_detect_cut(self, img, detail = None):
+        """returns openCV cut rama with drawn text areas"""
+        return img
+
+
+    # returns dict {'number', 'prod', 'year'} for openCV rama img or 'no_rama'
+    def rama_text_recognize(self, img):
+        """returns dict {'number', 'prod', 'year'} for openCV rama img or 'no_rama'"""
+        return self.rama_classify(img)
+
+    # returns openCV img with drawn number areas
+    def vagon_number_detect(self, img):
+        """returns openCV img with drawn number areas"""
+        return img
+
+    def vagon_number_recognize(self, img):
+        return Text_recognize_result(Text_cut_recognize_result('vagon',1.0))
+
+
+    def balka_classify(self, img):
+        detail_prod = Text_cut_recognize_result('balka', 1)
+        det = Text_recognize_result(detail_prod)
+        return det
+
+    def balka_text_detect(self, img):
+        return img
+
+    def balka_text_recognize(self, img):
+        return self.balka_classify(img)
---
+++ packages/danila-lib/danila-lib-2.2.3.tar.gz/danila-lib-2.2.3/data/neuro/models.py
@@ -0,0 +1,20 @@
+RAMA_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/4xtNt0zftHFFVg'
+RAMA_BEGICKAYA_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/sE5iSXYWL3xEkQ'
+RAMA_RUZHIMMASH_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/zAEw0_ALgtBg9Q'
+RAMA_RUZHIMMASH_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/rgthm-mMrbhOmw'
+RAMA_BEJICKAYA_TEXT_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/QhicLdm2-PT6oA'
+# TEXT_RECOGNIZE_RUZHIMMASH_MODEL_ADDRESS = 'https://disk.yandex.ru/d/C2hDMsbTZMnL8Q' -7500_b7500_r
+# TEXT_RECOGNIZE_BEGICKAYA_MODEL_ADDRESS = 'https://disk.yandex.ru/d/C2hDMsbTZMnL8Q'
+TEXT_RECOGNIZE_RUZHIMMASH_MODEL_ADDRESS = 'https://disk.yandex.ru/d/GcI-mAtwpb2I-g' # 11000_b_11000_r
+TEXT_RECOGNIZE_BEGICKAYA_MODEL_ADDRESS = 'https://disk.yandex.ru/d/GcI-mAtwpb2I-g'  # 11000_b_11000_r
+
+RAMA_PROD_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/8FI8TaTwaCfbSA'
+VAGON_NUMBER_DETECT_MODEL_ADDRESS = 'https://disk.yandex.ru/d/ZGS0sdk1syJmcQ'
+VAGON_NUMBER_DETECT_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/3artWauJULaWXw'
+DETAIL_CLASSIFY_MODEL_ADDRESS = 'https://disk.yandex.ru/d/Xamk_OYzYAbGhg'
+DETAIL_CLASSIFY_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/_t4r3N7a2Uy3cA'
+
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS = 'https://disk.yandex.ru/d/eh714_PrjFyLHg'
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS_2 = 'https://disk.yandex.ru/d/iZxGW6ZaXZDoQQ'
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS_3 = 'https://disk.yandex.ru/d/wd697DtHumhsjA'
+VAGON_NUMBER_RECOGNIZE_MODEL_ADDRESS_4 = 'https://disk.yandex.ru/d/4UqcWigCF5hCqw' 
