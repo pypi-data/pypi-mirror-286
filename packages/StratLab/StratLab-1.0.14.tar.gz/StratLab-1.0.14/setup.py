95ae1a764eb5fe96231c7251a8370ae0a8c9ebb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:06 +0000

    Add package StratLab/1.0.14/StratLab-1.0.14.tar.gz

---
+++ packages/StratLab/StratLab-1.0.14.tar.gz/StratLab-1.0.14/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='StratLab',
+    version='1.0.14',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+        'numpy',
+        'yfinance',
+        'matplotlib'
+    ],
+    author='Robert Stickles',
+    author_email='rstickles16@outlook.com',
+    description='StratLab is a Python library designed to backtest stock market strategies. The library currently uses the yfinance (Yahoo Finance) API as a means for extracting financial data, which is then manipulated utilizing Pandas dataframes and Numpy functions. There are also options to extract the backtested results directly into excel files.',
+    url='https://github.com/rstickles16/StratLab',
+)
