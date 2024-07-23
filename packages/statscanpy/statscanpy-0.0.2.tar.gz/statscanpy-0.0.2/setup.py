184e2169021c6bdd87e517573f3db2c2443be44f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:03:58 +0000

    Add package statscanpy/0.0.2/statscanpy-0.0.2.tar.gz

---
+++ packages/statscanpy/statscanpy-0.0.2.tar.gz/statscanpy-0.0.2/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name='statscanpy',
+    version='0.0.2',
+    description='A short description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown'
+)
