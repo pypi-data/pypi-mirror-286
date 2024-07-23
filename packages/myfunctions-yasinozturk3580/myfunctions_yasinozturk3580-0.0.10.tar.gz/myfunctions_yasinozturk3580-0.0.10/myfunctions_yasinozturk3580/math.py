82e98817f7f32e69aad47c63d06aa38e297e4287
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:03:33 +0000

    Add package myfunctions-yasinozturk3580/0.0.10/myfunctions_yasinozturk3580-0.0.10.tar.gz

---
+++ packages/myfunctions-yasinozturk3580/myfunctions_yasinozturk3580-0.0.10.tar.gz/myfunctions_yasinozturk3580-0.0.10/myfunctions_yasinozturk3580/math.py
@@ -0,0 +1,10 @@
+"""
+    This is how you use  this function
+"""
+
+def addition(NUMBER1,NUMBER2):
+    print(NUMBER1 + NUMBER2)
+
+
+    def multiplication(NUMBER1,NUMBER2):
+    print(NUMBER1 + NUMBER2)
