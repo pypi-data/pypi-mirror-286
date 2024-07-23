08b205bb5c5d63e267497ef1f8ed9857a82495a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:21 +0000

    Add package LibJciHitachi/1.6.0/libjcihitachi-1.6.0.tar.gz

---
+++ packages/LibJciHitachi/libjcihitachi-1.6.0.tar.gz/libjcihitachi-1.6.0/setup.py
@@ -0,0 +1,48 @@
+import setuptools
+
+from JciHitachi import __author__, __version__
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+install_requires = [
+    "awsiotsdk==1.20.0",
+    "httpx",
+    "paho-mqtt<=1.6.1",
+    "setuptools",
+]
+tests_require = [
+    "pre-commit",
+    "pytest>=6.2",
+    "pytest-cov",
+    "pytest-rerunfailures",
+]
+
+
+if __name__ == "__main__":
+    setuptools.setup(
+        name="LibJciHitachi",
+        version=__version__,
+        author=__author__,
+        author_email="qqaatw@gmail.com",
+        description="A library for controlling Jci Hitachi devices.",
+        long_description=long_description,
+        long_description_content_type="text/markdown",
+        url="https://github.com/qqaatw/LibJciHitachi",
+        project_urls={
+            "Issue Tracker": "https://github.com/qqaatw/LibJciHitachi/issues",
+            "Documentation": "https://libjcihitachi.readthedocs.io/en/latest/",
+        },
+        classifiers=[
+            "Programming Language :: Python :: 3.10",
+            "Programming Language :: Python :: 3.11",
+            "Programming Language :: Python :: 3.12",
+            "License :: OSI Approved :: Apache Software License",
+            "Operating System :: OS Independent",
+        ],
+        packages=setuptools.find_packages(include=["JciHitachi"]),
+        package_data={"JciHitachi": ["cert/*.pem"]},
+        python_requires=">=3.10",
+        install_requires=install_requires,
+        tests_require=tests_require,
+    )
