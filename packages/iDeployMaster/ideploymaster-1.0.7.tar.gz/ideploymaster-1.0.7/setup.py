4064c1905e42920fc9c48133b4e29503d5abc50a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:02:59 +0000

    Add package iDeployMaster/1.0.7/ideploymaster-1.0.7.tar.gz

---
+++ packages/iDeployMaster/ideploymaster-1.0.7.tar.gz/ideploymaster-1.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+setup(
+    name='iDeployMaster',
+    version='1.0.7',
+    packages=['iDeployMaster'],
+    entry_points={
+        'console_scripts': [
+            'iDeployMaster=iDeployMaster.main:initialize_ui',
+        ],
+    },
+    install_requires=[
+        'Pillow',
+        'requests',
+    ],
+    include_package_data=True,
+    package_data={
+        '': ['*.png'],
+    },
+    zip_safe=False,
+)
