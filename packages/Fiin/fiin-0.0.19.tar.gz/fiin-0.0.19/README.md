d5fe1e871be80daaa586766473fa895072819104
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:05:25 +0000

    Add package Fiin/0.0.19/fiin-0.0.19.tar.gz

---
+++ packages/Fiin/fiin-0.0.19.tar.gz/fiin-0.0.19/README.md
@@ -0,0 +1,10 @@
+# FiinQuant
+Python package for financial technical indicators.
+
+## Installation
+You can install the package via pip:
+
+```python
+pip install --upgrade Fiin
+```
+
---
+++ packages/Fiin/fiin-0.0.19.tar.gz/fiin-0.0.19/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="Fiin",
+    version="0.0.19",
+    packages=find_packages(),
+    description="A simple indicator library for stock tickers",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    author="NgocAn",
+    author_email="anlam9614@gmail.com",
+    install_requires=['requests', 'pandas', 'numpy', 'signalrcore'],
+    include_package_data=True,
+    package_data={
+        'Fiin': ['*.pyi']
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
