74c6ca48f15cf7a2bd90d0c04771f4af2303ebac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:03:42 +0000

    Add package qcanvas/1.0.3.post5/qcanvas-1.0.3.post5.tar.gz

---
+++ packages/qcanvas/qcanvas-1.0.3.post5.tar.gz/qcanvas-1.0.3.post5/qcanvas/run.py
@@ -0,0 +1,29 @@
+import logging
+from logging import DEBUG, INFO, WARNING
+
+import qcanvas.app_start
+from qcanvas.util import logs, paths
+
+paths.data_storage().mkdir(parents=True, exist_ok=True)
+
+logging.basicConfig(
+    filename=paths.data_storage() / "debug.log",
+    level="WARN",
+)
+
+logs.set_levels(
+    {
+        "qcanvas": INFO,
+        "qcanvas.ui": WARNING,
+        "qcanvas_backend": INFO,
+        "qcanvas.ui.main_ui.status_bar_progress_display": DEBUG,
+    }
+)
+
+
+def main():
+    qcanvas.app_start.launch()
+
+
+if __name__ == "__main__":
+    main()
