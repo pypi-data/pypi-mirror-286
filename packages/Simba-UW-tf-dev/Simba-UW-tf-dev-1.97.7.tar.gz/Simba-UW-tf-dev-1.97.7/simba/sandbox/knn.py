e6e1ba044005c04f4b58eee42f8963ff2e40c432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:17 +0000

    Add package Simba-UW-tf-dev/1.97.7/Simba-UW-tf-dev-1.97.7.tar.gz

---
+++ packages/Simba-UW-tf-dev/Simba-UW-tf-dev-1.97.7.tar.gz/Simba-UW-tf-dev-1.97.7/simba/sandbox/knn.py
@@ -0,0 +1,67 @@
+import numpy as np
+
+def knn(data: np.ndarray, k: int, bool_target: np.ndarray):
+    norm = np.full(data.shape, np.nan)
+    sum = np.full((data.shape[0], 1), np.nan)
+    results = {}
+    for i in range(data.shape[1]):
+        norm[:, i] = (data[:, i]-np.min(data[:, i]))/(np.max(data[:, i])-np.min(data[:, i]))
+    for i in range(norm.shape[0]):
+        sum[i] = np.sum(norm[i])
+    for i in np.unique(bool_target):
+        idx = np.argwhere(bool_target == i).flatten()
+        n_idx = np.argwhere(bool_target != i).flatten()
+        for j in idx:
+            results[j] = {}
+            for k in n_idx:
+                results[j][k] = np.abs(sum[j] - sum[k])[0]
+    for k, v in results.items():
+        keys = list(v.keys())
+        values = list(v.values())
+        sorted_value_index = np.argsort(values)
+        sorted_dict = [keys[i] for i in sorted_value_index[:k]]
+        print(sorted_dict)
+
+
+
+    #keys = list(dict.keys())
+    #values = list()
+
+
+
+
+    # for i in range(sum.shape[0]):
+    #     for j in range(i, sum.shape[0]):
+    #         dist = np.abs(sum[i] - sum[j])
+    #         dist_matrix[i, j] = dist
+    #         dist_matrix[j, i] = dist
+    # for i in range(dist_matrix.)
+
+
+       #x = np.linalg.norm()
+      # print(x)
+
+
+
+
+data = np.array([[1, 5],
+                 [2, 4],
+                 [3, 3],
+                 [4, 2],
+                 [0, 0]])
+
+bool_target = np.array([0, 0, 0, 1, 1])
+
+knn(data=data, k=4, bool_target=bool_target)
+
+
+
+
+
+
+
+
+
+
+
+
