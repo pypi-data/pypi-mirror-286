bf18c89e128bdc980f007839fdebe31b39c4c92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:04:48 +0000

    Add package telegrinder/0.1.dev169/telegrinder-0.1.dev169.tar.gz

---
+++ packages/telegrinder/telegrinder-0.1.dev169.tar.gz/telegrinder-0.1.dev169/telegrinder/bot/dispatch/__init__.py
@@ -0,0 +1,64 @@
+from .abc import ABCDispatch
+from .context import Context
+from .dispatch import ABCRule, Dispatch, TelegrinderCtx
+from .handler import ABCHandler, FuncHandler, MessageReplyHandler
+from .middleware import ABCMiddleware
+from .process import check_rule, process_inner
+from .return_manager import (
+    ABCReturnManager,
+    BaseReturnManager,
+    CallbackQueryReturnManager,
+    InlineQueryReturnManager,
+    Manager,
+    MessageReturnManager,
+    register_manager,
+)
+from .view import (
+    ABCStateView,
+    ABCView,
+    BaseStateView,
+    BaseView,
+    CallbackQueryView,
+    ChatJoinRequestView,
+    ChatMemberView,
+    InlineQueryView,
+    MessageView,
+    RawEventView,
+    ViewBox,
+)
+from .waiter_machine import ShortState, WaiterMachine, clear_wm_storage_worker
+
+__all__ = (
+    "ABCDispatch",
+    "ABCHandler",
+    "ABCMiddleware",
+    "ABCReturnManager",
+    "ABCRule",
+    "ABCStateView",
+    "ABCView",
+    "BaseReturnManager",
+    "BaseStateView",
+    "BaseView",
+    "CallbackQueryReturnManager",
+    "CallbackQueryView",
+    "ChatJoinRequestView",
+    "ChatMemberView",
+    "Context",
+    "Dispatch",
+    "FuncHandler",
+    "InlineQueryReturnManager",
+    "InlineQueryView",
+    "Manager",
+    "MessageReplyHandler",
+    "MessageReturnManager",
+    "MessageView",
+    "RawEventView",
+    "ShortState",
+    "TelegrinderCtx",
+    "ViewBox",
+    "WaiterMachine",
+    "check_rule",
+    "process_inner",
+    "register_manager",
+    "clear_wm_storage_worker",
+)
