84cea8d44f14297f8b37be2e97368bca060a8cae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:21 +0000

    Add package streamlit-imp-new-scenario/1.0.0/streamlit-imp-new-scenario-1.0.0.tar.gz

---
+++ packages/streamlit-imp-new-scenario/streamlit-imp-new-scenario-1.0.0.tar.gz/streamlit-imp-new-scenario-1.0.0/streamlit_imp_new_scenario/__init__.py
@@ -0,0 +1,25 @@
+import os
+import streamlit as st
+import streamlit.components.v1 as components
+
+# Create a _RELEASE constant. We'll set this to False while we're developing
+# the component, and True when we're ready to package and distribute it.
+_RELEASE = True
+if not _RELEASE:
+    _imp_new_scenario_component = components.declare_component(
+        "streamlit_imp_new_scenario",
+        url="http://localhost:3001",
+    )
+else:
+    parent_dir = os.path.dirname(os.path.abspath(__file__))
+    build_dir = os.path.join(parent_dir, "frontend/build")
+    _imp_new_scenario_component = components.declare_component("streamlit_imp_new_scenario", path=build_dir)
+
+def streamlit_imp_new_scenario_component(base_url):
+    component_value = _imp_new_scenario_component(base_url=base_url)
+    return component_value
+
+if not _RELEASE:
+    streamlit_imp_new_scenario_component(base_url="http://127.0.0.1:9000")
+else:
+    streamlit_imp_new_scenario_component(base_url="https://streamlit-imp-new-scenario.herokuapp.com")
