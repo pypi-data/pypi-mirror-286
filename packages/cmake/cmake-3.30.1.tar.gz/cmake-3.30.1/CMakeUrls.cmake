d9e039f092356b72f4be29688c295671a2099a3b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 05:03:34 +0000

    Add package cmake/3.30.1/cmake-3.30.1.tar.gz

---
+++ packages/cmake/cmake-3.30.1.tar.gz/cmake-3.30.1/CMakeUrls.cmake
@@ -0,0 +1,29 @@
+
+#-----------------------------------------------------------------------------
+# CMake sources
+set(unix_source_url          "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1.tar.gz")
+set(unix_source_sha256       "df9b3c53e3ce84c3c1b7c253e5ceff7d8d1f084ff0673d048f260e04ccb346e1")
+
+set(windows_source_url       "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1.zip")
+set(windows_source_sha256    "d096812e471ab7305c71182e17bf13dd595574f252083bc595341649e98b69ac")
+
+#-----------------------------------------------------------------------------
+# CMake binaries
+
+set(linux32_binary_url       "NA")  # Linux 32-bit binaries not available
+set(linux32_binary_sha256    "NA")
+
+set(linux64_binary_url       "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1-linux-x86_64.tar.gz")
+set(linux64_binary_sha256    "ac31f077ef3378641fa25a3cb980d21b2f083982d3149a8f2eb9154f2b53696b")
+
+set(macos10_10_binary_url    "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1-macos10.10-universal.tar.gz")
+set(macos10_10_binary_sha256 "4c1beda2bb2ab830f22fdb29aa6cc844f9b0eb32cca0fb4f01b407688944d181")
+
+set(win32_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1-windows-i386.zip")
+set(win32_binary_sha256      "f5fb1d93b82e9a5fbd5853d4b17a130605f0b4ed13a655d1371c2d6d55f9261d")
+
+set(win64_binary_url         "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1-windows-x86_64.zip")
+set(win64_binary_sha256      "cf7788ff9d92812da194847d4ec874fc576f34079987d0f20c96cd09e2a16220")
+
+set(winarm64_binary_url      "https://github.com/Kitware/CMake/releases/download/v3.30.1/cmake-3.30.1-windows-arm64.zip")
+set(winarm64_binary_sha256   "02b433f70aa549449be2d53046d0179590bf3b6290d9fda3fbbb23f96a4f2802")
