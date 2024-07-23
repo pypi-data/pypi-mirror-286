ce5e03cb0ac9b9957a111ce92ce746846dace458
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 04:06:27 +0000

    Add package nba-analytics/0.2.8/nba_analytics-0.2.8-py3-none-any.whl

---
+++ packages/nba-analytics/nba_analytics-0.2.8-py3-none-any.whl/nba_analytics/machine_learning/models/neuralnet.py
@@ -0,0 +1,29 @@
+import torch.nn as nn
+
+from ...utils.logger import get_logger
+
+
+# Create logger
+logger = get_logger(__name__)
+
+
+class CustomNN(nn.Module):
+    '''
+    Custom Neural Network class for the NBA player statistics dataset.
+    '''
+    def __init__(self, input_size, hidden_size, output_size):
+        super().__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.fc1 = nn.Linear(input_size, hidden_size)
+        self.relu = nn.ReLU()
+        self.fc2 = nn.Linear(hidden_size, output_size)
+        # self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        out = self.fc1(x)
+        out = self.relu(out)
+        out = self.fc2(out)
+        # out = self.sigmoid(out)
+        return out
