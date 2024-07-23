0a8056856519598a9a9353fce001534521fcbeb9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:03:49 +0000

    Add package cowgirl-ai/1.0.23/cowgirl_ai-1.0.23.tar.gz

---
+++ packages/cowgirl-ai/cowgirl_ai-1.0.23.tar.gz/cowgirl_ai-1.0.23/src/cowgirl_ai/client.py
@@ -0,0 +1,29 @@
+import os
+from openai import OpenAI
+
+
+class ConfigOpenAI:
+    """
+    Initializes environment and gets api key
+    """
+
+    def __init__(self):
+        self.open_ai_api_key = os.getenv("OPENAI_API_KEY")
+
+
+class Client(ConfigOpenAI):
+    """
+    Assistant Client
+    ----------------
+    Initializes client object for making API requests.
+
+    Passes Open AI API key to the API endpoint, initializing the client for use.
+    """
+
+    def __init__(self):
+        super().__init__()  # Parent ConfigOpenAI class contains the API key processing
+        self.client = OpenAI(api_key=self.open_ai_api_key)
+
+
+if __name__ == "__main__":
+    Client()
