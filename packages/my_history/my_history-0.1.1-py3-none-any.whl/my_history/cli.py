c3d8480e5107c640d6a87c31dfbe703735feaa7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:04:06 +0000

    Add package my_history/0.1.1/my_history-0.1.1-py3-none-any.whl

---
+++ packages/my_history/my_history-0.1.1-py3-none-any.whl/my_history/cli.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import sys
+
+def cnt():
+    df = pd.read_parquet("~/tmp/history.parquet")
+    fdf = df[df['cmd'].str.contains(int(sys.argv[1])]
+    cnt = fdf['cnt'].sum()
+    print(cnt)
