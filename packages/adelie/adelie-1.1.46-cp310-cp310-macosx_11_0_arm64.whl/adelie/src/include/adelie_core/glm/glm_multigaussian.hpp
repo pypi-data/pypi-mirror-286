bef81e4d4ffbee96e7ca2fa7d8508c8e35a62710
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:12 +0000

    Add package adelie/1.1.46/adelie-1.1.46-cp310-cp310-macosx_11_0_arm64.whl

---
+++ packages/adelie/adelie-1.1.46-cp310-cp310-macosx_11_0_arm64.whl/adelie/src/include/adelie_core/glm/glm_multigaussian.hpp
@@ -0,0 +1,64 @@
+#pragma once
+#include <adelie_core/glm/glm_multibase.hpp>
+
+namespace adelie_core {
+namespace glm {
+
+template <class ValueType>
+class GlmMultiGaussian: public GlmMultiBase<ValueType>
+{
+public:
+    using base_t = GlmMultiBase<ValueType>;
+    using typename base_t::value_t;
+    using typename base_t::vec_value_t;
+    using typename base_t::rowarr_value_t;
+    using base_t::y;
+    using base_t::weights;
+
+    explicit GlmMultiGaussian(
+        const Eigen::Ref<const rowarr_value_t>& y,
+        const Eigen::Ref<const vec_value_t>& weights
+    ):
+        base_t("multigaussian", y, weights)
+    {}
+
+    void gradient(
+        const Eigen::Ref<const rowarr_value_t>& eta,
+        Eigen::Ref<rowarr_value_t> grad
+    ) override
+    {
+        base_t::check_gradient(eta, grad);
+        grad = ((y-eta).colwise() * weights.matrix().transpose().array()) / eta.cols();
+    }
+
+    void hessian(
+        const Eigen::Ref<const rowarr_value_t>& eta,
+        const Eigen::Ref<const rowarr_value_t>& grad,
+        Eigen::Ref<rowarr_value_t> hess
+    ) override
+    {
+        base_t::check_hessian(eta, grad, hess);
+        hess.colwise() = weights.matrix().transpose().array() / hess.cols();
+    }
+
+    value_t loss(
+        const Eigen::Ref<const rowarr_value_t>& eta
+    ) override
+    {
+        base_t::check_loss(eta);
+        return (
+            weights.matrix().transpose().array() * 
+            (0.5 * eta.square() - y * eta).rowwise().sum()
+        ).sum() / y.cols();
+    }
+
+    value_t loss_full() override
+    {
+        return -0.5 * (
+            (y.square().colwise() * weights.matrix().transpose().array()).sum()
+        ) / y.cols();
+    }
+};
+
+} // namespace glm
+} // namespace adelie_core
