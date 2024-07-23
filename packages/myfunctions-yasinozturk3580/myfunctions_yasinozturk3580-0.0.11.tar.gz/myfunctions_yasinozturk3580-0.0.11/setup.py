40a46c6b18c82a56c2fb3319083ddc4068835ae9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 09:03:44 +0000

    Add package myfunctions-yasinozturk3580/0.0.11/myfunctions_yasinozturk3580-0.0.11.tar.gz

---
+++ packages/myfunctions-yasinozturk3580/myfunctions_yasinozturk3580-0.0.11.tar.gz/myfunctions_yasinozturk3580-0.0.11/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='myfunctions_yasinozturk3580',
+    version='0.0.11',
+    packages=find_packages(),
+    install_requires=[],
+    url='http://github.com/yasinozturk3580/',
+    license='MIT',
+    author='yasin ozturk',
+    author_email='yasinozturk3580@gmail.com',
+    description='A description of your package',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
