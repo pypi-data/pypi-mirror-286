35e1f54373ef12b4229ac9e39b7855b9c73e953e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 11:04:17 +0000

    Add package tjax/0.32.5/tjax-0.32.5.tar.gz

---
+++ packages/tjax/tjax-0.32.5.tar.gz/tjax-0.32.5/tjax/_src/tree_tools.py
@@ -0,0 +1,22 @@
+from __future__ import annotations
+
+from collections.abc import Callable
+from typing import Any, TypeVar
+
+T = TypeVar('T')
+
+
+def tree_map_with_path(structure: Any,
+                       transform: Callable[[T, tuple[str, ...]], T]
+                       ) -> Any:
+    def f(structure: Any,
+          transform: Callable[[T, tuple[str, ...]], T],
+          path: tuple[str, ...]
+          ) -> Any:
+        if isinstance(structure, dict):
+            out_structure = {}
+            for key, value in structure.items():
+                out_structure[key] = f(value, transform, (*path, key))
+            return out_structure
+        return transform(structure, path)
+    return f(structure, transform, ())
