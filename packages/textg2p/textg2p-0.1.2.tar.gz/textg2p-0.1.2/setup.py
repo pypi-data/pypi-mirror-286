dbdc36ecf478c970debcac9fc9d9efb5c0754ed9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 11:03:46 +0000

    Add package textg2p/0.1.2/textg2p-0.1.2.tar.gz

---
+++ packages/textg2p/textg2p-0.1.2.tar.gz/textg2p-0.1.2/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as readme_file:
+    readme = readme_file.read()
+
+setup(
+    name="textg2p",
+    version="0.1.2",
+    author="Ivan Shivalov",
+    author_email="ivansivalov396@gmail.com",
+    url="https://github.com/intexcor/textg2p",
+    description="A package to transform texts to ipa",
+    long_description=readme,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    install_requires=["eng-to-ipa", "epitran", "jamo", "g2pk"],
+    classifiers=[
+        "Programming Language :: Python",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    ],
+)
