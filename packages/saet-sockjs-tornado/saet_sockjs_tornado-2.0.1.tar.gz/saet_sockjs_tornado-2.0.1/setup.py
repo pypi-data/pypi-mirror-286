8d162e3c608723b5c94fa85c5e196adfc3d835cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:04:03 +0000

    Add package saet-sockjs-tornado/2.0.1/saet_sockjs_tornado-2.0.1.tar.gz

---
+++ packages/saet-sockjs-tornado/saet_sockjs_tornado-2.0.1.tar.gz/saet_sockjs_tornado-2.0.1/setup.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def desc():
+    info = read('README.rst')
+    try:
+        return info + '\n\n' + read('doc/changelog.rst')
+    except IOError:
+        return info
+
+setup(
+    name='saet-sockjs-tornado',
+    version='2.0.1',
+    author='Serge S. Koval',
+    author_email='serge.koval@gmail.com',
+    packages=['sockjs', 'sockjs.tornado', 'sockjs.tornado.transports'],
+    namespace_packages=['sockjs'],
+    scripts=[],
+    url='https://github.com/saet/sockjs-tornado',
+    license='MIT License',
+    description='SockJS python server implementation on top of Tornado framework',
+    long_description=desc(),
+    long_description_content_type='text/x-rst',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: Implementation :: CPython',
+    ],
+    requires=['tornado'],
+    install_requires=[
+        'tornado >= 6.0.0'
+    ]
+)
