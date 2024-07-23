19a88435461f908b56dee95b6d19713213f87abc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:12 +0000

    Add package simplyprint-ws-client/1.0.0rc24/simplyprint_ws_client-1.0.0rc24-py3-none-any.whl

---
+++ packages/simplyprint-ws-client/simplyprint_ws_client-1.0.0rc24-py3-none-any.whl/simplyprint_ws_client/client/lifetime/lifetime_manager.py
@@ -0,0 +1,132 @@
+import logging
+import traceback
+from enum import Enum
+from typing import Dict, TYPE_CHECKING
+
+from .lifetime import ClientLifetime, ClientAsyncLifetime
+from ..client import Client
+from ...utils import traceability
+from ...utils.stoppable import AsyncStoppable
+
+if TYPE_CHECKING:
+    from ..instance import Instance
+
+
+class LifetimeType(Enum):
+    ASYNC = 0
+
+    # SYNC = 1
+
+    def get_cls(self):
+        return {
+            LifetimeType.ASYNC: ClientAsyncLifetime,
+            # LifetimeType.SYNC: ClientSyncLifetime,
+        }[self]
+
+
+class LifetimeManager(AsyncStoppable):
+    logger: logging.Logger
+    lifetime_check_interval = 10
+
+    instance: 'Instance'
+    lifetimes: Dict[Client, ClientLifetime]
+
+    def __init__(self, instance: 'Instance', *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        self.logger = instance.logger.getChild("lifetime_manager")
+        self.instance = instance
+        self.lifetimes = {}
+
+    def contains(self, client: Client) -> bool:
+        return client in self.lifetimes
+
+    def get(self, client: Client) -> ClientLifetime:
+        return self.lifetimes.get(client)
+
+    def add(self, client: Client, lifetime_type: LifetimeType = LifetimeType.ASYNC) -> ClientLifetime:
+        if self.is_stopped():
+            raise RuntimeError("Lifetime manager is stopped - cannot add new lifetimes")
+
+        if self.contains(client):
+            return self.get(client)
+
+        lifetime = lifetime_type.get_cls()(self, client)
+        self.lifetimes[client] = lifetime
+        return lifetime
+
+    def should_consume(self, _client: Client) -> bool:
+        return self.instance.connection_is_ready.is_set()
+
+    async def loop(self) -> None:
+        self.logger.info("Starting lifetime manager loop")
+
+        # Start after a delay as this is just a manager
+        await self.wait(self.lifetime_check_interval)
+
+        while not self.is_stopped():
+            for client, lifetime in list(self.lifetimes.items()):
+                # TODO retry the lifetime in a bit...
+                if lifetime.is_stopped():
+                    continue
+
+                if not lifetime.is_healthy():
+                    client.logger.warning(f"Client lifetime unhealthy - restarting")
+                    await self.restart_lifetime(client)
+                    continue
+
+                if self.instance.connection.is_connected() and not client.connected:
+                    connected_trace = traceability.from_class(client).get("connected", None)
+
+                    client.logger.warning(
+                        f"Instance is connected but client has not received connected event yet. Last {len(connected_trace.call_record) if connected_trace else 0} traces:")
+
+                    if not connected_trace or not connected_trace.call_record:
+                        continue
+
+                    records = connected_trace.get_call_record()
+                    connected_trace.call_record.clear()
+
+                    for record in records:
+                        client.logger.warning(
+                            f"""[{record.called_at}] Called connected with args {record.args} retval {record.retval}
+{''.join(traceback.StackSummary.from_list(record.stack).format()) if record.stack else "No stack"}
+                            """)
+
+            await self.wait(self.lifetime_check_interval)
+
+        self.logger.info("Lifetime manager loop stopped - stopping all lifetimes")
+
+        for client, lifetime in list(self.lifetimes.items()):
+            await self.stop_lifetime(client)
+
+        self.logger.info("Lifetime manager loop stopped")
+
+    async def start_lifetime(self, client: Client) -> None:
+        if self.is_stopped():
+            raise RuntimeError("Lifetime manager is stopped - cannot start new lifetimes")
+
+        lifetime = self.get(client)
+
+        if not lifetime:
+            return
+
+        await lifetime.start()
+
+    async def stop_lifetime(self, client: Client) -> None:
+        lifetime = self.get(client)
+
+        if not lifetime:
+            return
+
+        lifetime.stop()
+
+    async def restart_lifetime(self, client: Client) -> None:
+        await self.stop_lifetime(client)
+        await self.start_lifetime(client)
+
+    def remove(self, client: Client) -> None:
+        lifetime = self.lifetimes.pop(client, None)
+
+        if lifetime is not None:
+            lifetime.stop()
