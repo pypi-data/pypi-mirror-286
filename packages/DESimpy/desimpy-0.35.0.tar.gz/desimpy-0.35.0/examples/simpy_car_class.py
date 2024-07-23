45463baf61132fae18c93ac3274dcbd93b936316
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 15:03:34 +0000

    Add package DESimpy/0.35.0/desimpy-0.35.0.tar.gz

---
+++ packages/DESimpy/desimpy-0.35.0.tar.gz/desimpy-0.35.0/examples/simpy_car_class.py
@@ -0,0 +1,42 @@
+from desimpy.des import Event, EventScheduler
+
+
+class Car:
+    def __init__(self, env: EventScheduler) -> None:
+        self.env = env
+        # Start the run process when an instance is created
+        self.schedule_run()
+
+    def schedule_run(self) -> None:
+        """Schedule the initial run event."""
+        start_time = self.env.current_time
+        self.env.schedule(Event(start_time, self.run))
+
+    def run(self) -> None:
+        """Handle the parking and charging, followed by driving."""
+        print(f"Start parking and charging at {self.env.current_time}")
+
+        charge_duration = 5
+        end_charge_time = self.env.current_time + charge_duration
+
+        # Define the action to be executed when charging ends
+        def charge_action() -> None:
+            print(f"Start driving at {self.env.current_time}")
+            trip_duration = 2
+            end_trip_time = self.env.current_time + trip_duration
+
+            # Schedule the next parking and charging event
+            self.env.schedule(Event(end_trip_time, self.run))
+
+        # Schedule the charge process
+        self.env.schedule(Event(end_charge_time, charge_action))
+
+
+# Initialize the event scheduler
+scheduler = EventScheduler()
+
+# Create a car instance
+Car(scheduler)
+
+# Run the simulation
+scheduler.run_until_max_time(15)
