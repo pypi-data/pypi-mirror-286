7cf3e56de53ff78c2f5da98b8868927c4ec9d652
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:23 +0000

    Add package vtat/0.0.3/vtat-0.0.3-py3-none-any.whl

---
+++ packages/vtat/vtat-0.0.3-py3-none-any.whl/vtat/__init__.py
@@ -0,0 +1,42 @@
+from .vtat import (
+    addGroup,
+    removeGroup,
+    addObject,
+    eraseObject,
+    userExists,
+    groupExists,
+    meet,
+    removeObject,
+    deleteObject,
+    replaceObject,
+    renameObject,
+    renameGroup,
+    listGroups,
+    listObjects,
+    listObjectsOfGroup,
+    getGroupsOfObject,
+    objectIsInAnyGroup,
+    getSubgroups
+)
+# __all__ 정의
+__all__ = [
+    'addGroup',
+    'removeGroup',
+    'addObject',
+    'eraseObject',
+    'userExists',
+    'groupExists',
+    'meet',
+    'removeObject',
+    'deleteObject',
+    'replaceObject',
+    'renameObject',
+    'renameGroup',
+    'listGroups',
+    'listObjects',
+    'listObjectsOfGroup',
+    'getGroupsOfObject',
+    'objectIsInAnyGroup',
+    'getSubgroups'
+]
+__version__ = '0.0.3'
