5b3ad64970ecb6ab3132f9e119cffa8702d33cff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:11 +0000

    Add package formatter-sql-script/1.1.0/formatter_sql_script-1.1.0.tar.gz

---
+++ packages/formatter-sql-script/formatter_sql_script-1.1.0.tar.gz/formatter_sql_script-1.1.0/setup.py
@@ -0,0 +1,18 @@
+from setuptools import find_packages, setup
+
+readme = open('./README.md', 'r')
+
+setup(
+    name='formatter_sql_script',
+    version='1.1.0',
+    description='Formateador de texto a sentencia de SQL (Insert y Update)',
+    long_description=readme.read(),
+    long_description_content_type='text/markdown',
+    author='Felipe Medel',
+    author_email='luispipemedel@gmail.com',
+    packages=find_packages(),
+    py_modules=['formatter_sql_script'],
+    keywords=['helper', 'sql Formatter', 'testing', 'dev'],
+    license='MIT',
+    include_package_data=True
+)
