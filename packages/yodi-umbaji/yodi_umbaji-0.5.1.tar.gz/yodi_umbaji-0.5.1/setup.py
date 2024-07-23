f9f94a8b38c6f3a03c192b8d8bb09a985e9759fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:21 +0000

    Add package yodi-umbaji/0.5.1/yodi_umbaji-0.5.1.tar.gz

---
+++ packages/yodi-umbaji/yodi_umbaji-0.5.1.tar.gz/yodi_umbaji-0.5.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name="yodi-umbaji",
+    version="0.5.1",
+    packages=find_packages(),
+    install_requires=[
+        'pathlib',
+        'tensorflow',
+        'matplotlib',
+        'numpy',
+        'seaborn',
+        'IPython',
+        'gradio',
+    ],
+
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
