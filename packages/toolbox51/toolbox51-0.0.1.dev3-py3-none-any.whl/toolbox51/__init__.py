2fe36a30011ad40c61d218112049f2f7b296af64
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 14:04:09 +0000

    Add package toolbox51/0.0.1.dev3/toolbox51-0.0.1.dev3-py3-none-any.whl

---
+++ packages/toolbox51/toolbox51-0.0.1.dev3-py3-none-any.whl/toolbox51/__init__.py
@@ -0,0 +1,22 @@
+r"""
+51's personal toolbox
+"""
+
+
+__version__ = "0.0.1dev3"
+
+__changelog__ = """\
+## update history
+- 0.0.1dev3
+    - logger: 从logging import了日志等级
+- 0.0.1dev2
+    - logger: 暂时移除了即开即用的logger
+- 0.0.1dev1
+    - logger: 修改了一些配置方式
+- 0.0.1dev
+    - (new)logger: 基于logging的个人用日志
+    - (new)const: 个人用常用常量
+        - (new)result: 常用返回值常亮
+"""
+
+####################################
