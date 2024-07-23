80c50efcea78a318344b4f5465024b42c462c67b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:57 +0000

    Add package tchrdmodel/3.0.1/tchrdmodel-3.0.1.tar.gz

---
+++ packages/tchrdmodel/tchrdmodel-3.0.1.tar.gz/tchrdmodel-3.0.1/setup.py
@@ -0,0 +1,23 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name='tchrdmodel',
+    version='3.0.1',
+    packages=find_packages(),
+    install_requires=[
+        'tensorflow>=2.0.0',
+    ],
+    author='Subhodeep Moitra, Deblina Banerjee, Dr. Pintu Pal',
+    author_email='subhodeep2000@gmail.com, banerjeedeblina07@gmail.com, dr.ppal.aec@gmail.com',
+    description='A custom model for tabular data using attention and feature transformer blocks',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/subhodeepmoitra/cheemsnetone',  # Replace with your GitHub repository URL
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
