2483808d95c38861688786ca1a0fa51020a48db2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 07:03:21 +0000

    Add package math-spec-mapping/0.3.9/math_spec_mapping-0.3.9.tar.gz

---
+++ packages/math-spec-mapping/math_spec_mapping-0.3.9.tar.gz/math_spec_mapping-0.3.9/src/math_spec_mapping/Classes/BoundaryAction.py
@@ -0,0 +1,19 @@
+from typing import Dict
+from .Block import Block
+
+
+class BoundaryAction(Block):
+    def __init__(self, data: Dict):
+        data["domain"] = tuple()
+        super().__init__(data)
+        self.boundary_action_options = data["boundary_action_options"]
+        self.block_type = "Boundary Action"
+        self.model_name = self.name.replace(" ", "_").lower()
+
+
+class BoundaryActionOption:
+    def __init__(self, data: Dict):
+        self.name = data["name"]
+        self.description = data["description"]
+        self.logic = data["logic"]
+        self.implementations = data["implementations"]
---
+++ packages/math-spec-mapping/math_spec_mapping-0.3.9.tar.gz/math_spec_mapping-0.3.9/src/math_spec_mapping/Classes/ControlAction.py
@@ -0,0 +1,18 @@
+from typing import Dict
+from .Block import Block
+
+
+class ControlAction(Block):
+    def __init__(self, data: Dict):
+        data["domain"] = tuple()
+        super().__init__(data)
+        self.control_action_options = data["control_action_options"]
+        self.block_type = "Control Action"
+
+
+class ControlActionOption:
+    def __init__(self, data: Dict):
+        self.name = data["name"]
+        self.description = data["description"]
+        self.logic = data["logic"]
+        self.implementations = data["implementations"]
