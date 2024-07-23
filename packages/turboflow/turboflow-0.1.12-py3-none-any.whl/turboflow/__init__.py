238d7815b20caaa0cfeff0a08b8c09e51f1f4fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 06:03:32 +0000

    Add package turboflow/0.1.12/turboflow-0.1.12-py3-none-any.whl

---
+++ packages/turboflow/turboflow-0.1.12-py3-none-any.whl/turboflow/__init__.py
@@ -0,0 +1,69 @@
+# Highlight exception messages
+# https://stackoverflow.com/questions/25109105/how-to-colorize-the-output-of-python-errors-in-the-gnome-terminal/52797444#52797444
+try:
+    import IPython.core.ultratb
+except ImportError:
+    # No IPython. Use default exception printing.
+    pass
+else:
+    import sys
+
+    sys.excepthook = IPython.core.ultratb.ColorTB()
+
+import toml
+
+# Import submodules
+from .math import *
+from .plot_functions import *
+from .config_validation import *
+
+# Import subpackages
+from .pysolver_view import *
+from .properties import *
+from .cycles import *
+from .utilities import *
+
+from .axial_turbine import *
+from .axial_turbine.performance_analysis import *
+from .axial_turbine.design_optimization import *
+
+# Set plot options
+set_plot_options()
+
+
+import os
+import toml
+
+BREAKLINE = 80 * "-"
+
+
+def print_banner():
+    """Prints a banner."""
+    banner = """
+        ______ __  __ ____   ____   ____   ______ __    ____  _       __
+       /_  __// / / // __ \ / __ ) / __ \ / ____// /   / __ \| |     / /
+        / /  / / / // /_/ // __  |/ / / // /_   / /   / / / /| | /| / / 
+       / /  / /_/ // _, _// /_/ // /_/ // __/  / /___/ /_/ / | |/ |/ /   
+      /_/   \____//_/ |_|/_____/ \____//_/    /_____/\____/  |__/|__/  
+    """
+    print(BREAKLINE)
+    print(banner)
+
+__version__ = "0.1.12"
+URL_GITHUB = "https://github.com/turbo-sim/TurboFlow"
+URL_DOCS = "https://turbo-sim.github.io/TurboFlow/"
+
+def print_package_info():
+    """Prints package information with predefined values."""
+
+    info = f""" Version:       {__version__}
+ Repository:    {URL_GITHUB}
+ Documentation: {URL_DOCS}"""
+    
+    # Assuming print_banner and BREAKLINE are defined elsewhere in your code
+    print_banner()
+    print(BREAKLINE)
+    print(info)
+
+
+
---
+++ packages/turboflow/turboflow-0.1.12-py3-none-any.whl/turboflow/axial_turbine/__init__.py
@@ -0,0 +1,7 @@
+from .performance_analysis import *
+from .design_optimization import *
+from .deviation_model import *
+from .geometry_model import *
+from .flow_model import *
+from .choking_model import *
+from .loss_model import *
