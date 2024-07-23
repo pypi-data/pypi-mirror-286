f5cff40eb08d5fd82d7fe4cf7bcffb995b5f2ad2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 16:04:43 +0000

    Add package nirvar/1.1.0/nirvar-1.1.0.tar.gz

---
+++ packages/nirvar/nirvar-1.1.0.tar.gz/nirvar-1.1.0/setup.py
@@ -0,0 +1,15 @@
+from setuptools import find_packages, setup
+
+with open("README.md", "r") as f:
+    description = f.read()
+
+setup(
+    name='nirvar',
+    packages=find_packages(),
+    version='1.1.0',
+    description='Network Informed Restricted Vector Autoregression',
+    author='Brendan Martin',
+    license='MIT',
+    long_description=description,
+    long_description_content_type="text/markdown",
+)
