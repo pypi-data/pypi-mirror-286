1d8e5f1472f2d176901f1652bfdcc9d719609ff1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 16:05:00 +0000

    Add package pydantic-tfl-api/0.27.2/pydantic_tfl_api-0.27.2-py3-none-any.whl

---
+++ packages/pydantic-tfl-api/pydantic_tfl_api-0.27.2-py3-none-any.whl/pydantic_tfl_api/models/disruption.py
@@ -0,0 +1,26 @@
+from pydantic import BaseModel, Field
+from datetime import datetime
+from typing import List, Optional
+
+from .affected_route import AffectedRoute
+from .stop_point import StopPoint
+
+
+class Disruption(BaseModel):
+    category: str = Field(alias='category')
+    disruption_type: str = Field(alias='type')
+    category_description: str = Field(alias='categoryDescription')
+    description: str = Field(alias='description')
+    summary: str = Field(alias='summary')
+    additional_info: str = Field(alias='additionalInfo')
+    created: datetime = Field(alias='created')
+    last_update: datetime = Field(alias='lastUpdate')
+    affected_routes: List[AffectedRoute] = Field(alias='affectedRoutes')
+    affected_stops: List[StopPoint] = Field(alias='affectedStops')
+    closure_text: str = Field(alias='closureText')
+    content_expires: Optional[datetime] = Field(None)
+    shared_expires: Optional[datetime] = Field(None)
+
+    model_config = {'populate_by_name': True}
+
+    
