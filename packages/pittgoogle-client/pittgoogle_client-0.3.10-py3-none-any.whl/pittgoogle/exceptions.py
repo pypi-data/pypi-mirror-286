d31c9c2d708ccbf9ef2e9b2650a4b42c7ca62bc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-23 08:03:40 +0000

    Add package pittgoogle-client/0.3.10/pittgoogle_client-0.3.10-py3-none-any.whl

---
+++ packages/pittgoogle-client/pittgoogle_client-0.3.10-py3-none-any.whl/pittgoogle/exceptions.py
@@ -0,0 +1,11 @@
+# -*- coding: UTF-8 -*-
+class BadRequest(Exception):
+    """Raised when a Flask request json envelope (e.g., from Cloud Run) is invalid."""
+
+
+class CloudConnectionError(Exception):
+    """Raised when a problem is encountered while trying to connect to a Google Cloud resource."""
+
+
+class SchemaError(Exception):
+    """Raised when the schema cannot be found in the registry or is incompatible with the data."""
