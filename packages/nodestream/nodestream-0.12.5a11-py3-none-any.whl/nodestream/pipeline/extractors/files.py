94107af4fc3d8c21330989a2f7177dc32cd6880c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 06:04:39 +0000

    Add package nodestream/0.12.5a11/nodestream-0.12.5a11-py3-none-any.whl

---
+++ packages/nodestream/nodestream-0.12.5a11-py3-none-any.whl/nodestream/pipeline/extractors/files.py
@@ -0,0 +1,299 @@
+import bz2
+import gzip
+import json
+import os
+import tempfile
+from abc import ABC, abstractmethod
+from contextlib import asynccontextmanager, contextmanager
+from csv import DictReader
+from glob import glob
+from io import BufferedReader, BytesIO, IOBase, StringIO, TextIOWrapper
+from pathlib import Path
+from typing import Any, AsyncGenerator, Callable, Generator, Iterable, Optional
+
+import pandas as pd
+from httpx import AsyncClient
+from yaml import safe_load
+
+from ...model import JsonLikeDocument
+from ...pluggable import Pluggable
+from ...subclass_registry import SubclassRegistry
+from .extractor import Extractor
+
+SUPPORTED_FILE_FORMAT_REGISTRY = SubclassRegistry()
+SUPPORTED_COMPRESSED_FILE_FORMAT_REGISTRY = SubclassRegistry()
+
+
+class IngestibleFile:
+    def __init__(
+        self,
+        path: Path,
+        fp: IOBase | None = None,
+        delete_on_ingestion: bool = False,
+        on_ingestion: Callable[[Any], Any] = lambda: (),
+    ) -> None:
+        self.extension = path.suffix
+        self.suffixes = path.suffixes
+        self.fp = fp
+        self.path = path
+        self.delete_on_ingestion = delete_on_ingestion
+        self.on_ingestion = on_ingestion
+
+    @classmethod
+    def from_file_pointer_and_suffixes(
+        cls,
+        fp: IOBase,
+        suffixes: str | list[str],
+        on_ingestion: Callable[[Any], Any] = lambda: (),
+    ) -> "IngestibleFile":
+        fd, temp_path = tempfile.mkstemp(suffix="".join(suffixes))
+        os.close(fd)
+
+        with open(temp_path, "wb") as temp_file:
+            for chunk in iter(lambda: fp.read(1024), b""):
+                temp_file.write(chunk)
+            temp_file.flush()
+
+        with open(temp_path, "rb+") as fp:
+            return IngestibleFile(Path(temp_path), fp, True, on_ingestion)
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, type, value, traceback):
+        if self.fp:
+            self.fp.close()
+        if self.delete_on_ingestion:
+            self.tempfile_cleanup()
+
+    def ingested(self):
+        if self.delete_on_ingestion:
+            self.tempfile_cleanup()
+        self.on_ingestion()
+
+    def tempfile_cleanup(self):
+        if self.fp:
+            self.fp.close()
+        if os.path.isfile(self.path):
+            os.remove(self.path)
+
+
+@SUPPORTED_FILE_FORMAT_REGISTRY.connect_baseclass
+class SupportedFileFormat(Pluggable, ABC):
+    reader = None
+    entrypoint_name = "file_formats"
+
+    def __init__(self, file: IngestibleFile) -> None:
+        self.file = file
+
+    @contextmanager
+    def read_handle(self) -> Iterable | BufferedReader:
+        if isinstance(self.file, Path):
+            with open(self.file, "rb") as fp:
+                yield fp
+        else:
+            yield self.file
+
+    def read_file(self) -> Iterable[JsonLikeDocument]:
+        with self.read_handle() as fp:
+            if self.reader is not None:
+                if self.reader is TextIOWrapper:
+                    reader = self.reader(fp, encoding="utf-8")
+                else:
+                    reader = self.reader(fp)
+
+            else:
+                reader = fp
+            return self.read_file_from_handle(reader)
+
+    @classmethod
+    @contextmanager
+    def open(
+        cls, file: IngestibleFile, extension_override: Optional[str] = ""
+    ) -> Generator["SupportedFileFormat", None, None]:
+        extension = extension_override or file.extension
+        if extension == "":
+            raise ValueError(f"File has no extension: '{file.path}'")
+        # Decompress file if in Supported Compressed File Format Registry
+        while extension in SUPPORTED_COMPRESSED_FILE_FORMAT_REGISTRY:
+            compressed_file_format = SupportedCompressedFileFormat.open(file)
+            file = compressed_file_format.decompress_file()
+            extension = file.extension
+        with open(file.path, "rb") as fp:
+            yield cls.from_file_pointer_and_format(fp, extension)
+
+    @classmethod
+    def from_file_pointer_and_format(
+        cls, fp: IOBase, file_format: str
+    ) -> "SupportedFileFormat":
+        # Import all file formats so that they can register themselves
+        cls.import_all()
+        file_format = SUPPORTED_FILE_FORMAT_REGISTRY.get(file_format)
+        return file_format(fp)
+
+    @abstractmethod
+    def read_file_from_handle(
+        self, fp: BufferedReader
+    ) -> Iterable[JsonLikeDocument]: ...
+
+
+@SUPPORTED_COMPRESSED_FILE_FORMAT_REGISTRY.connect_baseclass
+class SupportedCompressedFileFormat(Pluggable, ABC):
+    def __init__(self, file: IngestibleFile) -> None:
+        self.file = file
+
+    @classmethod
+    def open(cls, file: IngestibleFile) -> "SupportedCompressedFileFormat":
+        with open(file.path, "rb") as fp:
+            return cls.from_file_pointer_and_path(fp, file.path)
+
+    @classmethod
+    def from_file_pointer_and_path(
+        cls, fp: IOBase, path: Path
+    ) -> "SupportedCompressedFileFormat":
+        # Import all compression file formats so that they can register themselves
+        cls.import_all()
+        file_format = SUPPORTED_COMPRESSED_FILE_FORMAT_REGISTRY.get(path.suffix)
+        file = IngestibleFile(path, fp)
+        file.on_ingestion = lambda: file.tempfile_cleanup()
+        return file_format(file)
+
+    @abstractmethod
+    def decompress_file(self) -> IngestibleFile: ...
+
+
+class JsonFileFormat(SupportedFileFormat, alias=".json"):
+    reader = TextIOWrapper
+
+    def read_file_from_handle(
+        self, reader: TextIOWrapper
+    ) -> Iterable[JsonLikeDocument]:
+        return [json.load(reader)]
+
+
+class LineSeperatedJsonFileFormat(SupportedFileFormat, alias=".jsonl"):
+    reader = TextIOWrapper
+
+    def read_file_from_handle(
+        self, reader: TextIOWrapper
+    ) -> Iterable[JsonLikeDocument]:
+        return (json.loads(line.strip()) for line in reader.readlines())
+
+
+class ParquetFileFormat(SupportedFileFormat, alias=".parquet"):
+    def read_file_from_handle(self, fp: StringIO) -> Iterable[JsonLikeDocument]:
+        df = pd.read_parquet(fp, engine="pyarrow")
+        return (row[1].to_dict() for row in df.iterrows())
+
+
+class TextFileFormat(SupportedFileFormat, alias=".txt"):
+    reader = TextIOWrapper
+
+    def read_file_from_handle(
+        self, reader: TextIOWrapper
+    ) -> Iterable[JsonLikeDocument]:
+        return ({"line": line.strip()} for line in reader.readlines())
+
+
+class CommaSeperatedValuesFileFormat(SupportedFileFormat, alias=".csv"):
+    reader = TextIOWrapper
+
+    def read_file_from_handle(
+        self, reader: TextIOWrapper
+    ) -> Iterable[JsonLikeDocument]:
+        return DictReader(reader)
+
+
+class YamlFileFormat(SupportedFileFormat, alias=".yaml"):
+    reader = TextIOWrapper
+
+    def read_file_from_handle(
+        self, reader: TextIOWrapper
+    ) -> Iterable[JsonLikeDocument]:
+        return [safe_load(reader)]
+
+
+class GzipFileFormat(SupportedCompressedFileFormat, alias=".gz"):
+    def decompress_file(self) -> IngestibleFile:
+        decompressed_data = BytesIO()
+        with gzip.open(self.file.path, "rb") as f_in:
+            chunk_size = 1024 * 1024
+            while True:
+                chunk = f_in.read(chunk_size)
+                if len(chunk) == 0:
+                    break
+                decompressed_data.write(chunk)
+        decompressed_data.seek(0)
+        new_path = self.file.path.with_suffix("")
+        temp_file = IngestibleFile.from_file_pointer_and_suffixes(
+            decompressed_data, new_path.suffixes
+        )
+        self.file.ingested()
+        return temp_file
+
+
+class Bz2FileFormat(SupportedCompressedFileFormat, alias=".bz2"):
+    def decompress_file(self) -> IngestibleFile:
+        decompressed_data = BytesIO()
+        with bz2.open(self.file.path, "rb") as f_in:
+            chunk_size = 1024 * 1024
+            while True:
+                chunk = f_in.read(chunk_size)
+                if len(chunk) == 0:
+                    break
+                decompressed_data.write(chunk)
+        decompressed_data.seek(0)
+        new_path = self.file.path.with_suffix("")
+        temp_file = IngestibleFile.from_file_pointer_and_suffixes(
+            decompressed_data, new_path.suffixes
+        )
+        self.file.ingested()
+        return temp_file
+
+
+class FileExtractor(Extractor):
+    @classmethod
+    def from_file_data(cls, globs: Iterable[str]):
+        all_matching_paths = (
+            Path(file)
+            for glob_string in globs
+            for file in glob(glob_string, recursive=True)
+        )
+        final_paths = {p for p in all_matching_paths if p.is_file()}
+        return cls(final_paths)
+
+    def __init__(self, paths: Iterable[Path]) -> None:
+        self.paths = paths
+
+    def _ordered_paths(self) -> Iterable[Path]:
+        return sorted(self.paths)
+
+    async def extract_records(self) -> AsyncGenerator[Any, Any]:
+        for path in self._ordered_paths():
+            with SupportedFileFormat.open(IngestibleFile(path)) as file:
+                for record in file.read_file():
+                    yield record
+
+
+class RemoteFileExtractor(Extractor):
+    def __init__(
+        self, urls: Iterable[str], memory_spooling_max_size_in_mb: int = 5
+    ) -> None:
+        self.urls = urls
+        self.memory_spooling_max_size = memory_spooling_max_size_in_mb * 1024 * 1024
+
+    @asynccontextmanager
+    async def download_file(self, client: AsyncClient, url: str) -> SupportedFileFormat:
+        with tempfile.TemporaryFile() as fp:
+            async with client.stream("GET", url) as response:
+                async for chunk in response.aiter_bytes():
+                    fp.write(chunk)
+            fp.seek(0)
+            yield SupportedFileFormat.from_file_pointer_and_format(fp, Path(url).suffix)
+
+    async def extract_records(self) -> AsyncGenerator[Any, Any]:
+        async with AsyncClient() as client:
+            for url in self.urls:
+                async with self.download_file(client, url) as file:
+                    for record in file.read_file():
+                        yield record
---
+++ packages/nodestream/nodestream-0.12.5a11-py3-none-any.whl/nodestream/pipeline/extractors/stores/aws/s3_extractor.py
@@ -0,0 +1,91 @@
+from contextlib import contextmanager
+from logging import getLogger
+from pathlib import Path
+from typing import Any, AsyncGenerator, Generator, Optional
+
+from ...credential_utils import AwsClientFactory
+from ...extractor import Extractor
+from ...files import IngestibleFile, SupportedFileFormat
+
+
+class S3Extractor(Extractor):
+    @classmethod
+    def from_file_data(
+        cls,
+        bucket: str,
+        prefix: Optional[str] = None,
+        archive_dir: Optional[str] = None,
+        object_format: Optional[str] = None,
+        **aws_client_args,
+    ):
+        return cls(
+            bucket=bucket,
+            object_format=object_format,
+            prefix=prefix,
+            archive_dir=archive_dir,
+            s3_client=AwsClientFactory(**aws_client_args).make_client("s3"),
+        )
+
+    def __init__(
+        self,
+        bucket: str,
+        s3_client,
+        archive_dir: Optional[str] = None,
+        object_format: Optional[str] = None,
+        prefix: Optional[str] = None,
+    ) -> None:
+        self.object_format = object_format
+        self.prefix = prefix or ""
+        self.bucket = bucket
+        self.archive_dir = archive_dir
+        self.s3_client = s3_client
+        self.logger = getLogger(__name__)
+
+    @contextmanager
+    def get_object_as_tempfile(self, key: str):
+        streaming_body = self.s3_client.get_object(Bucket=self.bucket, Key=key)["Body"]
+        file = IngestibleFile.from_file_pointer_and_suffixes(
+            streaming_body, Path(key).suffixes, lambda: self.archive_s3_object(key)
+        )
+        yield file
+
+    def archive_s3_object(self, key: str):
+        if self.archive_dir:
+            self.logger.info("Archiving S3 Object", extra=dict(key=key))
+            filename = Path(key).name
+            self.s3_client.copy(
+                Bucket=self.bucket,
+                Key=f"{self.archive_dir}/{filename}",
+                CopySource={"Bucket": self.bucket, "Key": key},
+            )
+            self.s3_client.delete_object(Bucket=self.bucket, Key=key)
+
+    @contextmanager
+    def get_object_as_file(
+        self, key: str
+    ) -> Generator[SupportedFileFormat, None, None]:
+        with self.get_object_as_tempfile(key) as temp_file:
+            with SupportedFileFormat.open(temp_file, self.object_format) as file_format:
+                yield file_format
+
+    def is_object_in_archive(self, key: str) -> bool:
+        if self.archive_dir:
+            return key.startswith(self.archive_dir)
+        return False
+
+    def find_keys_in_bucket(self) -> list[str]:
+        # Returns all keys in the bucket that are not in the archive dir and have the prefix.
+        paginator = self.s3_client.get_paginator("list_objects_v2")
+        page_iterator = paginator.paginate(Bucket=self.bucket, Prefix=self.prefix)
+        for page in page_iterator:
+            keys = (obj["Key"] for obj in page.get("Contents", []))
+            yield from filter(lambda k: not self.is_object_in_archive(k), keys)
+
+    async def extract_records(self) -> AsyncGenerator[Any, Any]:
+        for key in self.find_keys_in_bucket():
+            try:
+                with self.get_object_as_file(key) as records:
+                    for record in records.read_file():
+                        yield record
+            finally:
+                self.archive_s3_object(key)
