fb0544a8c4ae99848395a954b7b50324e2cdecbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:33 +0000

    Add package pyterrier-alpha/0.2.2/pyterrier-alpha-0.2.2.tar.gz

---
+++ packages/pyterrier-alpha/pyterrier-alpha-0.2.2.tar.gz/pyterrier-alpha-0.2.2/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyterrier-alpha",
+    version="0.2.2",
+    author="Sean MacAvaney",
+    author_email='sean.macavaney@glasgow.ac.uk',
+    description="Alpha channel of features for PyTerrier",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=setuptools.find_packages(),
+    install_requires=[
+      'python-terrier',
+    ],
+    python_requires='>=3.6',
+)
