c391472cd27612b324961b270c3909a3dcf210d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 07:05:41 +0000

    Add package aita/0.1.5/aita-0.1.5-py3-none-any.whl

---
+++ packages/aita/aita-0.1.5-py3-none-any.whl/aita_lab/pages/catalog.py
@@ -0,0 +1,125 @@
+"""The data catalog page """
+
+import reflex as rx
+
+from aita_lab.states.catalog import CatalogState, CatalogTable
+from aita_lab.templates import template
+
+
+def catalog_search():
+    return rx.flex(
+        rx.input(
+            placeholder="Search for a table or column",
+            width="100%",
+        ),
+        rx.button("Search"),
+        spacing="3",
+        justify="center",
+        align="center",
+        width="100%",
+    )
+
+
+def add_data_catalog():
+    return rx.dialog.root(
+        rx.dialog.trigger(
+            rx.button(
+                rx.flex("Add Data Catalog", rx.icon(tag="plus", width=24, height=24), spacing="3"),
+                size="4",
+                radius="full",
+                on_click=CatalogState.toggle_dialog,
+            ),
+        ),
+        rx.dialog.content(
+            rx.dialog.title(
+                "Connect and Add Data Catalog",
+                size="1",
+                font_family="Inter",
+                padding_top="1em",
+            ),
+            rx.dialog.description(
+                "Select the data source to add a data catalog",
+                size="2",
+                mb="4",
+                padding_bottom="1em",
+            ),
+        ),
+    )
+
+
+def show_catalog(catalog: CatalogTable):
+    return rx.table.row(
+        rx.table.cell(catalog.catalog_name),
+        rx.table.cell(catalog.schema),
+        rx.table.cell(catalog.table),
+        rx.table.cell(catalog.datasource_name),
+        rx.table.cell(
+            rx.button(
+                "Delete",
+                on_click=lambda: CatalogState.delete_catalog(catalog),
+            )
+        ),
+    )
+
+
+def catalog_grid():
+    return rx.fragment(
+        rx.vstack(
+            rx.box(
+                add_data_catalog(),
+            ),
+            rx.hstack(
+                rx.select.root(
+                    rx.select.trigger(
+                        placeholder=f"Sort By:",
+                    ),
+                    rx.select.content(
+                        rx.select.group(
+                            rx.select.item("Catalog", value="catalog_name"),
+                            rx.select.item("Schema", value="feature"),
+                            rx.select.item("Table", value="table"),
+                            rx.select.item("Data Source", value="datasource_name"),
+                        )
+                    ),
+                    size="3",
+                    value=CatalogState.sort_value,
+                    on_change=lambda sort_value: CatalogState.sort_values(sort_value),
+                    font_family="Inter",
+                ),
+                width="100%",
+                padding_top="2em",
+                padding_bottom="1em",
+            ),
+            rx.table.root(
+                rx.table.header(
+                    rx.table.row(
+                        rx.table.column_header_cell("Catalog Name"),
+                        rx.table.column_header_cell("Schema"),
+                        rx.table.column_header_cell("Table"),
+                        rx.table.column_header_cell("Data Source"),
+                        rx.table.column_header_cell("Delete"),
+                    ),
+                ),
+                rx.table.body(rx.foreach(CatalogState.catalogs, show_catalog)),
+                # variant="surface",
+                size="3",
+                width="100%",
+                justify="stretch",
+            ),
+            margin_top="20px",
+        ),
+    )
+
+
+@template(route="/catalog", title="Data Catalog", on_load=CatalogState.on_load())
+def catalog():
+    return rx.vstack(
+        rx.heading("Data Catalog", size="8"),
+        rx.text("View your data catalog"),
+        rx.box(
+            catalog_search(),
+            catalog_grid(),
+            width="100%",
+        ),
+        width="100%",
+    )
---
+++ packages/aita/aita-0.1.5-py3-none-any.whl/aita_lab/states/agent.py
@@ -0,0 +1,88 @@
+from typing import Optional
+
+import importlib
+import random
+
+import reflex as rx
+from langchain_core.runnables.graph import Edge, Node
+
+from aita.agent.factory import AgentFactory, get_supported_agents
+from aita.agent.graph import GraphAgent
+from aita_lab.states.graph import Graph
+
+
+def get_node_position(node: Node):
+    if node.id == "__start__":
+        return {"x": 20, "y": 20}
+    elif node.id == "agent":
+        return {"x": 20, "y": 100}
+    elif node.id == "__end__":
+        return {"x": 20, "y": 200}
+    elif node.id == "tools":
+        return {"x": 200, "y": 100}
+    else:
+        return {"x": random.randint(0, 500), "y": random.randint(0, 500)}
+
+
+def create_agent_graph_node(node: Node):
+    return {
+        "id": node.id,
+        "data": {"label": node.id},
+        "position": get_node_position(node),
+    }
+
+
+def create_agent_graph_edge(id, edge: Edge):
+    return {"id": id, "source": edge.source, "target": edge.target, "animated": True}
+
+
+class Agent(rx.Model):
+    id: Optional[str]
+    name: str
+    description: Optional[str]
+    tools: Optional[list[str]]
+
+
+class AgentState(rx.State):
+    agents: list[Agent] = []
+    is_open: bool = False
+    current_agent: Optional[Agent] = None
+    current_agent_graph: Optional[Graph] = None
+
+    def open_agent(self, is_open: bool, agent: Agent):
+        self.is_open = is_open
+        passthrough_agent = AgentFactory.create_agent(agent["name"], model="")
+        if isinstance(passthrough_agent, GraphAgent):
+            graph = passthrough_agent.get_graph()
+            self.current_agent_graph = Graph(
+                nodes=[create_agent_graph_node(node) for _, node in graph.nodes.items()],
+                edges=[create_agent_graph_edge(id, edge) for id, edge in enumerate(graph.edges)],
+            )
+
+    @rx.var
+    def agent_names(self) -> list[str]:
+        return [agent.name for agent in self.agents]
+
+    def get_custom_agents(self):
+        return []
+
+    def on_load(self):
+        self.agents = [
+            Agent(name=agent.name, description=agent.value.description)
+            for agent in get_supported_agents() + self.get_custom_agents()
+        ]
+
+    def create_agent(self, graph: Graph):
+        state_graph = GraphAgent.init_state_graph()
+        tools = []
+        for node in graph.nodes:
+            if node["type"] == "tool":
+                tool_name = node["data"]["label"]
+                tool_cls = importlib.import_module(f"aita.tool.{tool_name}")
+                tools.append(tool_cls)
+
+        state_graph.set_entry_point("agent")
+        state_graph.add_edge("tools", "agent")
+
+        agent = GraphAgent(type="custom", tools=tools, model=self.current_model, graph=state_graph)
+        return
