4abe8e19a5e408f887cd69138bda7c36bfb29200
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:53 +0000

    Add package crackle-codec/0.14.0/crackle_codec-0.14.0-cp38-cp38-macosx_10_9_x86_64.whl

---
+++ packages/crackle-codec/crackle_codec-0.14.0-cp38-cp38-macosx_10_9_x86_64.whl/crackle/util.py
@@ -0,0 +1,74 @@
+from typing import Optional, Union
+
+import io
+import os
+import gzip
+import lzma
+
+from .array import CrackleArray
+from .codec import compress, decompress
+
+import numpy as np
+
+def _load(filelike):
+  if hasattr(filelike, 'read'):
+    binary = filelike.read()
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] == '.gz'
+  ):
+    with gzip.open(filelike, 'rb') as f:
+      binary = f.read()
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] in ('.lzma', '.xz')
+  ):
+    with lzma.open(filelike, 'rb') as f:
+      binary = f.read()
+  else:
+    with open(filelike, 'rb') as f:
+      binary = f.read()
+  
+  return binary
+
+def aload(filelike) -> CrackleArray:
+  """Load a CrackleArray from a file."""
+  return CrackleArray(_load(filelike))
+
+def load(filelike, label:Optional[int] = None) -> np.ndarray:
+  """Load an image from a file-like object or file path."""
+  return decompress(_load(filelike), label=label)
+
+def load_numpy(filelike):
+  f = io.BytesIO(_load(filelike))
+  return np.load(f)
+
+def save(
+  labels:Union[np.ndarray, CrackleArray], 
+  filelike, 
+  **kwargs
+):
+  """Save labels into the file-like object or file path."""
+  if isinstance(labels, CrackleArray):
+    binary = labels.binary
+  else:
+    binary = compress(labels, **kwargs)
+
+  if hasattr(filelike, 'write'):
+    filelike.write(binary)
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] == '.gz'
+  ):
+    with gzip.open(filelike, 'wb') as f:
+      f.write(binary)
+  elif (
+    isinstance(filelike, str) 
+    and os.path.splitext(filelike)[1] in ('.lzma', '.xz')
+  ):
+    with lzma.open(filelike, 'wb') as f:
+      f.write(binary)
+  else:
+    with open(filelike, 'wb') as f:
+      f.write(binary)
+
