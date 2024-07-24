005744fccb9663e4f63e1ec31512c0730bedefa1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:24 +0000

    Add package rustitude/0.9.1/rustitude-0.9.1.tar.gz

---
+++ packages/rustitude/rustitude-0.9.1.tar.gz/rustitude-0.9.1/crates/rustitude/benches/read_dataset.rs
@@ -0,0 +1,15 @@
+use criterion::{criterion_group, criterion_main, Criterion};
+use rustitude::prelude::*;
+
+pub fn criterion_read_dataset(c: &mut Criterion) {
+    c.bench_function("read_dataset", |b| {
+        b.iter(|| criterion::black_box(Dataset::<f64>::from_parquet("benches/test_data.parquet")))
+    });
+}
+
+criterion_group! {
+    name = benches;
+    config = Criterion::default().sample_size(20);
+    targets = criterion_read_dataset
+}
+criterion_main!(benches);
