8d1c68c5736946652f26e66af0e5087402057f0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:01 +0000

    Add package textscope/0.1.4/textscope-0.1.4.tar.gz

---
+++ packages/textscope/textscope-0.1.4.tar.gz/textscope-0.1.4/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="textscope",
+    version="0.1.4",
+    packages=find_packages(),
+    install_requires=[
+        "torch",
+        "transformers",
+        "numpy",
+        "pytest",
+        "sentence-transformers",
+        "nltk"
+    ],
+    include_package_data=True,
+    description="A Python text analysis library for relevance and subtheme detection",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author="Marcos Fernández-Pichel",
+    author_email="marcosfernandez.pichel@usc.es",
+)
+
