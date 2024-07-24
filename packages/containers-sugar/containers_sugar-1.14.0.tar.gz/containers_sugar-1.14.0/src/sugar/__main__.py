815453317b303aa6ceada7a96b4601def8b9310c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:47 +0000

    Add package containers-sugar/1.14.0/containers_sugar-1.14.0.tar.gz

---
+++ packages/containers-sugar/containers_sugar-1.14.0.tar.gz/containers_sugar-1.14.0/src/sugar/__main__.py
@@ -0,0 +1,6 @@
+"""The definition of the action for `python -m` command."""
+
+from sugar.cli import app
+
+if __name__ == '__main__':
+    app()
---
+++ packages/containers-sugar/containers_sugar-1.14.0.tar.gz/containers_sugar-1.14.0/src/sugar/logs.py
@@ -0,0 +1,41 @@
+"""Logs classes and function for sugar system."""
+
+import os
+
+from enum import Enum
+
+from colorama import Fore
+
+
+class KxgrErrorType(Enum):
+    """KxgrErrorType group all error types handled by the system."""
+
+    SH_ERROR_RETURN_CODE = 1
+    SH_KEYBOARD_INTERRUPT = 2
+    KXGR_COMPOSE_APP_NOT_SUPPORTED = 3
+    KXGR_COMPOSE_APP_NOT_FOUNDED = 4
+    KXGR_INVALID_PARAMETER = 5
+    KXGR_MISSING_PARAMETER = 6
+    KXGR_INVALID_CONFIGURATION = 7
+    KXGR_ACTION_NOT_IMPLEMENTED = 8
+    KXGR_NO_SERVICES_RUNNING = 9
+
+
+class KxgrLogs:
+    """KxgrLogs is responsible for handling system messages."""
+
+    @staticmethod
+    def raise_error(message: str, message_type: KxgrErrorType):
+        """Print error message and exit with given error code."""
+        print(Fore.RED, f'[EE] {message}', Fore.RESET)
+        os._exit(message_type.value)
+
+    @staticmethod
+    def print_info(message: str):
+        """Print info message."""
+        print(Fore.BLUE, message, Fore.RESET)
+
+    @staticmethod
+    def print_warning(message: str):
+        """Print warning message."""
+        print(Fore.YELLOW, message, Fore.RESET)
