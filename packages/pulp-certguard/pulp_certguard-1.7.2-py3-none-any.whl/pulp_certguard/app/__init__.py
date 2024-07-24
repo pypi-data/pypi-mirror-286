07dbd4907846d3fcc2a73943991c971e5cf33be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:56 +0000

    Add package pulp-certguard/1.7.2/pulp_certguard-1.7.2-py3-none-any.whl

---
+++ packages/pulp-certguard/pulp_certguard-1.7.2-py3-none-any.whl/pulp_certguard/app/__init__.py
@@ -0,0 +1,11 @@
+from pulpcore.plugin import PulpPluginAppConfig
+
+
+class PulpCertGuardPluginAppConfig(PulpPluginAppConfig):
+    """App config for cert guard plugin."""
+
+    name = "pulp_certguard.app"
+    label = "certguard"
+    version = "1.7.2"
+    python_package_name = "pulp-certguard"
+    domain_compatible = True
