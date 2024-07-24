350b7210e47d5a45d246852f4d94fbfe3a5757f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:02 +0000

    Add package breath-detector/0.1/breath_detector-0.1.tar.gz

---
+++ packages/breath-detector/breath_detector-0.1.tar.gz/breath_detector-0.1/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='breath_detector',  # 包名
+    version='0.1',              # 版本号
+    packages=find_packages(),   # 包含的包列表
+    install_requires=[          # 依赖列表
+        'scipy'
+    ],
+    author='jasine',         # 作者信息
+    author_email='jasinechen@gmail.com',
+    description='breath detector in Lingjing Env',  # 简要描述
+)
