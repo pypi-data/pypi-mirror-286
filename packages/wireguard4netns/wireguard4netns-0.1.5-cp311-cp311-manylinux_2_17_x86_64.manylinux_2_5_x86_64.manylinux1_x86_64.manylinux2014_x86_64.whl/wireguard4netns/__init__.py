0ccdd139222e964541bf58880d4163e89e2e0a5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 06:04:02 +0000

    Add package wireguard4netns/0.1.5/wireguard4netns-0.1.5-cp311-cp311-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl

---
+++ packages/wireguard4netns/wireguard4netns-0.1.5-cp311-cp311-manylinux_2_17_x86_64.manylinux_2_5_x86_64.manylinux1_x86_64.manylinux2014_x86_64.whl/wireguard4netns/__init__.py
@@ -0,0 +1,13 @@
+#
+# wireguard4netns
+#
+# Copyright (c) 2022-2023 Carnegie Mellon University
+# SPDX-License-Identifier: MIT
+#
+__version__ = "0.1.5"
+
+from .wireguard_daemon import create_wireguard_tunnel
+
+__all__ = [
+    "create_wireguard_tunnel",
+]
