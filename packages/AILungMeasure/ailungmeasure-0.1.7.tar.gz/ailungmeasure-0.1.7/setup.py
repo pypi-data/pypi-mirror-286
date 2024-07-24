f4b9c9a3795cfdf6f765a60ae1a6db9da7b62998
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 07:05:19 +0000

    Add package AILungMeasure/0.1.7/ailungmeasure-0.1.7.tar.gz

---
+++ packages/AILungMeasure/ailungmeasure-0.1.7.tar.gz/ailungmeasure-0.1.7/setup.py
@@ -0,0 +1,36 @@
+# setup.py
+from setuptools import setup, find_packages
+
+setup(
+    name="AILungMeasure",
+    version="0.1.7",  # Update this version number
+    packages=find_packages(include=["AILungMeasure", "AILungMeasure.*"]),
+    install_requires=[
+        "torch",
+        "Pillow",
+        "torchvision",
+        "matplotlib",
+        "opencv-python",
+        "numpy",
+        "imutils",
+        "gdown",
+        "pydicom",
+    ],
+    author="Mostafa Ismail",
+    author_email="mostafa.ismail.k@gmail.com",
+    description="Automated lung size measurements using deep learning and computer vision on portable chest radiographs.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/yourusername/AILungMeasure",  # Update with your project URL
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Free for non-commercial use",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    include_package_data=True,
+    package_data={
+        '': ['Images/*.png', 'README.md'],
+    },
+    license="Creative Commons ATTRIBUTION-NONCOMMERCIAL-SHAREALIKE 4.0 INTERNATIONAL",
+)
