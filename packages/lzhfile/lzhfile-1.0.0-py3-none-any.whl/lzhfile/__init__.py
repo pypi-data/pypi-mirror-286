ecc214af6395148c266ce97505ec788a4aa77714
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:21 +0000

    Add package lzhfile/1.0.0/lzhfile-1.0.0-py3-none-any.whl

---
+++ packages/lzhfile/lzhfile-1.0.0-py3-none-any.whl/lzhfile/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: UTF-8 -*-
+# Public package
+# Private package
+# Internal package
+from .core import *
+from .types.math import *
+from .types.txt import *
+from .types.pkl import *
+from .types.ini import *
+from .types.json import *
