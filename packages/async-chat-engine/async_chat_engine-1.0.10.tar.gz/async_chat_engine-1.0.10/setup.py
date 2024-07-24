da26a8068ccd92d9944e229f05d9ac8d4291bdaa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:11 +0000

    Add package async-chat-engine/1.0.10/async_chat_engine-1.0.10.tar.gz

---
+++ packages/async-chat-engine/async_chat_engine-1.0.10.tar.gz/async_chat_engine-1.0.10/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="async_chat_engine",
+    version="1.0.10",
+    author="Oscar",
+    author_email="om@holograph.digital",
+    description="An asynchronous chat engine using vLLM with a async producer-consumer pattern.",
+    long_description=open('README.md').read(),
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+    ],
+    python_requires='>=3.10',
+    install_requires=[
+        "vllm",
+        "websockets",
+        "PyJWT",
+    ],
+)
