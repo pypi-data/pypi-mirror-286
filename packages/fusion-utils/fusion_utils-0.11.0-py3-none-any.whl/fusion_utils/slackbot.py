00fd7f89d86590df10f40a5e93322a44fc6f2c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:47 +0000

    Add package fusion-utils/0.11.0/fusion_utils-0.11.0-py3-none-any.whl

---
+++ packages/fusion-utils/fusion_utils-0.11.0-py3-none-any.whl/fusion_utils/slackbot.py
@@ -0,0 +1,22 @@
+import os
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+class SlackBot:
+    def __init__(self, token):
+        self.client = WebClient(token=token)
+
+    def send_message(self, channel, text):
+        try:
+            response = self.client.chat_postMessage(
+                channel=channel,
+                text=text
+            )
+            assert response["message"]["text"] == text
+            print("Message sent successfully!")
+        except SlackApiError as e:
+            print(f"Error sending message: {e.response['error']}")
+
+
+
+
