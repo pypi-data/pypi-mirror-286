7251e9baf65952cbf5f3dfcfcb1c52bade2ac99c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 06:04:38 +0000

    Add package PyPanorama/0.1/pypanorama-0.1.tar.gz

---
+++ packages/PyPanorama/pypanorama-0.1.tar.gz/pypanorama-0.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as stream:
+    long_description = stream.read()
+
+setup(
+    name="PyPanorama",
+    author="ZOOM",
+    version="0.01",
+    description="A versatile Python library offering a wide range of functionalities for various applications.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    packages=find_packages(),
+    keywords=['Python', 'versatile', 'utilities', 'social media', 'Instagram', 'Facebook', 'TikTok'],
+    python_requires='>=3.6',
+)
