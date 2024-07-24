1eeda40b315044da752b696db6fe168a01ad0195
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:49 +0000

    Add package PhenoFeatureFinder/0.1.2/phenofeaturefinder-0.1.2-py3-none-any.whl

---
+++ packages/PhenoFeatureFinder/phenofeaturefinder-0.1.2-py3-none-any.whl/phenofeaturefinder/__init__.py
@@ -0,0 +1,6 @@
+# read version from installed package
+from importlib.metadata import version
+__version__ = version("PhenoFeatureFinder")
+
+__author__ = 'Marc Galland'
+__email__ = 'm.galland@uva.nl'
