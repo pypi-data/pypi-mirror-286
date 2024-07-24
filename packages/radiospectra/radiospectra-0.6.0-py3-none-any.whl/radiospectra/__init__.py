b3e7d2401c4f311cf04ce47113ed4bbae6bea3ad
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:23 +0000

    Add package radiospectra/0.6.0/radiospectra-0.6.0-py3-none-any.whl

---
+++ packages/radiospectra/radiospectra-0.6.0-py3-none-any.whl/radiospectra/__init__.py
@@ -0,0 +1,15 @@
+"""
+================
+``radiospectra``
+================
+
+An open-source Python library for radio spectra in solar physics.
+
+* Homepage: https://sunpy.org
+* Documentation: https://docs.radiospectra.org/en/stable/
+* Source code: https://github.com/sunpy/radiospectra
+"""
+
+from .version import version as __version__
+
+__all__ = ["__version__"]
