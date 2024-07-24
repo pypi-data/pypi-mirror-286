925273b9dac350c35c2286a266eef10eba2ae1bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:30 +0000

    Add package text2text/1.4.5/text2text-1.4.5.tar.gz

---
+++ packages/text2text/text2text-1.4.5.tar.gz/text2text-1.4.5/setup.py
@@ -0,0 +1,43 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+  long_description = fh.read()
+
+setuptools.setup(
+  name="text2text",
+  version="1.4.5",
+  author="artitw",
+  author_email="artitw@gmail.com",
+  description="Text2Text: Crosslingual NLP/G toolkit",
+  long_description=long_description,
+  long_description_content_type="text/markdown",
+  url="https://github.com/artitw/text2text",
+  packages=setuptools.find_packages(),
+  classifiers=[
+    "Programming Language :: Python :: 3",
+    "License :: OSI Approved :: MIT License",
+    "Operating System :: OS Independent",
+  ],
+  keywords='multilingual crosslingual gpt chatgpt bert natural language processing nlp nlg text generation gpt question answer answering information retrieval tfidf tf-idf bm25 search index summary summarizer summarization tokenizer tokenization translation backtranslation data augmentation science machine learning colab embedding levenshtein sub-word edit distance conversational dialog chatbot mixtral',
+  install_requires=[
+    'accelerate',
+    'bitsandbytes',
+    'peft',
+    'faiss-cpu',
+    'flask',
+    'langchain',
+    'googledrivedownloader',
+    'numpy',
+    'ollama',
+    'optimum',
+    'pandas',
+    'scikit-learn',
+    'scipy',
+    'sentencepiece',
+    'torch',
+    'tqdm',
+    'transformers',
+    'hqq',
+    'huggingface_hub'
+  ],
+)
