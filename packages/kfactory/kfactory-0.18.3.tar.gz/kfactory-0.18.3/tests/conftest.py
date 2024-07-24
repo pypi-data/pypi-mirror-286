1b0ddc2e26e9d8c4959efaec1d8542492b49d425
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:23 +0000

    Add package kfactory/0.18.3/kfactory-0.18.3.tar.gz

---
+++ packages/kfactory/kfactory-0.18.3.tar.gz/kfactory-0.18.3/tests/conftest.py
@@ -0,0 +1,159 @@
+from functools import partial
+
+import pytest
+
+import kfactory as kf
+from collections.abc import Callable
+
+# kf.config.logfilter.level = kf.conf.LogLevel.ERROR
+
+
+class LAYER_CLASS(kf.LayerEnum):
+    kcl = kf.constant(kf.kcl)
+    WG = (1, 0)
+    WGCLAD = (111, 0)
+    WGEXCLUDE = (1, 1)
+    WGCLADEXCLUDE = (111, 1)
+
+
+@pytest.fixture
+def LAYER() -> type[kf.LayerEnum]:
+    return LAYER_CLASS
+
+
+@pytest.fixture
+def wg_enc(LAYER: kf.LayerEnum) -> kf.LayerEnclosure:
+    return kf.LayerEnclosure(name="WGSTD", sections=[(LAYER.WGCLAD, 0, 2000)])
+
+
+@pytest.fixture
+def straight_factory_dbu(
+    LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure
+) -> Callable[..., kf.KCell]:
+    return partial(kf.cells.straight.straight_dbu, layer=LAYER.WG, enclosure=wg_enc)
+
+
+@pytest.fixture
+def straight_factory(
+    LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure
+) -> Callable[..., kf.KCell]:
+    return partial(kf.cells.straight.straight, layer=LAYER.WG, enclosure=wg_enc)
+
+
+@pytest.fixture
+def straight(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure) -> kf.KCell:
+    return kf.cells.straight.straight(
+        width=0.5, length=1, layer=LAYER.WG, enclosure=wg_enc
+    )
+
+
+@pytest.fixture
+def straight_blank(LAYER: kf.LayerEnum) -> kf.KCell:
+    return kf.cells.straight.straight(width=0.5, length=1, layer=LAYER.WG)
+
+
+@pytest.fixture
+def bend90(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnum) -> kf.KCell:
+    return kf.cells.circular.bend_circular(
+        width=0.5, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=90
+    )
+
+
+@pytest.fixture
+def bend90_small(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnum) -> kf.KCell:
+    return kf.cells.circular.bend_circular(
+        width=0.5, radius=5, layer=LAYER.WG, enclosure=wg_enc, angle=90
+    )
+
+
+@pytest.fixture
+def bend180(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure) -> kf.KCell:
+    return kf.cells.circular.bend_circular(
+        width=0.5, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=180
+    )
+
+
+@pytest.fixture
+def bend90_euler(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure) -> kf.KCell:
+    return kf.cells.euler.bend_euler(
+        width=0.5, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=90
+    )
+
+
+@pytest.fixture
+def bend180_euler(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure) -> kf.KCell:
+    return kf.cells.euler.bend_euler(
+        width=0.5, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=180
+    )
+
+
+@pytest.fixture
+def taper(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure) -> kf.KCell:
+    if kf.kcl.layout.cell("taper") is None:
+        c = kf.cells.taper.taper(
+            width1=0.5,
+            width2=1,
+            length=10,
+            layer=LAYER.WG,
+            enclosure=wg_enc,
+        )
+        c = c.dup()
+        c.name = "taper"
+    else:
+        c = kf.kcl["taper"]
+    return c
+
+
+@pytest.fixture
+def optical_port(LAYER: kf.LayerEnum) -> kf.Port:
+    return kf.Port(
+        name="o1",
+        trans=kf.kdb.Trans.R0,
+        layer=LAYER.WG,
+        width=500,
+        port_type="optical",
+    )
+
+
+@pytest.fixture
+def cells(
+    bend90: kf.KCell,
+    bend180: kf.KCell,
+    bend90_euler: kf.KCell,
+    taper: kf.KCell,
+    straight: kf.KCell,
+) -> list[kf.KCell]:
+    return [
+        bend90,
+        bend180,
+        bend90_euler,
+        taper,
+        straight,
+    ]
+
+
+@pytest.fixture
+def pdk(LAYER: LAYER_CLASS) -> kf.KCLayout:
+    layerstack = kf.LayerStack(
+        wg=kf.kcell.LayerLevel(
+            layer=LAYER.WG,
+            thickness=0.22,
+            zmin=0,
+            material="si",
+            info=kf.kcell.Info(mesh_order=1),
+        ),
+        clad=kf.kcell.LayerLevel(
+            layer=LAYER_CLASS.WGCLAD, thickness=3, zmin=0.22, material="sio2"
+        ),
+    )
+    kcl = kf.KCLayout("Test_PDK", layer_stack=layerstack)
+    return kcl
+
+
+@pytest.fixture
+@kf.kcl.cell
+def fill_cell() -> kf.KCell:
+    fc = kf.KCell()
+    fc.shapes(fc.kcl.layer(2, 0)).insert(kf.kdb.DBox(20, 40))
+    fc.shapes(fc.kcl.layer(3, 0)).insert(kf.kdb.DBox(30, 15))
+    return fc
---
+++ packages/kfactory/kfactory-0.18.3.tar.gz/kfactory-0.18.3/tests/test_cells.py
@@ -0,0 +1,144 @@
+import pathlib
+import pytest
+
+import kfactory as kf
+
+from functools import partial
+from kfactory.conf import logger
+
+
+class GeometryDifference(ValueError):
+    """Exception for Geometric differences."""
+
+    pass
+
+
+class LAYER(kf.LayerEnum):  # type: ignore[unused-ignore, misc]
+    kcl = kf.constant(kf.kcl)
+    WG = (1, 0)
+    WGCLAD = (111, 0)
+
+
+wg_enc = kf.LayerEnclosure(name="WGSTD", sections=[(LAYER.WGCLAD, 0, 2000)])
+
+
+def straight() -> kf.KCell:
+    return kf.cells.straight.straight(
+        width=0.5, length=1, layer=LAYER.WG, enclosure=wg_enc
+    )
+
+
+def bend90() -> kf.KCell:
+    return kf.cells.circular.bend_circular(
+        width=1, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=90
+    )
+
+
+def bend180() -> kf.KCell:
+    return kf.cells.circular.bend_circular(
+        width=1, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=180
+    )
+
+
+def bend90_euler() -> kf.KCell:
+    return kf.cells.euler.bend_euler(
+        width=1, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=90
+    )
+
+
+def bend180_euler() -> kf.KCell:
+    return kf.cells.euler.bend_euler(
+        width=1, radius=10, layer=LAYER.WG, enclosure=wg_enc, angle=180
+    )
+
+
+def taper() -> kf.KCell:
+    c = kf.cells.taper.taper(
+        width1=0.5,
+        width2=1,
+        length=10,
+        layer=LAYER.WG,
+        enclosure=wg_enc,
+    )
+    c = c.dup()
+    c.name = "taper"
+    return c
+
+
+cells = dict(
+    bend90=bend90,
+    bend180=bend180,
+    bend180_euler=bend180_euler,
+    bend90_euler=bend90_euler,
+    taper=taper,
+    straight=straight,
+)
+
+cell_names = set(cells.keys())
+
+
+@pytest.fixture(params=cell_names, scope="function")
+def cell_name(request: pytest.FixtureRequest) -> str:
+    """Returns cell name."""
+    return request.param  # type: ignore[no-any-return]
+
+
+def test_cells(cell_name: str) -> None:
+    """Ensure cells have the same geometry as their golden references."""
+    gds_ref = pathlib.Path(__file__).parent / "test_data" / "ref"
+    cell = cells[cell_name]()
+    ref_file = gds_ref / f"{cell.name}.gds"
+    run_cell = cell
+    if not ref_file.exists():
+        gds_ref.mkdir(parents=True, exist_ok=True)
+        run_cell.write(str(ref_file))
+        raise FileNotFoundError(f"GDS file not found. Saving it to {ref_file}")
+    kcl_ref = kf.KCLayout("TEST")
+    kcl_ref.read(gds_ref / f"{cell.name}.gds")
+    ref_cell = kcl_ref[kcl_ref.top_cell().name]
+
+    for layerinfo in kcl_ref.layer_infos():
+        layer = kcl_ref.layer(layerinfo)
+        region_run = kf.kdb.Region(run_cell.begin_shapes_rec(layer))
+        region_ref = kf.kdb.Region(ref_cell.begin_shapes_rec(layer))
+
+        region_diff = region_run - region_ref
+
+        if not region_diff.is_empty():
+            layer_tuple = kcl_ref.layer_infos()[layer]
+            region_xor = region_ref ^ region_run
+            c = kf.KCell(f"{cell.name}_diffs")
+            c_run = kf.KCell(f"{cell.name}_new")
+            c_ref = kf.KCell(f"{cell.name}_old")
+            c_xor = kf.KCell(f"{cell.name}_xor")
+            c_run.shapes(layer).insert(region_run)
+            c_ref.shapes(layer).insert(region_ref)
+            c_xor.shapes(layer).insert(region_xor)
+            c << c_run
+            c << c_ref
+            c << c_xor
+
+            kf.logger.critical(f"Differences found in {cell!r} on layer {layer_tuple}")
+            val = input("Save current GDS as new reference (Y)? [Y/n]")
+            if not val.upper().startswith("N"):
+                logger.info(f"replacing file {str(ref_file)!r}")
+                run_cell.write(ref_file.name)
+
+            raise GeometryDifference(
+                f"Differences found in {cell!r} on layer {layer_tuple}"
+            )
+
+
+def test_additional_info(LAYER: kf.LayerEnum, wg_enc: kf.LayerEnclosure) -> None:
+    test_bend_euler = partial(
+        kf.factories.euler.bend_euler_factory(
+            kcl=kf.kcl, additional_info={"creation_time": "2023-02-12Z23:00:00"}
+        ),
+        layer=kf.kcl.layer(1, 0),
+        radius=10,
+    )
+
+    bend = test_bend_euler(width=1)
+
+    assert bend._locked is True
+    assert bend.info.creation_time == "2023-02-12Z23:00:00"  # type: ignore[attr-defined, unused-ignore]
