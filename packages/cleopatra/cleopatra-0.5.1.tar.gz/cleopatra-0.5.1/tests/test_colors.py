ff4c7b3091a46501dfbf410915ea9736a5402172
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:46 +0000

    Add package cleopatra/0.5.1/cleopatra-0.5.1.tar.gz

---
+++ packages/cleopatra/cleopatra-0.5.1.tar.gz/cleopatra-0.5.1/tests/test_colors.py
@@ -0,0 +1,76 @@
+from cleopatra.colors import Colors
+
+
+class TestCreateColors:
+    def test_create_from_hex(self):
+        """test_create_colors_object."""
+        hex_number = "ff0000"
+        color = Colors(hex_number)
+        assert color._color_value == [hex_number]
+
+    def test_create_from_rgb(self):
+        """test_create_colors_object."""
+        rgb_color = (128, 51, 204)
+        color = Colors(rgb_color)
+        assert color._color_value == [rgb_color]
+
+
+def test_get_type():
+    """test_create_colors_object."""
+    mixed_color = [(128, 51, 204), "#23a9dd", (0.5, 0.2, 0.8)]
+    color = Colors(mixed_color)
+    color_types = color.get_type()
+    assert color_types == ["rgb", "hex", "rgb-normalized"]
+
+
+def test_is_valid_rgb_norm_255():
+    """test_create_colors_object."""
+    rgb_color = (128, 51, 204)
+    color = Colors(rgb_color)
+    assert color.is_valid_rgb_255(rgb_color) is True
+    rgb_color = (0.5, 0.2, 0.8)
+    color = Colors(rgb_color)
+    assert color.is_valid_rgb_norm(rgb_color) is True
+
+
+def test_is_valid_rgb():
+    """test_create_colors_object."""
+    rgb_color = [(128, 51, 204), (0.5, 0.2, 0.8)]
+    color = Colors(rgb_color)
+    assert all(color.is_valid_rgb())
+
+
+def test_is_valid():
+    """test_create_colors_object."""
+    hex_number = ["ff0000", "#23a9dd"]
+    color = Colors(hex_number)
+    valid = color.is_valid_hex()
+    assert valid == [False, True]
+
+
+def test_to_rgb():
+    """test_create_colors_object."""
+    hex_number = ["#ff0000", "#23a9dd", (0.5, 0.2, 0.8), (35, 169, 221)]
+    color = Colors(hex_number)
+    rgb_scale_1 = color.to_rgb(normalized=True)
+    assert rgb_scale_1 == [
+        (1.0, 0.0, 0.0),
+        (0.13725490196078433, 0.6627450980392157, 0.8666666666666667),
+        (0.5, 0.2, 0.8),
+        (0.13725490196078433, 0.6627450980392157, 0.8666666666666667),
+    ]
+    rgb_scale_255 = color.to_rgb(normalized=False)
+    assert rgb_scale_255 == [
+        (255, 0, 0),
+        (35, 169, 221),
+        (127, 51, 204),
+        (35, 169, 221),
+    ]
+
+
+def test_to_hex():
+    """test_create_colors_object."""
+    mixed_color = [(128, 51, 204), "#23a9dd", (0.5, 0.2, 0.8)]
+    color = Colors(mixed_color)
+    hex_colors = color.to_hex()
+    assert hex_colors == ["#8033cc", "#23a9dd", "#8033cc"]
