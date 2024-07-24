86c98ea56e881e98297b54ef1b22f7773fe55065
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:45 +0000

    Add package ai-assistant-manager/0.0.6/ai_assistant_manager-0.0.6-py3-none-any.whl

---
+++ packages/ai-assistant-manager/ai_assistant_manager-0.0.6-py3-none-any.whl/ai_assistant_manager/env_variables.py
@@ -0,0 +1,14 @@
+import os
+
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+OPENAI_MODEL = os.getenv("OPENAI_MODEL", "gpt-4o")
+
+ASSISTANT_DESCRIPTION = os.getenv("ASSISTANT_DESCRIPTION", "AI Assistant Manager")
+ASSISTANT_NAME = os.getenv("ASSISTANT_NAME", "AI Assistant Manager")
+BIN_DIR = os.getenv("BIN_DIR", "bin")
+DATA_DIR = os.getenv("DATA_DIR", "data")
+DATA_FILE_PREFIX = os.getenv("DATA_FILE_PREFIX", "AI Assistant Manager")
