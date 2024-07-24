54bab9a0a6cfa0423d750baf2d997d6e72cd683a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:38 +0000

    Add package getsecure/0.0.4/getsecure-0.0.4.tar.gz

---
+++ packages/getsecure/getsecure-0.0.4.tar.gz/getsecure-0.0.4/setup.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+"""
+setup package
+"""  # noqa: E501
+
+import setuptools
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setuptools.setup(
+    name='getsecure',
+    version='0.0.4',
+    packages=[''],
+    url='https://github.com/maratsh/getsecure',
+    license='MIT',
+    author='maratsh',
+    author_email='ya@maratsh.ru',
+    description='Utility for securing expiring links',
+    long_description_content_type='text/markdown',
+    long_description=long_description
+)
