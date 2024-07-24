3e9bbe5293240efadbafdd93200fd29f50ab5231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 06:04:38 +0000

    Add package pyclad/0.1.0a0/pyclad-0.1.0a0-py3-none-any.whl

---
+++ packages/pyclad/pyclad-0.1.0a0-py3-none-any.whl/pyclad/strategies/baselines/cumulative.py
@@ -0,0 +1,30 @@
+from typing import Dict
+
+import numpy as np
+
+from pyclad.models.model_base import Model
+from pyclad.strategies.strategy import (
+    ConceptAgnosticStrategy,
+    ConceptAwareStrategy,
+    ConceptIncrementalStrategy,
+)
+
+
+class CumulativeStrategy(ConceptIncrementalStrategy, ConceptAwareStrategy, ConceptAgnosticStrategy):
+    def __init__(self, model: Model):
+        self._replay = []
+        self._model = model
+
+    def learn(self, data: np.ndarray, *args, **kwargs) -> None:
+        """Learn from the data and store it in the replay buffer."""
+        self._replay.append(data)
+        self._model.learn(np.concatenate(self._replay))
+
+    def predict(self, data: np.ndarray, *args, **kwargs) -> (np.ndarray, np.ndarray):
+        return self._model.predict(data)
+
+    def name(self) -> str:
+        return "Cumulative"
+
+    def additional_info(self) -> Dict:
+        return {"model": self._model.name(), "replay_size": len(self._replay)}
