d6f03cb2c6f6142380db2e28727f8156e3806e6d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 05:04:37 +0000

    Add package rohmu/2.5.0/rohmu-2.5.0.tar.gz

---
+++ packages/rohmu/rohmu-2.5.0.tar.gz/rohmu-2.5.0/rohmu/errors.py
@@ -0,0 +1,47 @@
+# Copyright (c) 2016 Ohmu Ltd
+# See LICENSE for details
+"""Rohmu - exception classes"""
+
+
+class Error(Exception):
+    """Generic exception"""
+
+
+class StorageError(Error):
+    """Storage exception"""
+
+
+class FileNotFoundFromStorageError(StorageError):
+    """File not found from remote storage"""
+
+
+class InvalidConfigurationError(Error):
+    """Invalid configuration"""
+
+
+class LocalFileIsRemoteFileError(StorageError):
+    """File transfer operation source and destination point to the same file"""
+
+
+class MissingLibraryError(Exception):
+    """Missing dependency library"""
+
+
+class MaybeRecoverableError(Error):
+    """An error that may be recoverable"""
+
+
+class UninitializedError(Error):
+    """Error trying to access an uninitialized resource"""
+
+
+class InvalidByteRangeError(Error):
+    """Error specifying a content-range in a request"""
+
+
+class InvalidTransferError(Error):
+    """You tried to access a transfer object that you already returned to the pool"""
+
+
+class ConcurrentUploadError(StorageError):
+    """A generic error related to concurrent uploads"""
