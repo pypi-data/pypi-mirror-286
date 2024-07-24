9feb08b0e22767d8c31263e9cc5da7dbfbcca36c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:06:25 +0000

    Add package jijzept/1.18.1/jijzept-1.18.1-cp39-cp39-macosx_12_0_x86_64.whl

---
+++ packages/jijzept/jijzept-1.18.1-cp39-cp39-macosx_12_0_x86_64.whl/jijzept/sampler/mip/__init__.py
@@ -0,0 +1,12 @@
+from pkgutil import extend_path
+
+__path__ = extend_path(__path__, __name__)
+
+import jijzept.sampler.mip.minlp as minlp
+
+from jijzept.sampler.mip.minlp import JijMINLPSolver
+
+__all__ = [
+    "minlp",
+    "JijMINLPSolver",
+]
