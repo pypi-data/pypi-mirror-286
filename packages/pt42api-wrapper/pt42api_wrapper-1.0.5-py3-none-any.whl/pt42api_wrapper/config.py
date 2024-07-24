76df09f6c60ca5d5efc52a9baef0e89ad1ff50ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:44 +0000

    Add package pt42api-wrapper/1.0.5/pt42api_wrapper-1.0.5-py3-none-any.whl

---
+++ packages/pt42api-wrapper/pt42api_wrapper-1.0.5-py3-none-any.whl/pt42api_wrapper/config.py
@@ -0,0 +1,12 @@
+import os
+
+sentry = os.getenv('SENTRY_DSN', None)
+endpoint = os.getenv('API_ENDPOINT', None)
+auth_url = os.getenv('AUTH_URL', None)
+
+credentials = {
+    'grant_type': 'client_credentials',
+    'client_id': os.getenv('CLIENT_ID', None),
+    'client_secret': os.getenv('CLIENT_SECRET', None),
+    'scope': 'email profile openid',
+}
