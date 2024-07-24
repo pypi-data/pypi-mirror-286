aa90a27fb23d07aaf2971e18a3d8653f89dda431
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:05 +0000

    Add package ipfs-faiss/0.0.1/ipfs_faiss-0.0.1.tar.gz

---
+++ packages/ipfs-faiss/ipfs_faiss-0.0.1.tar.gz/ipfs_faiss-0.0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+	name='ipfs-faiss',
+	version='0.0.1',
+	packages=[
+		'ipfs_faiss',
+	],
+	install_requires=[
+        'ipfs_datasets@git+https://github.com/endomorphosis/ipfs_datasets.git',
+		'datasets',
+        "ipfs_model_manager@git+https://github.com/endomorphosis/ipfs_model_manager.git",
+        "orbitdb_kit@git@git+https://github.com/endomorphosis/orbitdb_kit.git",
+		"ipfs_kit@git+https://github.com/endomorphosis/ipfs_kit.git",
+		"faiss",
+		'urllib3',
+		'requests',
+		'boto3',
+	]
+)
