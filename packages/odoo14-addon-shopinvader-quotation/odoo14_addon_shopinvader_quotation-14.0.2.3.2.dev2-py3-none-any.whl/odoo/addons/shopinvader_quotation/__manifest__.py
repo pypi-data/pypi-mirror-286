4d5b3627d8151e4e644a5e4e3653ccbbeae6f81e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:29 +0000

    Add package odoo14-addon-shopinvader-quotation/14.0.2.3.2.dev2/odoo14_addon_shopinvader_quotation-14.0.2.3.2.dev2-py3-none-any.whl

---
+++ packages/odoo14-addon-shopinvader-quotation/odoo14_addon_shopinvader_quotation-14.0.2.3.2.dev2-py3-none-any.whl/odoo/addons/shopinvader_quotation/__manifest__.py
@@ -0,0 +1,23 @@
+# Copyright 2018 Akretion (http://www.akretion.com)
+# Copyright 2021 Camptocamp (http://www.camptocamp.com)
+# Benoît GUILLOT <benoit.guillot@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Shopinvader Quotation",
+    "summary": "Shopinvader Quotation",
+    "version": "14.0.2.3.1",
+    "category": "e-commerce",
+    "development_status": "Production/Stable",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "author": "Akretion",
+    "license": "AGPL-3",
+    "depends": ["shopinvader"],
+    "demo": ["demo/email_demo.xml", "demo/notification_demo.xml"],
+    "data": [
+        "views/product_view.xml",
+        "views/sale_view.xml",
+        "data/ir_export_product.xml",
+    ],
+    "installable": True,
+}
