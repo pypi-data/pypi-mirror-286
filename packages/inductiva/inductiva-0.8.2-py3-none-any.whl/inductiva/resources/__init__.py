a46380488dce09034919a3dcf23e85d1fd17f7c5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:26 +0000

    Add package inductiva/0.8.2/inductiva-0.8.2-py3-none-any.whl

---
+++ packages/inductiva/inductiva-0.8.2-py3-none-any.whl/inductiva/resources/__init__.py
@@ -0,0 +1,8 @@
+#pylint: disable=missing-module-docstring
+from .machines import MachineGroup, ElasticMachineGroup
+from . import machines_base
+from . import machine_groups
+from .machine_groups import estimate_machine_cost
+from .machine_cluster import MPICluster
+from .machine_types import list_available_machines
+from .machine_groups import get
