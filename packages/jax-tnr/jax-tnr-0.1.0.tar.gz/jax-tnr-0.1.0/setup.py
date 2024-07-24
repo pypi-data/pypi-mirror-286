82bd7b24fd34d33b0173656bffc0f0ad067809b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:35 +0000

    Add package jax-tnr/0.1.0/jax-tnr-0.1.0.tar.gz

---
+++ packages/jax-tnr/jax-tnr-0.1.0.tar.gz/jax-tnr-0.1.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="jax-tnr",
+    version="0.1.0",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    author="Thunder Compute",
+    author_email="support@thundercompute.com",
+    description="",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    install_requires=requirements,
+)
