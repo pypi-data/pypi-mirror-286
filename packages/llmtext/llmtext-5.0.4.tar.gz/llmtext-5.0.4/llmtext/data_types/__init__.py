c79df0e227b8dde2f6351c34d7c432765337bb84
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:31 +0000

    Add package llmtext/5.0.4/llmtext-5.0.4.tar.gz

---
+++ packages/llmtext/llmtext-5.0.4.tar.gz/llmtext-5.0.4/llmtext/data_types/__init__.py
@@ -0,0 +1,61 @@
+from abc import abstractmethod
+from typing import Annotated, Any
+from typing import Literal, TypedDict
+from pydantic import BaseModel, Field
+
+
+class Message(BaseModel):
+    role: str
+    content: str
+
+
+class Event(TypedDict):
+    step: int
+    type: Literal["tool_call", "tool_output", "message_stream", "message", "feedback"]
+    id: str
+    content: str
+
+
+class RunnableTool(BaseModel):
+    @abstractmethod
+    async def arun(self) -> str:
+        pass
+
+    def get_tool_call(self) -> dict[str, Any]:
+        tool_name = self.__class__.__name__
+        return {
+            "type": "tool_call",
+            "name": tool_name,
+            "description": self.__doc__ or "",
+            "params": self.model_dump(),
+        }
+
+    async def aget_tool_output(self) -> dict[str, Any]:
+        tool_name = self.__class__.__name__
+        return {
+            "type": "tool_output",
+            "name": tool_name,
+            "description": self.__doc__ or "",
+            "params": self.model_dump(),
+            "output": await self.arun(),
+        }
+
+
+class ToolSelector(BaseModel):
+    """Selected tools"""
+
+    choices: Annotated[
+        list[RunnableTool], Field(description="Selected tool to call")
+    ] = []
+
+
+class AnswerFeedback(BaseModel):
+    """Give feedback to your answer"""
+
+    answer_feedback: Annotated[
+        str | None,
+        Field(
+            description="Feedback to improve the answer, leave empty if none",
+            default=None,
+        ),
+    ]
