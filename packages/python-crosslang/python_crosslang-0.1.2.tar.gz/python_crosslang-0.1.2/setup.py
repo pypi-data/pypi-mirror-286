3a6ad7a0183213e956176be00af15d3c7f8cda7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:52 +0000

    Add package python-crosslang/0.1.2/python_crosslang-0.1.2.tar.gz

---
+++ packages/python-crosslang/python_crosslang-0.1.2.tar.gz/python_crosslang-0.1.2/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.md").read_text()
+
+setup(
+    name='python-crosslang',
+    version='0.1.2',
+    author='fl9ppy',
+    author_email='adi.calinescu16@gmail.com',
+    description='A Python library to run code from multiple programming languages.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/fl9ppy/crosslang',
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[],
+)
+
