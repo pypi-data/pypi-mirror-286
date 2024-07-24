410ee222210ab842a05507116a9f9a9b493ba0de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:03 +0000

    Add package gridtk/3.0.1/gridtk-3.0.1-py3-none-any.whl

---
+++ packages/gridtk/gridtk-3.0.1-py3-none-any.whl/gridtk/__main__.py
@@ -0,0 +1,9 @@
+# SPDX-FileCopyrightText: 2024 Idiap Research Institute <contact@idiap.ch>
+# SPDX-FileContributor: Amir Mohammadi  <amir.mohammadi@idiap.ch>
+#
+# SPDX-License-Identifier: GPL-3.0-or-later
+
+from .cli import cli
+
+if __name__ == "__main__":
+    cli()
