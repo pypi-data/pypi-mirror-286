cbace74f22ae61e5da899741bb657bb8d86f9056
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:15 +0000

    Add package bw2data/4.0.dev46/bw2data-4.0.dev46-py3-none-any.whl

---
+++ packages/bw2data/bw2data-4.0.dev46-py3-none-any.whl/bw2data/search/indices.py
@@ -0,0 +1,69 @@
+import os
+import shutil
+
+from whoosh import index
+
+from .. import projects
+from .schema import bw2_schema
+
+
+class IndexManager:
+    def __init__(self, database_path, dir_name="whoosh"):
+        self.path = os.path.join(projects.request_directory("whoosh"), database_path)
+        if not os.path.exists(self.path):
+            os.mkdir(self.path)
+
+    def get(self):
+        try:
+            return index.open_dir(self.path)
+        except index.EmptyIndexError:
+            return self.create()
+
+    def create(self):
+        self.delete_database()
+        os.mkdir(self.path)
+        return index.create_in(self.path, bw2_schema)
+
+    def _format_dataset(self, ds):
+        def _fix_location(string):
+            if isinstance(string, tuple):
+                string = string[1]
+            if isinstance(string, str):
+                if string.lower() == "none":
+                    return ""
+                else:
+                    return string.lower().strip()
+            else:
+                return ""
+
+        return dict(
+            name=(ds.get("name") or "").lower(),
+            comment=(ds.get("comment") or "").lower(),
+            product=(ds.get("reference product") or "").lower(),
+            categories=", ".join(ds.get("categories") or []).lower(),
+            synonyms=", ".join(ds.get("synonyms") or []).lower(),
+            location=_fix_location(ds.get("location") or ""),
+            database=ds["database"],
+            code=ds["code"],
+        )
+
+    def add_dataset(self, ds):
+        self.add_datasets([ds])
+
+    def add_datasets(self, datasets):
+        writer = self.get().writer()
+        for ds in datasets:
+            writer.add_document(**self._format_dataset(ds))
+        writer.commit()
+
+    def update_dataset(self, ds):
+        writer = self.get().writer()
+        writer.update_document(**self._format_dataset(ds))
+        writer.commit()
+
+    def delete_dataset(self, ds):
+        index = self.get()
+        index.delete_by_term("code", ds["code"])
+
+    def delete_database(self):
+        shutil.rmtree(self.path)
---
+++ packages/bw2data/bw2data-4.0.dev46-py3-none-any.whl/bw2data/search/search.py
@@ -0,0 +1,114 @@
+from whoosh.qparser import MultifieldParser
+from whoosh.query import And, Term
+
+from .indices import IndexManager
+
+
+def keysplit(strng):
+    """Split an activity key joined into a single string using the magic sequence `⊡|⊡`"""
+    return tuple(strng.split("⊡|⊡"))
+
+
+class Searcher:
+    def __init__(self, database):
+        self._database = database
+
+    def __enter__(self):
+        self.index = IndexManager(self._database).get()
+        return self
+
+    def __exit__(self, type, value, traceback):
+        self.index.close()
+
+    def search(
+        self,
+        string,
+        limit=25,
+        facet=None,
+        proxy=True,
+        boosts=None,
+        filter=None,
+        mask=None,
+        node_class=None,
+    ):
+        from .. import get_activity
+
+        lowercase = lambda x: x.lower() if hasattr(x, "lower") else x
+        string = lowercase(string)
+
+        fields = [
+            "name",
+            "comment",
+            "product",
+            "categories",
+            "synonyms",
+            "location",
+        ]
+
+        boosts = boosts or {
+            "name": 5,
+            "comment": 1,
+            "product": 3,
+            "categories": 2,
+            "synonyms": 3,
+            "location": 3,
+        }
+
+        qp = MultifieldParser(fields, self.index.schema, fieldboosts=boosts)
+
+        kwargs = {"limit": limit}
+        if filter is not None:
+            assert isinstance(filter, dict), "`filter` must be a dictionary"
+            for k in filter:
+                assert k in fields, "`filter` field {} not in search schema".format(k)
+            if len(filter) == 1:
+                kwargs["filter"] = [Term(k, lowercase(v)) for k, v in filter.items()][0]
+            else:
+                kwargs["filter"] = And(
+                    [Term(k, lowercase(v)) for k, v in filter.items()]
+                )
+        if mask is not None:
+            assert isinstance(mask, dict), "`mask` must be a dictionary"
+            for k in mask:
+                assert k in fields, "`mask` field {} not in search schema".format(k)
+            if len(mask) == 1:
+                kwargs["mask"] = [Term(k, lowercase(v)) for k, v in mask.items()][0]
+            else:
+                kwargs["mask"] = And([Term(k, lowercase(v)) for k, v in mask.items()])
+
+        with self.index.searcher() as searcher:
+            if facet is None:
+                results = searcher.search(qp.parse(string), **kwargs)
+                if "mask" in kwargs or "filter" in kwargs:
+                    print(
+                        "Excluding {} filtered results".format(results.filtered_count)
+                    )
+                results = [dict(obj.items()) for obj in results]
+            else:
+                kwargs.pop("limit")
+                results = {
+                    k: [searcher.stored_fields(i) for i in v]
+                    for k, v in searcher.search(
+                        qp.parse(string), groupedby=facet, **kwargs
+                    )
+                    .groups()
+                    .items()
+                }
+
+        if proxy and facet is not None:
+            return {
+                key: [
+                    get_activity(
+                        key=(obj["database"], obj["code"]), node_class=node_class
+                    )
+                    for obj in value
+                ]
+                for key, value in results.items()
+            }
+        elif proxy:
+            return [
+                get_activity(key=(obj["database"], obj["code"]), node_class=node_class)
+                for obj in results
+            ]
+        else:
+            return results
