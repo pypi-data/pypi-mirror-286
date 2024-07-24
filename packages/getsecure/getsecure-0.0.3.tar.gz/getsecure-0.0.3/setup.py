2815d88b52f376b6fb9bebf8cac78f6186f452de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:13 +0000

    Add package getsecure/0.0.3/getsecure-0.0.3.tar.gz

---
+++ packages/getsecure/getsecure-0.0.3.tar.gz/getsecure-0.0.3/setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='getsecure',
+    version='0.0.3',
+    packages=[''],
+    url='https://github.com/maratsh/getsecure',
+    license='MIT',
+    author='maratsh',
+    author_email='ya@maratsh.ru',
+    description='Utility for securing expiring links',
+    long_description="Utility for securing expiring links"
+)
