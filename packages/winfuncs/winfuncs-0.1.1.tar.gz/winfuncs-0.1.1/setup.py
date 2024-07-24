c8750276efd37cbe32cb22feb8b67ee56c4d8194
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:27 +0000

    Add package winfuncs/0.1.1/winfuncs-0.1.1.tar.gz

---
+++ packages/winfuncs/winfuncs-0.1.1.tar.gz/winfuncs-0.1.1/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+
+with open('README.md', 'r', encoding='utf-8') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='winfuncs',
+    version='0.1.1',
+    author='potichek',
+    author_email='bibilmeshka@gmail.com',
+    description='Simplifying various WinAPI functions, with this library you won`t need to use ctypes to call WinAPI functions.',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/potichek/WinFuncsLib/blob/main/WinFuncs',
+    packages=setuptools.find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'setuptools>=39.0.1',
+    ],
+    package_data={'winfuncs': ['WinFuncs.dll', 'KeyboardHookProc.dll']},
+)
