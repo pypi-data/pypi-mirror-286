5ff8738eda4e0bfc57fbe424ae3745c5bf907251
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:53 +0000

    Add package icij-worker/0.4.5/icij_worker-0.4.5.tar.gz

---
+++ packages/icij-worker/icij_worker-0.4.5.tar.gz/icij_worker-0.4.5/icij_worker/constants.py
@@ -0,0 +1,23 @@
+AMQP_EVENTS_X = "exchangeMainEvents"
+AMQP_EVENTS_QUEUE = "EVENT"
+AMQP_EVENTS_ROUTING_KEY = "routingKeyMainEvents"
+
+AMQP_RESULTS_X = "exchangeTaskResults"
+AMQP_RESULTS_QUEUE = "TASK_RESULT"
+AMQP_RESULTS_ROUTING_KEY = "routingKeyMainTaskResults"
+
+AMQP_RESULTS_DL_X = "exchangeDLQTaskResults"
+AMQP_RESULTS_DL_QUEUE = "TASK_RESULT_DLQ"
+AMQP_RESULTS_DL_ROUTING_KEY = "routingKeyDLQTaskResults"
+
+AMQP_TASKS_DL_X = "exchangeDLQTasks"
+AMQP_TASKS_DL_QUEUE = "TASK_DLQ"
+AMQP_TASKS_DL_ROUTING_KEY = "routingKeyDLQTasks"
+
+AMQP_TASKS_X = "exchangeMainTasks"
+AMQP_TASKS_QUEUE = "TASK"
+AMQP_TASKS_ROUTING_KEY = "routingKeyMainTasks"
+
+AMQP_WORKER_EVENTS_X = "exchangeRunnerEvents"
+AMQP_WORKER_EVENTS_QUEUE = "RUNNER_EVENT"
+AMQP_WORKER_EVENTS_ROUTING_KEY = "routingKeyRunnerEvents"
