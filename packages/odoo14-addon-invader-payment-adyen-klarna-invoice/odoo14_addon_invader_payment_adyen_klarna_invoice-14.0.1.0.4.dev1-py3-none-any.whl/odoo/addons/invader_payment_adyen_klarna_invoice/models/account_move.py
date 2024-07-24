05b6507444707e3f6223d3e09795266e0236d16f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:35 +0000

    Add package odoo14-addon-invader-payment-adyen-klarna-invoice/14.0.1.0.4.dev1/odoo14_addon_invader_payment_adyen_klarna_invoice-14.0.1.0.4.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-invader-payment-adyen-klarna-invoice/odoo14_addon_invader_payment_adyen_klarna_invoice-14.0.1.0.4.dev1-py3-none-any.whl/odoo/addons/invader_payment_adyen_klarna_invoice/models/account_move.py
@@ -0,0 +1,36 @@
+# Copyright 2023 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import models
+from odoo.fields import first
+
+
+class AccountMove(models.Model):
+    _inherit = "account.move"
+
+    def _get_shopper(self):
+        return self.partner_id
+
+    def _get_klarna_delivery(self):
+        return self.partner_shipping_id or super()._get_klarna_delivery()
+
+    def _get_klarna_internal_ref(self):
+        return self.ref or self.name
+
+    def _prepare_adyen_payment_klarna_line(
+        self, transaction, payment_method, line
+    ):
+        values = super()._prepare_adyen_payment_klarna_line(
+            transaction, payment_method, line
+        )
+        values.update(
+            {
+                "quantity": line.quantity,
+                "taxPercentage": self._get_formatted_amount(
+                    transaction, first(line.tax_ids).amount
+                ),
+                "amountIncludingTax": self._get_formatted_amount(
+                    transaction, line.price_unit
+                ),
+            }
+        )
+        return values
