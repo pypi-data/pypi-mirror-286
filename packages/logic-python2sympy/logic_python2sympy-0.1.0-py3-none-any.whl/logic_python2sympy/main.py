4b20962d08b546ef8fbc4dbc528d1b8d4d87df2e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:17 +0000

    Add package logic-python2sympy/0.1.0/logic_python2sympy-0.1.0-py3-none-any.whl

---
+++ packages/logic-python2sympy/logic_python2sympy-0.1.0-py3-none-any.whl/logic_python2sympy/main.py
@@ -0,0 +1,33 @@
+import ast
+import astor
+import sympy
+from typing import Any
+from logic_python2sympy.transformer import Transformer
+
+
+def convert(expr: str) -> str:
+    '''
+    Transform a `Python` logical expression into `Sympy` syntax.
+    expr: valid logical expression, i.e. expr must be valid `Python` syntax,
+    and must be a combintion of the following `ast` nodes:
+    `BoolOp, BinOp, UnaryOp, Compare, Call, Constant, Name`
+    raises: `NotImplentedError`, when the expression contains operators that are not supported in `Sympy`
+    '''
+
+    tree = ast.parse(expr)
+    transformer = Transformer()
+    new_tree = transformer.visit(tree)
+    return astor.to_source(new_tree)
+
+
+def get_expression(expr: str) -> Any:
+    '''
+    Transform a `Python` logical expression into a `Sympy` expression.
+    expr: valid logical expression, i.e. expr must be valid `Python` syntax,
+    and must be a combintion of the following `ast` nodes:
+    `BoolOp, BinOp, UnaryOp, Compare, Call, Constant, Name`
+    raises: `NotImplentedError`, when the expression contains operators that are not supported in `Sympy`
+    '''
+
+    new_expr = convert(expr)
+    return sympy.sympify(new_expr)
