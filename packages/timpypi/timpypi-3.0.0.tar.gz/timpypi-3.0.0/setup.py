0150e4437519e094669004f4607761306b1f8103
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 05:04:52 +0000

    Add package timpypi/3.0.0/timpypi-3.0.0.tar.gz

---
+++ packages/timpypi/timpypi-3.0.0.tar.gz/timpypi-3.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name="timpypi", version="3.0.0",
+      description="Pakages for Odoo Developer",
+      long_description="", packages=["timpypi"],
+      author="Bùi Đức Tuấn (Tim)",
+      author_email="buiductuan12081995@gmail.com",
+      )
