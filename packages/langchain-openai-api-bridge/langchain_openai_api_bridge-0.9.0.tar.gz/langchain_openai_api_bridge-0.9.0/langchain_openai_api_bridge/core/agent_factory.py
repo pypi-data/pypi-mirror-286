6f0b7e7be4be93bb4ce61f371028b5048b320967
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:06 +0000

    Add package langchain-openai-api-bridge/0.9.0/langchain_openai_api_bridge-0.9.0.tar.gz

---
+++ packages/langchain-openai-api-bridge/langchain_openai_api_bridge-0.9.0.tar.gz/langchain_openai_api_bridge-0.9.0/langchain_openai_api_bridge/core/agent_factory.py
@@ -0,0 +1,15 @@
+from abc import ABC, abstractmethod
+from langgraph.graph.graph import CompiledGraph
+from langchain_core.language_models import BaseChatModel
+from langchain_openai_api_bridge.core.create_agent_dto import CreateAgentDto
+
+
+class AgentFactory(ABC):
+
+    @abstractmethod
+    def create_agent(self, llm: BaseChatModel, dto: CreateAgentDto) -> CompiledGraph:
+        pass
+
+    @abstractmethod
+    def create_llm(self, dto: CreateAgentDto) -> BaseChatModel:
+        pass
