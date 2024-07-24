dcb5e83ef288282c60a8cd824c64748ca99372e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 06:04:11 +0000

    Add package pragma-sdk/2.0.0rc1/pragma_sdk-2.0.0rc1-py3-none-any.whl

---
+++ packages/pragma-sdk/pragma_sdk-2.0.0rc1-py3-none-any.whl/pragma_sdk/onchain/types/__init__.py
@@ -0,0 +1,31 @@
+from pragma_sdk.onchain.types.contract import Contract
+
+from pragma_sdk.onchain.types.types import (
+    ContractAddresses,
+    BlockNumber,
+    BlockId,
+    Network,
+    RequestStatus,
+    OracleResponse,
+    Checkpoint,
+    VRFRequestParams,
+    VRFSubmitParams,
+    VRFCancelParams,
+    RandomnessRequest,
+)
+
+
+__all__ = [
+    "Contract",
+    "BlockNumber",
+    "BlockId",
+    "ContractAddresses",
+    "Network",
+    "RequestStatus",
+    "OracleResponse",
+    "Checkpoint",
+    "VRFRequestParams",
+    "VRFSubmitParams",
+    "VRFCancelParams",
+    "RandomnessRequest",
+]
