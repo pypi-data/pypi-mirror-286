3b27d4550f965186d64b07802748ad2ce81baf26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:27 +0000

    Add package omotes-sdk-python/2.0.0/omotes_sdk_python-2.0.0-py3-none-any.whl

---
+++ packages/omotes-sdk-python/omotes_sdk_python-2.0.0-py3-none-any.whl/omotes_sdk/types.py
@@ -0,0 +1,7 @@
+from datetime import timedelta, datetime
+from typing import List, Union, Mapping
+
+ParamsDictValues = Union[
+    List["ParamsDictValues"], "ParamsDict", None, float, int, str, bool, datetime, timedelta
+]
+ParamsDict = Mapping[str, ParamsDictValues]
