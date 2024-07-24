8dd7b7ac7a34f7f5ef414849135d8f0ebc85ce26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:15 +0000

    Add package bw2data/4.0.dev46/bw2data-4.0.dev46.tar.gz

---
+++ packages/bw2data/bw2data-4.0.dev46.tar.gz/bw2data-4.0.dev46/bw2data/search/schema.py
@@ -0,0 +1,17 @@
+from whoosh.analysis import StandardAnalyzer
+from whoosh.fields import ID, TEXT, Schema
+
+bw2_schema = Schema(
+    name=TEXT(
+        stored=True,
+        sortable=True,
+        analyzer=StandardAnalyzer(stoplist=frozenset(), minsize=1),
+    ),
+    comment=TEXT(stored=True),
+    product=TEXT(stored=True, sortable=True),
+    categories=TEXT(stored=True),
+    synonyms=TEXT(stored=True),
+    location=TEXT(stored=True, sortable=True),
+    database=TEXT(stored=True),
+    code=ID(unique=True, stored=True),
+)
