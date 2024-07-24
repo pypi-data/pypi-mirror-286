37962d93e7810406ffde23cf9a7609144f216600
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:09 +0000

    Add package plexflow/0.0.12/plexflow-0.0.12.tar.gz

---
+++ packages/plexflow/plexflow-0.0.12.tar.gz/plexflow-0.0.12/plexflow/core/context/partial_context.py
@@ -0,0 +1,20 @@
+from plexflow.core.storage.object.plexflow_storage import PlexflowObjectStore
+from typing import Any
+
+class PartialContext:
+    def __init__(self, context_id: str, default_ttl: int, **kwargs) -> None:
+        self.context_id = context_id
+        self.default_ttl = default_ttl
+        self.store = PlexflowObjectStore(context_id, **kwargs)
+        
+    def set(self, key: str, value: Any, ttl: int = None) -> None:
+        self.store.store_temporarily(key=self.store.make_run_key(key), obj=value, ttl=ttl or self.default_ttl)
+    
+    def get(self, key: str) -> Any:
+        return self.store.retrieve(key=self.store.make_run_key(key))
+
+    def get_keys(self, pattern: str):
+        return self.store.retrieve_keys(self.store.make_run_key(pattern))
+    
+    def get_by_pattern(self, pattern: str):
+        return self.store.retrieve_values(self.store.make_run_key(pattern))
---
+++ packages/plexflow/plexflow-0.0.12.tar.gz/plexflow-0.0.12/plexflow/core/context/partials/__init__.py
@@ -0,0 +1,2 @@
+from plexflow.core.context.partials.ids import Ids
+from plexflow.core.context.partials.watchlist import Watchlist
