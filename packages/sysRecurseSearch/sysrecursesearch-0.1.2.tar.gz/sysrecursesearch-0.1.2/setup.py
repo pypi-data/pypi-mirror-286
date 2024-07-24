44444fd1b7aa566e5a10436f47de6923b72b9fe4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:40 +0000

    Add package sysRecurseSearch/0.1.2/sysrecursesearch-0.1.2.tar.gz

---
+++ packages/sysRecurseSearch/sysrecursesearch-0.1.2.tar.gz/sysrecursesearch-0.1.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sysRecurseSearch',
+    version='0.1.2',  # Increment the version number here
+    packages=find_packages(),
+    install_requires=[
+        # Add your package dependencies here
+    ],
+    entry_points={
+        'console_scripts': [
+            # Add console scripts here
+        ],
+    },
+)
