8f93e40cea8ac200717d184a1cdb0131f85414d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 03:04:53 +0000

    Add package event-tracking/3.0.0/event_tracking-3.0.0.tar.gz

---
+++ packages/event-tracking/event_tracking-3.0.0.tar.gz/event_tracking-3.0.0/setup.py
@@ -0,0 +1,94 @@
+"""
+Package metadata for event-tracking.
+"""
+
+import os
+import re
+
+from setuptools import find_packages, setup
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
+
+
+def is_requirement(line):
+    """
+    Return True if the requirement line is a package requirement;
+    that is, it is not blank, a comment, or editable.
+    """
+    # Remove whitespace at the start/end of the line
+    line = line.strip()
+
+    # Skip blank lines, comments, and editable installs
+    return not (
+        line == '' or
+        line.startswith('-c') or
+        line.startswith('-r') or
+        line.startswith('#') or
+        line.startswith('-e') or
+        line.startswith('git+')
+    )
+
+
+def load_requirements(*requirements_paths):
+    """
+    Load all requirements from the specified requirements files.
+    Returns a list of requirement strings.
+    """
+    requirements = set()
+    for path in requirements_paths:
+        requirements.update(
+            line.strip() for line in open(path, encoding='utf-8').readlines()
+            if is_requirement(line)
+        )
+    return list(requirements)
+
+
+def get_version(*file_paths):
+    """
+    Extract the version string from the file at the given relative path fragments.
+    """
+    filename = os.path.join(os.path.dirname(__file__), *file_paths)
+    with open(filename, encoding='utf-8') as opened_file:
+        version_file = opened_file.read()
+        version_match = re.search(r"^__version__ = ['\"]([^'\"]*)['\"]",
+                                  version_file, re.M)
+    if version_match:
+        return version_match.group(1)
+    raise RuntimeError('Unable to find version string.')
+
+
+VERSION = get_version("eventtracking", "__init__.py")
+README = open(os.path.join(os.path.dirname(__file__), 'README.rst'),
+              encoding='utf-8').read()
+REQUIREMENTS = load_requirements('requirements/base.in')
+
+
+setup(
+    name='event-tracking',
+    version=VERSION,
+    packages=find_packages(),
+    include_package_data=True,
+    license='AGPLv3 License',
+    description='A simple event tracking system.',
+    long_description=README,
+    install_requires=REQUIREMENTS,
+    url='https://github.com/openedx/event-tracking',
+    author='edX',
+    author_email='oscm@edx.org',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Framework :: Django :: 4.2',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.11',
+        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+    ],
+)
