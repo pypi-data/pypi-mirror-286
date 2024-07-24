6c4ad14d8023d2ab08872049ab5a70bc4543c0c3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:09 +0000

    Add package cbpi4-BLEHydrom/0.0.8/cbpi4-BLEHydrom-0.0.8.tar.gz

---
+++ packages/cbpi4-BLEHydrom/cbpi4-BLEHydrom-0.0.8.tar.gz/cbpi4-BLEHydrom-0.0.8/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup
+import os
+from setuptools.command.install import install
+from subprocess import check_call
+
+
+class PreInstallCommand(install):
+    """Pre-installation for installation mode."""
+    def run(self):
+        check_call("apt-get install pkg-config libboost-python-dev libboost-thread-dev libbluetooth-dev libglib2.0-dev python3-dev".split())
+        install.run(self)
+
+# read the contents of your README file
+from os import path
+this_directory = path.abspath(path.dirname(__file__))
+with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+
+setup(name='cbpi4-BLEHydrom',
+      version='0.0.8',
+      description='CraftBeerPi4 Plugin for Hydrom and Tilt (BLE connection)',
+      author='Alexander Vollkopf',
+      author_email='avollkopf@web.de',
+      url='https://github.com/avollkopf/cbpi4-BLEHydrom',
+      include_package_data=True,
+      package_data={
+        # If any package contains *.txt or *.rst files, include them:
+      '': ['*.txt', '*.rst', '*.yaml'],
+      'cbpi4-BLEHydrom': ['*','*.txt', '*.rst', '*.yaml']},
+      packages=['cbpi4-BLEHydrom'],
+      install_requires=[
+      'PyBluez @ https://github.com/AcrossTheCloud/pybluez/archive/master.zip#egg=PyBluez-0.30',
+      'gattlib-dbus @ https://github.com/oscaracena/pygattlib/archive/dbus.zip#egg=gattlib-dbus-24.1.8',
+      ],
+      long_description=long_description,
+      long_description_content_type='text/markdown'
+     )
