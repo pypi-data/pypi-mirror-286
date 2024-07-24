cd9991781828368358074af6478e2555ef9f2f4c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:35 +0000

    Add package odoo14-addon-invader-payment/14.0.1.0.6.dev1/odoo14_addon_invader_payment-14.0.1.0.6.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-invader-payment/odoo14_addon_invader_payment-14.0.1.0.6.dev1-py3-none-any.whl/odoo/addons/invader_payment/models/invader_payable.py
@@ -0,0 +1,79 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import api, models
+from odoo.tools.float_utils import float_round
+
+
+class InvaderPayable(models.AbstractModel):
+
+    _name = "invader.payable"
+    _description = "Interface for payable objects (e.g. cart, ...)"
+
+    def _invader_prepare_payment_transaction_data(self, acquirer_id):
+        """
+        Prepare a dictionary to create a ``payment.transaction`` for the
+        correct amount and linked to the payable object.
+
+        :param acquirer_id: ``payment.acquirer`` record
+        :return: dictionary suitable for ``payment.transaction`` ``create()``
+        """
+
+    def _invader_get_transactions(self):
+        """
+        Return payment transaction recordset depending on the payable model
+        """
+
+    def _invader_get_transactions_done(self):
+        """
+        Return only transactions considered as "done".
+        - state = "done"
+        - state = "authorized" (depending on the provider)
+        """
+        acquirer_authorize = (
+            self.env["payment.acquirer"]
+            ._get_feature_support()
+            .get("authorize", [])
+        )
+        transactions = self._invader_get_transactions()
+        transactions_done = transactions.filtered(
+            lambda tr: tr.state == "done"
+        )
+        if acquirer_authorize:
+            transactions_done |= transactions.filtered(
+                lambda tr: tr.acquirer_id.provider in acquirer_authorize
+                and tr.state == "authorized"
+            )
+        return transactions_done
+
+    def _get_payable_lines(self):
+        """
+        Return payable lines
+        """
+
+    def _get_shopper_partner(self):
+        raise NotImplementedError()
+
+    def _get_billing_partner(self):
+        return self._get_shopper_partner()
+
+    def _get_delivery_partner(self):
+        return self._get_shopper_partner()
+
+    def _get_internal_ref(self):
+        """
+        Get the reference to put on the transaction
+        """
+        raise NotImplementedError()
+
+    @api.model
+    def _get_formatted_amount(self, transaction, amount):
+        """
+        The expected amount format by Adyen
+        :param amount: float
+        :return: int
+        """
+        dp_name = transaction.acquirer_id.provider or ""
+        if dp_name:
+            dp_name = dp_name.capitalize()
+        digits = self.env["decimal.precision"].precision_get(dp_name)
+        return float_round(amount, digits)
---
+++ packages/odoo14-addon-invader-payment/odoo14_addon_invader_payment-14.0.1.0.6.dev1-py3-none-any.whl/odoo/addons/invader_payment/services/invader_payment_service.py
@@ -0,0 +1,95 @@
+# Copyright 2019 ACSONE SA/NV
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+from odoo import _
+from odoo.exceptions import UserError
+
+from odoo.addons.component.core import Component
+
+
+class InvaderPaymentService(Component):
+
+    _name = "invader.payment.service"
+    _usage = "invader.payment"
+
+    def _invader_find_payable_from_target(self, target, **params):
+        """
+        Find an invader.payable from a target parameter (e.g. current cart).
+
+        target and params comply with the schema returned by
+        ``_invader_get_target_validator``.
+        """
+        raise NotImplementedError()
+
+    def _invader_get_target_validator(self):
+        """
+        Return a cerberus validator schema fragment that specifies the
+        target being paid. Implementations must extend it by populating
+        the "allowed" field (eg with strings such as 'current_cart') and
+        possibly adding other fields.
+        """
+        return {"target": {"type": "string", "required": True, "allowed": []}}
+
+    def _check_provider(self, acquirer_id, provider):
+        """Check that the payment mode has the correct provider
+        If the provider is not the same, raise an error
+        """
+        acquirer = acquirer_id.sudo()
+        if acquirer.provider != provider:
+            raise UserError(
+                _(
+                    "Payment mode acquirer mismatch should be "
+                    "'{}' instead of '{}'."
+                ).format(provider, acquirer.provider)
+            )
+
+    def _get_transaction_validator(self):
+        """This is intended to be called by each implentation where transactions
+        dict is added to response
+
+        :return: _description_
+        :rtype: _type_
+        """
+        return {
+            "date": {"type": "datetime"},
+            "acquirer": {"type": "dict", "required": True},
+            "state": {
+                "type": "string",
+                "allowed": [
+                    "draft",
+                    "pending",
+                    "authorized",
+                    "done",
+                    "cancel",
+                    "error",
+                ],
+            },
+            "amount": {"type": "float"},
+        }
+
+    def _get_transactions_validator(self):
+        """This is intended to be called by each implentation where transactions
+        dict is added to response
+
+        :return: _description_
+        :rtype: _type_
+        """
+        return {
+            "transactions": {
+                "type": "list",
+                "schema": self._get_transaction_validator(),
+            }
+        }
+
+    def _json_parser(self):
+        res = [
+            "id",
+            "date",
+            ("acquirer_id:acquirer", ["id", "display_name:name"]),
+            "state",
+            "amount",
+        ]
+        return res
+
+    # Provide a way to retrieve transactions through payable object
+    def _to_json(self, transactions):
+        return {"transactions": transactions.jsonify(self._json_parser())}
