957f6898fd913dfce4204cfcd64aa92c547a43fc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:18 +0000

    Add package kwhmeter-utils/0.0.0/kwhmeter_utils-0.0.0.tar.gz

---
+++ packages/kwhmeter-utils/kwhmeter_utils-0.0.0.tar.gz/kwhmeter_utils-0.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="kwhmeter_utils",
+    version_config=True,
+    setup_requires=['setuptools-git-versioning'],
+    author="nachomas",
+    author_email="mas.ignacio@gmail.com",
+    description="Simulador de facturas PVPC",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nachoplus/kwhmeter_util",
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'': ['data/*.yml']},    
+    install_requires=[
+        'kwhmeter',
+        'pandas',
+        'influxdb'
+    ],
+    extras_require={
+
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
