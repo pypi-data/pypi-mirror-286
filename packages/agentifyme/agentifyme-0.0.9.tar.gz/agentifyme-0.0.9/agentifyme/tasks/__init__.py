8be718531d726d52d9404d79bd229cc812f96e7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:40 +0000

    Add package agentifyme/0.0.9/agentifyme-0.0.9.tar.gz

---
+++ packages/agentifyme/agentifyme-0.0.9.tar.gz/agentifyme-0.0.9/agentifyme/tasks/__init__.py
@@ -0,0 +1,4 @@
+from .structured_data_extractor_task import StructuredDataExtractorTask
+from .task import Task, TaskConfig, task
+
+__all__ = ["Task", "StructuredDataExtractorTask", "task", "TaskConfig"]
