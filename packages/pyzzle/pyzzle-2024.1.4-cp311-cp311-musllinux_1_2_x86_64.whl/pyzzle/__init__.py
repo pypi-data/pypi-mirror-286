fb4c3e4abe880d48d45baf3bd3a6b4d6f6ccff0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 07:05:13 +0000

    Add package pyzzle/2024.1.4/pyzzle-2024.1.4-cp311-cp311-musllinux_1_2_x86_64.whl

---
+++ packages/pyzzle/pyzzle-2024.1.4-cp311-cp311-musllinux_1_2_x86_64.whl/pyzzle/__init__.py
@@ -0,0 +1,14 @@
+__version__ = "0.1.0"
+
+from pyzzle.Puzzle import Puzzle
+from pyzzle import futils
+from pyzzle.Word import Word
+from pyzzle.Dictionary import Dictionary
+from pyzzle.Placeable import Placeable
+from pyzzle.ObjectiveFunction import ObjectiveFunction
+from pyzzle.Judgement import Judgement
+from pyzzle.History import HistoryItem, HistoryItemMove, HistoryCode
+from pyzzle.Mask import Mask
+
+from pyzzle.PyzzleAPI import PyzzleAPI
+from pyzzle.Gravity import Gravity
