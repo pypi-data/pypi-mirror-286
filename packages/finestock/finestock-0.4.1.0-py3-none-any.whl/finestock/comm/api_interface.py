6a26ff921559329dea09c3585d43b19088ae389f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:06:10 +0000

    Add package finestock/0.4.1.0/finestock-0.4.1.0-py3-none-any.whl

---
+++ packages/finestock/finestock-0.4.1.0-py3-none-any.whl/finestock/comm/api_interface.py
@@ -0,0 +1,35 @@
+from abc import ABC, abstractmethod
+
+
+class IAPI(ABC):
+    @abstractmethod
+    def oauth(self):
+        raise NotImplemented
+
+    @abstractmethod
+    def get_ohlcv(self, code, frdate, todate):
+        raise NotImplemented
+
+    @abstractmethod
+    def get_index(self, code, frdate, todate):
+        raise NotImplemented
+
+    @abstractmethod
+    def get_orderbook(self, code):
+        raise NotImplemented
+
+    @abstractmethod
+    def set_queue(self, queue, condition):
+        raise NotImplemented
+
+    @abstractmethod
+    def set_price_queue(self, price_queue, condition):
+        raise NotImplemented
+
+    @abstractmethod
+    def set_trade_queue(self, trade_queue, condition):
+        raise NotImplemented
+
+    @abstractmethod
+    def set_orderbook_queue(self, orderbook_queue, condition):
+        raise NotImplemented
