32d6686b65ff92c9e066b5961a219fb67781dd87
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:05:01 +0000

    Add package libpbskids-dl/3.2.0/libpbskids_dl-3.2.0.tar.gz

---
+++ packages/libpbskids-dl/libpbskids_dl-3.2.0.tar.gz/libpbskids_dl-3.2.0/test.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+# ONLY TO BE USED FOR TESTING libpbskids-dl
+import libpbskids_dl
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/wild-kratts-our-blue-and-green-world/1463710")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/nature-cats-nature-movie-special-extraordinaire/1485721")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/a-pinkerton-family-vacation/1604332")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/tiger-family-trip/27735")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/the-rhythm-and-roots-of-arthur/102371")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/the-cat-in-the-hat-knows-a-lot-about-space/29830")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/a-pinkaperfect-birthday/82688")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/dinosaurs-a-to-z/1561024")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/peg-and-cat-save-the-world-part-1-and-part-2/26249")
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/wild-kratts-our-blue-and-green-world/1463710", {"filename": "aa1.mp4"})
+libpbskids_dl.download("https://pbskids.org/videos/watch/pbs-kids-movies/1413130/wild-kratts-our-blue-and-green-world/1463710", {"filename": "aa2"})
