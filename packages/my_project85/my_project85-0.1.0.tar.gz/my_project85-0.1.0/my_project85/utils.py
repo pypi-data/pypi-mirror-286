c436a33fcaf4a4d36e941034c1a2f42e1e5a4826
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:06:03 +0000

    Add package my_project85/0.1.0/my_project85-0.1.0.tar.gz

---
+++ packages/my_project85/my_project85-0.1.0.tar.gz/my_project85-0.1.0/my_project85/utils.py
@@ -0,0 +1,11 @@
+# utils.py
+
+import logging
+
+# Get a logger instance for the utils module
+logger = logging.getLogger(__name__)
+
+def simple_utility_function():
+    logger.debug("Running the utility function")
+    # Some utility operations
+    logger.info("Utility function operation completed")
