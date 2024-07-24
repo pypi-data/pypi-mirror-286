72ce3c2e139670ff310447953bc5e24d9e009cfa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:26 +0000

    Add package odoo13-addon-shopinvader-customer-multi-user/13.0.2.3.1.dev1/odoo13_addon_shopinvader_customer_multi_user-13.0.2.3.1.dev1-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-customer-multi-user/odoo13_addon_shopinvader_customer_multi_user-13.0.2.3.1.dev1-py3-none-any.whl/odoo/addons/shopinvader_customer_multi_user/tests/common.py
@@ -0,0 +1,107 @@
+# Copyright 2019 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+from odoo.addons.shopinvader.tests.test_customer import TestCustomerCommon
+
+
+class TestMultiUserCommon(TestCustomerCommon):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.company_binding = cls._create_partner(
+            cls.env, invader_user_token="ABCDEF", is_company=True
+        )
+        cls.company = cls.company_binding.record_id
+        cls.user_binding = cls._create_partner(
+            cls.env,
+            name="Simple user",
+            parent_id=cls.company.id,
+            external_id="simple-user",
+            email="simpleuser@test.com",
+        )
+        # Create 2 more users so that we have
+        # 1 company user with 3 simple users
+        # and user 3 is a child of user 2.
+        cls.user_binding2 = cls._create_partner(
+            cls.env,
+            name="Simple user 2",
+            parent_id=cls.company.id,
+            external_id="simple-user-2",
+            email="simpleuser2@test.com",
+        )
+        cls.user_binding3 = cls._create_partner(
+            cls.env,
+            name="Simple user 3",
+            parent_id=cls.user_binding2.record_id.id,
+            main_partner_id=cls.user_binding2.record_id.id,
+            external_id="simple-user-3",
+            email="simpleuser3@test.com",
+        )
+        cls.backend.multi_user_records_policy = "record_id"
+        cls.backend.customer_multi_user = True
+
+    @staticmethod
+    def _create_partner(env, **kw):
+        values = {
+            "backend_id": env.ref("shopinvader.backend_1").id,
+            "name": "ACME ltd",
+            "external_id": "acme",
+            "email": "company@test.com",
+            "ref": "#ACME",
+        }
+        values.update(kw)
+        return env["shopinvader.partner"].create(values)
+
+    def _get_service(self, partner, usage="users"):
+        with self.work_on_services(
+            partner=partner, shopinvader_session=self.shopinvader_session
+        ) as work:
+            return work.component(usage=usage)
+
+
+class TestUserManagementCommmon(TestMultiUserCommon):
+    """Test interaction with /users endpoint.
+    """
+
+    def _get_service(self, partner, usage="users"):
+        with self.work_on_services(partner=partner) as work:
+            return work.component(usage=usage)
+
+    def _test_search(self, service, expected):
+        _res = sorted(
+            service.dispatch("search")["data"], key=lambda x: x["email"]
+        )
+        self.assertEqual(
+            _res,
+            [
+                {
+                    "email": x.email,
+                    "id": x.id,
+                    "name": x.name,
+                    "parent_id": x.parent_id.id,
+                    "state": x.state,
+                    "can_manage_users": x.can_manage_users,
+                }
+                for x in expected.sorted("email")
+            ],
+        )
+
+    def _test_create(self, service, params, expected_parent=None):
+        partner = expected_parent or service.partner_user
+        count_before = len(partner.child_ids)
+        res = service.dispatch("create", params=params)["data"]
+        self.assertEqual(len(partner.child_ids), count_before + 1)
+        new_partner = partner.child_ids.filtered_domain(
+            [("email", "=", params["email"])]
+        )
+        invader_partner = new_partner._get_invader_partner(self.backend)
+        expected = dict(
+            params,
+            parent_id=partner.id,
+            state="active",
+            id=invader_partner.id,
+            can_manage_users=invader_partner.can_manage_users,
+        )
+        self.assertEqual(res, expected)
+        return invader_partner
---
+++ packages/odoo13-addon-shopinvader-customer-multi-user/odoo13_addon_shopinvader_customer_multi_user-13.0.2.3.1.dev1-py3-none-any.whl/odoo/addons/shopinvader_customer_multi_user/tests/test_multi_user_token.py
@@ -0,0 +1,38 @@
+# Copyright 2019 Camptocamp (http://www.camptocamp.com).
+# @author Simone Orsi <simahawk@gmail.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+import random
+
+from .common import TestMultiUserCommon
+
+
+class TestMultiUserToken(TestMultiUserCommon):
+    """Test partner token generation and lookup.
+    """
+
+    def test_token_manual_override(self):
+        self.assertEqual(self.company.invader_user_token, "ABCDEF")
+
+    def _generate_random_companies(self, count=5):
+        tokens = set()
+        for x in range(5):
+            comp = self._create_partner(
+                self.env,
+                name="ACME %s" % x,
+                email="acme%s@test.com" % x,
+                is_company=True,
+                external_id="acme%s" % (x + random.randint(1, 100)),
+            )
+            if comp.invader_user_token:
+                tokens.add(comp.invader_user_token)
+        return tokens
+
+    def test_token_auto_gen_disabled(self):
+        self.backend.customer_multi_user = False
+        tokens = self._generate_random_companies()
+        self.assertEqual(len(tokens), 0)
+
+    def test_token_auto_gen(self):
+        tokens = self._generate_random_companies()
+        self.assertEqual(len(tokens), 5)
