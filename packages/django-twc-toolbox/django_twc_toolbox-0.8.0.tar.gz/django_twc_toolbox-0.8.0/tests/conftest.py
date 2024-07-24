bec46212c087a64c18f2b6ead24a32e1a881b400
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 06:04:37 +0000

    Add package django-twc-toolbox/0.8.0/django_twc_toolbox-0.8.0.tar.gz

---
+++ packages/django-twc-toolbox/django_twc_toolbox-0.8.0.tar.gz/django_twc_toolbox-0.8.0/tests/conftest.py
@@ -0,0 +1,41 @@
+from __future__ import annotations
+
+import logging
+
+import pytest
+from django.conf import settings
+from django.http import HttpRequest
+
+from .settings import DEFAULT_SETTINGS
+
+pytest_plugins = []  # type: ignore
+
+
+def pytest_configure(config):
+    logging.disable(logging.CRITICAL)
+
+    settings.configure(**DEFAULT_SETTINGS, **TEST_SETTINGS)
+
+
+TEST_SETTINGS = {
+    "INSTALLED_APPS": [
+        "django_twc_toolbox",
+        "django.contrib.admin",
+        "django.contrib.auth",
+        "django.contrib.contenttypes",
+        "simple_history",
+        "tests.dummy",
+        "tests.test_crud",
+    ],
+    "TEMPLATES": [
+        {
+            "BACKEND": "django.template.backends.django.DjangoTemplates",
+            "APP_DIRS": True,
+        }
+    ],
+}
+
+
+@pytest.fixture
+def req():
+    return HttpRequest()
