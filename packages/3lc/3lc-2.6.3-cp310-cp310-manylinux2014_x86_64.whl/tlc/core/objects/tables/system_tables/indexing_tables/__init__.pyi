fd3c329d1cece770aa6c6af87572d6c85658481e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 07:05:27 +0000

    Add package 3lc/2.6.3/3lc-2.6.3-cp310-cp310-manylinux2014_x86_64.whl

---
+++ packages/3lc/3lc-2.6.3-cp310-cp310-manylinux2014_x86_64.whl/tlc/core/objects/tables/system_tables/indexing_tables/__init__.pyi
@@ -0,0 +1,2 @@
+from .run_indexing_table import RunIndexingTable as RunIndexingTable
+from .table_indexing_table import TableIndexingTable as TableIndexingTable
---
+++ packages/3lc/3lc-2.6.3-cp310-cp310-manylinux2014_x86_64.whl/tlcsaas/pyarmor_runtime_005329/__init__.py
@@ -0,0 +1,2 @@
+# Pyarmor 8.5.10 (pro), 005329, 2024-07-23T00:12:57.967622
+from .pyarmor_runtime import __pyarmor__
