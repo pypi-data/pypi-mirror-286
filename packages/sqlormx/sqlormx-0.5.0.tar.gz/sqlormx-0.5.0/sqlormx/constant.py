466c907c60d8f5f2f470d425b5ecc36d47f9632d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:32 +0000

    Add package sqlormx/0.5.0/sqlormx-0.5.0.tar.gz

---
+++ packages/sqlormx/sqlormx-0.5.0.tar.gz/sqlormx-0.5.0/sqlormx/constant.py
@@ -0,0 +1,14 @@
+from sqlexecx.constant import CACHE_SIZE
+
+NO_LIMIT = 0
+
+MAPPER_PATH = 'mapper_path'
+
+DEFAULT_KEY_FIELD = 'id'
+
+KEY, SELECT_KEY, TABLE, UPDATE_BY, UPDATE_TIME, DEL_FLAG, KEY_STRATEGY = '__key__', '__select_key__', '__table__', \
+                                                                         '__update_by__', '__update_time__',\
+                                                                         '__del_flag__', '__key_strategy__'
+
+KEY_SEQ = '__key_seq__'
+
