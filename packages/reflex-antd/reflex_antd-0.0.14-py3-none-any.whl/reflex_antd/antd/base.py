116dd000b8645ee2d739a09b2d96523fcc2e38af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:53 +0000

    Add package reflex-antd/0.0.14/reflex_antd-0.0.14-py3-none-any.whl

---
+++ packages/reflex-antd/reflex_antd-0.0.14-py3-none-any.whl/reflex_antd/antd/base.py
@@ -0,0 +1,128 @@
+from typing import Optional, Union, Dict
+from types import SimpleNamespace
+from functools import lru_cache
+
+import reflex as rx
+from reflex import Var, Component
+from reflex.vars import BaseVar, VarData
+from reflex.utils import imports
+
+
+from .. import base
+from ..base import AntdComponent, ContainVar, JsValue
+from ..constant import SizeType
+
+
+next_theme_var_data = VarData(  # type: ignore
+    imports={
+        f"next-themes": {imports.ImportVar(tag="useTheme")},
+        "react": {imports.ImportVar(tag="useContext")},
+        "antd": {imports.ImportVar(tag="theme", alias="AntdTheme")}
+    },
+    hooks={
+        f"const nextTheme = useTheme()": None,
+    },
+)
+next_theme_var = BaseVar(
+    _var_name='nextTheme.theme',
+    _var_type="str",
+    _var_data=next_theme_var_data,
+)
+light_theme_var = BaseVar(
+    _var_name='AntdTheme.defaultAlgorithm',
+)
+dark_theme_var = BaseVar(
+    _var_name='AntdTheme.darkAlgorithm',
+)
+
+
+def theme(**kwargs):
+    if 'algorithm' not in kwargs:
+        kwargs['algorithm'] = rx.cond(next_theme_var == 'dark', dark_theme_var, light_theme_var)
+    return ContainVar.create(**kwargs)
+
+
+class Locale(JsValue):
+
+    @property
+    def lang(self) -> str:
+        return self.value.replace("_", "")
+
+    def serialize(self) -> str:
+        return self.lang
+
+    def get_imports(self) -> imports.ImportDict:
+        return {
+            f'antd/locale/{self.value}': [imports.ImportVar(tag=self.lang, install=False, is_default=True)],
+        }
+
+
+class ConfigProvider(AntdComponent):
+    """Top level antd provider must be included in any app using antd components."""
+
+    tag = "ConfigProvider"
+    alias = "AntdConfigProvider"
+
+    auto_insert_space_in_button: Optional[Var[bool]]
+    component_disabled: Optional[Var[bool]]
+    component_size: Optional[Var[SizeType]]
+    csp: Optional[Var[ContainVar]]
+    direction: Optional[Var[str]]
+    get_popup_container: Optional[Var[JsValue]]
+    get_target_container: Optional[Var[JsValue]]
+    icon_prefix_cls: Optional[Var[str]]
+    locale: Optional[Var[Locale]]
+    popup_match_select_width: Optional[Var[Union[bool, int]]]
+    popup_overflow: Optional[Var[str]]
+    prefix_cls: Optional[Var[str]]
+    render_empty: Optional[Var[JsValue]]
+    theme: Optional[Var[Union[Dict, ContainVar]]]
+    virtual: Optional[Var[bool]]
+    warning: Optional[Var[Dict]]
+
+    @classmethod
+    def create(cls, *children, **props) -> Component:
+        """Create a new ConfigProvider component.
+
+        Returns:
+            A new ConfigProvider component.
+        """
+        # if 'theme' not in props:
+        #     theme = Var.create('theme.styles.global.body.antd', _var_is_local=False)
+        #     props['theme'] = theme
+
+        return super().create(
+            *children,
+            **props
+        )
+
+    def _get_imports(self) -> imports.ImportDict:
+        _imports = super()._get_imports()
+        _imports.setdefault("/utils/theme.js", []).append(
+            imports.ImportVar(tag="theme", is_default=True),
+        )
+        _imports.setdefault("@ant-design/cssinjs", []).append(
+            imports.ImportVar(tag="Theme", alias='antdTheme'),
+        )
+        return _imports
+
+    @staticmethod
+    @lru_cache(maxsize=None)
+    def _get_app_wrap_components() -> dict[tuple[int, str], Component]:
+        """ support app router """
+        if base.APP_ROUTER:
+            return {
+                (170, "AntdRegistryProvider"): antd_registry_provider(),
+            }
+        else:
+            return {}
+
+
+class AntdRegistryProvider(Component):
+    library = "@ant-design/nextjs-registry"
+    tag = "AntdRegistry"
+
+
+config_provider = ConfigProvider.create
+antd_registry_provider = AntdRegistryProvider.create
+
---
+++ packages/reflex-antd/reflex_antd-0.0.14-py3-none-any.whl/reflex_antd/antd/form.py
@@ -0,0 +1,181 @@
+import uuid
+from typing import Optional, Union, Dict, Any, List
+from reflex import Var, Component
+
+from ..base import (AntdComponent, ContainVar, JsValue, ReactNode, js_value,
+                    memo_never_no_recursive, memo_always_no_recursive)
+from ..constant import AlignType, DirectionType, SizeType, VariantType
+
+
+class Form(AntdComponent):
+    tag = 'Form'
+
+    colon: Optional[Var[bool]]
+    component: Optional[Var[Union[bool, ReactNode]]]
+    disabled: Optional[Var[bool]]
+    fields: Optional[Var[ContainVar]]
+    form: Optional[Var[str]]
+    initial_values: Optional[Var[Dict]]
+    label_align: Optional[Var[AlignType]]
+    label_wrap: Optional[Var[bool]]
+    label_col: Optional[Var[Dict]]
+    layout: Optional[Var[DirectionType]]
+    name: Optional[Var[str]]
+    preserve: Optional[Var[bool]]
+    required_mark: Optional[Var[Union[bool, JsValue]]]
+    scrollTo_first_error: Optional[Var[Union[bool, Dict]]]
+    size: Optional[Var[SizeType]]
+    validate_messages: Optional[Var[Dict]]
+    validate_trigger: Optional[Var[Union[str, List[str]]]]
+    variant: Optional[Var[VariantType]]
+    wrapper_col: Optional[Var[Dict]]
+
+    # form and form.item can not split, if split some components like select don't work;
+    _memoization_mode = memo_never_no_recursive
+
+    @classmethod
+    def create(cls, *children, **props) -> Component:
+        if 'form' in props and isinstance(props['form'], str):
+            props['form'] = Var.create_safe(f'{props["form"]}', _var_is_local=False, _var_is_string=False)
+        comp = super().create(*children, **props)
+        if comp._get_all_hooks() or comp._get_all_hooks_internal():
+            comp._memoization_mode = memo_always_no_recursive
+        return comp
+
+    def _get_hooks(self) -> str | None:
+        if hasattr(self, 'form') and self.form is not None:
+            return f"const [{str(self.form).strip('{}')}] = Form.useForm();"
+
+    def get_event_triggers(self) -> Dict[str, Any]:
+        _triggers = super().get_event_triggers()
+        _triggers.update({
+            "on_fields_change": lambda changed, all: [changed],
+            'on_finish': lambda values: [values],
+            "on_finish_failed": lambda ev: [ev.values, ev.errorFields, ev.outOfDate],
+            "on_values_change": lambda changed, all: [changed, all],
+        })
+        return _triggers
+
+
+class FormItem(AntdComponent):
+    tag = 'Form.Item'
+
+    colon: Optional[Var[bool]]
+    dependencies: Optional[Var[List[Union[str, int, List[Union[str, int]]]]]]
+    extra: Optional[Var[ReactNode]]
+    getValue_from_event: Optional[Var[JsValue]]
+    get_value_props: Optional[Var[JsValue]]
+    has_feedback: Optional[Var[Union[bool, ContainVar]]]
+    help: Optional[Var[ReactNode]]
+    hidden: Optional[Var[bool]]
+    html_for: Optional[Var[str]]
+    initial_value: Optional[Var[str]]
+    label: Optional[Var[str]]
+    label_align: Optional[Var[AlignType]]
+    label_col: Optional[Var[Dict]]
+    message_variables: Optional[Var[JsValue]]
+    name: Optional[Var[Union[str, int, List[Union[str, int]]]]]
+    normalize: Optional[Var[JsValue]]
+    no_style: Optional[Var[bool]]
+    preserve: Optional[Var[bool]]
+    required: Optional[Var[bool]]
+    rules: Optional[Var[Union[List[Dict], ContainVar]]]
+    should_update: Optional[Var[bool]]
+    tooltip: Optional[Var[Union[ReactNode, ContainVar]]]
+    trigger: Optional[Var[str]]
+    validate_debounce: Optional[Var[float]]
+    validate_first: Optional[Var[Union[bool, str]]]
+    validate_status: Optional[Var[str]]
+    validate_trigger: Optional[Var[Union[str, List[str]]]]
+    value_prop_name: Optional[Var[str]]
+    wrapper_col: Optional[Var[Dict]]
+
+
+class FormList(AntdComponent):
+    tag = 'Form.List'
+
+    # children: Optional[Var[JsValue]]
+    initial_value: Optional[Var[List]]
+    name: Optional[Var[Union[str, int, List[Union[str, int]]]]]
+    rules: Optional[Var[ContainVar]]
+
+
+class FormProvider(AntdComponent):
+    tag = 'Form.Provider'
+
+    def get_event_triggers(self) -> Dict[str, Any]:
+        _triggers = super().get_event_triggers()
+        _triggers.update({
+            "on_form_change": lambda form_name, info: [form_name, info],
+            'on_form_finish': lambda form_name, info: [form_name, info],
+        })
+        return _triggers
+
+
+form = Form.create
+form_item = FormItem.create
+form_list = FormList.create
+form_provider = FormProvider.create
+
+
+def _modal_form(
+        modal_type: str, *children: Component,
+        others: list[Component] = None,
+        modal_config=None, form_id: str = None, **props) -> JsValue | Component:
+    from . import modal
+    if form_id is None:
+        form_id = f'form_{uuid.uuid4().hex}'
+    props.update(
+        form=form_id,
+        preserve=False,
+    )
+    f = form(*children, **props)
+    f._memoization_mode = memo_never_no_recursive
+
+    modal_config = modal_config or {}
+    modal_config.update(destroy_on_close=True)
+    modal_config['on_ok'] = js_value(f"""() => {{
+        return new Promise((resolve, reject) => {{
+    {form_id}
+      .validateFields({{
+        validateOnly: false,
+      }})
+      .then(() => {{
+        {form_id}.submit();
+      resolve()}})
+      .catch((err) => {{
+      console.log('{form_id}.validateFields error:', err)
+      reject()}});
+      }});//if catch, modal will close the form //.catch(() => console.log('Oops errors!'));
+      }}
+    """)
+    if others is None:
+        others = []
+    op = getattr(modal, modal_type)
+    if modal_type == 'confirm':
+        modal_config['content'] = [f, *others]
+        before_open = modal_config.pop('before_open', None)
+        return op(config=modal_config, before_open=before_open)
+    else:
+        modal_config['after_open_change'] = js_value(f"""(open) => {{{form_id}.resetFields()}}""", to_js=True)
+        modal = op(
+            f,
+            *others,
+            # on_open=JsValue(f'{form_id}.resetFields()'),
+            **modal_config)
+        return modal
+
+
+def modal_form(
+        *children, others: list[Component] = None,
+        modal_config=None, form_id: str = None, **props) -> Component:
+    return _modal_form('modal', *children,
+                       others=others, modal_config=modal_config, form_id=form_id, **props)
+
+
+def confirm_form(
+        *children, others: list[Component] = None,
+        confirm_config=None, form_id: str = None, **props) -> JsValue:
+    return _modal_form('confirm', *children,
+                       others=others,
+                       modal_config=confirm_config, form_id=form_id, **props)
