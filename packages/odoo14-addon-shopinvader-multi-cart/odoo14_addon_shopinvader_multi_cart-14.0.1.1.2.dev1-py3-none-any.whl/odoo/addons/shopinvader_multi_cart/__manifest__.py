66ca7b774cf04ac34a1c6b1fe66f2499c53943c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:29 +0000

    Add package odoo14-addon-shopinvader-multi-cart/14.0.1.1.2.dev1/odoo14_addon_shopinvader_multi_cart-14.0.1.1.2.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-shopinvader-multi-cart/odoo14_addon_shopinvader_multi_cart-14.0.1.1.2.dev1-py3-none-any.whl/odoo/addons/shopinvader_multi_cart/__manifest__.py
@@ -0,0 +1,14 @@
+# Copyright 2021 Camptocamp SA (https://www.camptocamp.com).
+# @author Iván Todorovich <ivan.todorovich@camptocamp.com>
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl).
+
+{
+    "name": "Shopinvader Multi-Cart",
+    "summary": "Manage multiple carts in Shopinvader",
+    "version": "14.0.1.1.1",
+    "license": "AGPL-3",
+    "author": "Camptocamp SA",
+    "website": "https://github.com/shopinvader/odoo-shopinvader",
+    "depends": ["shopinvader"],
+    "data": ["views/sale_order.xml"],
+}
