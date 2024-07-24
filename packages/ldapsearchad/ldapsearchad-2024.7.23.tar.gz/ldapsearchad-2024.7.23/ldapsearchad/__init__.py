46f258001303957acac7be5dc442a311e3571c3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 04:05:26 +0000

    Add package ldapsearchad/2024.7.23/ldapsearchad-2024.7.23.tar.gz

---
+++ packages/ldapsearchad/ldapsearchad-2024.7.23.tar.gz/ldapsearchad-2024.7.23/ldapsearchad/__init__.py
@@ -0,0 +1,11 @@
+from .example import add_one
+from .ldapsearchad import LdapsearchAd
+
+
+# version number is just the date of the release.
+# with format: YYYY.MM.DD with zero so setuptools is happy
+VERSION = "2024.7.23"
+
+
+def version():
+    return VERSION
