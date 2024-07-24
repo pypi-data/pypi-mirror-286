86ac6f8064870e63fcd728a9975594e3f982e059
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 07:05:33 +0000

    Add package finter/0.2.71/finter-0.2.71-py3-none-any.whl

---
+++ packages/finter/finter-0.2.71-py3-none-any.whl/finter/data/id_table.py
@@ -0,0 +1,30 @@
+import json
+
+import pandas as pd
+from finter.api.quanda_data_api import QuandaDataApi
+
+
+class IdTable:
+
+    def __init__(self, vendor_name):
+        self.vendor_name = vendor_name
+        self.cache = {}
+
+    @staticmethod
+    def support_list():
+        return QuandaDataApi().quanda_data_id_table_retrieve(vendor="")['data']
+
+    def _get(self, id_type):
+        df = self.cache.get(id_type)
+        if df is None or df.empty:
+            data = QuandaDataApi().quanda_data_id_table_retrieve(vendor=self.vendor_name, id_type=id_type)
+            data_dict = json.loads(data['data'])
+            df = pd.DataFrame.from_dict(data_dict)
+            self.cache[id_type] = df
+        return df
+
+    def get_company(self):
+        return self._get('company')
+
+    def get_stock(self):
+        return self._get('stock')
