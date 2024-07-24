9b2b7deaef0150bf0a3d880df14cc64be1c53d0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 14:04:34 +0000

    Add package odoo13-addon-shopinvader-payment-paypal/13.0.2.0.2.dev1/odoo13_addon_shopinvader_payment_paypal-13.0.2.0.2.dev1-py3-none-any.whl

---
+++ packages/odoo13-addon-shopinvader-payment-paypal/odoo13_addon_shopinvader_payment_paypal-13.0.2.0.2.dev1-py3-none-any.whl/odoo/addons/shopinvader_payment_paypal/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2019 Akretion (http://www.akretion.com)
+# Sébastien BEAU <sebastien.beau@akretion.com>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Paypal Payment Acquirer (REST, Shopinvader)",
+    "summary": "Shopinvader REST Services for Paypal Payments",
+    "version": "13.0.2.0.1",
+    "author": "Akretion",
+    "website": "https://github.com/shopinvader/odoo-shopinvader-payment",
+    "license": "AGPL-3",
+    "category": "e-commerce",
+    "depends": ["shopinvader_payment", "invader_payment_paypal"],
+    "autoinstall": True,
+    "demo": ["demo/shopinvader_payment.xml"],
+}
