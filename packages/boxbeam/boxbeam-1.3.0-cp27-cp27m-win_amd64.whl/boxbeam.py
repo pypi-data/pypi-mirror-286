dadd21f1da5fc0958a081d7e8d50a7cc58486244
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:06:06 +0000

    Add package boxbeam/1.3.0/boxbeam-1.3.0-cp27-cp27m-win_amd64.whl

---
+++ packages/boxbeam/boxbeam-1.3.0-cp27-cp27m-win_amd64.whl/boxbeam.py
@@ -0,0 +1,37 @@
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+#                               Boxbeam                          #
+# %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+"""
+Top-level compatibility interface for Boxbeam as a standalone python package.
+ 
+@note: Boxbeam
+Created on 22.07.2024
+
+@version: 1.0
+----------------------------------------------------------------------------------------------
+@requires:
+       - 
+
+@change: 
+       -    
+                           
+@author: garb_ma                                                     [DLR-SY,STM Braunschweig]
+----------------------------------------------------------------------------------------------
+"""
+
+## @package Boxbeam
+# Top-level compatibility interface for Boxbeam as a standalone python package.
+## @authors 
+# Marc Garbade
+## @date
+# 07.22.2024
+## @par Notes/Changes
+# - Added documentation // mg 07.22.2024
+
+import sys
+
+from bbeam import *
+
+if __name__ == '__main__':
+    sys.exit()
+    pass
