8cea6886c3c3c43fc0249e2002262245f1fbdb05
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:35 +0000

    Add package yarn-dev-tools/2.0.3/yarn_dev_tools-2.0.3.tar.gz

---
+++ packages/yarn-dev-tools/yarn_dev_tools-2.0.3.tar.gz/yarn_dev_tools-2.0.3/yarndevtools/cdsw/scripts/install-requirements.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+set -x
+
+echo "Uninstalling package: yarn-dev-tools"
+set +e
+pip3 -V
+pip3 show yarn-dev-tools
+pip3 uninstall -y yarn-dev-tools
+
+set -e
+echo $@
+if [ $# -ne 1 ]; then
+    echo "Usage: $0 <execution mode>"
+    echo "Example: $0 cloudera --> Uses execution mode: 'cloudera'"
+    echo "Example: $0 upstream --> Uses execution mode: 'upstream'"
+    exit 1
+fi
+
+EXEC_MODE="$1"
+echo "Installing package: yarn-dev-tools"
+pip3 install yarn-dev-tools --force-reinstall
+pip3 show yarn-dev-tools
