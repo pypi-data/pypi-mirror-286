5255e02ef502c06d1cc3dced4ca0dddd6e4dbb90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 05:04:26 +0000

    Add package PyQuadraticSolver/0.0.1/pyquadraticsolver-0.0.1.tar.gz

---
+++ packages/PyQuadraticSolver/pyquadraticsolver-0.0.1.tar.gz/pyquadraticsolver-0.0.1/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='PyQuadraticSolver',
+    version='0.0.1',
+    packages=find_packages(),
+    description='praticing oop',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='ShinapriLN',
+    author_email='shinapri.2003.1@gmail.com',
+    url='https://github.com/ShinapriLN/PyQuadraticSolver.git',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+    ],
+    python_requires='>=3.6',
+)
