bc402449fbac7a80e5e6a5bb7377442d425dc85f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 05:04:41 +0000

    Add package gauge-serverless/0.0.6/gauge_serverless-0.0.6-py3-none-any.whl

---
+++ packages/gauge-serverless/gauge_serverless-0.0.6-py3-none-any.whl/gauge/sdk/main.py
@@ -0,0 +1,38 @@
+from __future__ import annotations
+
+from typing import Any, Callable
+
+
+def endpoint(
+    name: str, python_version: str = "3.12", dependencies: list[str] = []
+) -> Callable[..., Any]:
+    def endpoint_decorator(
+        function: Callable[..., Any],
+    ) -> Callable[..., Any]:
+        def _gauge_register() -> tuple[str, dict[str, str | list[str]]]:
+            return name, {
+                "function": function.__name__,
+                "python_version": python_version,
+                "dependencies": dependencies,
+            }
+
+        function._gauge_register = _gauge_register  # pyright: ignore[reportFunctionMemberAccess]
+
+        def as_lambda_function_url_handler() -> Callable[[Any, Any], Any]:
+            def _lambda_handler(event: Any, context: Any) -> Any:
+                if not isinstance(event, dict):
+                    return {
+                        "status": 400,
+                        "detail": "Could not parse incoming data. The request body must be JSON.",
+                    }
+                try:
+                    return {"status": 200, "result": function(**event)}
+                except Exception as e:
+                    return {"status": 500, "detail": str(e)}
+
+            return _lambda_handler
+
+        function.as_lambda_function_url_handler = as_lambda_function_url_handler  # pyright: ignore[reportFunctionMemberAccess]
+        return function
+
+    return endpoint_decorator
---
+++ packages/gauge-serverless/gauge_serverless-0.0.6-py3-none-any.whl/gauge/sdk/tests/test_sdk.py
@@ -0,0 +1,49 @@
+from __future__ import annotations
+
+from gauge import endpoint
+
+
+def test_default_endpoint_decorator():
+    @endpoint(name="name")
+    def test_function():
+        return "testy"
+
+    assert test_function() == "testy"
+    assert hasattr(test_function, "_gauge_register")
+    assert test_function._gauge_register() == (
+        "name",
+        {
+            "dependencies": [],
+            "python_version": "3.12",
+            "function": "test_function",
+        },
+    )
+
+
+def test_custom_endpoint_decorator():
+    @endpoint(name="name2", dependencies=["fastapi"], python_version="3.13")
+    def test_function():
+        return "test2"
+
+    assert test_function() == "test2"
+    assert hasattr(test_function, "_gauge_register")
+    assert test_function._gauge_register() == (
+        "name2",
+        {
+            "dependencies": ["fastapi"],
+            "python_version": "3.13",
+            "function": "test_function",
+        },
+    )
+
+
+def test_as_lambda_handler():
+    @endpoint(name="name3", dependencies=["fastapi"], python_version="3.13")
+    def test_function():
+        return "test3"
+
+    assert test_function() == "test3"
+    assert hasattr(test_function, "as_lambda_function_url_handler")
+    handler_result = test_function.as_lambda_function_url_handler()({}, {})
+    assert handler_result.get("status") == 200
+    assert handler_result.get("result") == "test3"
