50b05a87cc16f6231bbe3727c1356e723a226d47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:51 +0000

    Add package akaocr/2.0.12/akaocr-2.0.12.tar.gz

---
+++ packages/akaocr/akaocr-2.0.12.tar.gz/akaocr-2.0.12/akaocr/__init__.py
@@ -0,0 +1,8 @@
+# WRITER: LauNT # DATE: 05/2024
+# FROM: akaOCR Team - QAI
+
+from .detect import BoxEngine
+from .recog import TextEngine
+
+__version__ = 'akaocr-v2.0.1'
+__all__ = ['BoxEngine', 'TextEngine']
