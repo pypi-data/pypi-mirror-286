e4ff40f92fce49da54b9dbc489506e11947d2aa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 16:05:01 +0000

    Add package nwon_django_toolbox/0.6.10/nwon_django_toolbox-0.6.10-py3-none-any.whl

---
+++ packages/nwon_django_toolbox/nwon_django_toolbox-0.6.10-py3-none-any.whl/nwon_django_toolbox/admin/__init__.py
@@ -0,0 +1,16 @@
+from nwon_django_toolbox.admin.custom_titled_filter import custom_titled_filter
+from nwon_django_toolbox.admin.model_admin_with_json_editor import (
+    ModelAdminWithJsonEditor,
+    ModelAdminWithJsonEditorReadOnly,
+    PolymorphicChildModelAdminWithJsonEditor,
+    PolymorphicParentModelAdminWithJsonEditor,
+)
+from nwon_django_toolbox.admin.read_only_admin_mixin import (
+    DeleteAndReadOnlyAdminMixin,
+    ReadOnlyAdminMixin,
+)
+from nwon_django_toolbox.admin.read_only_inline import (
+    ReadOnlyPolymorphicStackedInline,
+    ReadOnlyStackedInline,
+    ReadOnlyTabularInline,
+)
