70fc87ab9a3b45dadbab80e1e86019ee6358a821
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-24 15:05:09 +0000

    Add package requirements-checker/0.2.5/requirements_checker-0.2.5-py3-none-any.whl

---
+++ packages/requirements-checker/requirements_checker-0.2.5-py3-none-any.whl/requirements_checker/__init__.py
@@ -0,0 +1,73 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .check_string import (
+    # BASE
+    ReqCheckStr_Base,
+    ReqCheckStr_Os,
+    ReqCheckStr_Arch,
+
+    # AUX
+    _GetattrClassmethod_Meta,
+
+    # TYPES
+    TYPE__VALUES,
+    TYPE__RESULT_BOOL,
+    TYPE__RESULT_RAISE,
+
+    # EXX
+    Exx_RequirementCantGetActualValue,
+    Exx_Requirement,
+)
+from .check_pkg import (
+    # BASE
+    Packages,
+    CmdPattern,
+
+    # AUX
+
+    # TYPES
+
+    # EXX
+)
+from .check_version import (
+    # BASE
+    Version,
+    ReqCheckVersion,
+    ReqCheckVersion_Python,
+
+    # AUX
+    VersionBlock,
+
+    # TYPES
+    TYPE__VERSION_ELEMENT,
+    TYPE__VERSION_ELEMENTS,
+    TYPE__SOURCE_BLOCKS,
+
+    TYPE__VERSION_BLOCKS,
+    TYPE__SOURCE_VERSION,
+
+    # EXX
+    Exx_VersionIncompatible,
+    Exx_VersionIncompatibleBlock,
+    Exx_VersionIncompatibleCheck,
+)
+
+
+# =====================================================================================================================
