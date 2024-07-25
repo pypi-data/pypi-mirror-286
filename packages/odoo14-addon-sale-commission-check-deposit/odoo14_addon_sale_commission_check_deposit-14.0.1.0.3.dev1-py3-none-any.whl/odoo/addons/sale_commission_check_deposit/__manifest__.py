da66cd6418fc5511943aed0208522d488c6ad6f3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:51 +0000

    Add package odoo14-addon-sale-commission-check-deposit/14.0.1.0.3.dev1/odoo14_addon_sale_commission_check_deposit-14.0.1.0.3.dev1-py3-none-any.whl

---
+++ packages/odoo14-addon-sale-commission-check-deposit/odoo14_addon_sale_commission_check_deposit-14.0.1.0.3.dev1-py3-none-any.whl/odoo/addons/sale_commission_check_deposit/__manifest__.py
@@ -0,0 +1,16 @@
+# Copyright 2023 Nextev Srl
+{
+    "name": "Sales commission check deposit",
+    "version": "14.0.1.0.2",
+    "author": "Nextev Srl," "Odoo Community Association (OCA)",
+    "maintainers": ["aleuffre", "renda-dev", "PicchiSeba"],
+    "category": "Sales Management",
+    "license": "AGPL-3",
+    "depends": [
+        "sale_commission",
+        "account_check_deposit",
+    ],
+    "website": "https://github.com/OCA/commission",
+    "data": ["views/account_journal_view.xml"],
+    "installable": True,
+}
