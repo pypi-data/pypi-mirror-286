550496f27b4d404da156dc20e751b347452cbccf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 16:04:58 +0000

    Add package MeUtils/2024.7.25.17.23.27/MeUtils-2024.7.25.17.23.27-py3-none-any.whl

---
+++ packages/MeUtils/MeUtils-2024.7.25.17.23.27-py3-none-any.whl/meutils/fileparser/filetype.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Project      : AI.  @by PyCharm
+# @File         : filetype
+# @Time         : 2023/7/15 15:05
+# @Author       : betterme
+# @WeChat       : meutils
+# @Software     : PyCharm
+# @Description  : 文件或扩展名被更改
+
+from meutils.pipe import *
+
+import filetype
+import magic
+import mimetypes
+
+import mimetypes
+
+# mimetype, _ = mimetypes.guess_type('example.txt')
+# print(mimetype)
+#
+# import magic
+#
+# file_type = magic.from_file('example.pdf', mime=True)
+# print(file_type)
+#
+# import filetype
+#
+# kind = filetype.guess('example.jpg')
+# if kind is None:
+#     print('Cannot guess file type!')
+# else:
+#     print('File extension: %s' % kind.extension)
+#     print('File MIME type: %s' % kind.mime)
+
+if __name__ == '__main__':
+    mimetype, _ = mimetypes.guess_type('example.mp3')
+    print(mimetype)
