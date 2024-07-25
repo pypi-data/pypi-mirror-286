4a7862446040d58c0ec0d3a20ea5ee59be7eb211
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:45 +0000

    Add package rezolva/0.1.0/rezolva-0.1.0.tar.gz

---
+++ packages/rezolva/rezolva-0.1.0.tar.gz/rezolva-0.1.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="rezolva",
+    version="0.1.0",
+    author="Josh Meek",
+    author_email="mail@josh.dev",
+    description="A minimalist entity resolution library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/joshmeek/rezolva",
+    packages=["rezolva"],
+    python_requires=">=3.7",
+    install_requires=[],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License"
+      ]
+)
