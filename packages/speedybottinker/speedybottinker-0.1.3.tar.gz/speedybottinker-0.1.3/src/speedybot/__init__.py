ca646e268e4e7b84b8c6ab18265122547e7b9dac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:05:09 +0000

    Add package speedybottinker/0.1.3/speedybottinker-0.1.3.tar.gz

---
+++ packages/speedybottinker/speedybottinker-0.1.3.tar.gz/speedybottinker-0.1.3/src/speedybot/__init__.py
@@ -0,0 +1,10 @@
+# from .speedybot import SpeedyBot
+# from .cards import Cards
+
+# __all__ = ["SpeedyBot", "Cards"]
+
+
+from .speedybot import SpeedyBot
+from .cards import Cards
+
+# ⬆️ Trick to get nice imports (ie no SpeedyBot.SpeedyBot() junk)
