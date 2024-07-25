9cfb33e909365e56560a6566b43e346a8ae93036
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:06:06 +0000

    Add package notion-logger/0.0.5/notion_logger-0.0.5.tar.gz

---
+++ packages/notion-logger/notion_logger-0.0.5.tar.gz/notion_logger-0.0.5/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="notion-logger",
+    version="0.0.5",
+    author="_Kamikotto_",
+    author_email="kamikotto3@gmail.com",
+    description="A small logger for notion.so",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/KamikottoGG/NotionLogger",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "requests",
+    ],
+    python_requires='>=3.10',
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
