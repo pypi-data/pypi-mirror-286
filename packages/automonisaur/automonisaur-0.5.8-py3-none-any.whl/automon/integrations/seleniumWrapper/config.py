daecf7f31daa6eb2b42d5e17b6f90babf9d485a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 16:04:55 +0000

    Add package automonisaur/0.5.8/automonisaur-0.5.8-py3-none-any.whl

---
+++ packages/automonisaur/automonisaur-0.5.8-py3-none-any.whl/automon/integrations/seleniumWrapper/config.py
@@ -0,0 +1,59 @@
+import selenium.webdriver
+
+from automon import log
+from automon.helpers.osWrapper import environ
+
+logger = log.logging.getLogger(__name__)
+logger.setLevel(log.DEBUG)
+
+
+class SeleniumConfig(object):
+    def __init__(self):
+        self._webdriver = None
+        self.webdriver_wrapper = None
+
+        self.cookies_autosave: bool = environ('SELENIUM_COOKIES_AUTOSAVE', False)
+        self._cookies_base64 = environ('SELENIUM_COOKIES_BASE64')
+        self._cookies_file = environ('SELENIUM_COOKIES_FILE')
+
+    @property
+    def webdriver(self):
+        try:
+            return self.webdriver_wrapper.webdriver
+        except:
+            return self._webdriver
+
+    @property
+    def window_size(self):
+        """get window size
+
+        """
+        if self.webdriver_wrapper:
+            return self.webdriver_wrapper.window_size
+
+    @property
+    def cookies_base64(self):
+        logger.debug(f'{len(self._cookies_base64) if self._cookies_base64 else None}')
+        return self._cookies_base64
+
+    @property
+    def cookies_file(self):
+        logger.info(f'{self._cookies_file}')
+        return self._cookies_file
+
+    async def run(self):
+        """run webdriver"""
+        run = await self.webdriver_wrapper.run()
+        self._webdriver = self.webdriver_wrapper.webdriver
+        logger.info(str(dict(
+            webdriver=self.webdriver
+        )))
+        return run
+
+    async def start(self):
+        """alias to run"""
+        return await self.run()
+
+    async def quit(self):
+        """quit webdriver"""
+        return
