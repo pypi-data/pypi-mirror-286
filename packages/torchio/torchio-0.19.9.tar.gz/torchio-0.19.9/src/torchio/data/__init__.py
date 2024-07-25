710bd4b1efa3efe7bfb5d38b3b4d0e21666fd707
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:05:40 +0000

    Add package torchio/0.19.9/torchio-0.19.9.tar.gz

---
+++ packages/torchio/torchio-0.19.9.tar.gz/torchio-0.19.9/src/torchio/data/__init__.py
@@ -0,0 +1,28 @@
+from .dataset import SubjectsDataset
+from .image import Image
+from .image import LabelMap
+from .image import ScalarImage
+from .inference import GridAggregator
+from .queue import Queue
+from .sampler import GridSampler
+from .sampler import LabelSampler
+from .sampler import PatchSampler
+from .sampler import UniformSampler
+from .sampler import WeightedSampler
+from .subject import Subject
+
+
+__all__ = [
+    'Queue',
+    'Subject',
+    'SubjectsDataset',
+    'Image',
+    'ScalarImage',
+    'LabelMap',
+    'GridSampler',
+    'GridAggregator',
+    'PatchSampler',
+    'LabelSampler',
+    'WeightedSampler',
+    'UniformSampler',
+]
