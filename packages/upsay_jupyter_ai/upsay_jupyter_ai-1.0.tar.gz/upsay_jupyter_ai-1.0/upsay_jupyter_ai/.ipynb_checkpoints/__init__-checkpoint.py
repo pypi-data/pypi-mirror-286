7c5e69158303b4e1c91e268234275006a1766dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:05:06 +0000

    Add package upsay_jupyter_ai/1.0/upsay_jupyter_ai-1.0.tar.gz

---
+++ packages/upsay_jupyter_ai/upsay_jupyter_ai-1.0.tar.gz/upsay_jupyter_ai-1.0/upsay_jupyter_ai/.ipynb_checkpoints/__init__-checkpoint.py
@@ -0,0 +1,7 @@
+"""Testing the deployment of UPSayAI, a Jupyter-integrated Generative AI toolkit for Université Paris-Saclay students."""
+
+__version__ = "1.0"
+
+from .config import UPSayAI
+
+__all__ = ["UPSayAI"]
