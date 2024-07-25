77b7a4546432fc6b20231e1ef9c424e28f2f7fe0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:09 +0000

    Add package nlgmal/0.1.2/nlgmal-0.1.2.tar.gz

---
+++ packages/nlgmal/nlgmal-0.1.2.tar.gz/nlgmal-0.1.2/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='nlgmal',
+    version='0.1.2',
+    packages=find_packages(),
+    description='A custom cryptographic package using the NLGdecimal number system.',
+    author='NLG SAKIB',
+    author_email='nlgarts@outlook.com',
+    license='MIT',
+    install_requires=[],
+)
