62488003499ef5a81a2c9e9f54f4794e279c3173
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:05:28 +0000

    Add package drawlib/0.2.3.dev2/drawlib-0.2.3.dev2.tar.gz

---
+++ packages/drawlib/drawlib-0.2.3.dev2.tar.gz/drawlib-0.2.3.dev2/drawlib/v0_2/private/smartarts/dsart.py
@@ -0,0 +1,18 @@
+# Copyright (c) 2024 Yuichi Ito (yuichi@yuichi.com)
+#
+# This software is licensed under the Apache License, Version 2.0.
+# For more information, please visit: https://github.com/yuichi110/drawlib
+#
+# This software is provided "as is", without warranty of any kind,
+# express or implied, including but not limited to the warranties of
+# merchantability, fitness for a particular purpose and noninfringement.
+
+# ruff: noqa: F401
+
+"""Smart art module."""
+
+from drawlib.v0_2.private.smartarts.bubblespeech import bubblespeech as bubblespeech
+from drawlib.v0_2.private.smartarts.gridlayout import GridLayout as GridLayout
+from drawlib.v0_2.private.smartarts.pyramid import Pyramid as Pyramid
+from drawlib.v0_2.private.smartarts.sourcecode import SourceCode as SourceCode
+from drawlib.v0_2.private.smartarts.tree import TreeNode as TreeNode
