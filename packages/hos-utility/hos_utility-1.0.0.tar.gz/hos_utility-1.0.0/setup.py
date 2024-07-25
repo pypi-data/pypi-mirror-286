224cd15bd0ae95759c3476f5afd19b42e6f39712
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:05:27 +0000

    Add package hos-utility/1.0.0/hos_utility-1.0.0.tar.gz

---
+++ packages/hos-utility/hos_utility-1.0.0.tar.gz/hos_utility-1.0.0/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import pathlib
+
+# Директория проекта
+HERE = pathlib.Path(__file__).parent
+
+# Читаем содержимое файла README.md
+long_description = (HERE / "README.md").read_text()
+
+setup(
+    name="hos-utility",
+    version="1.0.0",
+    packages=find_packages(),
+    install_requires=[
+        "urwid",
+    ],
+    entry_points={
+        "console_scripts": [
+            "hos = hos:main",
+        ],
+    },
+    author="nesnite",
+    author_email="nesnite@example.com",
+    description="A simple utility for managing hostnames",
+    long_description=long_description,
+    long_description_content_type="text/markdown",  # Використовуємо Markdown для довгого опису
+    url="https://github.com/YourUsername/hos-utility",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
