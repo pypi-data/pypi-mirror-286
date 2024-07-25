5695397c6bed37ee4eab5178dc3166bc7fb166dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:04:43 +0000

    Add package obstechutils/0.1.18/obstechutils-0.1.18-py3-none-any.whl

---
+++ packages/obstechutils/obstechutils-0.1.18-py3-none-any.whl/obstechutils/types.py
@@ -0,0 +1,9 @@
+from astropy import time
+from typing_extensions import Annotated
+
+from .dataclasses import autoconverted, Field
+
+TimeType = autoconverted(time.Time)
+TimeDeltaType = autoconvert(time.TimeDelta)
+PortType = Annotated[int, Field(ge=0, lt=65535)]
+QOSType = Annotated[int, Field(ge=0, le=2)]
