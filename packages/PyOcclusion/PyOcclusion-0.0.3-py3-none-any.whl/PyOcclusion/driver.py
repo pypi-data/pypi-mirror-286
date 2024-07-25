6265518c60a610c0298297402ec90c9bcb0b9bb7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:55 +0000

    Add package PyOcclusion/0.0.3/PyOcclusion-0.0.3-py3-none-any.whl

---
+++ packages/PyOcclusion/PyOcclusion-0.0.3-py3-none-any.whl/PyOcclusion/driver.py
@@ -0,0 +1,4 @@
+import occlusion
+
+editor = occlusion.VideoEditor(20, 0, 17, 17, 300)
+editor.edit('/Users/bohdansynytskyi/research/Actor_01/output.mp4', "/Users/bohdansynytskyi/desktop/noisyName.mp4")
