d3fb70f6963ebc881ba444869ca5dcbb7a0e0c45
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:04:44 +0000

    Add package amphibian/0.0.1/amphibian-0.0.1.tar.gz

---
+++ packages/amphibian/amphibian-0.0.1.tar.gz/amphibian-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='amphibian',
+    version='0.0.1',
+    description='amphibian',
+    author='LM Studio',
+    author_email='team@lmstudio.ai',
+    zip_safe=False
+)
