cb2bd6d06edf9498d1d508a660e6002a25e8e9a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:46 +0000

    Add package qDNA/0.1.1/qDNA-0.1.1.tar.gz

---
+++ packages/qDNA/qDNA-0.1.1.tar.gz/qDNA-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qDNA",
+    version="0.1.1",
+    author="Dennis Herb",
+    author_email="dennis.herb@uni-ulm.de",
+    description="A package to calculate lifetimes, average charge separation and dipole moments of excited states along DNA within the formalism of open quantum systems.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dehe1011/QuantumDNA",
+    packages=find_packages(),
+    include_package_data=True,
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=[
+        'qutip==4.7.5',
+    ],
+)
