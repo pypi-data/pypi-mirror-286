b7ee0898922b7e7652eb8ccaf7df7489cbb31884
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:44 +0000

    Add package fqr/0.6.0rc1/fqr-0.6.0rc1-py3-none-any.whl

---
+++ packages/fqr/fqr-0.6.0rc1-py3-none-any.whl/tests/core/cfg.py
@@ -0,0 +1,11 @@
+"""Constant values specific to module unit tests."""
+
+__all__ = (
+    'Constants',
+    )
+
+from fqr import core
+
+
+class Constants(core.cfg.Constants):
+    """Constant values specific to unit tests in this module."""
