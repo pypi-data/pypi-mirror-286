616098e840f902d5af238e21cb669f816e1d1a1d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:51 +0000

    Add package everai/0.2.29/everai-0.2.29-py3-none-any.whl

---
+++ packages/everai/everai-0.2.29-py3-none-any.whl/generated/apps_v1alpha2/api/__init__.py
@@ -0,0 +1,6 @@
+# flake8: noqa
+
+# import apis into api package
+from generated.apps_v1alpha2.api.app_service_api import AppServiceApi
+from generated.apps_v1alpha2.api.namespace_service_api import NamespaceServiceApi
+
