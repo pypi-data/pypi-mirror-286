ad56a388ac36e4cf4325aec08a192ef0fe436bb2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:06:00 +0000

    Add package kima/6.1.1/kima-6.1.1-cp311-cp311-macosx_11_0_arm64.whl

---
+++ packages/kima/kima-6.1.1-cp311-cp311-macosx_11_0_arm64.whl/kima/__init__.py
@@ -0,0 +1,49 @@
+
+
+from .Data import RVData, PHOTdata, GAIAdata, ETVData
+
+from .RVmodel import RVmodel
+from .GPmodel import GPmodel
+
+from .RVFWHMmodel import RVFWHMmodel
+from .TRANSITmodel import TRANSITmodel
+from .RVFWHMRHKmodel import RVFWHMRHKmodel
+
+from .OutlierRVmodel import OutlierRVmodel
+from .BINARIESmodel import BINARIESmodel
+from .GAIAmodel import GAIAmodel
+from .RVGAIAmodel import RVGAIAmodel
+from .ETVmodel import ETVmodel
+
+__models__ = (
+    RVmodel,
+    GPmodel,
+    RVFWHMmodel,
+    RVFWHMRHKmodel,
+    TRANSITmodel,
+    OutlierRVmodel,
+    BINARIESmodel,
+    GAIAmodel,
+    RVGAIAmodel,
+    ETVmodel,
+)
+
+# add plot method to data classes
+from .pykima.display import plot_RVData
+RVData.plot = plot_RVData
+
+
+# kima.run
+from .Sampler import run
+# kima.load_results
+from .pykima.results import load_results #, KimaResults
+# kima.cleanup
+from .pykima.cli import cli_clean as cleanup
+
+# sub-packages
+from .kepler import keplerian
+#from . import spleaf
+from . import distributions
+
+# examples
+from . import examples
