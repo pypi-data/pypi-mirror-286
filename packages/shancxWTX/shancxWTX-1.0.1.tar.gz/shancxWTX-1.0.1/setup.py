df7c3fdb71ad1ae49a6a89f3f332b1fec780ad2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:05:04 +0000

    Add package shancxWTX/1.0.1/shancxWTX-1.0.1.tar.gz

---
+++ packages/shancxWTX/shancxWTX-1.0.1.tar.gz/shancxWTX-1.0.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='shancxWTX',
+    version='1.0.1',
+    packages=find_packages(),
+    description='A simple timer decorator',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='shanchangxi',
+    author_email='shanhe12@163.com',
+    url='https://github.com/shanchanghua',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
