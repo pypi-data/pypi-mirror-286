b9ce45c4ee02528aaa12f90b2539d54c22bbf938
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:11 +0000

    Add package corecon/1.4.3/corecon-1.4.3-py3-none-any.whl

---
+++ packages/corecon/corecon-1.4.3-py3-none-any.whl/corecon/data/HII_fraction/Umeda_et_al_2023.py
@@ -0,0 +1,33 @@
+dictionary_tag = "Umeda et al. 2023 (subm.)"
+
+reference   = "Umeda, Ouchi, Nakajima, Harikane, Ono, Xu, Isobe, Zhang; arXiv:2306.00487"
+ 
+url         = "https://ui.adsabs.harvard.edu/abs/2023arXiv230600487U/abstract"
+
+description = \
+"""From the damping wing absorption of 26 UV-bright galaxies at 7<z<12.
+"""
+
+data_structure         = "points" #grid or points
+
+extracted              = False
+
+ndim                   = 1
+
+dimensions_descriptors = ["redshift"]
+
+axes                   = [7.12, 7.44, 8.28, 10.28]
+
+err_left               = [0.75, 0.243, 0.446, 1.407]
+
+err_right              = [0.059, 0.341, 0.403, 1.12]
+
+values        = [0.47, 0.35, 0.09, 0.08]
+
+err_down      = [0.13,0.30,0.08,0.06]
+
+err_up        = [0.54, 0.38, 0.56, 0.41]
+
+upper_lim     = False
+
+lower_lim     = False
