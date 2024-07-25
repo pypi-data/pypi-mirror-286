f9ae29248c03113b850783b667e65e3d484279c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:06:29 +0000

    Add package densitypeakclustering/1.0.0/densitypeakclustering-1.0.0.tar.gz

---
+++ packages/densitypeakclustering/densitypeakclustering-1.0.0.tar.gz/densitypeakclustering-1.0.0/setup.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name='densitypeakclustering',
+    version='1.0.0',
+    description='Implementation of the Clustering by fast search and find of density peaks algorithm',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    author='Pieter Goltstein',
+    author_email='xpieter@mac.com',
+    url='https://github.com/pgoltstein/densitypeakclustering',
+    packages=find_packages(),
+    install_requires=[
+        'numpy',
+        'scikit-learn',
+        'matplotlib',
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Programming Language :: Python :: 3',
+        'Topic :: Scientific/Engineering',
+    ],
+    python_requires='>=3.6',
+    include_package_data=True,
+)
