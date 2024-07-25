289dfb982d0a717937ee8adfe9edf01afb634e6f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:41 +0000

    Add package zuspec-arl-dm/0.0.1.10086629414/zuspec_arl_dm-0.0.1.10086629414-cp38-cp38-win_amd64.whl

---
+++ packages/zuspec-arl-dm/zuspec_arl_dm-0.0.1.10086629414-cp38-cp38-win_amd64.whl/zsp_arl_dm/share/include/zsp/arl/dm/IDataTypeActivitySequence.h
@@ -0,0 +1,25 @@
+/*
+ * IDataTypeActivitySequence.h
+ *
+ *  Created on: Jun 4, 2022
+ *      Author: mballance
+ */
+
+#pragma once
+#include "zsp/arl/dm/IDataTypeActivityScope.h"
+
+namespace zsp {
+namespace arl {
+namespace dm {
+
+
+class IDataTypeActivitySequence : public virtual IDataTypeActivityScope {
+public:
+
+	virtual ~IDataTypeActivitySequence() { }
+
+};
+
+}
+}
+}
