ce53109ee5a4459708fd26ef032ae4b2c2a585e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:04:48 +0000

    Add package bits-aviso-python-sdk/0.4.0a0/bits_aviso_python_sdk-0.4.0a0.tar.gz

---
+++ packages/bits-aviso-python-sdk/bits_aviso_python_sdk-0.4.0a0.tar.gz/bits_aviso_python_sdk-0.4.0a0/bits_aviso_python_sdk/helpers/utilities.py
@@ -0,0 +1,45 @@
+import datetime
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+def initialize_logger(file_handler_path=None):
+    """Initializes a logger with a stream handler and an optional file handler.
+
+    Args:
+        file_handler_path (str, optional): The path to save the log file if a file handler is desired. Defaults to None.
+    """
+    # set up logger
+    today = datetime.datetime.now().strftime("%Y_%m_%d")
+    logger = logging.getLogger()  # root logger
+
+    # check if there's any handlers already
+    if not logger.handlers:
+        # create file handler if path is provided
+        if file_handler_path:
+            # check if the path ends with a slash
+            if file_handler_path.endswith('/'):
+                file_handler = logging.FileHandler(f"{file_handler_path}{today}.log")
+            else:
+                file_handler = logging.FileHandler(f"{file_handler_path}/{today}.log")
+            # set level to DEBUG
+            file_handler.setLevel(logging.DEBUG)
+            # set format
+            file_handler.setFormatter(logging.Formatter(
+                "%(module)s %(asctime)s [%(levelname)s]: %(message)s", "%I:%M:%S %p"))
+            # add file handler to the logger
+            logger.addHandler(file_handler)
+
+        # Create stream handler and set level to ERROR
+        stream_handler = logging.StreamHandler()
+        stream_handler.setLevel(logging.INFO)
+        stream_handler.setFormatter(logging.Formatter(
+            "%(module)s %(asctime)s [%(levelname)s]: %(message)s", "%I:%M:%S %p"))
+        # add stream handler to the logger
+        logger.addHandler(stream_handler)
+
+    # Set the logger's level to the lowest level among all handlers
+    logger.setLevel(logging.DEBUG)
+
+    return logger
