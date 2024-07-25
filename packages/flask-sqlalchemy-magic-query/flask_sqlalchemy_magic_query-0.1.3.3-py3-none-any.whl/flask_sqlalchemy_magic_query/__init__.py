8b05d0804ebff8b1f41042fdf3e1f92aad76c9fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:48 +0000

    Add package flask-sqlalchemy-magic-query/0.1.3.3/flask_sqlalchemy_magic_query-0.1.3.3-py3-none-any.whl

---
+++ packages/flask-sqlalchemy-magic-query/flask_sqlalchemy_magic_query-0.1.3.3-py3-none-any.whl/flask_sqlalchemy_magic_query/__init__.py
@@ -0,0 +1,9 @@
+"""
+flask_sqlalchemy_magic_query.
+
+Converts HTTP URL query string parameters to flask_sqlalchemy query results.
+"""
+
+__all__ = ["flask_sqlalchemy_magic_query"]
+__version__ = "0.1.3"
+__author__ = 'Gabriel Ligoski'
