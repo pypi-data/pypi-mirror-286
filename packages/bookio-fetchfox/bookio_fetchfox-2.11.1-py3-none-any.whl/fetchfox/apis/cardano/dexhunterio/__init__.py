80b3a87cc5d51e2ecebe3445e5830fa0a083db15
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:19 +0000

    Add package bookio-fetchfox/2.11.1/bookio_fetchfox-2.11.1-py3-none-any.whl

---
+++ packages/bookio-fetchfox/bookio_fetchfox-2.11.1-py3-none-any.whl/fetchfox/apis/cardano/dexhunterio/__init__.py
@@ -0,0 +1,4 @@
+# assets
+from .assets import get_average_price as get_asset_average_price
+from .assets import get_orders as get_asset_orders
+from .assets import get_pair_stats as get_asset_pair_stats
