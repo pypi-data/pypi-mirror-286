813bc59af9fd6a0bb641a6b4453411a16ff8a48d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:19 +0000

    Add package kitvector/0.0.2/kitvector-0.0.2.tar.gz

---
+++ packages/kitvector/kitvector-0.0.2.tar.gz/kitvector-0.0.2/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+setuptools.setup(
+    name = "kitvector",                                                                                                                              
+    version = "0.0.2",
+    author = "Cheng",
+    author_email="cjenf844747@gmail.com",
+    description="This package has basic Vector operations (not suitable for machine learning)",
+    url="https://github.com/Cjenf/sqlbox", 
+    python_requires='>=3.11.4'
+)
