9552e4d9b78e715b7354336a29f7591376df28c4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:05:32 +0000

    Add package dragonfly-logging-config/1.0.0/dragonfly_logging_config-1.0.0.tar.gz

---
+++ packages/dragonfly-logging-config/dragonfly_logging_config-1.0.0.tar.gz/dragonfly_logging_config-1.0.0/src/logging_config/__init__.py
@@ -0,0 +1,58 @@
+import logging
+import logging.config
+
+import structlog
+
+
+def configure_logger(config: dict):
+    # Define the shared processors, regardless of whether API is running in prod or dev.
+    shared_processors: list[structlog.types.Processor] = [
+        structlog.contextvars.merge_contextvars,
+        structlog.stdlib.add_log_level,
+        structlog.stdlib.PositionalArgumentsFormatter(),
+        structlog.stdlib.ExtraAdder(),
+        structlog.processors.format_exc_info,
+        structlog.processors.TimeStamper(fmt="iso"),
+        structlog.processors.StackInfoRenderer(),
+        structlog.processors.CallsiteParameterAdder(
+            {
+                structlog.processors.CallsiteParameter.FILENAME,
+                structlog.processors.CallsiteParameter.FUNC_NAME,
+                structlog.processors.CallsiteParameter.MODULE,
+                structlog.processors.CallsiteParameter.LINENO,
+            }
+        ),
+    ]
+
+    config.update(
+        {
+            "version": 1,
+            "formatters": {
+                "json": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processors": [
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        structlog.processors.JSONRenderer(),
+                    ],
+                    "foreign_pre_chain": shared_processors,
+                },
+                "development": {
+                    "()": structlog.stdlib.ProcessorFormatter,
+                    "processors": [
+                        structlog.stdlib.ProcessorFormatter.remove_processors_meta,
+                        structlog.dev.ConsoleRenderer(colors=False),
+                    ],
+                    "foreign_pre_chain": shared_processors,
+                },
+            },
+        }
+    )
+
+    logging.config.dictConfig(config)
+
+    structlog.configure(
+        processors=shared_processors + [structlog.stdlib.ProcessorFormatter.wrap_for_formatter],
+        logger_factory=structlog.stdlib.LoggerFactory(),
+        wrapper_class=structlog.stdlib.BoundLogger,
+        cache_logger_on_first_use=True,
+    )
