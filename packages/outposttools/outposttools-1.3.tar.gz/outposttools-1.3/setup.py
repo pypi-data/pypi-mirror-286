af405b1ad92309945608b65fa98a4e8705f2c484
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:09 +0000

    Add package outposttools/1.3/outposttools-1.3.tar.gz

---
+++ packages/outposttools/outposttools-1.3.tar.gz/outposttools-1.3/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='outposttools',
+    version='1.3',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=open('requirements.txt').read().splitlines(),
+    entry_points={
+        'console_scripts': [
+            'outpost=outpost.main:main',  # Update with correct entry point
+        ],
+    },
+    package_data={
+        'outposttools': ['images/*.ico', 'images/*.png'],  # Include image files
+    },
+)
