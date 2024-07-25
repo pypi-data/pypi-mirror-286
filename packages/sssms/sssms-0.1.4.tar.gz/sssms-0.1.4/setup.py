1ad637834e3f13a5421dcac41980ff0402ac5570
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:49 +0000

    Add package sssms/0.1.4/sssms-0.1.4.tar.gz

---
+++ packages/sssms/sssms-0.1.4.tar.gz/sssms-0.1.4/setup.py
@@ -0,0 +1,35 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="sssms",
+    version="0.1.4",
+    author="Joel Yisrael",
+    author_email="joel@sss.bot",
+    description="Self-Service SMS via eMail",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/schizoprada/sssms",
+    packages=find_packages(),
+    install_requires=[
+        "click",
+        "requests",
+        "beautifulsoup4",
+        "rich",
+        "pyyaml",
+        "cryptography",
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+    entry_points={
+        "console_scripts": [
+            "sssms=sssms.main:cli",
+        ],
+    },
+)
