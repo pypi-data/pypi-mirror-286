ff6ef0043ccb478c2dbe88f8e65126742162692c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:39 +0000

    Add package pass-prot-pdf/0.1.3/pass_prot_pdf-0.1.3-py3-none-any.whl

---
+++ packages/pass-prot-pdf/pass_prot_pdf-0.1.3-py3-none-any.whl/pass_prot_pdf/start.py
@@ -0,0 +1,10 @@
+import subprocess
+import pass_prot_pdf
+import os
+
+def main() -> None:
+    app_path = pass_prot_pdf.start.__file__.rsplit(os.sep,1)[0]
+    subprocess.run(["streamlit", "run", f"{app_path}{os.sep}app.py"])
+
+if __name__ == "__main__":
+    main()
