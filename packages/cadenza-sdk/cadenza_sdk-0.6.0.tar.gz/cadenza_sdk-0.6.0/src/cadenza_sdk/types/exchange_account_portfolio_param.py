f3ea3015aa5965575e2cac90c3d5ec10429c0ee5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:17 +0000

    Add package cadenza-sdk/0.6.0/cadenza_sdk-0.6.0.tar.gz

---
+++ packages/cadenza-sdk/cadenza_sdk-0.6.0.tar.gz/cadenza_sdk-0.6.0/src/cadenza_sdk/types/exchange_account_portfolio_param.py
@@ -0,0 +1,72 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Literal, Required, Annotated, TypedDict
+
+from .._utils import PropertyInfo
+from .exchange_account_credit_param import ExchangeAccountCreditParam
+
+__all__ = ["ExchangeAccountPortfolioParam", "Payload", "PayloadBalance", "PayloadPosition"]
+
+
+class PayloadBalance(TypedDict, total=False):
+    asset: Required[str]
+    """Asset"""
+
+    free: Required[float]
+    """Free balance"""
+
+    locked: Required[float]
+    """Locked balance"""
+
+    total: Required[float]
+    """Total balance"""
+
+
+class PayloadPosition(TypedDict, total=False):
+    amount: Required[float]
+    """Amount"""
+
+    position_side: Required[Annotated[Literal["LONG", "SHORT"], PropertyInfo(alias="positionSide")]]
+    """Position side"""
+
+    status: Required[Literal["OPEN"]]
+    """Status"""
+
+    symbol: Required[str]
+    """Symbol"""
+
+    cost: float
+    """Cost"""
+
+    entry_price: Annotated[float, PropertyInfo(alias="entryPrice")]
+    """Entry price"""
+
+
+class Payload(TypedDict, total=False):
+    balances: Required[Iterable[PayloadBalance]]
+
+    credit: Required[ExchangeAccountCreditParam]
+    """Exchange Account Credit Info"""
+
+    exchange_account_id: Required[Annotated[str, PropertyInfo(alias="exchangeAccountId")]]
+    """The unique identifier for the account."""
+
+    exchange_type: Required[
+        Annotated[
+            Literal["BINANCE", "BINANCE_MARGIN", "B2C2", "WINTERMUTE", "BLOCKFILLS", "STONEX"],
+            PropertyInfo(alias="exchangeType"),
+        ]
+    ]
+    """Exchange type"""
+
+    positions: Required[Iterable[PayloadPosition]]
+
+    updated_at: Required[Annotated[int, PropertyInfo(alias="updatedAt")]]
+    """The timestamp when the portfolio information was updated."""
+
+
+class ExchangeAccountPortfolioParam(TypedDict, total=False):
+    payload: Payload
