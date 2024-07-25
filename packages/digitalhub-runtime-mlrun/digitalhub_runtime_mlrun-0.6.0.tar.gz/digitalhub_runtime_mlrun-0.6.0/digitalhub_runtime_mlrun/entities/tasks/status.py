802871847336606e58b67fb809073f3685b9dc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:20 +0000

    Add package digitalhub-runtime-mlrun/0.6.0/digitalhub_runtime_mlrun-0.6.0.tar.gz

---
+++ packages/digitalhub-runtime-mlrun/digitalhub_runtime_mlrun-0.6.0.tar.gz/digitalhub_runtime_mlrun-0.6.0/digitalhub_runtime_mlrun/entities/tasks/status.py
@@ -0,0 +1,15 @@
+from __future__ import annotations
+
+from digitalhub_core.entities.tasks.status import TaskStatus
+
+
+class TaskStatusJob(TaskStatus):
+    """
+    Task Job status.
+    """
+
+
+class TaskStatusBuild(TaskStatus):
+    """
+    Task Build status.
+    """
