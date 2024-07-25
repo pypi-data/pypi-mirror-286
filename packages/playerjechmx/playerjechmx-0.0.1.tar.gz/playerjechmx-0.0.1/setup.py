a610b41b47ae81fb602b11f399173a7bad8f42d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:13 +0000

    Add package playerjechmx/0.0.1/playerjechmx-0.0.1.tar.gz

---
+++ packages/playerjechmx/playerjechmx-0.0.1.tar.gz/playerjechmx-0.0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="playerjechmx",
+    author="Jose Chavez",
+    author_email="jechmx1@gmail.com",
+    version="0.0.1",
+    description="reproductor de musica"
+)
