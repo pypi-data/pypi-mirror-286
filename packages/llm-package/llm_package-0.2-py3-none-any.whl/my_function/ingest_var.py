bbf071148368c6f9478db17046e1a655c6410500
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:05:44 +0000

    Add package llm-package/0.2/llm_package-0.2-py3-none-any.whl

---
+++ packages/llm-package/llm_package-0.2-py3-none-any.whl/my_function/ingest_var.py
@@ -0,0 +1,5 @@
+def fn_var():
+    bucket_name = 'llm_gateway'
+    source_blob_name = 'diamonds.csv'
+    destination_file_name = 'diamonds.csv'
+    return (bucket_name,source_blob_name,destination_file_name)
