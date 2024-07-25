d40a8917a045385ff8d9977c4b7505eed130cd26
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:05:47 +0000

    Add package cowgirl-ai-auto-code/1.1.5/cowgirl_ai_auto_code-1.1.5-py3-none-any.whl

---
+++ packages/cowgirl-ai-auto-code/cowgirl_ai_auto_code-1.1.5-py3-none-any.whl/cli/main.py
@@ -0,0 +1,30 @@
+import argparse
+import os
+import json
+from auto_code.auto_code import AutoCode
+
+TEMP_FILE = 'temp_files.json'
+
+def main():
+    """Main entry point for the Auto Code CLI Tool."""
+    parser = argparse.ArgumentParser(description="Auto Code CLI Tool")
+    subparsers = parser.add_subparsers(dest='command', required=True)
+
+    parser_get_files = subparsers.add_parser('refine', help='Get list of files in directory with given extension, sends to refinery for updates')
+    parser_get_files.add_argument('dir', type=str, help='Directory containing files')
+    parser_get_files.add_argument('ext', type=str, help='File extension to load')
+
+    args = parser.parse_args()
+
+    auto_code = AutoCode()
+
+    if args.command == 'refine':
+        assert os.path.isdir(args.dir), f"Error: The specified directory '{args.dir}' does not exist."
+        file_list = auto_code.get_files(args.dir, args.ext)
+        auto_code.refine(file_list)
+
+    else:
+        parser.print_help()
+
+if __name__ == "__main__":
+    main()
