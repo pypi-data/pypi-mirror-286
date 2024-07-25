546f356862735fdba8327162572107756857b265
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:05:20 +0000

    Add package pluthon/1.0.0/pluthon-1.0.0-py3-none-any.whl

---
+++ packages/pluthon/pluthon-1.0.0-py3-none-any.whl/pluthon/__init__.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import logging
+
+try:
+    from .pluthon_ast import *
+    from .pluthon_sugar import *
+    from .pluthon_functional_data import *
+    from .tools import *
+    from .compiler_config import *
+except ImportError as e:
+    logging.error(
+        "Error, trying to import dependencies. Should only occur upon package installation",
+        exc_info=e,
+    )
+
+VERSION = (1, 0, 0)
+
+__version__ = ".".join([str(i) for i in VERSION])
+__author__ = "nielstron"
+__author_email__ = "n.muendler@web.de"
+__copyright__ = "Copyright (C) 2023 nielstron"
+__license__ = "MIT"
+__url__ = "https://github.com/opshin/pluthon"
