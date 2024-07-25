4b50a43f7e58ddd1f96df9806c31da53eb64687e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:50 +0000

    Add package awss3gonla/1.0.0/awss3gonla-1.0.0.tar.gz

---
+++ packages/awss3gonla/awss3gonla-1.0.0.tar.gz/awss3gonla-1.0.0/setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+import setuptools_scm
+
+setup(
+    name='awss3gonla',  # Replace with your library name
+    use_scm_version=True,
+    setup_requires=["setuptools>=42", "setuptools_scm"],
+    packages=find_packages(include=['awss3gonla', 'awss3gonla.*']),
+    install_requires=[
+        'boto3',  # AWS SDK for Python
+        'polars',  # DataFrame library for Python
+        'rich', # Progress bar library
+    ],
+    description='The `awss3gonla` class provides a Python interface for interacting with AWS S3 storage. It allows for operations such as uploading and downloading files, managing parquet files, and handling data partitions. This class is designed to work with the AWS SDK for Python (`boto3`) and the Polars library for data manipulation.',  # Short description
+    long_description=open('README.md').read(),  # Long description from README
+    long_description_content_type='text/markdown',  # Type of long description
+    author='Leonardo Daniel Gonzalo Laura',  # Replace with your name
+    author_email='glleonardodaniel@gmail.com',  # Replace with your email
+    url='https://github.com/leonardogonzalolaura/workspace_libraries',  # Replace with your project URL
+    license='MIT',  # Replace with the license you choose
+)
+
