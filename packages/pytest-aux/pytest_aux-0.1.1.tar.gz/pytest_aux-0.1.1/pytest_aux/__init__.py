ffde3e26b8b0cb6581fb0ee8bc6a1fc2301fd80f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:09 +0000

    Add package pytest-aux/0.1.1/pytest_aux-0.1.1.tar.gz

---
+++ packages/pytest-aux/pytest_aux-0.1.1.tar.gz/pytest_aux-0.1.1/pytest_aux/__init__.py
@@ -0,0 +1,77 @@
+# =====================================================================================================================
+# VERSION = (0, 0, 1)   # use import EXACT_OBJECTS! not *
+#   from .main import *                 # INcorrect
+#   from .main import EXACT_OBJECTS     # CORRECT
+
+
+# =====================================================================================================================
+# TEMPLATE
+# from .main import (
+#     # BASE
+#     EXACT_OBJECTS,
+#
+#     # AUX
+#
+#     # TYPES
+#
+#     # EXX
+# )
+# ---------------------------------------------------------------------------------------------------------------------
+from .pytest_tester import (
+    # BASE
+    pytest_func_tester,
+    pytest_func_tester__no_kwargs,
+    pytest_func_tester__no_args,
+    pytest_func_tester__no_args_kwargs,
+
+    # AUX
+
+    # TYPES
+
+    # EXX
+)
+# ---------------------------------------------------------------------------------------------------------------------
+from .primitives import (
+    # BASE
+    GEN_COMPR,
+
+    FUNC,
+    FUNC_NONE,
+    FUNC_TRUE,
+    FUNC_FALSE,
+    FUNC_EXX,
+    FUNC_GEN,
+
+    LAMBDA,
+    LAMBDA_NONE,
+    LAMBDA_TRUE,
+    LAMBDA_FALSE,
+    LAMBDA_EXX,
+    LAMBDA_GEN,
+
+    Exx,
+
+    ClsInt,
+    ClsFloat,
+    ClsStr,
+    ClsList,
+    ClsSet,
+    ClsDict,
+
+    ClsEmpty,
+    ClsCallable,
+    ClsIterable,
+    ClsGen,
+
+    ClsEq,
+    ClsEqExx,
+
+    ClsFullTypes,
+
+    # AUX
+    # TYPES
+    # EXX
+)
+
+
+# =====================================================================================================================
