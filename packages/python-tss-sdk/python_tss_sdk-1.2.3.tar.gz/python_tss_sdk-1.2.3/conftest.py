9c7cce19d4b323e47daaea9f759e9f905daf2744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:05:42 +0000

    Add package python-tss-sdk/1.2.3/python_tss_sdk-1.2.3.tar.gz

---
+++ packages/python-tss-sdk/python_tss_sdk-1.2.3.tar.gz/python_tss_sdk-1.2.3/conftest.py
@@ -0,0 +1,33 @@
+import os
+import pytest
+from dotenv import load_dotenv
+from delinea.secrets.server import PasswordGrantAuthorizer, SecretServerCloud
+
+load_dotenv()
+
+
+@pytest.fixture
+def env_vars():
+    return {
+        "username": os.getenv("TSS_USERNAME"),
+        "password": os.getenv("TSS_PASSWORD"),
+        "tenant": os.getenv("TSS_TENANT"),
+        "secret_id": os.getenv("TSS_SECRET_ID"),
+        "secret_path": os.getenv("TSS_SECRET_PATH"),
+        "folder_id": os.getenv("TSS_FOLDER_ID"),
+        "folder_path": os.getenv("TSS_FOLDER_PATH")
+    }
+
+
+@pytest.fixture
+def authorizer(env_vars):
+    return PasswordGrantAuthorizer(
+        f"https://{env_vars['tenant']}.secretservercloud.com",
+        env_vars["username"],
+        env_vars["password"],
+    )
+
+
+@pytest.fixture
+def secret_server(env_vars, authorizer):
+    return SecretServerCloud(env_vars["tenant"], authorizer)
---
+++ packages/python-tss-sdk/python_tss_sdk-1.2.3.tar.gz/python_tss_sdk-1.2.3/tests/test_server.py
@@ -0,0 +1,78 @@
+import pytest
+
+from delinea.secrets.server import (
+    AccessTokenAuthorizer,
+    SecretServer,
+    SecretServerClientError,
+    SecretServerError,
+    ServerSecret,
+    ServerFolder,
+)
+
+
+def test_bad_url(env_vars, authorizer):
+    bad_server = SecretServer(
+        f"https://{env_vars['tenant']}.secretservercloud.com/nonexistent",
+        authorizer,
+    )
+    with pytest.raises(SecretServerError):
+        bad_server.get_secret(env_vars["secret_id"])
+
+
+def test_token_url(env_vars, authorizer):
+    assert (
+        authorizer.token_url
+        == f"https://{env_vars['tenant']}.secretservercloud.com/oauth2/token"
+    )
+
+
+def test_api_url(secret_server, env_vars):
+    assert (
+        secret_server.api_url
+        == f"https://{env_vars['tenant']}.secretservercloud.com/api/v1"
+    )
+
+def test_access_token_authorizer(env_vars, authorizer):
+    assert SecretServer(
+        f"https://{env_vars['tenant']}.secretservercloud.com/",
+        AccessTokenAuthorizer(authorizer.get_access_token()),
+    ).get_secret(env_vars["secret_id"])["id"] == int(env_vars["secret_id"])
+
+
+def test_server_secret(env_vars, secret_server):
+    assert ServerSecret(**secret_server.get_secret(env_vars["secret_id"])).id == int(
+        env_vars["secret_id"]
+    )
+
+
+def test_server_secret_by_path(env_vars, secret_server):
+    assert ServerSecret(
+        **secret_server.get_secret_by_path(env_vars["secret_path"])
+    ).id == int(env_vars["secret_id"])
+
+
+def test_server_folder_by_path(env_vars, secret_server):
+    assert ServerFolder(
+        **secret_server.get_folder_by_path(env_vars["folder_path"])
+    ).id == int(env_vars["folder_id"])
+
+
+def test_nonexistent_secret(secret_server):
+    with pytest.raises(SecretServerClientError):
+        secret_server.get_secret(1000)
+
+
+def test_nonexistent_folder(secret_server):
+    with pytest.raises(SecretServerClientError):
+        secret_server.get_folder(1000)
+
+
+def test_server_secret_ids_by_folderid(env_vars, secret_server):
+    assert type(secret_server.get_secret_ids_by_folderid(env_vars["folder_id"])) is list
+
+
+def test_server_child_folder_ids_by_folderid(env_vars, secret_server):
+    assert (
+        type(secret_server.get_child_folder_ids_by_folderid(env_vars["folder_id"]))
+        is list
+    )
