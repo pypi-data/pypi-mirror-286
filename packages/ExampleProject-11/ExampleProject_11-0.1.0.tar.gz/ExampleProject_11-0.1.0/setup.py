0d65a601bb80334c81569408632e49cd78a3a99d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:05:56 +0000

    Add package ExampleProject-11/0.1.0/ExampleProject_11-0.1.0.tar.gz

---
+++ packages/ExampleProject-11/ExampleProject_11-0.1.0.tar.gz/ExampleProject_11-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ExampleProject_11",
+    version="0.1.0",
+    author="Dennis Herb",
+    author_email="dennis.herb@uni-ulm.de",
+    description="An example project for testing.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/dehe1011/ExampleProject",
+    packages=find_packages(),
+)
