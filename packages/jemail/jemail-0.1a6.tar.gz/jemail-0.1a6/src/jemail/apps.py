ff3481e3a6d21691944bffadd836774a0973b1c2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:49 +0000

    Add package jemail/0.1a6/jemail-0.1a6.tar.gz

---
+++ packages/jemail/jemail-0.1a6.tar.gz/jemail-0.1a6/src/jemail/apps.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+from django.apps import AppConfig
+
+
+class JemailConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "jemail"
