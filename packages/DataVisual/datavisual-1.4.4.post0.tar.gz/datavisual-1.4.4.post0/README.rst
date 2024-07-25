73c2c0fdddf427e6b2f1749bcda357fb4b7ca1bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:13 +0000

    Add package DataVisual/1.4.4.post0/datavisual-1.4.4.post0.tar.gz

---
+++ packages/DataVisual/datavisual-1.4.4.post0.tar.gz/datavisual-1.4.4.post0/README.rst
@@ -0,0 +1,66 @@
+|logo|
+
+DataVisual
+==========
+
+|python|
+|unittest|
+|docs|
+|PyPi|
+|PyPi_download|
+
+
+Personal high-dimensional package
+---------------------------------
+
+DataVisual is a package designed to streamline your data visualization process, offering a simplified interface for creating complex plots with ease.
+
+**Installation**
+
+Install DataVisual directly from PyPi:
+
+.. code:: shell
+
+    pip install DataVisual
+
+**Getting Started**
+
+For coding examples and how to get started, please visit the [examples section](https://datavisual.readthedocs.io/en/master/gallery/index.html) of our documentation.
+
+**Local Testing**
+
+To contribute or test locally, clone the GitHub repository and install the required dependencies:
+
+.. code:: shell
+
+    git clone https://github.com/MartinPdeS/DataVisual.git
+    cd DataVisual
+    pip install -r requirements/requirements.txt
+    coverage run --source=DataVisual --module pytest --verbose tests
+    coverage report --show-missing
+
+**Contact Information**
+
+DataVisual is actively being developed by Martin Poinsinet de Sivry-Houle. If you're interested in collaborating or have questions, feel free to reach out:
+
+- [GitHub](https://github.com/MartinPdS)
+- [Email](mailto:martin.poinsinet-de-sivry@polymtl.ca?subject=DataVisual)
+
+DataVisual aims to continuously improve and welcomes contributions from the community.
+
+
+.. |Logo| image:: https://github.com/MartinPdeS/DataVisual/raw/master/docs/images/logo.png
+
+.. |python| image:: https://img.shields.io/pypi/pyversions/datavisual.svg
+   :target: https://www.python.org/
+
+.. |unittest| image:: https://img.shields.io/endpoint?url=https://gist.githubusercontent.com/MartinPdeS/f0955be398d59efac69042c1b0fbece2/raw/06ffc2ef9781e62f4eb489a45fed5e3d1739ce25/DataVisualcoverage_badge.json
+
+.. |docs| image:: https://readthedocs.org/projects/datavisual/badge/?version=latest
+   :target: https://datavisual.readthedocs.io/en/latest/
+
+.. |PyPi| image:: https://badge.fury.io/py/DataVisual.svg
+   :target: https://pypi.org/project/DataVisual/
+
+.. |PyPi_download| image:: https://img.shields.io/pypi/dm/datavisual.svg
+   :target: https://pypistats.org/packages/datavisual
---
+++ packages/DataVisual/datavisual-1.4.4.post0.tar.gz/datavisual-1.4.4.post0/tests/test_api.py
@@ -0,0 +1,152 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from unittest.mock import patch
+import numpy
+import pytest
+from DataVisual import Array
+from DataVisual import Table
+from DataVisual.units import Length, Power, Area
+
+
+@pytest.fixture
+def mock_x_table_2() -> list:
+    parameter_0 = Length(
+        base_values=numpy.linspace(0, 1, 10),
+        long_label='Length: 0',
+        short_label='L: 0'
+    )
+
+    parameter_1 = Length(
+        base_values=numpy.linspace(0, 4, 10),
+        long_label='Length: 1',
+        short_label='L: 1'
+    )
+
+    x_table = [parameter_0, parameter_1]
+
+    return Table(x_table)
+
+
+@pytest.fixture
+def mock_x_table_3() -> list:
+    parameter_0 = Length(
+        base_values=numpy.linspace(0, 1, 10),
+        long_label='Length: 0',
+        short_label='L: 0'
+    )
+
+    parameter_1 = Length(
+        base_values=numpy.linspace(0, 1, 10),
+        long_label='Length: 1',
+        short_label='L: 1'
+    )
+
+    parameter_2 = Area(
+        base_values=numpy.linspace(0, 1, 10),
+        long_label='Area: 1',
+        short_label='A: 1'
+    )
+
+    x_table = [parameter_0, parameter_1, parameter_2]
+
+    return Table(x_table)
+
+
+@pytest.fixture
+def mock_measure_2() -> list:
+    measure = Power(
+        long_label='Arbitrary measure',
+        short_label='Arbit. measure',
+        base_values=1 + 0.3 * numpy.random.rand(10, 10)
+    )
+
+    return measure
+
+
+@pytest.fixture
+def mock_measure_3() -> list:
+    measure = Power(
+        long_label='Arbitrary measure',
+        short_label='Arbit. measure',
+        base_values=1 + 0.3 * numpy.random.rand(10, 10, 10)
+    )
+
+    return measure
+
+
+@patch("matplotlib.pyplot.show")
+def test_plot_line(patch, mock_x_table_2, mock_measure_2):
+    data = Array(
+        x_table=mock_x_table_2,
+        y=mock_measure_2
+    )
+
+    figure = data.plot(x=mock_x_table_2[1])
+
+    figure.show()
+
+
+@patch("matplotlib.pyplot.show")
+def test_plot_std_line(patch, mock_x_table_3, mock_measure_3):
+    parameter_0, parameter_1, parameter_2 = mock_x_table_3
+
+    data = Array(
+        x_table=mock_x_table_3,
+        y=mock_measure_3,
+    )
+
+    figure = data.plot(x=parameter_1, std=parameter_2)
+
+    figure.show()
+
+
+@patch("matplotlib.pyplot.show")
+def test_mean_plot_line(patch, mock_x_table_3, mock_measure_3):
+    parameter_0, parameter_1, parameter_2 = mock_x_table_3
+
+    data = Array(
+        x_table=mock_x_table_3,
+        y=mock_measure_3
+    )
+
+    data = data.mean(axis=parameter_0)
+
+    figure = data.plot(x=parameter_1)
+
+    figure.show()
+
+
+@patch("matplotlib.pyplot.show")
+def test_std_plot_line(patch, mock_x_table_3, mock_measure_3):
+    parameter_0, parameter_1, parameter_2 = mock_x_table_3
+
+    data = Array(
+        x_table=mock_x_table_3,
+        y=mock_measure_3
+    )
+
+    data = data.std(axis=parameter_0)
+
+    figure = data.plot(x=parameter_1)
+
+    figure.show()
+
+
+@patch("matplotlib.pyplot.show")
+def test_rsd_plot_line(patch, mock_x_table_3, mock_measure_3):
+    parameter_0, parameter_1, parameter_2 = mock_x_table_3
+
+    data = Array(
+        x_table=mock_x_table_3,
+        y=mock_measure_3
+    )
+
+    data = data.rsd(axis=parameter_0)
+
+    figure = data.plot(x=parameter_1)
+
+    figure.show()
+
+
+# -
