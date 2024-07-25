36739cdf11f7fd30b9637fb58e55b8d6df8cc992
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:06:41 +0000

    Add package optimodel-py/1.4.4/optimodel_py-1.4.4.tar.gz

---
+++ packages/optimodel-py/optimodel_py-1.4.4.tar.gz/optimodel_py-1.4.4/src/optimodel/ListModels/ListModels.py
@@ -0,0 +1,14 @@
+import aiohttp
+
+from optimodel.Consts import BASE_URL
+from optimodel.Utils import HttpClient
+
+
+async def listModels():
+    """
+    List all available models
+    """
+    response = await HttpClient.getRequest(
+        f"{BASE_URL.rstrip('/')}/optimodel/api/v1/list-models"
+    )
+    return response
