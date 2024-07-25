6add75c7c3a728548835c516d12617f95f882791
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:52 +0000

    Add package bighummingbird/0.0.2024072406/bighummingbird-0.0.2024072406.tar.gz

---
+++ packages/bighummingbird/bighummingbird-0.0.2024072406.tar.gz/bighummingbird-0.0.2024072406/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.0.2024072406'
+DESCRIPTION = 'bighummingbird'
+
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
+# Setting up
+setup(
+    name="bighummingbird",
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=find_packages(),
+    install_requires=required,
+
+    keywords=['python', 'first package'],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Education",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 3",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
