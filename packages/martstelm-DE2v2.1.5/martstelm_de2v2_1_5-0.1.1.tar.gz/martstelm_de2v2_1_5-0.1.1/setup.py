f8669a717b840cff6c351dfc5ef70279f9131b41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:06:05 +0000

    Add package martstelm-DE2v2.1.5/0.1.1/martstelm_de2v2_1_5-0.1.1.tar.gz

---
+++ packages/martstelm-DE2v2.1.5/martstelm_de2v2_1_5-0.1.1.tar.gz/martstelm_de2v2_1_5-0.1.1/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='martstelm-DE2v2.1.5',
+    version='0.1.1',
+    author='Martynas Stelmokas',
+    author_email='stelmokas00@gmail.com',
+    description='A package containing utility functions for matrix operations including transpose, 1D windowing, and 2D convolution.',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/TuringCollegeSubmissions/martstelm-DE2v2.1.5',
+    packages=find_packages(),
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
