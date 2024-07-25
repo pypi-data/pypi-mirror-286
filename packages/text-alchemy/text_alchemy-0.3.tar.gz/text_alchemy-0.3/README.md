a068046177d274704feab84e521ec57668951ac6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:42 +0000

    Add package text-alchemy/0.3/text_alchemy-0.3.tar.gz

---
+++ packages/text-alchemy/text_alchemy-0.3.tar.gz/text_alchemy-0.3/README.md
@@ -0,0 +1,3 @@
+# Text Extractor
+
+A package to extract text and structured information using OpenAI and Google Vision APIs.
---
+++ packages/text-alchemy/text_alchemy-0.3.tar.gz/text_alchemy-0.3/text_extractor/__init__.py
@@ -0,0 +1,4 @@
+# text_extractor/__init__.py
+from .extractor import Extractor
+from .openai import OpenAIAPI
+from .vision import GoogleVisionAPI
