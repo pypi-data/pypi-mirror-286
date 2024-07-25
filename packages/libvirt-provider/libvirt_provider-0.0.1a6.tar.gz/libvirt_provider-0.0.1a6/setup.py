50d2f55b5fc6be5005af411a7a8db7cb6c9a10d2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:06:37 +0000

    Add package libvirt-provider/0.0.1a6/libvirt_provider-0.0.1a6.tar.gz

---
+++ packages/libvirt-provider/libvirt_provider-0.0.1a6.tar.gz/libvirt_provider-0.0.1a6/setup.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python
+# coding: utf-8
+import os
+from setuptools import setup, find_packages
+
+cur_dir = os.path.abspath(os.path.dirname(__file__))
+
+
+def read(path):
+    with open(path, "r") as _file:
+        return _file.read()
+
+
+def read_req(name):
+    path = os.path.join(cur_dir, name)
+    return [req.strip() for req in read(path).splitlines() if req.strip()]
+
+
+version_ns = {}
+with open(os.path.join(cur_dir, "version.py")) as f:
+    exec(f.read(), {}, version_ns)
+
+
+long_description = open("README.rst").read()
+setup(
+    name="libvirt_provider",
+    version=version_ns["__version__"],
+    description="A corc plugin for a libvirt orchestration provider",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    author="Rasmus Munk",
+    author_email="munk1@live.dk",
+    packages=find_packages(),
+    url="https://github.com/rasmunk/libvirt_provider",
+    license="MIT",
+    keywords=["Orchstration", "Virtual Machine"],
+    install_requires=read_req("requirements.txt"),
+    extras_require={
+        "test": read_req("tests/requirements.txt"),
+        "dev": read_req("requirements-dev.txt"),
+    },
+    # Ensures that the plugin can be discovered/loaded by corc
+    entry_points={
+        "console_scripts": ["libvirt-provider = libvirt_provider.cli.cli:run"],
+        "corc.plugins": ["libvirt_provider=libvirt_provider.instance"],
+        "corc.plugins.orchestration": ["libvirt_provider=libvirt_provider.instance"],
+    },
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Intended Audience :: System Administrators",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
+    ],
+)
