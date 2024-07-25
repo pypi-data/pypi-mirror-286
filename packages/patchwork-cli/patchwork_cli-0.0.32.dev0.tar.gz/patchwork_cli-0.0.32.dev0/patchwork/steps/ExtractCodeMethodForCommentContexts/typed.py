a64bd767559529eb6649782eeab45341bdf1aca0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:04:50 +0000

    Add package patchwork-cli/0.0.32.dev0/patchwork_cli-0.0.32.dev0.tar.gz

---
+++ packages/patchwork-cli/patchwork_cli-0.0.32.dev0.tar.gz/patchwork_cli-0.0.32.dev0/patchwork/steps/ExtractCodeMethodForCommentContexts/typed.py
@@ -0,0 +1,20 @@
+from __future__ import annotations
+
+from typing_extensions import Annotated, TypedDict
+
+from patchwork.common.utils.typing import IS_PATH
+
+
+class ExtractCodeMethodForCommentContextsInputs(TypedDict):
+    base_path: Annotated[str, IS_PATH]
+
+
+class ExtractCodeMethodForCommentContextsOutputs(TypedDict):
+    files_to_patch: list["ExtractedCode"]
+
+
+class ExtractedCode(TypedDict):
+    uri: str
+    startLine: int
+    endLine: int
+    affectedCode: str
