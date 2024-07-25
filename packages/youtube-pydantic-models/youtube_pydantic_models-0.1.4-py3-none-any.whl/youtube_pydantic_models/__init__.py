e6aa2b35675f7b5691371c540cb945bb6a2b6eed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 04:06:56 +0000

    Add package youtube-pydantic-models/0.1.4/youtube_pydantic_models-0.1.4-py3-none-any.whl

---
+++ packages/youtube-pydantic-models/youtube_pydantic_models-0.1.4-py3-none-any.whl/youtube_pydantic_models/__init__.py
@@ -0,0 +1,49 @@
+from youtube_pydantic_models.base_model_config import get_base_model_config
+from youtube_pydantic_models.base_resource import YoutubeBaseResource
+from youtube_pydantic_models.channel_resource import YoutubeChannelResource
+from youtube_pydantic_models.playlist_resource import YoutubePlaylistResource
+from youtube_pydantic_models.video_resource import YoutubeVideoResource
+from youtube_pydantic_models.search_resource import YoutubeSearchResource
+from youtube_pydantic_models.youtube_client import YoutubeClient
+
+# from youtube_pydantic_models._parts.audit_details import BaseAuditDetails
+# from youtube_pydantic_models._parts.branding_settings import BaseBrandingSettings
+# from youtube_pydantic_models._parts.content_details import (
+#     ChannelContentDetails,
+#     PlaylistContentDetails,
+#     VideoContentDetails
+# )
+# from youtube_pydantic_models._parts.content_owner_details import BaseContentOwnerDetails
+# from youtube_pydantic_models._parts.file_details import BaseFileDetails
+# from youtube_pydantic_models._parts.id import SearchId
+# from youtube_pydantic_models._parts.live_streaming_details import BaseLiveStreamingDetails
+# from youtube_pydantic_models._parts.page_info import BasePageInfo
+# from youtube_pydantic_models._parts.player import (
+#     BasePlayer,
+#     VideoPlayer
+# )
+# from youtube_pydantic_models._parts.processing_details import BaseProcessingDetails
+# from youtube_pydantic_models._parts.recording_details import BaseRecordingDetails
+# from youtube_pydantic_models._parts.snippet import (
+#     BaseSnippet,
+#     SearchSnippet,
+#     ChannelSnippet,
+#     PlaylistSnippet,
+#     VideoSnippet
+# )
+# from youtube_pydantic_models._parts.statistics import (
+#     BaseStatistics,
+#     ChannelStatistics,
+#     VideoStatistics
+# )
+# from youtube_pydantic_models._parts.status import (
+#     BaseStatus,
+#     SharedStatus,
+#     ChannelStatus,
+#     VideoStatus
+# )
+# from youtube_pydantic_models._parts.suggestions import BaseSuggestions
+# from youtube_pydantic_models._parts.topic_details import (
+#     BaseTopicDetails,
+#     VideoTopicDetails
+# )
