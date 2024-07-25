70527fe8dfffd8297785480ae236c1822f23f12c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:11 +0000

    Add package rmrag/0.3.1/rmrag-0.3.1.tar.gz

---
+++ packages/rmrag/rmrag-0.3.1.tar.gz/rmrag-0.3.1/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='rmrag',
+    version='0.3.1',
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=[],
+    author='Lasse Tranekjær Leed',
+    author_email='lasseleed@gmail.com',
+    description='RAG functions',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    classifiers=[
+        'Development Status :: 3 - Alpha', 
+        'Intended Audience :: Developers',
+        'Programming Language :: Python :: 3.9']
+)
+
+#0.2 = added tests for all function + get_response into class 
+#0.3 = Fixed tests and added Chatbot_OpenSource
+#0.31 = Updated Chatbot_OpenAI and made default model to GPT-4o mini
