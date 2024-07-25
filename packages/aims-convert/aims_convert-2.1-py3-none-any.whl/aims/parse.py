f596f9363960eae52715e36015ea66b0e9a91154
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 15:05:44 +0000

    Add package aims-convert/2.1/aims_convert-2.1-py3-none-any.whl

---
+++ packages/aims-convert/aims_convert-2.1-py3-none-any.whl/aims/parse.py
@@ -0,0 +1,24 @@
+from bs4 import BeautifulSoup  # type: ignore
+
+from aims.data_structures import Duty, InputFileException
+import aims.roster
+import aims.logbook_report
+
+
+def parse(html: str) -> tuple[Duty, ...]:
+    # check it's an html5 file
+    html5_header = "<!DOCTYPE html><html>"
+    if html[:len(html5_header)] != html5_header:
+        raise InputFileException("HTML5 header not found.")
+    # make some soup
+    soup = BeautifulSoup(html, "html5lib")
+    # route the soup
+    strings = list(soup.find("body").stripped_strings)
+    if "Personal Crew Schedule Report" in strings:
+        return aims.roster.duties(soup)
+    elif "Pilot Logbook" in strings:
+        return aims.logbook_report.duties(soup)
+    else:
+        raise InputFileException(
+            "Report marker not found: \n" +
+            "; ".join(strings[:20]))
