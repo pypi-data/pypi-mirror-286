d045631ec4747f9bd1a320fac37cddd7c209e3d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 06:05:32 +0000

    Add package kfinance-assistant/0.1/kfinance_assistant-0.1.tar.gz

---
+++ packages/kfinance-assistant/kfinance_assistant-0.1.tar.gz/kfinance_assistant-0.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='kfinance_assistant',
+    version='0.1',
+    packages=find_packages(),
+    description='Система для управления финансами и анализа расходов.',
+    author='Kuerton',
+    author_email='',
+    url='https://github.com/artemkuerton/finance_assistant',
+    install_requires=[],
+)
