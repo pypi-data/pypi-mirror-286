0aced305ab36a1de39b03227447139dd5a1e2831
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 14:05:01 +0000

    Add package atorch/0.1.11rc1/atorch-0.1.11rc1-py3-none-any.whl

---
+++ packages/atorch/atorch-0.1.11rc1-py3-none-any.whl/atorch/ops/git_version_info_installed.py
@@ -0,0 +1,6 @@
+version = '0.1.11rc1+0faa0a8a'
+git_hash = '0faa0a8a'
+git_branch = '1.4.1'
+installed_ops = {'quantization_optimizer': False, 'quantizer': False}
+compatible_ops = {'quantization_optimizer': True, 'quantizer': True, 'atorch_not_implemented': False}
+torch_info = {'version': '1.13', 'bf16_support': False, 'cuda_version': '11.6', 'nccl_version': '2.14'}
