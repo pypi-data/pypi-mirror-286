6f6d9ffe0eef27c99a3c831d2b72763372abe804
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:06:02 +0000

    Add package corecon/1.4.2/corecon-1.4.2-py3-none-any.whl

---
+++ packages/corecon/corecon-1.4.2-py3-none-any.whl/corecon/data/data_entry_template.py
@@ -0,0 +1,29 @@
+dictionary_tag         = ""
+
+reference              = ""
+
+url                    = ""
+
+description            = \
+"""
+"""
+
+data_structure         = "grid" #grid or points
+
+extracted              = True #False if the original paper provides number, True if extracted from plots
+
+ndim                   = 
+
+dimensions_descriptors = []
+
+axes                   = []
+
+values                 = []
+
+err_up                 = []
+
+err_down               = []
+
+upper_lim              = []
+
+lower_lim              = []
