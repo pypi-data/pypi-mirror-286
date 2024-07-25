a5c25ab76f52bc3350340d71cd73b966c29e3805
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 05:04:39 +0000

    Add package collab-learning-extension/0.2.5/collab_learning_extension-0.2.5.tar.gz

---
+++ packages/collab-learning-extension/collab_learning_extension-0.2.5.tar.gz/collab_learning_extension-0.2.5/page/src/project-page/part/content/AbstractContent.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export const AbstractContent: React.FC = () => {
+  return (
+    <p>
+      Peer assessment as a form of collaborative learning can encourage students
+      to learn actively and improve their learning progress. Previous research
+      has demonstrated the potential of systems that integrate peer assessment
+      mechanisms. In this project, we aimed to develop a JupyterLab extension to
+      make computational notebooks more usable and engaging for teaching and
+      learning introductory programming. We were particularly interested in
+      supporting instructors to share and demonstrate coding to students,
+      allowing instructors to assess students’ notebooks at scale, and
+      supporting students to collaboratively write and assess their code. During
+      development, we extracted core functionality into separate libraries to
+      enhance reusability, including a React quiz UI component library and a Yjs
+      extension for handling normalized data. Last but no least, we evaluated
+      the system and reached out for user feedback.
+    </p>
+  );
+};
