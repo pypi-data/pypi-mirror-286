9c13bdf3b917640ae4ce0439f7804de03deb3ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-25 03:07:09 +0000

    Add package barebonesnn/0.1.0/barebonesnn-0.1.0.tar.gz

---
+++ packages/barebonesnn/barebonesnn-0.1.0.tar.gz/barebonesnn-0.1.0/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='barebonesnn',
+    version='0.1.0',
+    packages=find_packages(),
+    description='A neural network library built from scratch.',
+    author='Amir Mohammadikarbalaei',
+    author_email='a.mohammadikarbalaei@gmail.com',
+    url='https://github.com/AmirMohammadiKarbalaei/BareBonesNN/barebonesnn',
+    install_requires=[
+        'numpy>=1.21.0', 
+    ],
+    python_requires='>=3.6',
+    long_description=open("README_pypi.md").read(),
+)
