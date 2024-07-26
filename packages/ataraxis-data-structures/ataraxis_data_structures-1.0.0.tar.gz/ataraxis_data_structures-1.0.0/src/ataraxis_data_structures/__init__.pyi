7c98f128e59699a52c9ea7559c24a04c3a03823e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:48 +0000

    Add package ataraxis-data-structures/1.0.0/ataraxis_data_structures-1.0.0.tar.gz

---
+++ packages/ataraxis-data-structures/ataraxis_data_structures-1.0.0.tar.gz/ataraxis_data_structures-1.0.0/src/ataraxis_data_structures/__init__.pyi
@@ -0,0 +1,7 @@
+from .shared_memory import SharedMemoryArray as SharedMemoryArray
+from .data_structures import (
+    YamlConfig as YamlConfig,
+    NestedDictionary as NestedDictionary,
+)
+
+__all__ = ["SharedMemoryArray", "NestedDictionary", "YamlConfig"]
