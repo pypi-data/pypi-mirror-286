f6da0bb96ec712341ef917e660317a273809b59d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:20 +0000

    Add package astrago/0.0.5/astrago-0.0.5.tar.gz

---
+++ packages/astrago/astrago-0.0.5.tar.gz/astrago-0.0.5/astrago/__init__.py
@@ -0,0 +1,14 @@
+from .tracking import *
+from .val import workload_name, url
+from .tracking import LoggingClient
+
+__version__ = '0.0.5'
+
+__all__ = (
+    'log'
+)
+
+
+def log(log_data: Dict):
+    client = LoggingClient()
+    client.log(log_data)
