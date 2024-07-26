cf62a45515192352fc47a58e258ac05ab60b8a53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 07:04:53 +0000

    Add package fqr/0.7.0rc1/fqr-0.7.0rc1-py3-none-any.whl

---
+++ packages/fqr/fqr-0.7.0rc1-py3-none-any.whl/tests/loggers/cfg.py
@@ -0,0 +1,11 @@
+"""Constant values specific to module unit tests."""
+
+__all__ = (
+    'Constants',
+    )
+
+from fqr import loggers
+
+
+class Constants(loggers.cfg.Constants):
+    """Constant values specific to unit tests in this module."""
