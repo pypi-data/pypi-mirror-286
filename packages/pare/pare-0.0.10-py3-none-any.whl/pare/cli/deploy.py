c2824484a78b5eed0e0ea17e5fa44f6acb9a7cfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:47 +0000

    Add package pare/0.0.10/pare-0.0.10-py3-none-any.whl

---
+++ packages/pare/pare-0.0.10-py3-none-any.whl/pare/cli/deploy.py
@@ -0,0 +1,123 @@
+from __future__ import annotations
+
+import importlib.util
+import inspect
+import json
+import os
+import sys
+import tempfile
+import zipfile
+from pathlib import Path
+from typing import Dict, TypedDict
+
+import requests
+from rich.console import Console
+
+from pare import settings
+from pare.cli.console import log_error, log_task
+
+
+class DeployType(TypedDict):
+    module: str | None
+    reference: str
+    function: str
+    python_version: str
+    dependencies: list[str]
+
+
+DeployConfigType = Dict[str, DeployType]
+
+
+class DeployHandler:
+    def __init__(
+        self,
+        file_paths: list[str],
+        api_url: str | None = None,
+        client_secret: str | None = None,
+    ) -> None:
+        self.file_paths = {Path(file_path) for file_path in file_paths}
+        self.deploy_url = (api_url or settings.PARE_API_URL) + "/deploy/"
+        self.client_secret = client_secret or settings.CLIENT_SECRET
+
+    @property
+    def headers(self) -> dict[str, str]:
+        return {"X-Client-Secret": self.client_secret}
+
+    def validate_file_paths(self) -> None:
+        errored = False
+        for file_path in self.file_paths:
+            if not file_path.exists():
+                errored = True
+                log_error(f"{file_path} does not exist")
+            elif not file_path.is_file():
+                errored = True
+                log_error(f"{file_path} is not a file")
+            elif file_path.suffix.lower() != ".py":
+                errored = True
+                log_error(f"{file_path} is not a python file")
+        if errored:
+            sys.exit(1)
+
+    def bundle(self, temp_dir: str) -> Path:
+        with log_task(start_message="Bundling...", end_message="Project bundled"):
+            zip_filename = "pare_project_bundle.zip"
+            zip_path = Path(temp_dir) / zip_filename
+
+            with zipfile.ZipFile(zip_path, "w", zipfile.ZIP_DEFLATED) as zipf:
+                for file_path in self.file_paths:
+                    zipf.write(file_path)
+        return zip_path
+
+    def upload(self, zip_path: Path, deployments: DeployConfigType) -> None:
+        with log_task(
+            start_message="Uploading bundle...", end_message="Bundle uploaded"
+        ):
+            with open(zip_path, "rb") as zip_file:
+                files = {"file": zip_file, "json_data": (None, json.dumps(deployments))}
+                resp = requests.post(
+                    self.deploy_url,
+                    headers=self.headers,
+                    files=files,
+                )
+            if resp.status_code != 200:
+                log_error("Failed to trigger the deploy")
+                sys.exit(1)
+
+    def register_deployments(self) -> DeployConfigType:
+        results = {}
+        for file_path in self.file_paths:
+            module_name = str(file_path).replace(os.path.sep, ".").replace(".py", "")
+            spec = importlib.util.spec_from_file_location(file_path.stem, file_path)
+            if spec and spec.loader:
+                module = importlib.util.module_from_spec(spec)
+                spec.loader.exec_module(module)
+                for name, obj in inspect.getmembers(module):
+                    if inspect.isfunction(obj) and hasattr(obj, "_pare_register"):
+                        try:
+                            name, config = obj._pare_register()  # type: ignore
+                            if name in results:
+                                raise ValueError(f"Duplicate endpoint {name}")
+                            results[name] = {
+                                "module": module_name,
+                                "reference": f"{module_name}:{config['function']}",
+                                **config,
+                            }
+
+                        except Exception as e:
+                            print(f"Error calling _pare_register on {name}: {e}")
+                            break
+            else:
+                print(f"Couldn't load module from {file_path}")
+        return results  # type: ignore
+
+    def deploy(self):
+        console = Console()
+        console.print(
+            "[bold white]Deploying...[/bold white]",
+        )
+        self.validate_file_paths()
+        deployments = self.register_deployments()
+        with tempfile.TemporaryDirectory() as temp_dir:
+            zip_path = self.bundle(temp_dir)
+            self.upload(zip_path, deployments)
+        console.print("[bold green]Deployed![/bold green]")
---
+++ packages/pare/pare-0.0.10-py3-none-any.whl/pare/sdk/main.py
@@ -0,0 +1,120 @@
+from __future__ import annotations
+
+import json
+from dataclasses import asdict, dataclass, field
+from typing import Any, Callable
+
+import aiohttp
+import requests
+
+from pare import errors, settings
+
+
+@dataclass
+class RemoteInvocationArguments:
+    args: list[Any] = field(default_factory=list)
+    kwargs: dict[Any, Any] = field(default_factory=dict)
+
+
+def invoke_endpoint(function_name: str, arguments: RemoteInvocationArguments) -> Any:
+    try:
+        response = requests.post(
+            f"{settings.PARE_API_URL}/invoke/{function_name}/",
+            headers={"X-Client-Secret": settings.CLIENT_SECRET},
+            json=json.dumps(asdict(arguments)),
+        )
+        response.raise_for_status()
+        return response.json()
+    except requests.HTTPError as e:
+        raise errors.PareInvokeError(
+            f"Function invocation for '{function_name}' failed with status: {e.response.status_code}"
+        )
+    except Exception as e:
+        raise errors.PareInvokeError(
+            f"Could not invoke function: '{function_name}' due to error:\n{e}"
+        )
+
+
+async def async_invoke_endpoint(
+    function_name: str, arguments: RemoteInvocationArguments
+) -> Any:
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.post(
+                f"{settings.PARE_API_URL}/invoke/{function_name}/",
+                headers={"X-Client-Secret": settings.CLIENT_SECRET},
+                json=json.dumps(asdict(arguments)),
+            ) as response:
+                response.raise_for_status()
+                return await response.json()
+        except aiohttp.ClientResponseError as e:
+            raise errors.PareInvokeError(
+                f"Function invocation for '{function_name}' failed with status: {e.status}"
+            )
+        except Exception as e:
+            raise errors.PareInvokeError(
+                f"Could not invoke function: '{function_name}' due to error:\n{e}"
+            )
+
+
+def endpoint(
+    name: str, python_version: str = "3.12", dependencies: list[str] = []
+) -> Callable[..., Any]:
+    def endpoint_decorator(
+        function: Callable[..., Any],
+    ) -> Callable[..., Any]:
+        def _pare_register() -> tuple[str, dict[str, str | list[str]]]:
+            return name, {
+                "function": function.__name__,
+                "python_version": python_version,
+                "dependencies": dependencies,
+            }
+
+        function._pare_register = _pare_register  # pyright: ignore[reportFunctionMemberAccess]
+
+        def _as_lambda_handler() -> Callable[[Any, Any], Any]:
+            def _lambda_handler(event: Any, context: Any) -> Any:
+                if not isinstance(event, dict):
+                    return {
+                        "status": 400,
+                        "detail": "Could not parse incoming data. The request body must be JSON.",
+                    }
+                if "args" not in event and "kwargs" not in event:
+                    return {
+                        "status": 400,
+                        "detail": "Incoming JSON should contain 'args' or 'kwargs' to invoke the function.",
+                    }
+                try:
+                    return {
+                        "status": 200,
+                        "result": function(
+                            *event.get("args", []),  # type: ignore
+                            **event.get("kwargs", {}),  # type: ignore
+                        ),
+                    }
+                except Exception as e:
+                    return {"status": 500, "detail": str(e)}
+
+            return _lambda_handler
+
+        function.as_lambda_function_url_handler = _as_lambda_handler  # pyright: ignore[reportFunctionMemberAccess]
+
+        def _invoke_fn(*args, **kwargs) -> Callable[..., Any]:  # type: ignore
+            return invoke_endpoint(
+                name,
+                RemoteInvocationArguments(args=args, kwargs=kwargs),  # type: ignore
+            )
+
+        function.invoke = _invoke_fn  # pyright: ignore[reportFunctionMemberAccess]
+
+        async def _async_invoke_fn(*args, **kwargs) -> Callable[..., Any]:  # type: ignore
+            return await async_invoke_endpoint(
+                name,
+                RemoteInvocationArguments(args=args, kwargs=kwargs),  # type: ignore
+            )
+
+        function.invoke_async = _async_invoke_fn  # pyright: ignore[reportFunctionMemberAccess]
+
+        return function
+
+    return endpoint_decorator
