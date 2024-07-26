d8cf1593edab7447055eac7190f571ca1a2cb564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:39 +0000

    Add package nautobot/2.3.0b1/nautobot-2.3.0b1-py3-none-any.whl

---
+++ packages/nautobot/nautobot-2.3.0b1-py3-none-any.whl/nautobot/extras/forms/base.py
@@ -0,0 +1,58 @@
+from nautobot.core.forms import BootstrapMixin
+
+from .mixins import (
+    ContactTeamModelFilterFormMixin,
+    CustomFieldModelBulkEditFormMixin,
+    CustomFieldModelFilterFormMixin,
+    CustomFieldModelFormMixin,
+    DynamicGroupModelFormMixin,
+    NoteModelBulkEditFormMixin,
+    NoteModelFormMixin,
+    RelationshipModelBulkEditFormMixin,
+    RelationshipModelFilterFormMixin,
+    RelationshipModelFormMixin,
+)
+
+__all__ = (
+    "NautobotModelForm",
+    "NautobotFilterForm",
+    "NautobotBulkEditForm",
+)
+
+
+#
+# Nautobot base forms for use in most new custom model forms.
+#
+
+
+class NautobotModelForm(
+    CustomFieldModelFormMixin,
+    DynamicGroupModelFormMixin,
+    NoteModelFormMixin,
+    RelationshipModelFormMixin,
+    BootstrapMixin,
+):
+    """
+    This class exists to combine common functionality and is used to inherit from throughout the
+    codebase where all of BootstrapMixin, CustomFieldModelFormMixin, RelationshipModelFormMixin, and
+    NoteModelFormMixin are needed.
+    """
+
+
+class NautobotFilterForm(
+    ContactTeamModelFilterFormMixin,
+    BootstrapMixin,
+    CustomFieldModelFilterFormMixin,  # currently must come *after* BootstrapMixin to get proper CSS classes applied
+    RelationshipModelFilterFormMixin,
+):
+    """
+    This class exists to combine common functionality and is used to inherit from throughout the
+    codebase where all of ContactTeamModelFilterFormMixin, CustomFieldModelFilterFormMixin, and
+    RelationshipModelFilterFormMixin are needed.
+    """
+
+
+class NautobotBulkEditForm(
+    BootstrapMixin, CustomFieldModelBulkEditFormMixin, RelationshipModelBulkEditFormMixin, NoteModelBulkEditFormMixin
+):
+    """Base class for bulk-edit forms for models that support relationships, custom fields and notes."""
