939b284bb286d87adfb0d9346a935ff8a503e5f1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 07:04:43 +0000

    Add package pymedials/0.1.0/pymedials-0.1.0.tar.gz

---
+++ packages/pymedials/pymedials-0.1.0.tar.gz/pymedials-0.1.0/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='pymedials',
+    version='0.1.0',
+    description='A Python library for downloading images from Pinterest',
+    author='Amal nath',
+    author_email='amalnath0600@gmail.com',
+    url="https://github.com/MrTG-CodeBot",
+    telegram_url="https://t.me/MrTG_Coder",
+    packages=find_packages(),
+    install_requires=['requests', 'bs4'],
+)
