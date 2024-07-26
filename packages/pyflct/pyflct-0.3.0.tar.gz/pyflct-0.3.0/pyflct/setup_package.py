6ed9b7335e59f77a39ed016b3cc3eef5b546c8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:46 +0000

    Add package pyflct/0.3.0/pyflct-0.3.0.tar.gz

---
+++ packages/pyflct/pyflct-0.3.0.tar.gz/pyflct-0.3.0/pyflct/setup_package.py
@@ -0,0 +1,40 @@
+import os
+import sys
+import platform
+import subprocess
+from glob import glob
+from collections import defaultdict
+from distutils.core import Extension
+
+import numpy as np
+from extension_helpers import get_compiler
+
+ROOT = os.path.dirname(__file__)
+
+
+def get_extensions():
+    cfg = defaultdict(list)
+    cfg["include_dirs"].append(np.get_include())
+    cfg["include_dirs"].append(os.path.join(sys.prefix, "include"))
+    cfg["include_dirs"].append(os.path.join("cextern"))
+    cfg["sources"].extend(sorted(glob(os.path.join("cextern", "*.c"))))
+    cfg["sources"].extend(sorted(glob(os.path.join(ROOT, "*.c"))))
+    cfg["sources"].extend(sorted(glob(os.path.join(ROOT, "flct.pyx"))))
+
+    if get_compiler().lower() == "msvc":
+        # Anaconda paths
+        cfg["include_dirs"].append(os.path.join(sys.prefix, "Library", "include"))
+        cfg["library_dirs"].append(os.path.join(sys.prefix, "Library", "lib"))
+    else:
+        cfg["libraries"].append("m")
+        # We assume we have brew installed on Mac OS to provide FFTW3
+        if platform.system().lower() == "darwin":
+            brew_path = (
+                subprocess.run(["brew", "--prefix"], stdout=subprocess.PIPE).stdout.decode("utf-8").replace("\n", "")
+            )
+            cfg["include_dirs"].append(f"{brew_path}/include")
+        cfg["libraries"].append("fftw3")
+        cfg["include_dirs"].append("/usr/include")
+        cfg["include_dirs"].append("/usr/local/include")
+        cfg["extra_compile_args"].extend(["-O3", "-w", "-fomit-frame-pointer", "-fPIC"])
+    return [Extension("pyflct._flct", **cfg)]
