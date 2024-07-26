dce4fb5373d71c2de4e820884e286616bcf61faf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:04:22 +0000

    Add package unlimited-classifier/0.0.3/unlimited_classifier-0.0.3.tar.gz

---
+++ packages/unlimited-classifier/unlimited_classifier-0.0.3.tar.gz/unlimited_classifier-0.0.3/README.md
@@ -0,0 +1,120 @@
+# Ultimited classifier
+
+![Tree](images/tree.jpeg)
+
+This is a universal text classifier that uses text generation with constraints for large-scale zero-shot text categorization. We represent labels as a tree of tokens and limit the possible variants of generation to provided labels. Such approach allows to make classification of text having millions of labels. 
+
+## Install
+
+``` console
+pip install -U unlimited-classifier
+```
+
+## How to use
+
+Initialize TextClassifier with labels for classification and transformers model and tokenizer (names or actual model/tokenizer).
+
+You can choose any appropriate model on HuggingFace 🤗 including [encoder-decoders](https://huggingface.co/models?pipeline_tag=text2text-generation&sort=trending) and just [decoders](https://huggingface.co/models?pipeline_tag=text-generation&sort=trending) ones. 
+
+
+``` python
+from unlimited_classifier import TextClassifier
+
+classifier = TextClassifier(
+    labels=[
+        '''label for classification 1''',
+        '''label for classification 2'''    
+    ],
+    model='''model name''',
+    tokenizer='''tokenizer name''',
+)
+```
+
+To update labels trie run the following method:
+```python
+
+classifier.initialize_labels_trie(labels)
+
+```
+
+Call method that suits you best:
+
+- **invoke:** for single text;
+- **ainvoke:** asynchronous for single text;
+- **invoke_batch:** for multiple texts;
+- **ainvoke_batch:** asynchronous for multiple texts;
+
+``` python
+text = '''text for classification'''
+texts = [
+    '''text for classification 1''',
+    '''text for classification 2'''
+]
+
+output = classifier.invoke(text)
+
+OR
+
+output = classifier.ainvoke(text)
+
+OR
+
+output = classifier.invoke_batch(texts)
+
+OR
+
+output = classifier.ainvoke_batch(texts)
+```
+
+## Scorer:
+
+Without a scorer, the results yield beam scores that are difficult to interpret.
+
+You can utilize the Scorer class to interpret scores within the range of 0 to 1.
+
+Initialization with scorer:
+
+``` python
+classifier = TextClassifier(
+    labels=[
+        '''label for classification 1''',
+        '''label for classification 2'''    
+    ],
+    model='''model name''',
+    tokenizer='''tokenizer name''',
+    scorer=Scorer()
+)
+```
+
+Scorere initialized with "knowledgator/comprehend_it-base" model.
+
+## Examples:
+
+``` python
+classifier = TextClassifier(
+    labels=["positive", "negative", "neutral"],
+    model="t5-base",
+    tokenizer="t5-base",
+)
+
+text = "Characterize movie review: I rented I AM CURIOUS-YELLOW from my video store because of all the controversy that surrounded it when it was first released in 1967. I also heard that at first it was seized by U.S. customs if it ever tried to enter this country, therefore being a fan of films considered controversial I really had to see this for myself.<br /><br />The plot is centered around a young Swedish drama student named Lena who wants to learn everything she can about life. In particular she wants to focus her attentions to making some sort of documentary on what the average Swede thought about certain political issues such as the Vietnam War and race issues in the United States. In between asking politicians and ordinary denizens of Stockholm about their opinions on politics, she has sex with her drama teacher, classmates, and married men. What kills me about I AM CURIOUS-YELLOW is that 40 years ago, this was considered pornographic. Really, the sex and nudity scenes are few and far between, even then it\"s not shot like some cheaply made porno. While my countrymen mind find it shocking, in reality sex and nudity are a major staple in Swedish cinema. Even Ingmar Bergman, arguably their answer to good old boy John Ford, had sex scenes in his films. I do commend the filmmakers for the fact that any sex shown in the film is shown for artistic purposes rather than just to shock people and make money to be shown in pornographic theaters in America. I AM CURIOUS-YELLOW is a good film for anyone wanting to study the meat and potatoes (no pun intended) of Swedish cinema. But really, this film doesn\"t have much of a plot."
+
+output = classifier.invoke(text)
+# [('negative', 0.0007813576376065612), ('positive', 0.0006674602045677602), ('neutral', 0.00023184997553471476)]
+```
+
+With scorer:
+
+``` python
+from unlimited_classifier.scorer import Scorer
+
+classifier = TextClassifier(
+    labels=["positive", "negative", "neutral"],
+    model="t5-base",
+    tokenizer="t5-base",
+    scorer=Scorer()
+)
+
+output = classifier.invoke(text)
+# [('positive', 0.9700802564620972), ('negative', 0.9582770466804504), ('neutral', 0.6801289319992065)]
+```
---
+++ packages/unlimited-classifier/unlimited_classifier-0.0.3.tar.gz/unlimited_classifier-0.0.3/unlimited_classifier/main.py
@@ -0,0 +1,331 @@
+from typing import Tuple, List, Union, Optional, cast
+
+from transformers import ( # type: ignore
+    PreTrainedTokenizer,
+    PreTrainedTokenizerFast,
+    PreTrainedModel,
+    AutoTokenizer, 
+    AutoModelForSeq2SeqLM,
+    GenerationMixin,
+    TFGenerationMixin,
+    FlaxGenerationMixin,
+    AutoConfig,
+    AutoModelForCausalLM
+)
+from transformers.utils import ModelOutput # type: ignore
+import torch
+import pyximport # type: ignore
+pyximport.install() # type: ignore
+
+from unlimited_classifier.labels_trie import LabelsTrie # type: ignore
+from unlimited_classifier.scorer import Scorer
+
+class TextClassifier:
+    """
+    Class for text classification
+    """
+
+    model: PreTrainedModel
+    trie: LabelsTrie
+
+    def initialize_labels_trie(self, labels: List[str]) -> None:
+        """
+        Initializing the labels trie
+
+        Args:
+            labels (List[str]): Labels that will be used.
+        """ 
+        tokenized_labels = []
+        for label in labels:
+            tokens = self.tokenizer.encode(label) # type: ignore
+            if tokens[0] == self.tokenizer.bos_token_id:
+                tokens = tokens[1:]
+            tokenized_labels.append([self.pad_token_id] + tokens) # type: ignore
+        self.trie = LabelsTrie(tokenized_labels)
+
+
+    def initialize_model(self, model: str) -> PreTrainedModel:
+        """
+        Model initialization
+
+        Args:
+            model (str): Model name or path.
+
+        Raises:
+            ValueError: If model doesnt exist or cant be found.
+
+            ValueError: If provided model is not generative.
+        
+        Returns:
+            PreTrainedModel: Initialized model.
+        """        
+        try:
+            config = AutoConfig.from_pretrained(model) # type: ignore
+        except Exception:
+            raise ValueError(
+                "The path to the model does't exist or it's unavailable on"
+                " Hugging Face."
+            )
+
+        if config.is_encoder_decoder: # type: ignore
+            return ( # type: ignore
+                AutoModelForSeq2SeqLM # type: ignore
+                .from_pretrained(model, device_map=self.device_map, quantization_config=self.quantization_config)
+            )
+        else:
+            try:
+                return ( # type: ignore
+                    AutoModelForCausalLM # type: ignore
+                    .from_pretrained(model, device_map=self.device_map, quantization_config=self.quantization_config)
+                )
+            except:
+                raise ValueError("Expected generative model.")
+
+
+    def __init__(
+        self,
+        labels: List[str], 
+        model: Union[str, PreTrainedModel],
+        tokenizer: Union[
+            str, PreTrainedTokenizer, PreTrainedTokenizerFast
+        ],
+        prompt: str = "Classifity the following text:\n {}\nLabel:",
+        device: str="cpu",
+        device_map=None,
+        quantization_config=None,
+        num_beams: int=5,
+        max_new_tokens: int=512,
+        pad_token_id: Optional[int]=None,
+        eos_token_id: Optional[int]=None,
+        scorer: Optional[Scorer]=None,
+    ) -> None:
+        """
+        Args:
+            labels (List[str]): Labels for classification.
+            
+            model (Union[str, PreTrainedModel]): Model.
+            
+            tokenizer (Union[
+                str, PreTrainedTokenizer, PreTrainedTokenizerFast
+            ]): Tokenizer.
+            
+            device (str, optional): Device. Defaults to "cpu".
+
+            device_map (str, optional): Device. Defaults to `device` value. "auto" - use GPU and CPU. "cuda" - use GPU
+
+            quantization_config: None by default. Useful to load model in 4 or 8 bits.
+            
+            num_beams (int, optional): Number of beams. Defaults to 5.
+
+            max_new_tokens (int, optional): Maximum newly generated tokens.
+        Defaults to 512.
+
+            pad_token_id (int, optional): Pad token that will be used.
+        If not provided will be equal to provided in tokenizer config. 
+
+            eos_token_id (int, optional): EOS token that will be used.
+        If not provided will be equal to provided in tokenizer config.
+
+            scorer (Scorer, optional): Scorer class. Used for rescoring.
+
+        Raises:
+            ValueError: If no labels are provided.
+
+            ValueError: If provided model is not generative.
+        """        
+        if not labels:
+            raise ValueError("No labels provided.")
+        
+        self.device = device
+        self.device_map = device_map if device_map is not None else device
+        self.num_beams = min(num_beams, len(labels))
+        self.max_new_tokens = max_new_tokens
+        self.scorer = scorer
+        self.prompt = prompt
+        self.quantization_config = quantization_config
+
+        if isinstance(model, str):
+            self.model = self.initialize_model(model)
+        else:
+            self.model = model
+
+        if (
+            not any(
+                isinstance(self.model, t) for t in ( # type: ignore
+                    GenerationMixin, 
+                    TFGenerationMixin, 
+                    FlaxGenerationMixin
+                ) 
+            ) 
+            and not self.model.config.is_decoder # type: ignore
+            and not self.model.config.is_encoder_decoder # type: ignore
+        ): 
+            raise ValueError("Expected generative model.")
+        
+        if isinstance(tokenizer, str):
+            self.tokenizer = AutoTokenizer.from_pretrained(tokenizer) # type: ignore
+        else:
+            self.tokenizer = tokenizer
+
+        self.tokenizer.padding_side = "left" # type: ignore
+        
+        if eos_token_id is not None:
+            self.tokenizer.eos_token_id = eos_token_id
+
+        if pad_token_id is not None:
+            self.tokenizer.pad_token_id = pad_token_id
+
+        if self.tokenizer.pad_token_id is None:
+            self.tokenizer.pad_token_id = self.tokenizer.eos_token_id
+
+        self.pad_token_id: int = cast(int, self.tokenizer.pad_token_id)
+
+        self.encoder_decoder: bool = self.model.config.is_encoder_decoder # type: ignore
+
+        self.initialize_labels_trie(labels)
+
+
+    def _get_candidates(
+        self, sent: torch.Tensor, prompt_len: int
+    ) -> List[int]:
+        """
+        Get next possible candidates
+
+        Args:
+            sent (torch.Tensor): Tensor.
+            prompt_len (int): Prompt length.
+
+        Returns:
+            List[int]: Possible next tokens.
+        """        
+        gen_sent: List[int] = sent.tolist() # type: ignore
+        if not self.encoder_decoder:
+            gen_sent = [self.pad_token_id, *gen_sent[prompt_len:]]
+
+        return (
+            self.trie.get(gen_sent) # type: ignore
+            or [self.tokenizer.eos_token_id]
+        )
+    
+
+    def predict(self, prompts: List[str]) -> ModelOutput:
+        """
+        Model prediction
+
+        Args:
+            prompts (List[str]): Texts joined with prompts.
+
+        Returns:
+            ModelOutput: Output generated by the model.
+        """
+        tokenized_prompt = self.tokenizer( # type: ignore
+            prompts, 
+            return_tensors="pt",
+            padding=True,
+            truncation=True
+        ).to(self.device)
+        prompt_len = tokenized_prompt['input_ids'].shape[-1] # type: ignore
+        outputs = self.model.generate( # type: ignore
+            **tokenized_prompt, # type: ignore
+            pad_token_id=self.pad_token_id,
+            max_new_tokens=self.max_new_tokens,
+            num_beams=self.num_beams,
+            num_return_sequences=self.num_beams,
+            return_dict_in_generate=True,
+            output_scores=True,
+            prefix_allowed_tokens_fn=(
+                lambda _, sent: self._get_candidates(sent, prompt_len) # type: ignore
+            )
+        )
+        return outputs # type: ignore
+
+
+    def invoke(self, text: str) -> List[Tuple[str, float]]:
+        """
+        Invokation for single text
+
+        Args:
+            text (str): Text for classification.
+
+        Returns:
+            List[Tuple[str, float]]: List of tuples containing classes and
+        their corresponding scores. The tuples are sorted by score in 
+        descending order.
+        """        
+        return self.invoke_batch([text])[0]
+
+
+    def invoke_batch(self, texts: List[str]) -> List[List[Tuple[str, float]]]:
+        """
+        Invokation for multiple texts
+
+        Args:
+            texts (List[str]): Texts for classification.
+
+        Returns:
+            List[List[Tuple[str, float]]]: A list of lists where each inner
+        list contains tuples of classes and their corresponding scores for the
+        corresponding texts. The tuples are sorted by score in descending order.
+        The order of the inner lists matches the order of the input texts.
+        """
+        inputs = [self.prompt.format(text) for text in texts]
+        outputs = self.predict(inputs)
+        decodes = self.tokenizer.batch_decode( # type: ignore
+            outputs.sequences, # type: ignore
+            skip_special_tokens=True
+        )
+
+        if self.num_beams == 1:
+            scores = torch.ones(len(decodes))
+        else:
+            scores = outputs.sequences_scores # type: ignore
+        
+        outputs2scores: List[List[Tuple[str, float]]] = []
+        for text_id in range(len(texts)):
+            input_ = inputs[text_id]
+            input_len = len(input_)
+            batch: List[Tuple[str, float]] = []
+            for beam_id in range(self.num_beams):
+                score = scores[text_id*self.num_beams+beam_id] # type: ignore
+                p = torch.exp(score).item() # type: ignore
+                label = decodes[text_id*self.num_beams+beam_id]
+                if not self.encoder_decoder:
+                    label = label[input_len:].strip()
+                batch.append((label, p))
+            outputs2scores.append(
+                self.scorer.score(texts[text_id], batch) 
+                if self.scorer 
+                else batch
+            )
+        return outputs2scores
+    
+
+    async def ainvoke(self, text: str) -> List[Tuple[str, float]]:
+        """
+        Async invoke
+
+        Args:
+            text (str): Text for classification.
+
+        Returns:
+            List[Tuple[str, float]]: List of tuples containing classes and
+        their corresponding scores. The tuples are sorted by score in 
+        descending order.
+        """        
+        return self.invoke(text)
+
+
+    async def ainvoke_batch(self, texts: List[str]) -> List[List[Tuple[str, float]]]:
+        """
+        Async invokation for multiple texts
+
+        Args:
+            texts (List[str]): Texts for classification.
+
+        Returns:
+            List[List[Tuple[str, float]]]: A list of lists where each inner
+        list contains tuples of classes and their corresponding scores for the
+        corresponding texts. The tuples are sorted by score in descending order.
+        The order of the inner lists matches the order of the input texts.
+        """    
+        return self.invoke_batch(texts)
