9cc03f3cebcbca788a48250a20528c5bf0cca59a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:10 +0000

    Add package cf-speedtest/0.1.9/cf_speedtest-0.1.9.tar.gz

---
+++ packages/cf-speedtest/cf_speedtest-0.1.9.tar.gz/cf_speedtest-0.1.9/cf_speedtest/__init__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+import sys
+
+if sys.hexversion < 0x3060000:
+    sys.exit('Python 3.6+ required')
---
+++ packages/cf-speedtest/cf_speedtest-0.1.9.tar.gz/cf_speedtest-0.1.9/cf_speedtest/__main__.py
@@ -0,0 +1,6 @@
+from __future__ import annotations
+
+from cf_speedtest.speedtest import main
+
+if __name__ == '__main__':
+    raise SystemExit(main())
