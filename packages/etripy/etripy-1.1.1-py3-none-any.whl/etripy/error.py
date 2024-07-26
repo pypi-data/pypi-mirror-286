f303013458a03b93af143ec9adef330e1b3dddd7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 05:05:36 +0000

    Add package etripy/1.1.1/etripy-1.1.1-py3-none-any.whl

---
+++ packages/etripy/etripy-1.1.1-py3-none-any.whl/etripy/error.py
@@ -0,0 +1,14 @@
+class HTTPException(Exception):
+    pass
+
+
+class SentencesException(Exception):
+    pass
+
+
+class QAException(Exception):
+    pass
+
+
+class AnalysisException(Exception):
+    pass
