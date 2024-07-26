f07ecd300c0ba220fa28a0578f453ba9c6499374
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 05:05:04 +0000

    Add package typeshi/2.0.0/typeshi-2.0.0.tar.gz

---
+++ packages/typeshi/typeshi-2.0.0.tar.gz/typeshi-2.0.0/typeshi/__init__.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+"""
+typeshi - Dict to TypedDict generation and conversion utilities
+~~~~~~~~~~~~~~~~~~
+:copyright: (c) 2024 Paul Przybyszewski
+:license: MIT, see LICENSE for more details.
+"""
+
+from .cls import *
+from .str_repr import *
+from .jsonf import *
+from .version import __version__, version_info
+
+__title__ = 'typeshi'
+__license__ = 'MIT'
+__author__ = 'Paul Przybyszewski'
+__email__ = 'paul@przybyszew.ski'
+__uri__ = "https://github.com/seven7ty/typeshi"
+__copyright__ = 'Copyright 2024 %s' % __author__
+
+__path__ = __import__('pkgutil').extend_path(__path__, __name__)
