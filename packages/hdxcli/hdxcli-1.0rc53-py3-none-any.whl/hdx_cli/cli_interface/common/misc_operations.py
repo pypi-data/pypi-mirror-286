e6f39462a96721199c03f77940cc1d85452a1a6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:04:58 +0000

    Add package hdxcli/1.0rc53/hdxcli-1.0rc53-py3-none-any.whl

---
+++ packages/hdxcli/hdxcli-1.0rc53-py3-none-any.whl/hdx_cli/cli_interface/common/misc_operations.py
@@ -0,0 +1,35 @@
+from typing import List, Tuple
+
+import json
+import click
+
+from ...library_api.common.exceptions import HdxCliException, LogicException
+from ...library_api.common import rest_operations as rest_ops
+from ...library_api.utility.decorators import report_error_and_exit
+from ..common.undecorated_click_commands import basic_settings
+
+
+
+
+
+
+@click.command(help="Get, set or list settings on a resource. When invoked with "
+               "only the key, it retrieves the value of the setting. If retrieved "
+               "with both key and value, the value for the key, if it exists, will "
+               "be set.\n"
+               "Otherwise, when invoked with no arguments, all the settings will be listed.")
+@click.argument("key", required=False, default=None)
+@click.argument("value", required=False, default=None)
+@click.pass_context
+@report_error_and_exit(exctype=Exception)
+def settings(ctx: click.Context,
+             key,
+             value):
+    resource_path = ctx.parent.obj["resource_path"]
+    profile = ctx.parent.obj["usercontext"]
+    the_value = value
+    if value:
+        the_value = value
+        if (stripped := value.strip()).startswith('[') and stripped.endswith(']'):
+            the_value = json.loads(stripped)
+    basic_settings(profile, resource_path, key, the_value)
