9b8fb2db2b751bf4b7cf2b9db24ddfee885d9432
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 07:04:54 +0000

    Add package azureml-rag/0.2.35/azureml_rag-0.2.35-py3-none-any.whl

---
+++ packages/azureml-rag/azureml_rag-0.2.35-py3-none-any.whl/azureml/rag/utils/constants.py
@@ -0,0 +1,6 @@
+# ---------------------------------------------------------
+# Copyright (c) Microsoft Corporation. All rights reserved.
+# ---------------------------------------------------------
+"""MLIndex constants."""
+
+ACS_API_VERSION = "2023-11-01"
