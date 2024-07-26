904c135954d9975be2d322a837eb48704f1cefab
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:06:05 +0000

    Add package cv-parser-espanol/0.2.8/cv_parser_espanol-0.2.8.tar.gz

---
+++ packages/cv-parser-espanol/cv_parser_espanol-0.2.8.tar.gz/cv_parser_espanol-0.2.8/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='cv_parser_espanol',
+    version='0.2.8',
+    packages=find_packages(),
+    install_requires=[
+        'spacy',
+        'PyMuPDF'
+    ],
+    entry_points={
+        'console_scripts': [
+            # Si quieres agregar scripts ejecutables
+        ],
+    },
+    long_description=long_description,
+    long_description_content_type='text/markdown',  
+)
