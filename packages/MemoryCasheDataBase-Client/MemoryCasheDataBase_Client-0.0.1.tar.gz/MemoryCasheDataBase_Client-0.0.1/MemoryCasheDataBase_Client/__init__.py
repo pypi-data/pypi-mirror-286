aa358ae554efd2a3a42f90226f9d8b8417b29419
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:56 +0000

    Add package MemoryCasheDataBase-Client/0.0.1/MemoryCasheDataBase_Client-0.0.1.tar.gz

---
+++ packages/MemoryCasheDataBase-Client/MemoryCasheDataBase_Client-0.0.1.tar.gz/MemoryCasheDataBase_Client-0.0.1/MemoryCasheDataBase_Client/__init__.py
@@ -0,0 +1,13 @@
+""" 
+:authors: Никита Макаров, Мария Кутыркина
+:license: Apache License, Version 2.0, see LICENSE file  
+
+:copyright: (c) 2024 Никита Макаров, Мария Кутыркина 
+"""
+
+from StaticSystemNumeral import StaticSystemNumeral; 
+from MemoryCasheDataBase_Client import MemoryCasheDataBase_Client; 
+
+__author__ = "Nikita Makarov, Mariya Kutirkina" 
+__version__ = "0.1.0" 
+__email__ = "Никита Макаров, Мария Кутыркина"
