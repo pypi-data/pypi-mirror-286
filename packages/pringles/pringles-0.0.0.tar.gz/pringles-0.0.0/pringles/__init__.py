7d11428ef0f96f357092dadfc800aa50ffd76f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:06:10 +0000

    Add package pringles/0.0.0/pringles-0.0.0.tar.gz

---
+++ packages/pringles/pringles-0.0.0.tar.gz/pringles-0.0.0/pringles/__init__.py
@@ -0,0 +1,9 @@
+# Copyright Piérard Sébastien, 2022-2024
+
+__version__ = '0.0.0'
+
+print ( "Hi! You are using pringles, the performance library :-)" )
+import sys
+print ( "You are using python", sys.version )
+
+
