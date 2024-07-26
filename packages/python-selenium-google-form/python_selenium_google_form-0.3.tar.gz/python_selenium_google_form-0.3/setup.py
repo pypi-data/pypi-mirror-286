7eb1002fd191153d70b1ce768455668e4412904c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:02 +0000

    Add package python-selenium-google-form/0.3/python_selenium_google_form-0.3.tar.gz

---
+++ packages/python-selenium-google-form/python_selenium_google_form-0.3.tar.gz/python_selenium_google_form-0.3/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='python_selenium_google_form',
+    version='0.3',
+    packages=find_packages(),
+    install_requires=[
+        'undetected_chromedriver==3.5.5',
+        'selenium==4.23.1'
+    ],
+)
