bdbd28d7001402b4ba6a256274d937ab0cf11d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:06:03 +0000

    Add package incremental/24.7.0/incremental-24.7.0.tar.gz

---
+++ packages/incremental/incremental-24.7.0.tar.gz/incremental-24.7.0/src/incremental/_version.py
@@ -0,0 +1,11 @@
+"""
+Provides Incremental version information.
+"""
+
+# This file is auto-generated! Do not edit!
+# Use `python -m incremental.update Incremental` to change this file.
+
+from incremental import Version
+
+__version__ = Version("Incremental", 24, 7, 0)
+__all__ = ["__version__"]
