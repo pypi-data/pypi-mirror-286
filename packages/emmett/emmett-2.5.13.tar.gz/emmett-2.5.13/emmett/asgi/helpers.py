66ab43c575069991b8e339b5835e6c4144b48a90
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 05:05:31 +0000

    Add package emmett/2.5.13/emmett-2.5.13.tar.gz

---
+++ packages/emmett/emmett-2.5.13.tar.gz/emmett-2.5.13/emmett/asgi/helpers.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+    emmett.asgi.helpers
+    -------------------
+
+    Provides ASGI helpers
+
+    :copyright: 2014 Giovanni Barillari
+    :license: BSD-3-Clause
+"""
+
+
+class RequestCancelled(Exception):
+    ...
