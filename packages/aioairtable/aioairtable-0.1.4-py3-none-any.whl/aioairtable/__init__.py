22c1b4ee150d5bd7b20d1fc3f2127bc121949d24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:57 +0000

    Add package aioairtable/0.1.4/aioairtable-0.1.4-py3-none-any.whl

---
+++ packages/aioairtable/aioairtable-0.1.4-py3-none-any.whl/aioairtable/__init__.py
@@ -0,0 +1,30 @@
+__version__ = "0.1.4"
+
+from .aioairtable import (
+    Airtable,
+    AirtableBase,
+    AirtableRecord,
+    AirtableTable,
+    Attachment,
+    CellFormat,
+    Collaborator,
+    Method,
+    NewAttachment,
+    SortDirection,
+    Thumbnail,
+)
+
+__all__ = (
+    "Airtable",
+    "AirtableBase",
+    "AirtableRecord",
+    "AirtableTable",
+    "Attachment",
+    "CellFormat",
+    "Collaborator",
+    "Method",
+    "NewAttachment",
+    "SortDirection",
+    "Thumbnail",
+    "__version__",
+)
