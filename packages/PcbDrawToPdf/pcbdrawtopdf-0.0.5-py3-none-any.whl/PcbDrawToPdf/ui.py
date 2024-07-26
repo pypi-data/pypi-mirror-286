58da181c7a57aa53143c84c8baf47baab8a985a3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:04:46 +0000

    Add package PcbDrawToPdf/0.0.5/pcbdrawtopdf-0.0.5-py3-none-any.whl

---
+++ packages/PcbDrawToPdf/pcbdrawtopdf-0.0.5-py3-none-any.whl/PcbDrawToPdf/ui.py
@@ -0,0 +1,47 @@
+import click
+import os
+from .isolate_masks import PcbDrawSvg
+
+
+@click.command()
+@click.argument("input", type=click.Path(file_okay=True, dir_okay=False, exists=True))
+@click.argument("output", type=click.Path(file_okay=True, dir_okay=False))
+def convert_masks(input: str, output: str) -> None:
+    svg = PcbDrawSvg()
+    svg.load(input)
+    svg.get_masks()
+    svg.rm_masks()
+    svg.add_mask_patterns()
+    os.makedirs(os.path.dirname(output), exist_ok=True)
+    svg.save(output)
+
+
+@click.command()
+@click.argument("input", type=click.Path(file_okay=True, dir_okay=False, exists=True))
+@click.argument("output", type=click.Path(file_okay=False, dir_okay=True))
+def extract_masks(input: str, output: str) -> None:
+    svg = PcbDrawSvg()
+    os.makedirs(os.path.dirname(output), exist_ok=True)
+    svg.load(input)
+    svg.get_masks()
+    svg.rm_masks()
+    svg.save(os.path.join(output, f"{svg.filename}_no_masked.svg"))
+    svg.isolate_board()
+    svg.save_mask_files(output)
+
+
+@click.group()
+@click.version_option("v0.0.5")
+def run() -> None:
+    """
+    PcbDrawToPDF exports the masks employed by PcbDraw in order to apply masking on Adobe
+    software and be able to save on PDF correctly.
+    """
+    pass
+
+
+run.add_command(convert_masks)
+run.add_command(extract_masks)
+
+if __name__ == "__main__":
+    run()
