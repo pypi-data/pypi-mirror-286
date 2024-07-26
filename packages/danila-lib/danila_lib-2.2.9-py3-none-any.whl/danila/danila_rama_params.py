a052918bb11cd7b53c455d0c88f906781d11a0df
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:05:04 +0000

    Add package danila-lib/2.2.9/danila_lib-2.2.9-py3-none-any.whl

---
+++ packages/danila-lib/danila_lib-2.2.9-py3-none-any.whl/danila/danila_rama_params.py
@@ -0,0 +1,4 @@
+class Danila_rama_params:
+    def __init__(self, danila_rama_classify_params, danila_rama_text_detect_params):
+        self.danila_rama_classify_params = danila_rama_classify_params
+        self.danila_rama_text_detect_params = danila_rama_text_detect_params
