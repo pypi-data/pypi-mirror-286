c7e9d555f433ac89c592d7fe9c7985e9ead5f3a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:04:53 +0000

    Add package sparkx/1.3.0/sparkx-1.3.0.tar.gz

---
+++ packages/sparkx/sparkx-1.3.0.tar.gz/sparkx-1.3.0/setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='sparkx',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        "particle==0.23.0",
+        "numpy>=1.23.5",
+        "scipy>=1.10.1",
+        "abc-property==1.0",
+        "fastjet==3.4.1.3",
+        "matplotlib>=3.7.1",
+    ],
+    version='1.3.0',
+    description='Software Package for Analyzing Relativistic Kinematics in Collision eXperiments',
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author='Niklas Götz, Hendrik Roch, Nils Sass',
+    author_email="goetz@itp.uni-frankfurt.de, hroch@wayne.edu, nsass@itp.uni-frankfurt.de",
+    url="https://smash-transport.github.io/sparkx/",
+    download_url="https://github.com/smash-transport/sparkx",
+    license='MIT',
+    include_package_data = True
+)
