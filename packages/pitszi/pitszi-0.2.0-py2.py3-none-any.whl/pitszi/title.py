473441807781e4f27b78d21cb8a0d2beebdd1fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:04:42 +0000

    Add package pitszi/0.2.0/pitszi-0.2.0-py2.py3-none-any.whl

---
+++ packages/pitszi/pitszi-0.2.0-py2.py3-none-any.whl/pitszi/title.py
@@ -0,0 +1,107 @@
+"""
+This file contains the code name.
+
+"""
+
+def show_model():
+    """
+    Show the title of the code.
+    See also http://patorjk.com/software/taag
+
+    Parameters
+    ----------
+
+    Outputs
+    ----------
+
+    """
+    
+    print("============================================================")
+    print("                        _  _    ___  ____ _                 ")
+    print("                  _ __ (_)| |_ / __||_  /(_)                ")
+    print("                 | '_ \| ||  _|\__ \ / / | |                ")
+    print("                 | .__/|_| \__||___//___||_|                ")
+    print("                 |_|                                        ")
+    print("============================================================")
+    print("   Probing ICM Turbulence from Sunyaev-Zel'dovich Imaging   ")
+    print("------------------------------------------------------------")
+    print("                           Model                            ")
+    print("------------------------------------------------------------")
+
+
+def show_data():
+    """
+    Show the title of the code.
+    See also http://patorjk.com/software/taag
+
+    Parameters
+    ----------
+
+    Outputs
+    ----------
+
+    """
+    
+    print("============================================================")
+    print("                        _  _    ___  ____ _                 ")
+    print("                  _ __ (_)| |_ / __||_  /(_)                ")
+    print("                 | '_ \| ||  _|\__ \ / / | |                ")
+    print("                 | .__/|_| \__||___//___||_|                ")
+    print("                 |_|                                        ")
+    print("============================================================")
+    print("   Probing ICM Turbulence from Sunyaev-Zel'dovich Imaging   ")
+    print("------------------------------------------------------------")
+    print("                           Data                             ")
+    print("------------------------------------------------------------")
+
+
+def show_inference_radial():
+    """
+    Show the title of the code.
+    See also http://patorjk.com/software/taag
+
+    Parameters
+    ----------
+
+    Outputs
+    ----------
+
+    """
+    
+    print("============================================================")
+    print("                        _  _    ___  ____ _                 ")
+    print("                  _ __ (_)| |_ / __||_  /(_)                ")
+    print("                 | '_ \| ||  _|\__ \ / / | |                ")
+    print("                 | .__/|_| \__||___//___||_|                ")
+    print("                 |_|                                        ")
+    print("============================================================")
+    print("   Probing ICM Turbulence from Sunyaev-Zel'dovich Imaging   ")
+    print("------------------------------------------------------------")
+    print("                    Radial Inference                        ")
+    print("------------------------------------------------------------")
+
+
+def show_inference_fluctuation():
+    """
+    Show the title of the code.
+    See also http://patorjk.com/software/taag
+
+    Parameters
+    ----------
+
+    Outputs
+    ----------
+
+    """
+    
+    print("============================================================")
+    print("                        _  _    ___  ____ _                 ")
+    print("                  _ __ (_)| |_ / __||_  /(_)                ")
+    print("                 | '_ \| ||  _|\__ \ / / | |                ")
+    print("                 | .__/|_| \__||___//___||_|                ")
+    print("                 |_|                                        ")
+    print("============================================================")
+    print("   Probing ICM Turbulence from Sunyaev-Zel'dovich Imaging   ")
+    print("------------------------------------------------------------")
+    print("                 Fluctuation Inference                      ")
+    print("------------------------------------------------------------")
