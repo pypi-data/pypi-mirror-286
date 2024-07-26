4255b6ecf0684e224ff56fae3e4e71beeb16d762
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:37 +0000

    Add package mypythondb/0.2.4/mypythondb-0.2.4.tar.gz

---
+++ packages/mypythondb/mypythondb-0.2.4.tar.gz/mypythondb-0.2.4/src/mypythondb/__init__.py
@@ -0,0 +1,13 @@
+"""The PythonDB project."""
+try:
+    from importlib.metadata import version, PackageNotFoundError  # type: ignore
+except ImportError:  # pragma: no cover
+    from importlib_metadata import version, PackageNotFoundError  # type: ignore
+
+
+try:
+    __version__ = version(__name__)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+
+from .console import start, QueryProcessor
