c844ee654118a2f97b62e60a0aac60c41464ec63
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:26 +0000

    Add package plus_sync/0.3.3/plus_sync-0.3.3.tar.gz

---
+++ packages/plus_sync/plus_sync-0.3.3.tar.gz/plus_sync-0.3.3/plus_sync/cmd/helpers/typer.py
@@ -0,0 +1,21 @@
+from typing import Annotated
+
+import typer
+
+from plus_sync.config import Config
+
+
+def get_hashed_default() -> bool:
+    config = Config.from_cmdargs()
+    return config.hash_subject_ids
+
+
+def HashSubjectIDs():  # noqa: N802
+    return Annotated[
+        bool,
+        typer.Option(
+            help='Whether to hash the subject IDs. Overrides the `hash_subject_ids` setting.',
+            default_factory=get_hashed_default,
+            show_default=False,
+        ),
+    ]
