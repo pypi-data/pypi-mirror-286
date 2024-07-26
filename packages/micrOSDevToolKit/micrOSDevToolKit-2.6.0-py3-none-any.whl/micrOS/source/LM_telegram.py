2a0de9ae252965fd9621a7ea6031eef9bba7c6d1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 05:06:41 +0000

    Add package micrOSDevToolKit/2.6.0/micrOSDevToolKit-2.6.0-py3-none-any.whl

---
+++ packages/micrOSDevToolKit/micrOSDevToolKit-2.6.0-py3-none-any.whl/micrOS/source/LM_telegram.py
@@ -0,0 +1,96 @@
+import uasyncio as asyncio
+from Notify import Telegram
+from Common import micro_task
+from Network import ifconfig
+
+#########################################
+#          micrOS Notifications         #
+#########################################
+
+# ENABLE TELEGRAM IF NW IS STA - CONNECTED TO THE WEB
+_ENABLE = True if ifconfig()[0] == "STA" else False
+if _ENABLE:
+    TELEGRAM_OBJ = Telegram()
+else:
+    # NO NETWORK CONNECTION (STA)
+    TELEGRAM_OBJ = None
+
+
+def load():
+    """
+    Set custom chat commands for Telegram
+    - /ping
+    - /cmd module function (params)
+    """
+    if TELEGRAM_OBJ is None:
+        return "Network unavailable."
+    verdict = TELEGRAM_OBJ.set_commands()
+    return "Missing telegram bot token" if verdict is None else verdict
+
+
+def send(text):
+    """
+    Send Telegram message - micrOS notification
+    :param text: text to send
+    return verdict
+    """
+    if TELEGRAM_OBJ is None:
+        return "Network unavailable."
+    verdict = TELEGRAM_OBJ.send_msg(text)
+    return "Missing telegram bot token" if verdict is None else verdict
+
+
+def receive():
+    """
+    Receive Telegram message
+    - if all value None, then no incoming messages
+    One successful msg receive is necessary to get chat_id for msg send as well!
+    """
+    if TELEGRAM_OBJ is None:
+        return "Network unavailable."
+    verdict = TELEGRAM_OBJ.get_msg()
+    return "Missing telegram bot token" if verdict is None else verdict
+
+
+async def __task():
+    cancel_cnt = 0
+    with micro_task(tag='telegram._loop') as my_task:
+        my_task.out = "[UP] Running"
+        while True:
+            # Normal task period
+            await asyncio.sleep(5)
+            try:
+                v = TELEGRAM_OBJ.receive_eval()
+                my_task.out = "Missing bot token" if v is None else v
+                cancel_cnt = 0
+            except Exception as e:
+                my_task.out = str(e)
+                # Auto scale - blocking nature - in case of serial failures (5) - hibernate task (increase async sleep)
+                cancel_cnt += 1
+                if cancel_cnt > 5:
+                    my_task.out = f"[DOWN] {e}"
+                    cancel_cnt = 5
+                    # SLOW DOWN - hibernate task
+                    asyncio.sleep(115)
+
+
+def receiver_loop():
+    """
+    Telegram msg receiver loop - automatic LM execution
+    - Only executes module (function) if the module is already loaded
+    on the endpoint / micrOS node
+    """
+    if TELEGRAM_OBJ is None:
+        return "Network unavailable."
+    state = micro_task(tag='telegram._loop', task=__task())
+    return "Starting" if state else "Already running"
+
+
+def help(widgets=False):
+    """
+    [i] micrOS LM naming convention - built-in help message
+    :return tuple:
+        (widgets=False) list of functions implemented by this application
+        (widgets=True) list of widget json for UI generation
+    """
+    return 'send "text"', 'receive', 'receiver_loop', 'load', 'INFO: Send & Receive messages with Telegram bot'
