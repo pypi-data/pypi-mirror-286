9e5a68df99d65ce74340736a6e13c50cea3375dc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:48 +0000

    Add package p-exe/0.1.1/p_exe-0.1.1.tar.gz

---
+++ packages/p-exe/p_exe-0.1.1.tar.gz/p_exe-0.1.1/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='p_exe',
+    version='0.1.1',
+    packages=find_packages(),
+    install_requires=[
+        'maturin>=1.7.0'
+    ],
+    python_requires='>=3.7',
+    long_description='Nuh uh'
+)
