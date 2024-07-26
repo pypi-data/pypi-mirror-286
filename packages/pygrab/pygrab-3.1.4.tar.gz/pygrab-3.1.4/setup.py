0857a21b82ed6b7a7875c7f8067c39319f3501a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:42 +0000

    Add package pygrab/3.1.4/pygrab-3.1.4.tar.gz

---
+++ packages/pygrab/pygrab-3.1.4.tar.gz/pygrab-3.1.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+import os
+
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='pygrab',
+    version='3.1.4',
+    description='A secure python library for fetching data with async, JS, and Tor support',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    license='MIT',
+    author='Anish Kanthamneni',
+    author_email='akneni@gmail.com',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'requests',
+        'pyppeteer',
+        'nest_asyncio',
+        'PySocks',
+        "pygrab_ll",
+    ],
+)
