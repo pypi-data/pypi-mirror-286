86de08395641b64daa6b008eee94472b878479bb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:04:58 +0000

    Add package FinOL/0.1.16/FinOL-0.1.16.tar.gz

---
+++ packages/FinOL/FinOL-0.1.16.tar.gz/FinOL-0.1.16/docs/source/conf.py
@@ -0,0 +1,79 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+
+from datetime import datetime
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'FinOL'
+# copyright = f'2024–{datetime.now().year}, FinOL contributors'
+copyright = f'2024, FinOL contributors'
+author = 'FinOL contributors'
+release = 'MIT'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = [
+    # "recommonmark",
+    # "sphinx_markdown_tables",
+    "sphinx_rtd_theme",
+    "sphinx_copybutton"
+]
+
+templates_path = ['_templates']
+
+# The suffix(es) of source filenames.
+# You can specify multiple suffix as a list of string:
+#
+# source_suffix = ['.rst', '.md']
+source_suffix = ".rst"
+
+# The master toctree document.
+master_doc = "index"
+
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path .
+exclude_patterns = []
+
+
+# The name of the Pygments (syntax highlighting) style to use.
+pygments_style = "sphinx"
+
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+# import sphinx_rtd_theme
+html_theme = "sphinx_rtd_theme"  # 'alabaster'
+# html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
+
+
+# Theme options are theme-specific and customize the look and feel of a theme
+# further.  For a list of options available for each theme, see the
+# documentation.
+#
+html_theme_options = {"logo_only": True, "navigation_with_keys": True}
+# html_favicon = "../image/favicon.ico"
+html_logo = "../images/finol_logo_pure.png"
+
+
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+html_css_files = ["css/custom.css"]
+
+
+
+# -- Extension configuration -------------------------------------------------
+
+# sphinx_copybutton option to not copy prompt.
+copybutton_prompt_text = "$ "
---
+++ packages/FinOL/FinOL-0.1.16.tar.gz/FinOL-0.1.16/finol/__init__.py
@@ -0,0 +1,6 @@
+__version__ = "0.1.16"
+__author__ = "FinOL contributors"
+
+# python setup.py sdist build
+# twine upload dist/*
+
