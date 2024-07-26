1f132b21fad9de3f351109f765bdfe2fbfcbf641
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:06:02 +0000

    Add package wtf-calculator/0.2/wtf_calculator-0.2.tar.gz

---
+++ packages/wtf-calculator/wtf_calculator-0.2.tar.gz/wtf_calculator-0.2/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='wtf_calculator',
+    version='0.2',
+    packages=find_packages(),
+    description='Une librairie pour faire des calculs complètement faux.',
+    author='Zmeypp',
+    author_email='lmayel@gmail.com',
+    url='https://github.com/votre-utilisateur/wtf_calculator',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    python_requires='>=3.6',
+)
