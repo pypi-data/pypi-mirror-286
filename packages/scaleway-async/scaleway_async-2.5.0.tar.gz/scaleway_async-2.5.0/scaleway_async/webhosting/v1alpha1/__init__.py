0e41fba6206538500a628b39257255996b05505f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:12 +0000

    Add package scaleway-async/2.5.0/scaleway_async-2.5.0.tar.gz

---
+++ packages/scaleway-async/scaleway_async-2.5.0.tar.gz/scaleway_async-2.5.0/scaleway_async/webhosting/v1alpha1/__init__.py
@@ -0,0 +1,83 @@
+# This file was automatically generated. DO NOT EDIT.
+# If you have any remark or suggestion do not hesitate to open an issue.
+from .types import DnsRecordStatus
+from .types import DnsRecordType
+from .types import DnsRecordsStatus
+from .types import HostingDnsStatus
+from .types import HostingStatus
+from .content import HOSTING_TRANSIENT_STATUSES
+from .types import ListHostingsRequestOrderBy
+from .types import ListOffersRequestOrderBy
+from .types import NameserverStatus
+from .types import OfferQuotaWarning
+from .types import HostingCpanelUrls
+from .types import HostingOption
+from .types import OfferProduct
+from .types import CreateHostingRequestDomainConfiguration
+from .types import DnsRecord
+from .types import Nameserver
+from .types import ControlPanel
+from .types import Hosting
+from .types import Offer
+from .types import CheckUserOwnsDomainRequest
+from .types import CheckUserOwnsDomainResponse
+from .types import CreateHostingRequest
+from .types import CreateSessionRequest
+from .types import DeleteHostingRequest
+from .types import DnsRecords
+from .types import GetDomainDnsRecordsRequest
+from .types import GetHostingRequest
+from .types import ListControlPanelsRequest
+from .types import ListControlPanelsResponse
+from .types import ListHostingsRequest
+from .types import ListHostingsResponse
+from .types import ListOffersRequest
+from .types import ListOffersResponse
+from .types import ResetHostingPasswordRequest
+from .types import ResetHostingPasswordResponse
+from .types import RestoreHostingRequest
+from .types import Session
+from .types import UpdateHostingRequest
+from .api import WebhostingV1Alpha1API
+
+__all__ = [
+    "DnsRecordStatus",
+    "DnsRecordType",
+    "DnsRecordsStatus",
+    "HostingDnsStatus",
+    "HostingStatus",
+    "HOSTING_TRANSIENT_STATUSES",
+    "ListHostingsRequestOrderBy",
+    "ListOffersRequestOrderBy",
+    "NameserverStatus",
+    "OfferQuotaWarning",
+    "HostingCpanelUrls",
+    "HostingOption",
+    "OfferProduct",
+    "CreateHostingRequestDomainConfiguration",
+    "DnsRecord",
+    "Nameserver",
+    "ControlPanel",
+    "Hosting",
+    "Offer",
+    "CheckUserOwnsDomainRequest",
+    "CheckUserOwnsDomainResponse",
+    "CreateHostingRequest",
+    "CreateSessionRequest",
+    "DeleteHostingRequest",
+    "DnsRecords",
+    "GetDomainDnsRecordsRequest",
+    "GetHostingRequest",
+    "ListControlPanelsRequest",
+    "ListControlPanelsResponse",
+    "ListHostingsRequest",
+    "ListHostingsResponse",
+    "ListOffersRequest",
+    "ListOffersResponse",
+    "ResetHostingPasswordRequest",
+    "ResetHostingPasswordResponse",
+    "RestoreHostingRequest",
+    "Session",
+    "UpdateHostingRequest",
+    "WebhostingV1Alpha1API",
+]
