d5f8d2b2be8110bd706d8594347e3b778b6b9994
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:04:55 +0000

    Add package pyneurons/0.3.2/pyneurons-0.3.2.tar.gz

---
+++ packages/pyneurons/pyneurons-0.3.2.tar.gz/pyneurons-0.3.2/pyneurons/random/__init__.py
@@ -0,0 +1,15 @@
+from .integer import integer
+from .weight import weight
+from .param import param
+from .bias import bias
+from .seed import seed
+from .key import key
+
+__all__ = [
+    "integer",
+    "weight",
+    "param",
+    "bias",
+    "seed",
+    "key",
+]
---
+++ packages/pyneurons/pyneurons-0.3.2.tar.gz/pyneurons-0.3.2/pyneurons/random/integer.py
@@ -0,0 +1,6 @@
+from random import randint
+from time import time
+
+
+def integer():
+    return randint(0, int(time()))
