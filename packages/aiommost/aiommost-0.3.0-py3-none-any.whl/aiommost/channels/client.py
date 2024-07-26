3af7e13f7a637a75ff6111fc869976492289dd9a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:05:12 +0000

    Add package aiommost/0.3.0/aiommost-0.3.0-py3-none-any.whl

---
+++ packages/aiommost/aiommost-0.3.0-py3-none-any.whl/aiommost/channels/client.py
@@ -0,0 +1,22 @@
+import orjson
+from httpx import AsyncClient
+
+from aiommost import errors
+from aiommost.channels.schemas import Channel
+
+
+class ChannelsClient:
+
+    def __init__(self, session: AsyncClient) -> None:
+        self.session = session
+
+    async def create_direct(self, first: str, second: str) -> Channel:
+        """Create direct channel between users."""
+        url = '/channels/direct'
+        request = [first, second]
+
+        response = await self.session.post(url, content=orjson.dumps(request))
+        errors.validate(response)
+
+        channel = orjson.loads(response.content)
+        return Channel(**channel)
