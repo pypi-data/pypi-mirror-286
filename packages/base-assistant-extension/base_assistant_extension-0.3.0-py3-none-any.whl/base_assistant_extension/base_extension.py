69272448fa458628e8721b70411b9bcc78ee5460
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 05:05:40 +0000

    Add package base-assistant-extension/0.3.0/base_assistant_extension-0.3.0-py3-none-any.whl

---
+++ packages/base-assistant-extension/base_assistant_extension-0.3.0-py3-none-any.whl/base_assistant_extension/base_extension.py
@@ -0,0 +1,26 @@
+from abc import ABC, abstractmethod
+from typing import TypedDict
+from langchain_core.runnables import Runnable
+from langchain_core.messages import BaseMessage
+
+
+class ExtensionInput(TypedDict):
+    messages: list[BaseMessage]
+
+
+class BaseExtension(ABC):
+    @abstractmethod
+    def name(self) -> str:
+        pass
+
+    @abstractmethod
+    def description(self) -> str:
+        """
+        Will be used by LLM for tool calling selection.
+        It's prefered to have a relevant description
+        """
+        pass
+
+    @abstractmethod
+    def create_runnable(self) -> Runnable:
+        pass
