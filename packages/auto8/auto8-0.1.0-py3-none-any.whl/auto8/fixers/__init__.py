58eef63fe68d7016b17c2708dea4383264a6a4a8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:17 +0000

    Add package auto8/0.1.0/auto8-0.1.0-py3-none-any.whl

---
+++ packages/auto8/auto8-0.1.0-py3-none-any.whl/auto8/fixers/__init__.py
@@ -0,0 +1,6 @@
+from . import e501_line_too_long
+from . import w292_no_newline_at_eof
+from . import e502_redundant_backslash
+from . import w291_trailing_whitespace
+from . import w293_blank_line_whitespace
+from . import e128_continuation_line_under_indented
