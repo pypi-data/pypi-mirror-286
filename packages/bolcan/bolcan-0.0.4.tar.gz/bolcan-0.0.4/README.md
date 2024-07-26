59a2e8420cd966323033648b095ef49a0bb4f1c0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:05:09 +0000

    Add package bolcan/0.0.4/bolcan-0.0.4.tar.gz

---
+++ packages/bolcan/bolcan-0.0.4.tar.gz/bolcan-0.0.4/README.md
@@ -0,0 +1,10 @@
+# bolcan
+
+'**Bolcan** is a versatile Python package designed to empower engineers by providing a comprehensive suite of functions for various engineering calculations. With a focus on petroleum engineering, it streamlines workflows and facilitates efficient problem-solving.'
+
+## Installation
+
+pip install bolcan
+
+
+
---
+++ packages/bolcan/bolcan-0.0.4.tar.gz/bolcan-0.0.4/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='bolcan',
+    version='0.0.4',
+    packages=find_packages(),
+    install_requires=[
+        'pandas',
+    ],
+    author='Justice Nyame',
+    author_email='nyamejustice2000@gmail.com',
+    description='A Python package offering essential tools for petroleum and other engineering calculations',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url='https://github.com/Jnyame21/bolcan',
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+)
+
+
