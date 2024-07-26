a33ecff9ffa36ee5d644fdea42628ed3fe962496
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:44 +0000

    Add package grunnur/0.4.0/grunnur-0.4.0.tar.gz

---
+++ packages/grunnur/grunnur-0.4.0.tar.gz/grunnur-0.4.0/tests/test_array_metadata.py
@@ -0,0 +1,173 @@
+import itertools
+import re
+
+import numpy
+import pytest
+
+from grunnur.array_metadata import (
+    ArrayMetadata,
+    _get_range,
+    _get_strides,
+    _get_view,
+    _normalize_slice,
+)
+
+
+def test_normalize_slice():
+    # default slice - returns zero offset and unchanged length/stride
+    assert _normalize_slice(10, 4, slice(None)) == (0, 10, 4)
+    # a slice with end-based limits
+    assert _normalize_slice(10, 4, slice(-3, -1)) == (7 * 4, 2, 4)
+    # a slice with a negative step
+    assert _normalize_slice(10, 4, slice(-1, -5, -2)) == (9 * 4, 2, -8)
+
+    assert _normalize_slice(5, 4, slice(1, 4, 2)) == (1 * 4, 2, 8)
+
+
+def test_get_view():
+    ref = numpy.empty((5, 6), numpy.int32)
+    slices = (slice(1, 4, 2), slice(-5, -1, 2))
+    ref_v = ref[slices]
+    assert _get_view(ref.shape, ref.strides, slices) == (
+        1 * 24 + (6 - 5) * 4,  # the offset of the first element in the view: (1, -5)
+        ref_v.shape,
+        ref_v.strides,
+    )
+
+    with pytest.raises(ValueError, match="Shape and strides must have the same length"):
+        _get_view(ref.shape, ref.strides[:-1], slices)
+
+    with pytest.raises(ValueError, match="Shape and slices must have the same length"):
+        _get_view(ref.shape, ref.strides, slices[:-1])
+
+
+def ref_range(shape, itemsize, strides):
+    # Reference range calculation - simply find the minimum and the maximum across all indices.
+    indices = numpy.meshgrid(*[numpy.arange(length) for length in shape], indexing="ij")
+    addresses = sum(idx * stride for idx, stride in zip(indices, strides, strict=True))
+    min_offset = addresses.min()
+    max_offset = addresses.max() + itemsize
+    return min_offset, max_offset
+
+
+def test_get_range():
+    min_offset, max_offset = _get_range((3, 4, 5), 4, (100, -30, -5))
+    ref_min_offset, ref_max_offset = ref_range((3, 4, 5), 4, (100, -30, -5))
+    assert min_offset == ref_min_offset
+    assert max_offset == ref_max_offset
+
+    with pytest.raises(ValueError, match="Shape and strides must have the same length"):
+        _get_range((3, 4, 5), 4, (100, -30))
+
+
+def test_get_strides():
+    ref = numpy.empty((5, 6), numpy.int32)
+    strides = _get_strides((5, 6), ref.dtype.itemsize)
+    assert strides == ref.strides
+
+
+def check_metadata(meta, *, check_max=False):
+    """
+    Checks array metadata by creating a buffer of the size specified in the metadata
+    and trying to access every element there.
+    """
+    buf = numpy.zeros(meta.buffer_size, numpy.uint8)
+
+    itemsize = meta.dtype.itemsize
+
+    for indices in itertools.product(*[range(length) for length in meta.shape]):
+        flat_idx = sum(idx * stride for idx, stride in zip(indices, meta.strides, strict=True))
+        addr = flat_idx + meta.first_element_offset
+        buf[addr : addr + itemsize] = 1
+
+    nz = numpy.flatnonzero(buf)
+    min_addr = nz[0]
+    max_addr = nz[-1] + 1
+    assert min_addr == meta.first_element_offset
+    if check_max:
+        assert max_addr == meta.buffer_size
+
+
+def test_metadata_constructor():
+    # a scalar shape is converted into a tuple
+    check_metadata(ArrayMetadata([5], numpy.float64), check_max=True)
+
+    # strides are created automatically if not provided
+    meta = ArrayMetadata((6, 7), numpy.complex128)
+    check_metadata(meta, check_max=True)
+    ref = numpy.empty((6, 7), numpy.complex128)
+    assert meta.strides == ref.strides
+
+    # setting strides manually
+    check_metadata(ArrayMetadata((6, 7), numpy.complex128, strides=[200, 20]), check_max=True)
+
+    # setting buffer size
+    check_metadata(ArrayMetadata((5, 6), numpy.complex64, strides=[100, 10], buffer_size=1000))
+
+    # Minimum offset is too small
+    message = re.escape(
+        "The minimum offset for given strides (-16) " "is outside the given buffer range (100)"
+    )
+    with pytest.raises(ValueError, match=message):
+        meta = ArrayMetadata(
+            (4, 5), numpy.int32, strides=(20, -4), first_element_offset=0, buffer_size=100
+        )
+
+    # Minimum offset is too big
+    message = re.escape(
+        "The minimum offset for given strides (120) " "is outside the given buffer range (100)"
+    )
+    with pytest.raises(ValueError, match=message):
+        meta = ArrayMetadata(
+            (4, 5), numpy.int32, strides=(20, 4), first_element_offset=120, buffer_size=100
+        )
+
+    # Maximum offset is too big
+    message = re.escape(
+        "The maximum offset for given strides (80) " "is outside the given buffer range (70)"
+    )
+    with pytest.raises(ValueError, match=message):
+        meta = ArrayMetadata(
+            (4, 5), numpy.int32, strides=(20, 4), first_element_offset=0, buffer_size=70
+        )
+
+
+def test_view():
+    meta = ArrayMetadata((5, 6), numpy.int32)
+    view = meta[1:4, -1:-5:-2]
+    ref_view = numpy.empty(meta.shape, meta.dtype)[1:4, -1:-5:-2]
+    assert view.shape == ref_view.shape
+    assert view.strides == ref_view.strides
+
+    meta = ArrayMetadata((5, 6), numpy.int32)
+    view = meta[1:4]  # omitting the innermost slices
+    ref_view = numpy.empty(meta.shape, meta.dtype)[1:4]
+    assert view.shape == ref_view.shape
+    assert view.strides == ref_view.strides
+
+
+def test_minimal_subregion():
+    meta = ArrayMetadata((5, 6), numpy.int32)
+    view = meta[1:4, -1:-5:-2]
+    origin, size, new_meta = view.minimal_subregion()
+
+    # the address of the elem with the lowest address, that is (1, -3) == (1, 3)
+    assert origin == 1 * meta.strides[0] + 3 * meta.strides[1]
+
+    # the distance between the lowest address ((1, -3) == (1, 3))
+    # and the highest one ((3, -1) == (3, 5)) plus itemsize
+    assert size == (
+        (3 * meta.strides[0] + 5 * meta.strides[1])
+        + meta.dtype.itemsize
+        - (1 * meta.strides[0] + 3 * meta.strides[1])
+    )
+
+    # The new first element address is the address of (1, -1) == (1, 5)
+    # minus the origin.
+    assert new_meta.first_element_offset == 1 * meta.strides[0] + 5 * meta.strides[1] - origin
+    assert new_meta.buffer_size == size
+
+
+def test_empty_shape():
+    with pytest.raises(ValueError, match="Array shape cannot be an empty sequence"):
+        ArrayMetadata((), numpy.int32)
---
+++ packages/grunnur/grunnur-0.4.0.tar.gz/grunnur-0.4.0/tests/test_buffer.py
@@ -0,0 +1,172 @@
+import numpy
+import pytest
+
+from grunnur import API, Buffer, Context, Queue, cuda_api_id, opencl_api_id
+
+
+def test_allocate_and_copy(mock_or_real_context):  # noqa: PLR0915
+    context, mocked = mock_or_real_context
+
+    length = 100
+    dtype = numpy.dtype("int32")
+    size = length * dtype.itemsize
+
+    arr = numpy.arange(length).astype(dtype)
+
+    buf = Buffer.allocate(context.device, size)
+    assert buf.size == size
+    assert buf.offset == 0
+
+    # Just covering the existence of the attribute.
+    # Hard to actually check it without running a kernel
+    assert buf.kernel_arg is not None
+
+    queue = Queue(context.device)
+    buf.set(queue, arr)
+
+    # Read the whole buffer
+    res = numpy.empty_like(arr)
+    buf.get(queue, res)
+    queue.synchronize()
+    assert (res == arr).all()
+
+    # Read a subregion
+    buf_region = buf.get_sub_region(25 * dtype.itemsize, 50 * dtype.itemsize)
+    arr_region = arr[25 : 25 + 50]
+    res_region = numpy.empty_like(arr_region)
+    buf_region.get(queue, res_region)
+    queue.synchronize()
+    assert (res_region == arr_region).all()
+
+    # Check that our mock can detect taking a sub-region of a sub-region (segfault in OpenCL)
+    if mocked and context.api.id == opencl_api_id():
+        with pytest.raises(RuntimeError, match="Cannot create a subregion of subregion"):
+            buf_region.get_sub_region(0, 10)
+
+    # Write a subregion
+    arr_region = (numpy.ones(50) * 100).astype(dtype)
+    arr[25 : 25 + 50] = arr_region
+    buf_region.set(queue, arr_region)
+    buf.get(queue, res)
+    queue.synchronize()
+    assert (res == arr).all()
+
+    # Subregion of subregion
+    if context.api.id == cuda_api_id():
+        # In OpenCL that leads to segfault, but with CUDA we just emulate that with pointers.
+        arr_region2 = (numpy.ones(20) * 200).astype(dtype)
+        arr[25 + 20 : 25 + 40] = arr_region2
+        buf_region2 = buf_region.get_sub_region(20 * dtype.itemsize, 20 * dtype.itemsize)
+        buf_region2.set(queue, arr_region2)
+        buf.get(queue, res)
+        queue.synchronize()
+        assert (res == arr).all()
+
+    # Device-to-device copy
+    buf2 = Buffer.allocate(context.device, size * 2)
+    buf2.set(queue, numpy.ones(length * 2, dtype))
+    buf2_view = buf2.get_sub_region(50 * dtype.itemsize, 100 * dtype.itemsize)
+    buf2_view.set(queue, buf)
+    res2 = numpy.empty(length * 2, dtype)
+    buf2.get(queue, res2)
+    queue.synchronize()
+    assert (res2[50:150] == arr).all()
+    assert (res2[:50] == 1).all()
+    assert (res2[150:] == 1).all()
+
+    # Device-to-device copy (no_async)
+    buf2 = Buffer.allocate(context.device, size * 2)
+    buf2.set(queue, numpy.ones(length * 2, dtype))
+    buf2_view = buf2.get_sub_region(50 * dtype.itemsize, 100 * dtype.itemsize)
+    buf2_view.set(queue, buf, no_async=True)
+    res2 = numpy.empty(length * 2, dtype)
+    buf2.get(queue, res2)
+    queue.synchronize()
+    assert (res2[50:150] == arr).all()
+    assert (res2[:50] == 1).all()
+    assert (res2[150:] == 1).all()
+
+
+def test_subregion_overflow(mock_context):
+    buf = Buffer.allocate(mock_context.device, 100)
+    with pytest.raises(
+        ValueError, match="The requested subregion extends beyond the buffer length"
+    ):
+        buf.get_sub_region(50, 70)
+
+
+def test_migrate_on_copy(monkeypatch, mock_4_device_context):
+    context = mock_4_device_context
+    size = 100
+    src = Buffer.allocate(context.devices[0], size)
+    dest = Buffer.allocate(context.devices[0], size)
+
+    queue0 = Queue(context.devices[0])
+    queue1 = Queue(context.devices[1])
+
+    arr = numpy.arange(size).astype(numpy.uint8)
+
+    src.set(queue0, arr)
+    queue0.synchronize()
+
+    monkeypatch.setattr(queue1, "device", queue0.device)
+
+    if context.api.id == cuda_api_id():
+        message = "Trying to access an allocation from a device different to where it was created"
+    else:
+        message = "Trying to access a buffer from a device different to where it was migrated to"
+
+    with pytest.raises(RuntimeError, match=message):
+        dest.set(queue1, src)
+
+
+def test_flags(mock_backend_pyopencl):
+    backend = mock_backend_pyopencl
+
+    normal_flags = backend.pyopencl.mem_flags.READ_WRITE
+    special_flags = normal_flags | backend.pyopencl.mem_flags.ALLOC_HOST_PTR
+
+    backend.add_platform_with_devices("Apple", ["GeForce", "Foo", "Bar"])
+    backend.add_platform_with_devices("Baz", ["GeForce", "Foo", "Bar"])
+
+    # Multi-device on Apple platform with one of the devices being GeForce:
+    # need special Buffer flags
+    api = API.from_api_id(backend.api_id)
+    context = Context.from_devices([api.platforms[0].devices[0], api.platforms[0].devices[1]])
+    buf = Buffer.allocate(context.devices[0], 100)
+    assert buf._buffer_adapter._pyopencl_buffer.flags == special_flags
+
+    # None of the devices is GeForce
+    context = Context.from_devices([api.platforms[0].devices[1], api.platforms[0].devices[2]])
+    buf = Buffer.allocate(context.devices[0], 100)
+    assert buf._buffer_adapter._pyopencl_buffer.flags == normal_flags
+
+    # Only one device
+    context = Context.from_devices([api.platforms[0].devices[0]])
+    buf = Buffer.allocate(context.device, 100)
+    assert buf._buffer_adapter._pyopencl_buffer.flags == normal_flags
+
+    # Not an Apple platform
+    context = Context.from_devices([api.platforms[1].devices[0], api.platforms[1].devices[1]])
+    buf = Buffer.allocate(context.devices[0], 100)
+    assert buf._buffer_adapter._pyopencl_buffer.flags == normal_flags
+
+
+def test_set_from_wrong_type(mock_context):
+    buf = Buffer.allocate(mock_context.device, 100)
+    queue = Queue(mock_context.device)
+    with pytest.raises(TypeError, match="Cannot set from an object of type <class 'int'>"):
+        buf.set(queue, 1)
+
+
+def test_mismatched_devices(mock_4_device_context):
+    context = mock_4_device_context
+    buf = Buffer.allocate(context.devices[0], 100)
+    queue = Queue(context.devices[1])
+    arr = numpy.ones(100, numpy.uint8)
+
+    with pytest.raises(ValueError, match="Mismatched devices: queue on device"):
+        buf.get(queue, arr)
+
+    with pytest.raises(ValueError, match="Mismatched devices: queue on device"):
+        buf.set(queue, arr)
