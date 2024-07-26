5420832922cf14a1e9b0bba28e8de21d399f6247
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 06:05:17 +0000

    Add package groundx-python-sdk/1.3.20/groundx_python_sdk-1.3.20.tar.gz

---
+++ packages/groundx-python-sdk/groundx_python_sdk-1.3.20.tar.gz/groundx_python_sdk-1.3.20/groundx/type/search_result_item.py
@@ -0,0 +1,48 @@
+# coding: utf-8
+
+"""
+    GroundX API
+
+    Ground Your RAG Apps in Fact not Fiction
+
+    The version of the OpenAPI document: 1.0.0
+    Contact: support@groundx.ai
+    Created by: https://www.groundx.ai/
+"""
+
+from datetime import datetime, date
+import typing
+from enum import Enum
+from typing_extensions import TypedDict, Literal, TYPE_CHECKING
+
+
+class RequiredSearchResultItem(TypedDict):
+    pass
+
+class OptionalSearchResultItem(TypedDict, total=False):
+    # Content bucket the search result belongs to
+    bucketId: int
+
+    # Unique system generated ID for the document
+    documentId: str
+
+    # Name of ingested file
+    fileName: str
+
+    # Confidence score in the search result
+    score: typing.Union[int, float]
+
+    # Document, section, and chunk search data, both custom and system-generated
+    searchData: typing.Dict[str, typing.Union[bool, date, datetime, dict, float, int, list, str, None]]
+
+    # Source document URL
+    sourceUrl: str
+
+    # System-generated text, re-written for LLM completions
+    suggestedText: str
+
+    # Original text from the source document
+    text: str
+
+class SearchResultItem(RequiredSearchResultItem, OptionalSearchResultItem):
+    pass
