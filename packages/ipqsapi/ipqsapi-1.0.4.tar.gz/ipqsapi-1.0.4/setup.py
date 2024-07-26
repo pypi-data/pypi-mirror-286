34efa5acfbd21de044927ce618ec77fa26a0ec91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:52 +0000

    Add package ipqsapi/1.0.4/ipqsapi-1.0.4.tar.gz

---
+++ packages/ipqsapi/ipqsapi-1.0.4.tar.gz/ipqsapi-1.0.4/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r', encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='ipqsapi',
+    version='1.0.4',
+    description='API wrapper - www.ipqualityscore.com',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    author='SawaPlayGO',
+    author_email='sawaglumov2006@gmail.com',
+    url='https://github.com/SawaPlayGO/ipqsapi',
+    install_requires=[
+        'requests>=2.25.1'
+    ],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.8',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires='>=3.6',
+    keywords='IPQualityScore API wrapper proxy VPN email validation phone validation. Darknet username, email, password checker.',
+)
