d0de708a73e2476291953e253d829c06663acd27
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 05:07:10 +0000

    Add package deebot-client/8.2.0/deebot_client-8.2.0.tar.gz

---
+++ packages/deebot-client/deebot_client-8.2.0.tar.gz/deebot_client-8.2.0/deebot_client/exceptions.py
@@ -0,0 +1,34 @@
+"""Deebot client exception module."""
+
+from __future__ import annotations
+
+
+class DeebotError(Exception):
+    """Deebot error."""
+
+
+class AuthenticationError(DeebotError):
+    """Authentication error."""
+
+
+class InvalidAuthenticationError(AuthenticationError):
+    """Invalid authentication error."""
+
+
+class ApiError(DeebotError):
+    """Api error."""
+
+
+class ApiTimeoutError(ApiError):
+    """Api timeout error."""
+
+    def __init__(self, path: str, timeout: int, *args: object) -> None:
+        super().__init__(f"Timeout ({timeout}) reached on path: {path}", *args)
+
+
+class MapError(DeebotError):
+    """Map error."""
+
+
+class MqttError(DeebotError):
+    """Mqtt error."""
