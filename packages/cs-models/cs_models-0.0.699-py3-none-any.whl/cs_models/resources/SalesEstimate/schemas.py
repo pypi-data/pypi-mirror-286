a121d814f698ae64364cd08ef8d283e3ced88a5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:52 +0000

    Add package cs-models/0.0.699/cs_models-0.0.699-py3-none-any.whl

---
+++ packages/cs-models/cs_models-0.0.699-py3-none-any.whl/cs_models/resources/SalesEstimate/schemas.py
@@ -0,0 +1,22 @@
+from marshmallow import (
+    Schema,
+    fields,
+    validate,
+)
+
+
+class SalesEstimateResourceSchema(Schema):
+    not_blank = validate.Length(min=1, error='Field cannot be blank')
+
+    id = fields.Integer(dump_only=True)
+    fiscal_year = fields.Integer(allow_none=True)
+    request_id = fields.String(allow_none=True)
+    drug_name = fields.String(required=True)
+    currency = fields.String(allow_none=True)
+    fiscal_end_date = fields.DateTime(allow_none=True)
+    mean = fields.Decimal(allow_none=True)
+    median = fields.Decimal(allow_none=True)
+    sd = fields.Float(allow_none=True)
+    high = fields.Decimal(allow_none=True)
+    low = fields.Decimal(allow_none=True)
+    updated_at = fields.DateTime()
