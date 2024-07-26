8e8a798237483769a57eb382da894a75223ecc5e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:31 +0000

    Add package osutool/0.1.2/osutool-0.1.2.tar.gz

---
+++ packages/osutool/osutool-0.1.2.tar.gz/osutool-0.1.2/setup.py
@@ -0,0 +1,39 @@
+'Setup script for osutool.'
+
+from pathlib import Path
+
+from osutool import (
+    __author__,
+    __doc__,
+    __license__,
+    __module_name__,
+    __version__,
+)
+from setuptools import setup
+
+setup(
+	name=__module_name__,
+	version=__version__,
+	description=__doc__.split('\n')[0],
+	long_description=Path('README.md').read_text(),
+	long_description_content_type='text/markdown',
+	url=f'https://github.com/{__author__}/{__module_name__}',
+	author=__author__,
+	include_package_data=True,
+	license=__license__,
+	packages=[__module_name__],
+	package_data={},
+	install_requires=['colorama==0.4.6'],
+	setup_requires=['pytest_runner'],
+	python_requires='>=3.8',
+	scripts=[],
+	tests_require=['pytest'],
+	entry_points={'console_scripts': [f'{__module_name__}={__module_name__}:main']},
+	zip_safe=True,
+	classifiers=[
+		'Development Status :: 4 - Beta',
+		'Intended Audience :: End Users/Desktop',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+	],
+)
