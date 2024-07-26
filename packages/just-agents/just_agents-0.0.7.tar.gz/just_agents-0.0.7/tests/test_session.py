3b345ab13a750842da7ad1aba523ce2d9bd5c7f6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:05:20 +0000

    Add package just-agents/0.0.7/just_agents-0.0.7.tar.gz

---
+++ packages/just-agents/just_agents-0.0.7.tar.gz/just_agents-0.0.7/tests/test_session.py
@@ -0,0 +1,44 @@
+#from just_agents.chat_agent import ChatAgent
+import json
+import pprint
+
+import pytest
+from dotenv import load_dotenv
+
+import just_agents.llm_options
+from just_agents.llm_session import LLMSession
+
+
+def get_current_weather(location: str):
+    """Gets the current weather in a given location"""
+    if "tokyo" in location.lower():
+        return json.dumps({"location": "Tokyo", "temperature": "10", "unit": "celsius"})
+    elif "san francisco" in location.lower():
+        return json.dumps({"location": "San Francisco", "temperature": "72", "unit": "fahrenheit"})
+    elif "paris" in location.lower():
+        return json.dumps({"location": "Paris", "temperature": "22", "unit": "celsius"})
+    else:
+        return json.dumps({"location": location, "temperature": "unknown"})
+
+def test_sync_llama_function_calling():
+    load_dotenv()
+    session: LLMSession = LLMSession(
+        llm_options=just_agents.llm_options.LLAMA3_1,
+        tools=[get_current_weather]
+    )
+    result = session.query("What's the weather like in San Francisco, Tokyo, and Paris?")
+    assert "72" in result
+    assert "22" in result
+    assert "10" in result
+
+@pytest.mark.skip(reason="so far qwen inference we are using has issues with json function calling")
+def test_async_gwen2_function_calling():
+    load_dotenv()
+    session: LLMSession = LLMSession(
+        llm_options=just_agents.llm_options.OPEN_ROUTER_Qwen_2_72B_Instruct,
+        tools=[get_current_weather]
+    )
+    result = session.query_all_messages("What's the weather like in San Francisco, Tokyo, and Paris?")
+    assert "72" in result
+    assert "22" in result
+    assert "10" in result
