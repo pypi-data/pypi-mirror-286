1945e4b707a3d9d2f64750b0f54b96e83f026dde
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 14:05:33 +0000

    Add package envmate/0.1.0/envmate-0.1.0.tar.gz

---
+++ packages/envmate/envmate-0.1.0.tar.gz/envmate-0.1.0/envmate/__init__.py
@@ -0,0 +1,5 @@
+# your_package/__init__.py
+
+__version__ = '0.1.0'
+
+from .main import main
---
+++ packages/envmate/envmate-0.1.0.tar.gz/envmate-0.1.0/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="envmate",
+    version="0.1.0",
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        "inquirer",
+    ],
+    entry_points={
+        'console_scripts': [
+            'your_command=your_package.main:main',
+        ],
+    },
+    author="Your Name",
+    author_email="your.email@example.com",
+    description="A brief description of your package",
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    url="https://github.com/harris-ahmad/your_project",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
