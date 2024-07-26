0b8f7e71d3b5c2249bd8d3d10ab043a17d87750c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 03:05:26 +0000

    Add package plus_sync/0.3.3/plus_sync-0.3.3-py3-none-any.whl

---
+++ packages/plus_sync/plus_sync-0.3.3-py3-none-any.whl/plus_sync/cmd/commands/init.py
@@ -0,0 +1,26 @@
+from typing import Annotated
+
+import typer
+
+from plus_sync.cmd.helpers.options import global_options
+from plus_sync.config import Config
+
+from ..app import app
+
+
+@app.command()
+def init(
+    project_name: Annotated[str, typer.Option(help='The name of the project.', prompt=True)],
+    overwrite: Annotated[bool, typer.Option(help='Overwrite the configuration file if it already exists.')] = False,
+):
+    """
+    Initialize a new configuration file.
+    """
+    typer.echo(f'Initializing a new configuration file at {global_options["config_file"]}.')
+    config = Config(project_name=project_name)
+    try:
+        config.save(global_options['config_file'], overwrite=overwrite)
+    except FileExistsError:
+        typer.echo('The file already exists. Use --overwrite to overwrite it.')
+        raise typer.Exit(code=1)
+    typer.echo('Done.')
