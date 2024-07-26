9f9ec302c33d727c5f74d09f18f854f28589ce06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 04:05:09 +0000

    Add package open-variant/1.0.0/open-variant-1.0.0.tar.gz

---
+++ packages/open-variant/open-variant-1.0.0.tar.gz/open-variant-1.0.0/openvariant/utils/utils.py
@@ -0,0 +1,25 @@
+import re
+import os
+from appdirs import user_data_dir
+from fnmatch import fnmatch
+from os.path import basename
+
+
+ENV_VAR = {
+    'OPENVAR_PLUGIN': user_data_dir('openvariant', 'bbglab')
+}
+
+def loadEnvironmentVariables() -> None:
+    """Load environment variable into the environment."""
+
+    missing_vars = set(ENV_VAR.keys()).difference(set(os.environ))
+
+    for env_var in missing_vars:
+        os.environ[env_var] = ENV_VAR[env_var]
+        os.makedirs(ENV_VAR[env_var], exist_ok=True)
+
+    return
+
+def check_extension(ext: str, path: str) -> bool:
+    """Check if file matches with the annotation pattern"""
+    return fnmatch(basename(path), ext) if ext[0] == '*' else re.match(ext, basename(path)) is not None
