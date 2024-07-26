fd61b241088289e603e3d790886c614eee943703
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2024-07-26 07:04:57 +0000

    Add package txm-sandbox/0.3.1.post14/txm_sandbox-0.3.1.post14.tar.gz

---
+++ packages/txm-sandbox/txm_sandbox-0.3.1.post14.tar.gz/txm_sandbox-0.3.1.post14/txm_sandbox/napari_gui/scripts/tomo_recon_cmd.py
@@ -0,0 +1,19 @@
+import json
+from pathlib import Path
+
+from txm_sandbox.utils.tomo_recon_tools import run_engine
+from txm_sandbox.utils.io import data_reader, tomo_h5_reader, data_info, tomo_h5_info
+
+cfg_fn = Path(__file__).parents[1] / "configs/txm_simple_gui_script_cfg.json"
+
+
+with open(cfg_fn, "r") as f:
+    with open(json.load(f)["tomo_recon"]["cfg_file"], "r") as ft:
+        tem = json.load(ft)
+
+params = tem[list(tem.keys())[0]]
+params["file_params"]["reader"] = data_reader(tomo_h5_reader)
+params["file_params"]["info_reader"] = data_info(tomo_h5_info)
+
+if __name__ == "__main__":
+    run_engine(**params)
